1dfc22b16f030acc791a6a7f3fb95442d94e45f7
Charles Nurse <charlesnurse@gmail.com>
2013-08-30 16:07:09 -0700

    Remove unneccessary files

--- Build/DNNSetup/BuildAndSetup.bat
+++
@@ -1,8 +0,0 @@
-SET SRC_FOLDER=C:\Git\Dnn.Platform
-devenv /build release C:\Git\Dnn.Build\DNNSetup\DNNSetup.sln
-devenv /build release %SRC_FOLDER%\DNN_Platform.sln
-
-attrib -r %SRC_FOLDER%\Website\Install\Config\07.00.06.config
-attrib -r %SRC_FOLDER%\Website\Install\DotNetNuke.install.config.resources
-
-C:\Git\Dnn.Build\DNNSetup\bin\DNNSetup.exe C:\Git\Dnn.Build\DNNSetup\bin all
