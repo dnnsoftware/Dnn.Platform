2fb41fc70ed714cf3591788273a47cb86d189928
Philip Beadle <philip.beadle@dnnsoftware.com>
2013-09-03 08:51:02 +1000

    Rolled back my change as Cathal had already fixed the issue.

--- DNN Platform/Library/Entities/Users/Membership/MembershipPasswordController.cs
+++ DNN Platform/Library/Entities/Users/Membership/MembershipPasswordController.cs
@@ -86,7 +86,7 @@ namespace DotNetNuke.Entities.Users.Membership
         /// <param name="portalId">portalid - futureproofing against any setting become site level</param>
         /// <param name="newPassword">users new password suggestion</param>
         /// <returns>true if password is in users history, false otherwise</returns>
-        public bool PasswordIsUniqueInHistory(int userId, int portalId, string newPassword)
+        public bool IsPasswordInHistory(int userId, int portalId, string newPassword)
         {
             Requires.NotNullOrEmpty("newPassword", newPassword);
             bool isPasswordUsed = false;
