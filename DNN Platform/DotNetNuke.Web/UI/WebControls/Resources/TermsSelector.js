b3b9aa3df000e62b34e26cbd77c18f9e5dca6c7d
Ben <ben.zhong@dnnsoftware.com>
2013-09-24 20:51:11 +0800

    Fix bug DNN-3720: let terms selector load treeview in client side.

--- DNN Platform/DotNetNuke.Web/UI/WebControls/Resources/TermsSelector.js
+++ DNN Platform/DotNetNuke.Web/UI/WebControls/Resources/TermsSelector.js
@@ -1,70 +1,109 @@
-﻿(function ($, t, sys) {
-	t.registerNamespace("dnn.controls");
-	dnn.controls.termsSelector = {};
-	
-	dnn.controls.termsSelector.OnClientDropDownOpened = function (sender, e) {
-		dnn.controls.termsSelector.fire(sender);
-	};
-	
-	dnn.controls.termsSelector.OnClientNodeChecked = function (sender, e) {
-		dnn.controls.termsSelector.update(sender);
-	};
-
-	dnn.controls.termsSelector.update = function (tree) {
-		var treeDiv = $("#" + tree.get_id());
-		var comboBox = treeDiv.data("combo");
-		var nodes = tree.get_checkedNodes();
-		var text = '', value = '';
-		for (var i = 0; i < nodes.length; i++) {
-			text += nodes[i].get_text() + ", ";
-			value += nodes[i].get_value() + ", ";
-		}
-		if (text != '' && text.substr(text.length - 2, 2) == ', ') {
-			text = text.substr(0, text.length - 2);
-		}
-		if (value != '' && value.substr(value.length - 2, 2) == ', ') {
-			value = value.substr(0, value.length - 2);
-		}
-
-		comboBox.trackChanges();
-		var valueItem;
-		if (comboBox.get_items().get_count() == 1) {
-			valueItem = new Telerik.Web.UI.RadComboBoxItem();
-			comboBox.get_items().add(valueItem);
-			valueItem.set_visible(false);
-		} else {
-			valueItem = comboBox.get_items().getItem(1);
-		}
-		valueItem.set_text(text);
-		valueItem.set_value(value);
-		valueItem.select();
-		comboBox.commitChanges();
-	};
-
-	dnn.controls.termsSelector.fire = function(combobox) {
-		var $this = $("#" + combobox.get_id());
-		var treeDiv = $("div[id^=" + combobox.get_id() + "][id$=_TreeView]");
-		if (treeDiv.data("combo")) {
-			return;
-		}
-		treeDiv.data("combo", combobox);
-
-		treeDiv.click(function (e) {
-			if (!$(e.srcElement).is(":checkbox")) {
-				return false;
+﻿(function ($, sys) {
+	if (typeof webcontrols === "undefined" || webcontrols === null) { webcontrols = {}; };
+	webcontrols.termsSelector = {
+		OnClientDropDownOpened: function (sender, e) {
+			webcontrols.termsSelector.fire(sender);
+		},
+		OnClientNodeChecked: function (sender, e) {
+			webcontrols.termsSelector.update(sender);
+		},
+		itemDataLoaded: function(result, context) {
+			var itemsData = eval("(" + result + ")");
+			var clientId = itemsData[0].clientId;
+			var selectedTerms = $("#" + clientId).attr("selectedterms").split(',');
+			var selectedObj = {};
+			for (var i = 0; i < selectedTerms.length; i++) {
+				selectedObj["t_" + selectedTerms[i]] = true;
 			}
-		});
+			var tree = $find($("div[id^=" + clientId + "][id$=_TreeView]").attr("id"));
+			tree.trackChanges();
+			for (var i = 1; i < itemsData.length; i++) {
+				var data = itemsData[i];
+				var node = new Telerik.Web.UI.RadTreeNode();
+				node.set_text(data.name);
+				node.set_value(data.termId);
+				if (itemsData[i].termId < 0) {
+					node.set_checkable(false);
+				} else {
+					node.set_checked(selectedObj["t_" + data.termId]);
+				}
+
+				var parentNode = tree.findNodeByValue(data.parentTermId);
+				if (parentNode && data.termId > 0) {
+					parentNode.get_nodes().add(node);
+					if (!parentNode.get_expanded()) {
+						parentNode.set_expanded(true);
+					}
+				} else {
+					tree.get_nodes().add(node);
+				}
+			}
+			tree.commitChanges();
+			webcontrols.termsSelector.update(tree);
+			$("div[id^=" + clientId + "][id$=_TreeView] input[type=checkbox]").dnnCheckbox();
+		},
+		itemDataLoadError: function(result, context) {
+
+		},
+		update: function (tree) {
+			var treeDiv = $("#" + tree.get_id());
+			var comboBox = treeDiv.data("combo");
+			var nodes = tree.get_checkedNodes();
+			var text = '', value = '';
+			for (var i = 0; i < nodes.length; i++) {
+				text += nodes[i].get_text() + ", ";
+				value += nodes[i].get_value() + ", ";
+			}
+			if (text != '' && text.substr(text.length - 2, 2) == ', ') {
+				text = text.substr(0, text.length - 2);
+			}
+			if (value != '' && value.substr(value.length - 2, 2) == ', ') {
+				value = value.substr(0, value.length - 2);
+			}
+
+			comboBox.trackChanges();
+			var valueItem;
+			if (comboBox.get_items().get_count() == 1) {
+				valueItem = new Telerik.Web.UI.RadComboBoxItem();
+				comboBox.get_items().add(valueItem);
+				valueItem.set_visible(false);
+			} else {
+				valueItem = comboBox.get_items().getItem(1);
+			}
+			valueItem.set_text(text);
+			valueItem.set_value(value);
+			valueItem.select();
+			comboBox.commitChanges();
+		},
+		fire: function(combobox) {
+			var $this = $("#" + combobox.get_id());
+			var treeDiv = $("div[id^=" + combobox.get_id() + "][id$=_TreeView]");
+			if (treeDiv.data("combo")) {
+				return;
+			}
+			treeDiv.data("combo", combobox);
+
+			treeDiv.click(function (e) {
+				if (!$(e.srcElement).is(":checkbox")) {
+					return false;
+				}
+			});
+		}
 	};
 
 	var updateTerms = function() {
 		setTimeout(function() {
 			$("div[class*=TermsSelector]").each(function() {
 				var clientId = $(this).attr("id");
+				var includeSystemVocabularies = $(this).attr("includesystemvocabularies");
+				var includeTags = $(this).attr("includetags");
+				var portalId = $(this).attr("portalid");
+				
 				var combo = $find(clientId);
 				if (combo != null) {
-					dnn.controls.termsSelector.fire(combo);
-					var tree = $find($("div[id^=" + clientId + "][id$=_TreeView]").attr("id"));
-					dnn.controls.termsSelector.update(tree);
+					webcontrols.termsSelector.fire(combo);
+					combo.set_text("Loading...");
+					eval(dnn.getVar('TermsSelectorCallback').replace('[PARAMS]', clientId + '-' + portalId + '-' + includeTags + '-' + includeSystemVocabularies));
 				}
 			});
 		}, 0);
@@ -78,5 +117,5 @@
 			});
 		}
 	});
-}(jQuery, Type, window.Sys));
+}(jQuery, window.Sys));
 
