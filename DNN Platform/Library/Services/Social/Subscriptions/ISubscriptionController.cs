96f59f5296e94f78e7ce130c041fff36d3e55796
Francesco Rivola <francesco.rivola@gmail.com>
2013-09-26 19:58:18 +0200

    CONTENT-1799 - Implemented the SubscriptionController and SubscriptionSecurityController

---
+++ DNN Platform/Library/Services/Social/Subscriptions/ISubscriptionController.cs
@@ -0,0 +1,73 @@
+﻿#region Copyright
+// 
+// DotNetNuke® - http://www.dotnetnuke.com
+// Copyright (c) 2002-2013
+// by DotNetNuke Corporation
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System.Collections.Generic;
+using DotNetNuke.Services.Social.Subscriptions.Entities;
+
+namespace DotNetNuke.Services.Social.Subscriptions
+{
+    public interface ISubscriptionController
+    {
+        /// <summary>
+        /// Returns the User Subscriptions
+        /// </summary>
+        /// <param name="userId">User Id</param>
+        /// <param name="portalId">Portal Id</param>
+        /// <param name="subscriptionTypeId">Subscription Type Id</param>
+        /// <returns>Collection of subscriptions</returns>
+        IEnumerable<Subscription> GetUserSubscriptions(int userId, int portalId, int subscriptionTypeId = -1);
+        
+        /// <summary>
+        /// Returns the Content Subscriptions
+        /// </summary>
+        /// <param name="portalId">Portal Id</param>
+        /// <param name="subscriptionTypeId">Subscription Type Id</param>
+        /// <param name="objectKey">Object Key</param>
+        /// <returns>Collection of subscriptions</returns>
+        IEnumerable<Subscription> GetContentSubscriptions(int portalId, int subscriptionTypeId, string objectKey);
+
+        /// <summary>
+        /// Returns true if a user is subscribed to a Content
+        /// </summary>
+        /// <param name="userId">User Id</param>
+        /// <param name="portalId">Portal Id</param>
+        /// <param name="subscriptionTypeId">Subscription Type Id</param>
+        /// <param name="objectKey">Object Key</param>
+        /// <param name="moduleId">Module Id</param>
+        /// <param name="tabId">Tab Id</param>
+        /// <returns>True if the user is subscribed to the content, false otherwise</returns>
+        bool IsSubscribed(int userId, int portalId, int subscriptionTypeId, string objectKey, int moduleId = -1, int tabId = -1);
+        
+        /// <summary>
+        /// Adds a new Subscription
+        /// </summary>
+        /// <param name="subscription">Subscription</param>
+        /// <returns>Subsription Id</returns>
+        int AddSubscription(Subscription subscription);
+
+        /// <summary>
+        ///  Delete a Subscription
+        /// </summary>
+        /// <param name="subscriptionId">Subscription Id</param>
+        void DeleteSubscription(int subscriptionId);
+
+    }
+}
---
+++ DNN Platform/Library/Services/Social/Subscriptions/SubscriptionSecurityController.cs
@@ -0,0 +1,65 @@
+﻿#region Copyright
+// 
+// DotNetNuke® - http://www.dotnetnuke.com
+// Copyright (c) 2002-2013
+// by DotNetNuke Corporation
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Entities.Tabs;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Framework;
+using DotNetNuke.Security;
+using DotNetNuke.Services.Social.Subscriptions.Entities;
+
+namespace DotNetNuke.Services.Social.Subscriptions
+{
+    public class SubscriptionSecurityController : ServiceLocator<ISubscriptionSecurityController, SubscriptionSecurityController>, ISubscriptionSecurityController
+    {
+        protected override Func<ISubscriptionSecurityController> GetFactory()
+        {
+            return () => new SubscriptionSecurityController();
+        }
+
+        public bool HasPermission(Subscription subscription)
+        {
+            var userInfo = new UserController().GetUser(subscription.PortalId, subscription.UserId);
+
+            // Check Module user Permission
+            var moduleInfo = new ModuleController().GetModule(subscription.ModuleId);
+            if (moduleInfo != null)
+            {
+                var portalSettings = new PortalSettings(moduleInfo.PortalID);
+
+                return PortalSecurity.IsInRoles(userInfo, portalSettings, moduleInfo.ModulePermissions.ToString("VIEW"));
+            }
+
+            // Check Tab user Permission
+            var tabInfo = new TabController().GetTab(subscription.TabId, subscription.PortalId, false);
+            if (tabInfo != null)
+            {
+                var portalSettings = new PortalSettings(tabInfo.PortalID);
+
+                return PortalSecurity.IsInRoles(userInfo, portalSettings, tabInfo.TabPermissions.ToString("VIEW"));
+            }
+
+            return true;
+        }
+    }
+}
