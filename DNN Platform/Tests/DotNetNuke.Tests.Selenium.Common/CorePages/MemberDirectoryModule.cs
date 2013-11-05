623abce4c308e25ea31eb758df5e67edc99ee20e
Natasha Zavorotny <natasha.zavorotny@dnncorp.com>
2013-11-05 15:41:08 -0800

    AUTO-110 Automation for Member Directory (Module)

--- DNN Platform/Tests/DotNetNuke.Tests.Selenium.Common/CorePages/MemberDirectoryModule.cs
+++ DNN Platform/Tests/DotNetNuke.Tests.Selenium.Common/CorePages/MemberDirectoryModule.cs
@@ -23,22 +23,24 @@ namespace DNNSelenium.Common.CorePages
 		}
 
 		public static string AddAsFriendLink = "//";
+		public static string FriendsNotFoundMessage = "//div[@class = 'dnnFormMessage dnnFormInfo']";
+		public static string FriendsNotFoundMessageText = "No members were found that satisfy the search conditions.";
 
 		public void AddAsFriend(string newFriendName)
 		{
-			Trace.WriteLine(BasePage.TraceLevelPage + "Click on 'Add a Friend link':");
+			Trace.WriteLine(TraceLevelPage + "Click on 'Add a Friend link':");
 			WaitAndClick(By.XPath("//div[not(@style)]/div[ul[@class = 'MdMemberInfo']//span[contains(text(), '" + newFriendName + "')]]/ul/li[@class = 'mdFriendRequest']/a"));
 		}
 
 		public void AcceptFriendRequest(string friendToAccept)
 		{
-			Trace.WriteLine(BasePage.TraceLevelPage + "Click on 'Accept Request link':");
+			Trace.WriteLine(TraceLevelPage + "Click on 'Accept Request link':");
 			WaitAndClick(By.XPath("//div[not(@style)]/div[ul[@class = 'MdMemberInfo']//span[contains(text(), '" + friendToAccept + "')]]/ul/li[@class = 'mdFriendAccept']/a"));
 		}
 
 		public void RemoveFriend(string friendToRemove)
 		{
-			Trace.WriteLine(BasePage.TraceLevelPage + "Click on 'Remove Friend link':");
+			Trace.WriteLine(TraceLevelPage + "Click on 'Remove Friend link':");
 			WaitAndClick(By.XPath("//div[not(@style)]/div[ul[@class = 'MdMemberInfo']//span[contains(text(), '" + friendToRemove + "')]]/ul/li[@class = 'mdFriendRemove']/a"));
 		}
 	}
