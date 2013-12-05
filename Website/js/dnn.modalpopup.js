612ce9a4830cc390ba6731d565bbb7fd423e6054
Charles Nurse <charlesnurse@gmail.com>
2013-12-05 15:19:01 -0800

    Fix broken minimized file by temporaily using unminimized version

--- Website/js/dnn.modalpopup.js
+++ Website/js/dnn.modalpopup.js
@@ -1,25 +1,181 @@
-(function (k, e) {
-    k.d = {
+(function (window, $) {
+    window.dnnModal = {
+
         load: function () {
+
             try {
-                if (void 0 !== parent.location.href) {
-                    var a = parent; if ("undefined" != typeof a.parent.l) if (-1 == location.href.indexOf("popUp") || -1 < a.location.href.indexOf("popUp")) { var d = a.e("#iPopUp"), b = d.a("option", "refresh"), e = d.a("option", "closingUrl"), g = d.a("option", "minWidth"), h = d.a("option", "minHeight"), c = d.a("option", "showReturn"); e || (e = location.href); !0 === d.a("isOpen") && d.a("option", { close: function () { dnnModal.j({ url: e, width: g, height: h, f: c, refresh: b }) } }).a("close") } else a.e("#iPopUp").a({
-                        g: !1,
-                        title: document.title
-                    })
-                } return !0
-            } catch (l) { return !1 }
-        }, show: function (a, d, b, f, g, h) {
-            var c = e("#iPopUp"); 0 == c.length ? (c = e('<iframe id="iPopUp" src="about:blank" scrolling="auto" frameborder="0"></iframe>'), e(document.body).append(c)) : c.n("src", "about:blank"); e(document).find("html").c("overflow", "hidden"); c.a({ r: !0, g: !0, p: "dnnFormPopup", position: "center", minWidth: f, minHeight: b, maxWidth: 1920, maxHeight: 1080, s: !0, o: !0, refresh: g, f: d, i: h, close: function () { dnnModal.b(g, h) } }).width(f - 11).height(b - 11); b = dnn.m(e(".ui-widget-overlay")[0]);
-            null != b && (b.style.zIndex = 1); 0 === c.parent().find(".ui-dialog-title").next("a.dnnModalCtrl").length && (b = e('<a class="dnnModalCtrl"></a>'), c.parent().find(".ui-dialog-titlebar-close").t(b), b = e('<a href="#" class="dnnToggleMax"><span>Max</span></a>'), c.parent().find(".ui-dialog-titlebar-close").h(b), b.click(function (a) {
-                a.preventDefault(); var b = e(k); c.data("isMaximized") ? (a = c.data("height"), b = c.data("width"), c.data("isMaximized", !1)) : (c.data("height", c.a("option", "minHeight")).data("width", c.a("option",
-                "minWidth")).data("position", c.a("option", "position")), a = b.height() - 46, b = b.width() - 40, c.data("isMaximized", !0)); c.a({ height: a, width: b }); c.a({ position: "center" })
-            })); (function () { var a = e('<div class="dnnLoading"></div>'); a.c({ width: c.width(), height: c.height() }); c.h(a) })(); c[0].src = a; if ("true" == d.toString()) return !1
-        }, b: function (a, d) {
-            var b = parent, f = b.e("#iPopUp"); if ("undefined" === typeof a || null == a) a = !0; if ("true" == a.toString()) {
-                if ("undefined" === typeof d || "" == d) d = b.location.href; b.location.href = d; 0 < e(".ui-widget-overlay").length &&
-                dnn.k(e(".ui-widget-overlay")[0]); f.q()
-            } else 0 < e(".ui-widget-overlay").length && dnn.k(e(".ui-widget-overlay")[0]), f.a("option", "close", null).a("close"); e(b.document).find("html").c("overflow", "")
-        }, j: function (a) { var d = parent, b = d.parent; d.location.href !== b.location.href && d.location.href !== a.url ? b.d.show(a.url, a.f, a.height, a.width, a.refresh, a.i) : dnnModal.b(a.refresh, a.url) }
-    }; k.d.load()
-})(window, jQuery);
+                if (parent.location.href !== undefined) {
+
+                    var windowTop = parent;
+                    var parentTop = windowTop.parent;
+
+                    if (typeof (parentTop.$find) != "undefined") {
+                        if (location.href.indexOf('popUp') == -1 || windowTop.location.href.indexOf("popUp") > -1) {
+
+                            var popup = windowTop.jQuery("#iPopUp");
+                            var refresh = popup.dialog("option", "refresh");
+                            var closingUrl = popup.dialog("option", "closingUrl");
+                            var width = popup.dialog("option", "minWidth");
+                            var height = popup.dialog("option", "minHeight");
+                            var showReturn = popup.dialog("option", "showReturn");
+
+                            if (!closingUrl) {
+                                closingUrl = location.href;
+                            }
+
+                            if (popup.dialog('isOpen') === true) {
+
+                                popup.dialog("option", {
+                                    close: function (event, ui) {
+                                        dnnModal.refreshPopup({
+                                            url: closingUrl,
+                                            width: width,
+                                            height: height,
+                                            showReturn: showReturn,
+                                            refresh: refresh
+                                        });
+                                    }
+                                }).dialog('close');
+                            }
+                        } else {
+                            windowTop.jQuery("#iPopUp").dialog({ autoOpen: false, title: document.title });
+                        }
+                    }
+                }
+                return true;
+            } catch (err) {
+                return false;
+            }
+        },
+
+        show: function (url, showReturn, height, width, refresh, closingUrl) {
+            var $modal = $("#iPopUp");
+            if ($modal.length == 0) {
+                $modal = $("<iframe id=\"iPopUp\" src=\"about:blank\" scrolling=\"auto\" frameborder=\"0\"></iframe>");
+                $(document.body).append($modal);
+            } else {
+                $modal.attr('src', 'about:blank');
+            }
+            $(document).find('html').css('overflow', 'hidden');
+
+            $modal.dialog({
+                modal: true,
+                autoOpen: true,
+                dialogClass: "dnnFormPopup",
+                position: "center",
+                minWidth: width,
+                minHeight: height,
+                maxWidth: 1920,
+                maxHeight: 1080,
+                resizable: true,
+                closeOnEscape: true,
+                refresh: refresh,
+                showReturn: showReturn,
+                closingUrl: closingUrl,
+                close: function (event, ui) { dnnModal.closePopUp(refresh, closingUrl); }
+            })
+                .width(width - 11)
+                .height(height - 11);
+            var mask = dnn.addIframeMask($(".ui-widget-overlay")[0]);
+            if (mask != null) {
+                mask.style.zIndex = 1;
+            }
+            if ($modal.parent().find('.ui-dialog-title').next('a.dnnModalCtrl').length === 0) {
+                var $dnnModalCtrl = $('<a class="dnnModalCtrl"></a>');
+                $modal.parent().find('.ui-dialog-titlebar-close').wrap($dnnModalCtrl);
+                var $dnnToggleMax = $('<a href="#" class="dnnToggleMax"><span>Max</span></a>');
+                $modal.parent().find('.ui-dialog-titlebar-close').before($dnnToggleMax);
+
+                $dnnToggleMax.click(function (e) {
+                    e.preventDefault();
+
+                    var $window = $(window),
+                        newHeight,
+                        newWidth;
+
+                    if ($modal.data('isMaximized')) {
+                        newHeight = $modal.data('height');
+                        newWidth = $modal.data('width');
+                        $modal.data('isMaximized', false);
+                    } else {
+                        $modal.data('height', $modal.dialog("option", "minHeight"))
+                            .data('width', $modal.dialog("option", "minWidth"))
+                            .data('position', $modal.dialog("option", "position"));
+
+                        newHeight = $window.height() - 46;
+                        newWidth = $window.width() - 40;
+                        $modal.data('isMaximized', true);
+                    }
+
+                    $modal.dialog({ height: newHeight, width: newWidth });
+                    $modal.dialog({ position: 'center' });
+                });
+            }
+
+            var showLoading = function () {
+                var loading = $("<div class=\"dnnLoading\"></div>");
+                loading.css({
+                    width: $modal.width(),
+                    height: $modal.height()
+                });
+                $modal.before(loading);
+            };
+
+            var hideLoading = function () {
+                $modal.prev(".dnnLoading").remove();
+            };
+
+            showLoading();
+
+            $modal[0].src = url;
+
+            $modal.bind("load", function () {
+                hideLoading();
+            });
+
+            if (showReturn.toString() == "true") {
+                return false;
+            }
+        },
+
+        closePopUp: function (refresh, url) {
+            var windowTop = parent; //needs to be assign to a varaible for Opera compatibility issues.
+            var popup = windowTop.jQuery("#iPopUp");
+
+            if (typeof refresh === "undefined" || refresh == null) {
+                refresh = true;
+            }
+
+            if (refresh.toString() == "true") {
+                if (typeof url === "undefined" || url == "") {
+                    url = windowTop.location.href;
+                }
+
+                windowTop.location.href = url;
+                if ($(".ui-widget-overlay").length > 0) {
+                    dnn.removeIframeMask($(".ui-widget-overlay")[0]);
+                }
+                popup.hide();
+            } else {
+                if ($(".ui-widget-overlay").length > 0) {
+                    dnn.removeIframeMask($(".ui-widget-overlay")[0]);
+                }
+                popup.dialog('option', 'close', null).dialog('close');
+            }
+            $(windowTop.document).find('html').css('overflow', '');
+        },
+
+        refreshPopup: function (options) {
+            var windowTop = parent;
+            var windowTopTop = windowTop.parent;
+            if (windowTop.location.href !== windowTopTop.location.href &&
+                windowTop.location.href !== options.url) {
+                windowTopTop.dnnModal.show(options.url, options.showReturn, options.height, options.width, options.refresh, options.closingUrl);
+            } else {
+                dnnModal.closePopUp(options.refresh, options.url);
+            }
+        }
+    };
+
+    window.dnnModal.load();
+}(window, jQuery));
