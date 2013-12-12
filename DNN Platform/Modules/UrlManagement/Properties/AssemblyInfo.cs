340d8a0949e0116ab679c9ca15147b704b7d5237
Jamie Clayton <jamie.clayton@jenasysdesign.com.au>
2013-12-13 09:24:58 +1000

    Fix an inconsistency in the assembly and file version numbers for a few of the DNN projects.

--- DNN Platform/Modules/UrlManagement/Properties/AssemblyInfo.cs
+++ DNN Platform/Modules/UrlManagement/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.1.1.0")]
-[assembly: AssemblyFileVersion("7.1.1.0")]
+[assembly: AssemblyVersion("7.1.2.0")]
+[assembly: AssemblyFileVersion("7.1.2.0")]
