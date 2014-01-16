abb9031becaa353af0fcab43d1d9c7b67b2c28b6
James Coles-Nash <james.coles-nash@dnncorp.com>
2014-01-16 13:41:31 -0800

    Added some spaces to library assembly info to hopefully fix the git cache issue with teamcity.

--- DNN Platform/Library/AssemblyInfo.cs
+++ DNN Platform/Library/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using DotNetNuke.Application;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// associated with an assembly.  
 
 // Review the values of the assembly attributes
 
