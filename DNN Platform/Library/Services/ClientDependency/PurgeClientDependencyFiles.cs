18fa4322c1701a1cd0e45155253fe149adad9525
evidnn <eleonora@dnncorp.com>
2014-02-06 10:31:51 -0800

    DNN-4463 SI: Remove old versions of Client Dependency files

---
+++ DNN Platform/Library/Services/ClientDependency/PurgeClientDependencyFiles.cs
@@ -0,0 +1,64 @@
+﻿#region Copyright
+// 
+// DotNetNuke® - http://www.dotnetnuke.com
+// Copyright (c) 2002-2014
+// by DotNetNuke Corporation
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// **********************************
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+#endregion
+#region Usings
+
+using System;
+using System.IO;
+using DotNetNuke.Services.Scheduling;
+
+#endregion
+
+namespace DotNetNuke.Services.ClientDependency
+{
+    class PurgeClientDependencyFiles : SchedulerClient
+    {
+        public PurgeClientDependencyFiles(ScheduleHistoryItem objScheduleHistoryItem)
+        {
+            ScheduleHistoryItem = objScheduleHistoryItem; //REQUIRED
+        }
+
+        public override void DoWork()
+        {
+            try
+            {
+                string[] filePaths = Directory.GetFiles(string.Format("{0}/App_Data/ClientDependency", Common.Globals.ApplicationMapPath));
+                foreach (string filePath in filePaths)
+                {
+                    File.Delete(filePath);
+                }
+                ScheduleHistoryItem.Succeeded = true; //REQUIRED
+                ScheduleHistoryItem.AddLogNote("Purging client dependency files task succeeded");
+            }
+            catch (Exception exc) //REQUIRED
+            {
+                ScheduleHistoryItem.Succeeded = false; //REQUIRED
+
+                ScheduleHistoryItem.AddLogNote(string.Format("Purging client dependency files task failed: {0}.", exc.ToString()));
+
+                //notification that we have errored
+                Errored(ref exc); //REQUIRED
+
+                //log the exception
+                Exceptions.Exceptions.LogException(exc); //OPTIONAL
+            }
+        }
+    }
+}
