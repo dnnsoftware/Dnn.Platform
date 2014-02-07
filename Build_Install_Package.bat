f02ec3e2e81facc77b9986d096f395665782f6d4
Charles Nurse <charlesnurse@gmail.com>
2014-02-07 11:09:22 -0800

    Updated MSBuild support for local packages

--- Build_Install_Package.bat
+++ Build_Install_Package.bat
@@ -1,1 +1,2 @@
-%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:CreateInstall Build/BuildScripts/DNN_Package.build
+%WinDir%\Microsoft.NET\Framework\v4.0.30319\MSBuild /t:CreateInstall /v:m Build/BuildScripts/DNN_Package.build
+pause
