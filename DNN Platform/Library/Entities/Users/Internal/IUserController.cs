50fcf9baa04c349016a42fae1a4575e1db289360
Cathal Connolly <cathal@dnncorp.com>
2014-03-13 23:43:39 +0000

    DNN-4909 Cleanup UserController API add missed xmldocumentation

--- DNN Platform/Library/Entities/Users/Internal/IUserController.cs
+++ DNN Platform/Library/Entities/Users/Internal/IUserController.cs
@@ -27,6 +27,12 @@ namespace DotNetNuke.Entities.Users.Internal
 {
     public interface IUserController
     {
+        /// <summary>
+        /// Get a user based on their display name and portal
+        /// </summary>
+        /// <param name="portalId">the portalid</param>
+        /// <param name="displayName">the displayname</param>
+        /// <returns>The User as a UserInfo object</returns>
         UserInfo GetUserByDisplayname(int portalId, string displayName);
 
         /// <summary>
@@ -39,10 +45,37 @@ namespace DotNetNuke.Entities.Users.Internal
         /// <returns>The User as a UserInfo object</returns>
         UserInfo GetUserById(int portalId, int userId);
 
+        /// <summary>
+        /// Get a filtered list of users based on a number of criteria and filters - utilised for advanced searches
+        /// </summary>
+        /// <param name="portalId">the portalid of the user(s)</param>
+        /// <param name="userId">the userid accessing - for determining correct visibility permissions</param>
+        /// <param name="filterUserId">for filtering relationships on</param>
+        /// <param name="filterRoleId">for filtering by roles</param>
+        /// <param name="relationTypeId">for filtering by relationships</param>
+        /// <param name="isAdmin">determines visibility</param>
+        /// <param name="pageIndex">0 based page index</param>
+        /// <param name="pageSize">page size</param>
+        /// <param name="sortColumn">sort field</param>
+        /// <param name="sortAscending">sort flag indicating whether sort is asc or desc</param>
+        /// <param name="propertyNames">list of property names to filter</param>
+        /// <param name="propertyValues">list of property values to filter</param>
+        /// <returns>Users as a list of UserInfo objects</returns>
         IList<UserInfo> GetUsersAdvancedSearch(int portalId, int userId, int filterUserId, int filterRoleId, int relationTypeId,
                                                     bool isAdmin, int pageIndex, int pageSize, string sortColumn,
                                                     bool sortAscending, string propertyNames, string propertyValues);
 
+        /// <summary>
+        /// Get a filtered list of users based on a set of simple filters - utilised for basic searches
+        /// </summary>
+        /// <param name="portalId">the portalid of the user(s)</param>
+        /// <param name="pageIndex">0 based page index</param>
+        /// <param name="pageSize">page size</param>
+        /// <param name="sortColumn">sort field</param>
+        /// <param name="sortAscending">sort flag indicating whether sort is asc or desc</param>
+        /// <param name="propertyName">list of property names to filter</param>
+        /// <param name="propertyValue">list of property values to filter</param>
+        /// <returns>Users as a list of UserInfo objects</returns>
         IList<UserInfo> GetUsersBasicSearch(int portalId, int pageIndex, int pageSize, string sortColumn,
                                                 bool sortAscending, string propertyName, string propertyValue);
     }
