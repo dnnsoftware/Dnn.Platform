c8412d50e429a12518e6424c9eb252ab339851ee
Charles Nurse <charlesnurse@gmail.com>
2014-05-30 11:45:43 -0700

    remove unneccessary code.

--- DNN Platform/Library/Entities/Urls/AdvancedUrlRewriter.cs
+++ DNN Platform/Library/Entities/Urls/AdvancedUrlRewriter.cs
@@ -783,22 +783,11 @@ namespace DotNetNuke.Entities.Urls
                 response.AppendHeader("X-" + _productName + "-Debug",
                                       string.Format(debugMsg, requestUri, finalUrl, rewritePath, action, productVer,
                                                     portalSettings, browser));
-#if (DEBUG)
-                var rawOutput = new StringWriter();
-                rawOutput.WriteLine("<div style='background-color:black;color:white;'>");
-                rawOutput.WriteLine("<p>Advanced Url Rewriting Debug Output</p>");
-                rawOutput.WriteLine("<p>" +
-                                    string.Format(debugMsg, requestUri, finalUrl, rewritePath, action, productVer,
-                                                  portalSettings, browser) + "</p>");
-#endif
                 int msgNum = 1;
                 if (result != null)
                 {
                     foreach (string msg in result.DebugMessages)
                     {
-#if (DEBUG)
-                        rawOutput.WriteLine("<div>Debug Message " + msgNum.ToString("00") + ": " + msg + "</div>");
-#endif
                         response.AppendHeader("X-" + _productName + "-Debug-" + msgNum.ToString("00"), msg);
                         msgNum++;
                     }
@@ -807,19 +796,6 @@ namespace DotNetNuke.Entities.Urls
                 {
                     response.AppendHeader("X-" + _productName + "-Ex", ex.Message);
                 }
-#if (DEBUG)
-                if (ex != null)
-                {
-                    rawOutput.WriteLine("Exception : " + ex.Message);
-                }
-                else
-                {
-                    rawOutput.WriteLine("No Exception");
-                }
-
-                rawOutput.WriteLine("</div>");
-                response.Write(rawOutput.ToString());
-#endif
             }
         }
 
@@ -1157,17 +1133,7 @@ namespace DotNetNuke.Entities.Urls
                         }
                         else
                         {
-#if (DEBUG)
-                            //955: xss vulnerability when outputting raw url back to the page
-                            //only do so in debug mode, and sanitize the Url.
-                            //sanitize output Url to prevent xss attacks on the default 404 page
-                            string requestedUrl = request.Url.ToString();
-                            requestedUrl = HttpUtility.HtmlEncode(requestedUrl);
-                            errorPageHtml.Write(status + "<br>The requested Url (" + requestedUrl +
-                                                ") does not return any valid content.");
-#else
                             errorPageHtml.Write(status + "<br>The requested Url does not return any valid content.");
-#endif
                             if (reason404 != null)
                             {
                                 errorPageHtml.Write(status + "<br>" + reason404);
