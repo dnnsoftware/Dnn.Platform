2f385a6f5519f89bd75cd35073545f5fe4851d53
janjonas <mail@janjonas.net>
2014-08-22 17:01:39 +0200

    Make admin page "User Account" more robust against invalid URL

--- Website/DesktopModules/Admin/Security/Users.ascx.cs
+++ Website/DesktopModules/Admin/Security/Users.ascx.cs
@@ -498,7 +498,11 @@ namespace DotNetNuke.Modules.Admin.Users
         {
             if (Request.QueryString["pagesize"] != null)
             {
-                grdUsers.PageSize = Convert.ToInt32(Request.QueryString["pagesize"]);
+                int pageSize;
+                if (Int32.TryParse(Request.QueryString["pagesize"], out pageSize))
+                {
+                    grdUsers.PageSize = pageSize;    
+                }
             }            
             else
             {
