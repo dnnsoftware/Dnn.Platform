57715adeb628dcef382e85260086c0e239e84890
Oliver Hine <oliver.hine@gmail.com>
2014-08-27 10:38:38 -0400

    update copyright info

--- DNN Platform/DotNetNuke.WebUtility/Browser Caps/BrowserCollection.vb
+++ DNN Platform/DotNetNuke.WebUtility/Browser Caps/BrowserCollection.vb
@@ -1,7 +1,7 @@
 '
 ' DotNetNuke® - http://www.dotnetnuke.com
 ' Copyright (c) 2002-2014
-' by Perpetual Motion Interactive Systems Inc. ( http://www.perpetualmotion.ca )
+' by DotNetNuke Corporation
 '
 ' Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
 ' documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
