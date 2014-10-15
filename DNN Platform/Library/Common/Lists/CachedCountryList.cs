bc5a7538fe60dcdec00835b872fddc5fd8a3adb9
Peter Donker <peter@bring2mind.net>
2014-10-15 23:00:08 +0200

    Addition of new control for countries

---
+++ DNN Platform/Library/Common/Lists/CachedCountryList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotNetNuke.Common.Utilities;
+
+namespace DotNetNuke.Common.Lists
+{
+	public class CachedCountryList : Dictionary<string, CachedCountryList.Country>
+	{
+
+		#region " Structures "
+		public struct Country
+		{
+			public string Name;
+			public string Code;
+			public string FullName;
+			public string NormalizedFullName;
+		}
+		#endregion
+
+		#region " Constructors "
+		public CachedCountryList(string locale)
+			: base()
+		{
+
+			foreach (ListEntryInfo li in (new ListController()).GetListEntryInfoItems("Country"))
+			{
+				string text = li.Text;
+				Country c = new Country
+				{
+					Code = li.Value,
+					FullName = string.Format("{0} ({1})", text, li.Value),
+					Name = text
+				};
+				c.NormalizedFullName = c.FullName.NormalizeString();
+				Add(li.Value, c);
+			}
+
+		}
+		#endregion
+
+		#region " Static Methods "
+		public static CachedCountryList GetCountryList(string locale)
+		{
+
+			CachedCountryList res = null;
+			try
+			{
+				res = (CachedCountryList)DotNetNuke.Common.Utilities.DataCache.GetCache(CacheKey(locale));
+			}
+			catch (Exception ex)
+			{
+			}
+			if (res == null)
+			{
+				res = new CachedCountryList(locale);
+				DotNetNuke.Common.Utilities.DataCache.SetCache(CacheKey(locale), res);
+			}
+			return res;
+
+		}
+
+		public static string CacheKey(string locale)
+		{
+			return string.Format("CountryList:{0}", locale);
+		}
+		#endregion
+
+	}
+}
---
+++ DNN Platform/Library/UI/WebControls/PropertyEditor/Edit Controls/DNN Edit Controls/DNNCountryAutocompleteControl.cs
@@ -0,0 +1,186 @@
+#region Copyright
+// 
+// DotNetNuke® - http://www.dotnetnuke.com
+// Copyright (c) 2002-2014
+// by DotNetNuke Corporation
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+#endregion
+#region Usings
+
+using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+using DotNetNuke.Common.Lists;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Framework.JavaScriptLibraries;
+
+#endregion
+
+namespace DotNetNuke.UI.WebControls
+{
+
+	[ToolboxData("<{0}:DnnCountryAutocompleteControl runat=server></{0}:DnnCountryAutocompleteControl>")]
+	public class DnnCountryAutocompleteControl : EditControl
+	{
+
+		#region " Controls "
+		private TextBox _CountryName;
+		private TextBox CountryName
+		{
+			get
+			{
+				if (_CountryName == null)
+				{
+					_CountryName = new TextBox();
+				}
+				return _CountryName;
+			}
+		}
+
+		private HiddenField _CountryCode;
+		private HiddenField CountryCode
+		{
+			get
+			{
+				if (_CountryCode == null)
+				{
+					_CountryCode = new HiddenField();
+				}
+				return _CountryCode;
+			}
+		}
+		#endregion
+
+		#region " Properties "
+		protected override string StringValue
+		{
+			get
+			{
+				string strValue = Null.NullString;
+				if (Value != null)
+				{
+					strValue = Convert.ToString(Value);
+				}
+				return strValue;
+			}
+			set { this.Value = value; }
+		}
+
+		protected string OldStringValue
+		{
+			get { return Convert.ToString(OldValue); }
+		}
+		#endregion
+
+		#region " Constructors "
+		public DnnCountryAutocompleteControl()
+		{
+			Init += DnnCountryRegionControl_Init;
+		}
+		public DnnCountryAutocompleteControl(string type)
+		{
+			Init += DnnCountryRegionControl_Init;
+			SystemType = type;
+		}
+		#endregion
+
+		#region " Overrides "
+		protected override void OnDataChanged(EventArgs e)
+		{
+			PropertyEditorEventArgs args = new PropertyEditorEventArgs(Name);
+			args.Value = StringValue;
+			args.OldValue = OldStringValue;
+			args.StringValue = StringValue;
+			base.OnValueChanged(args);
+		}
+
+		protected override void CreateChildControls()
+		{
+			base.CreateChildControls();
+
+			CountryName.ControlStyle.CopyFrom(ControlStyle);
+			CountryName.ID = ID + "_name";
+			CountryName.Attributes.Add("data-name", Name);
+			CountryName.Attributes.Add("data-list", "Country");
+			CountryName.Attributes.Add("data-category", Category);
+			CountryName.Attributes.Add("data-editor", "DnnCountryAutocompleteControl");
+			Controls.Add(CountryName);
+
+			CountryCode.ID = ID + "_code";
+			Controls.Add(CountryCode);
+
+		}
+
+		public override bool LoadPostData(string postDataKey, System.Collections.Specialized.NameValueCollection postCollection)
+		{
+			EnsureChildControls();
+			bool dataChanged = false;
+			string presentValue = StringValue;
+			string postedValue = postCollection[postDataKey + "_code"];
+			if (!presentValue.Equals(postedValue))
+			{
+				Value = postedValue;
+				dataChanged = true;
+			}
+			return dataChanged;
+		}
+
+		protected override void OnPreRender(System.EventArgs e)
+		{
+			base.OnPreRender(e);
+
+			LoadControls();
+
+			if (Page != null & EditMode == PropertyEditorMode.Edit)
+			{
+				Page.RegisterRequiresPostBack(this);
+				Page.RegisterRequiresPostBack(CountryCode);
+			}
+
+		}
+
+		protected override void RenderEditMode(HtmlTextWriter writer)
+		{
+			RenderChildren(writer);
+		}
+		#endregion
+
+		#region " Page Events "
+		private void DnnCountryRegionControl_Init(object sender, System.EventArgs e)
+		{
+			// DotNetNuke.Web.Client.ClientResourceManagement.ClientResourceManager.RegisterScript(Page, ResolveUrl("~/DesktopModules/Albatros/Registration/js/countryregionbox.js"), 70);
+			JavaScript.RequestRegistration(CommonJs.jQuery);
+			JavaScript.RequestRegistration(CommonJs.jQueryUI);
+		}
+
+		#endregion
+
+		#region " Private Methods "
+		private void LoadControls()
+		{
+			CountryName.Text = StringValue;
+			CachedCountryList countries = CachedCountryList.GetCountryList(System.Threading.Thread.CurrentThread.CurrentCulture.Name);
+			if (countries.ContainsKey(StringValue))
+			{
+				CountryName.Text = countries[StringValue].Name;
+			}
+			CountryCode.Value = StringValue;
+		}
+		#endregion
+
+	}
+}
