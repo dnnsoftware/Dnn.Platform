24400a8432fa1d2b65268ec6e012aa38dc05f0d9
Peter Donker <peter@bring2mind.net>
2014-10-28 22:58:15 +0100

    User ParentKey in localization key

--- DNN Platform/Library/Common/Lists/ListEntryInfo.cs
+++ DNN Platform/Library/Common/Lists/ListEntryInfo.cs
@@ -72,7 +72,7 @@ namespace DotNetNuke.Common.Lists
 
         private string _Text = Null.NullString;
         /// <summary>
-        /// Localized text value of the list entry item. An attempt is made to look up the key "[Value].Text" in the resource file 
+        /// Localized text value of the list entry item. An attempt is made to look up the key "[ParentKey].[Value].Text" in the resource file 
         /// "App_GlobalResources/List_[ListName]". If not found the original (database) value is used.
         /// </summary>
         /// <value>
@@ -85,7 +85,7 @@ namespace DotNetNuke.Common.Lists
                 string res = null;
                 try
                 {
-                    res = Services.Localization.Localization.GetString(Value + ".Text", "~/App_GlobalResources/List_" + ListName + ".resx");
+                    res = Services.Localization.Localization.GetString(Value + ".Text", "~/App_GlobalResources/List_" + ParentKey + '.' + ListName + ".resx");
                 }
                 catch { }
                 if (string.IsNullOrEmpty(res)) { res = _Text; };
