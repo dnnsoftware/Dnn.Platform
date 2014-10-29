633c37d47b721a20cf0b4d66029877537dc20f02
Peter Donker <peter@bring2mind.net>
2014-10-29 11:15:48 +0100

    Repair to parentkey logic to retrieve localized value

--- DNN Platform/Library/Common/Lists/ListEntryInfo.cs
+++ DNN Platform/Library/Common/Lists/ListEntryInfo.cs
@@ -85,7 +85,14 @@ namespace DotNetNuke.Common.Lists
                 string res = null;
                 try
                 {
-                    res = Services.Localization.Localization.GetString(Value + ".Text", "~/App_GlobalResources/List_" + ParentKey + '.' + ListName + ".resx");
+					if (string.IsNullOrEmpty(ParentKey))
+					{
+						res = Services.Localization.Localization.GetString(Value + ".Text", "~/App_GlobalResources/List_" + ListName + ".resx");
+					}
+					else
+					{
+						res = Services.Localization.Localization.GetString(ParentKey + '.' + Value + ".Text", "~/App_GlobalResources/List_" + ListName + ".resx");
+					}
                 }
                 catch { }
                 if (string.IsNullOrEmpty(res)) { res = _Text; };
