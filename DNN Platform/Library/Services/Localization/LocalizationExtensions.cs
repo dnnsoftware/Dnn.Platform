bf4ecb3497da98ee2a310a5bd46f86cc89ab25c4
Peter Donker <peter@bring2mind.net>
2014-12-08 17:32:43 +0100

    Added tweak to localization locale regex to include 3 letter languages

--- DNN Platform/Library/Services/Localization/LocalizationExtensions.cs
+++ DNN Platform/Library/Services/Localization/LocalizationExtensions.cs
@@ -27,7 +27,7 @@ namespace DotNetNuke.Services.Localization
 {
     public static class LocalizationExtensions
     {
-        public const string ResxFileLocaleRegex = "(?i)(.*)\\.((\\w\\w-)?\\w\\w-\\w{2,3})(\\.resx)$(?-i)";
+        public const string ResxFileLocaleRegex = "(?i)(.*)\\.((\\w\\w-)?\\w{2,3}-\\w{2,3})(\\.resx)$(?-i)";
 
         /// <summary>
         /// Gets the name of the locale code from a resource file.
