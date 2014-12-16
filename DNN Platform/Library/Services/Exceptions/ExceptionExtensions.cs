771e9dbeaafb303f1acadadb7663d122d07801a9
George Alatrash <george.alatrash@dnnsoftware.com>
2014-12-16 12:15:51 -0800

    DNN-6240: fix for Violation of PRIMARY KEY constraint 'PK_dnn_Exceptions'
    The fix contains a performance improvement as well: using BASE64 for byte conversion instead of iterating and concatenating HEX formatted values to a string.

--- DNN Platform/Library/Services/Exceptions/ExceptionExtensions.cs
+++ DNN Platform/Library/Services/Exceptions/ExceptionExtensions.cs
@@ -1,28 +1,30 @@
 ﻿using System;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace DotNetNuke.Services.Exceptions
 {
-	public static class ExceptionExtensions
-	{
-		public static string Hash(this Exception exc)
-		{
-			var sb = new StringBuilder();
-			sb.AppendLine(exc.Message);
-			sb.AppendLine(exc.StackTrace);
-			if (exc.InnerException != null)
-			{
-				sb.AppendLine(exc.InnerException.Message);
-				sb.AppendLine(exc.InnerException.StackTrace);
-			}
-			sb.AppendLine(exc.Source);
-			using (var hasher = new MD5CryptoServiceProvider())
-			{
-				var byteArray = hasher.ComputeHash(Encoding.Unicode.GetBytes(sb.ToString()));
-				return byteArray.Aggregate("", (current, b) => current + b.ToString("x2"));
-			}
-		}
-	}
+    public static class ExceptionExtensions
+    {
+        public static string Hash(this Exception exc)
+        {
+            var ex = exc ?? new Exception();
+            var sb = new StringBuilder()
+                .AppendLine(DateTime.UtcNow.ToString("O")) // make it unique in case two exact exceptions were thrown
+                .AppendLine(ex.Source)
+                .AppendLine(ex.ToString());
+
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+                sb.AppendLine(ex.ToString());
+            }
+
+            using (var hasher = new MD5CryptoServiceProvider())
+            {
+                var byteArray = hasher.ComputeHash(Encoding.Unicode.GetBytes(sb.ToString()));
+                return Convert.ToBase64String(byteArray);
+            }
+        }
+    }
 }
