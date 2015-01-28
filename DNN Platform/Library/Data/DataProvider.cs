05e291991d0c0aafbae9cfd2b30b4c4e540c61ff
Cathal Connolly <cathal@dnncorp.com>
2015-01-28 14:10:58 +0000

    DNN-6518 CLONE - Workflow/Versions not enabled after non-English Installation

--- DNN Platform/Library/Data/DataProvider.cs
+++ DNN Platform/Library/Data/DataProvider.cs
@@ -559,6 +559,11 @@ namespace DotNetNuke.Data
             return ExecuteReader("GetAllPortals");
         }
 
+        public virtual IDataReader GetDistinctPortals()
+        {
+            return ExecuteReader("GetDistinctPortals");
+        }
+
 		public virtual IDataReader GetPortalsByName(string nameToMatch, int pageIndex, int pageSize)
 		{
 			return ExecuteReader("GetPortalsByName", nameToMatch, pageIndex, pageSize);
