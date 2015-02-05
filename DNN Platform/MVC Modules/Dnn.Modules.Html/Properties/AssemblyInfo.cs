137e6ff0b73ca92fc3db66dfd8df5cf2c7960375
Charles Nurse <charlesnurse@gmail.com>
2015-02-05 08:03:19 -0800

    DNN-6321 Support MVC Modules as a First Class citizen

--- DNN Platform/MVC Modules/Dnn.Modules.Html/Properties/AssemblyInfo.cs
+++ DNN Platform/MVC Modules/Dnn.Modules.Html/Properties/AssemblyInfo.cs
@@ -8,10 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DotNetNuke.Web.Mvc.Html")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNetNuke.Web.Mvc.Html")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -21,15 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("39ae62e8-2098-4347-a09a-8a0e0ff20614")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
