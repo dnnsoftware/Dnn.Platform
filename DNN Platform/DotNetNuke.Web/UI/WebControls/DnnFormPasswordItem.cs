a5ada8906ce0e467f74352fafb2c5ab04b597b9e
Ben <ben.zhong@dnnsoftware.com>
2015-03-10 13:15:06 +0800

    DNN-6105: if register failed, keep password field's value; if register complete but show message, then hide register form.

--- DNN Platform/DotNetNuke.Web/UI/WebControls/DnnFormPasswordItem.cs
+++ DNN Platform/DotNetNuke.Web/UI/WebControls/DnnFormPasswordItem.cs
@@ -108,6 +108,8 @@ namespace DotNetNuke.Web.UI.WebControls
         {
             base.OnPreRender(e);
 
+			_password.Attributes.Add("value", Convert.ToString(Value));
+
             var options = new DnnPaswordStrengthOptions();
             var optionsAsJsonString = Json.Serialize(options);
             var script = string.Format("dnn.initializePasswordStrength('.{0}', {1});{2}",
