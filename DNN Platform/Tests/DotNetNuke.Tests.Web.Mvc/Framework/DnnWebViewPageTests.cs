0e576e7affef4a1913eb92ccdddb189ff1a2b7d8
Charles Nurse <charlesnurse@gmail.com>
2015-03-24 15:52:29 -0700

    Fix DnnWebViewPage Tests

--- DNN Platform/Tests/DotNetNuke.Tests.Web.Mvc/Framework/DnnWebViewPageTests.cs
+++ DNN Platform/Tests/DotNetNuke.Tests.Web.Mvc/Framework/DnnWebViewPageTests.cs
@@ -24,6 +24,8 @@
 using System.Web.Mvc;
 using DotNetNuke.Tests.Web.Mvc.Fakes;
 using DotNetNuke.Web.Mvc.Framework;
+using DotNetNuke.Web.Mvc.Framework.Controllers;
+using DotNetNuke.Web.Mvc.Helpers;
 using Moq;
 using NUnit.Framework;
 
@@ -32,34 +34,88 @@ namespace DotNetNuke.Tests.Web.Mvc.Framework
     [TestFixture]
     public class DnnWebViewPageTests
     {
-        //[Test]
-        //public void InitHelpers_Sets_Dnn_Property()
-        //{
-        //    //Arrange
-        //    var mockViewPage = new Mock<DnnWebViewPage>() {CallBase = true};
-        //    mockViewPage.Object.ViewContext = new ViewContext();
+        [Test]
+        public void InitHelpers_Sets_Dnn_Property()
+        {
+            //Arrange
+            var mockViewPage = new Mock<DnnWebViewPage>() { CallBase = true };
+            var mockController = new Mock<ControllerBase>();
+            var mockDnnController = mockController.As<IDnnController>();
+            var viewContext = new ViewContext
+                                    {
+                                        Controller = mockController.Object
+                                    };
+            mockViewPage.Object.ViewContext = viewContext;
 
-        //    //Act
-        //    mockViewPage.Object.InitHelpers();
+            //Act
+            mockViewPage.Object.InitHelpers();
 
-        //    //Assert
-        //    Assert.NotNull(mockViewPage.Object.Dnn);
-        //    Assert.IsInstanceOf<DnnHelper>(mockViewPage.Object.Dnn);
-        //}
+            //Assert
+            Assert.NotNull(mockViewPage.Object.Dnn);
+            Assert.IsInstanceOf<DnnHelper>(mockViewPage.Object.Dnn);
+        }
 
-        //[Test]
-        //public void InitHelpers_Sets_Dnn_Property_For_Strongly_Typed_Helper()
-        //{
-        //    //Arrange
-        //    var mockViewPage = new Mock<DnnWebViewPage<Dog>>() { CallBase = true };
-        //    mockViewPage.Object.ViewContext = new ViewContext();
+        [Test]
+        public void InitHelpers_Sets_Html_Property()
+        {
+            //Arrange
+            var mockViewPage = new Mock<DnnWebViewPage>() { CallBase = true };
+            var mockController = new Mock<ControllerBase>();
+            var mockDnnController = mockController.As<IDnnController>();
+            var viewContext = new ViewContext
+            {
+                Controller = mockController.Object
+            };
+            mockViewPage.Object.ViewContext = viewContext;
 
-        //    //Act
-        //    mockViewPage.Object.InitHelpers();
+            //Act
+            mockViewPage.Object.InitHelpers();
 
-        //    //Assert
-        //    Assert.NotNull(mockViewPage.Object.Dnn);
-        //    Assert.IsInstanceOf<DnnHelper<Dog>>(mockViewPage.Object.Dnn);
-        //}
+            //Assert
+            Assert.NotNull(mockViewPage.Object.Html);
+            Assert.IsInstanceOf<DnnHtmlHelper>(mockViewPage.Object.Html);
+        }
+
+        [Test]
+        public void InitHelpers_Sets_Dnn_Property_For_Strongly_Typed_Helper()
+        {
+            //Arrange
+            var mockViewPage = new Mock<DnnWebViewPage<Dog>>() { CallBase = true };
+            var mockController = new Mock<ControllerBase>();
+            var mockDnnController = mockController.As<IDnnController>();
+            var viewContext = new ViewContext
+            {
+                Controller = mockController.Object
+            };
+            mockViewPage.Object.ViewContext = viewContext;
+
+            //Act
+            mockViewPage.Object.InitHelpers();
+
+            //Assert
+            Assert.NotNull(mockViewPage.Object.Html);
+            Assert.IsInstanceOf<DnnHtmlHelper<Dog>>(mockViewPage.Object.Html);
+        }
+
+        [Test]
+        public void InitHelpers_Sets_Url_Property()
+        {
+            //Arrange
+            var mockViewPage = new Mock<DnnWebViewPage>() { CallBase = true };
+            var mockController = new Mock<ControllerBase>();
+            var mockDnnController = mockController.As<IDnnController>();
+            var viewContext = new ViewContext
+            {
+                Controller = mockController.Object
+            };
+            mockViewPage.Object.ViewContext = viewContext;
+
+            //Act
+            mockViewPage.Object.InitHelpers();
+
+            //Assert
+            Assert.NotNull(mockViewPage.Object.Url);
+            Assert.IsInstanceOf<DnnUrlHelper>(mockViewPage.Object.Url);
+        }
     }
 }
