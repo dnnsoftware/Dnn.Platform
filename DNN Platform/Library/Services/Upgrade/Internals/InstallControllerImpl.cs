dcd692e46168ed2bf823ca6febd813ac53671ae6
Cathal Connolly <Cathal.Connolly@dnnsoftware.com>
2015-04-16 13:52:37 +0100

    Revert "DNN-6134 Compatibility with LocalDB"

--- DNN Platform/Library/Services/Upgrade/Internals/InstallControllerImpl.cs
+++ DNN Platform/Library/Services/Upgrade/Internals/InstallControllerImpl.cs
@@ -600,11 +600,7 @@ namespace DotNetNuke.Services.Upgrade.Internals
             if (!string.IsNullOrEmpty(config.File))
             {
                 builder["attachDbFilename"] = "|DataDirectory|" + config.File;
-                // LocalDB does not support User Instance attribute
-                // perhaps a better solution is to not force this from code and let it to the decision fo the person writing the connection string
-                if (string.IsNullOrEmpty(config.Server) || config.Server.IndexOf("(localdb)", StringComparison.OrdinalIgnoreCase) == -1)
-                    builder["user instance"] = true;
-
+                builder["user instance"] = true;
             }
             else
             {
