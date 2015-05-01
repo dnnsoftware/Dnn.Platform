c78d49cb87d14561b0132211d6494fa44da26959
evidnn <eleonora@dnncorp.com>
2015-05-01 16:54:46 -0700

    DNN-6135 Sitemap don't work on multilanguage DNN 7.3.4

--- DNN Platform/Library/Services/Sitemap/CoreSitemapProvider.cs
+++ DNN Platform/Library/Services/Sitemap/CoreSitemapProvider.cs
@@ -32,6 +32,7 @@ using DotNetNuke.Common.Utilities;
 using DotNetNuke.Entities.Modules;
 using DotNetNuke.Entities.Portals;
 using DotNetNuke.Entities.Tabs;
+using DotNetNuke.Instrumentation;
 using DotNetNuke.Security.Permissions;
 
 #endregion
@@ -42,6 +43,7 @@ namespace DotNetNuke.Services.Sitemap
     {
         private bool includeHiddenPages;
         private float minPagePriority;
+        private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(CoreSitemapProvider));
 
         private PortalSettings ps;
         private bool useLevelBasedPagePriority;
@@ -74,8 +76,16 @@ namespace DotNetNuke.Services.Sitemap
                 {
                     if ((includeHiddenPages || tab.IsVisible) && tab.HasBeenPublished)
                     {
-                        pageUrl = GetPageUrl(tab, (ps.ContentLocalizationEnabled) ? tab.CultureCode : null);
-                        urls.Add(pageUrl);
+                        try
+                        {
+                            pageUrl = GetPageUrl(tab, (ps.ContentLocalizationEnabled) ? tab.CultureCode : null);
+                            urls.Add(pageUrl);
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.ErrorFormat("Error has occurred getting PageUrl for {0}", tab.TabName);
+                        }
+
                     }
                 }
             }
