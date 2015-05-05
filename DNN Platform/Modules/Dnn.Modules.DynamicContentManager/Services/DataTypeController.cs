8eee649ab725a78252ff4b98efc16db62f102c76
Charles Nurse <charlesnurse@gmail.com>
2015-05-05 14:53:04 -0700

    Add Service Framework classes

---
+++ DNN Platform/Modules/Dnn.Modules.DynamicContentManager/Services/DataTypeController.cs
@@ -0,0 +1,50 @@
+﻿#region Copyright
+// 
+// DotNetNuke® - http://www.dotnetnuke.com
+// Copyright (c) 2002-2014
+// by DotNetNuke Corporation
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
+#endregion
+
+using System.Net.Http;
+using Dnn.DynamicContent;
+using DotNetNuke.Web.Api;
+using DotNetNuke.Security;
+
+namespace Dnn.Modules.DynamicContentManager.Services
+{
+    /// <summary>
+    /// DataTypeService provides the Web Services to manage Data Types
+    /// </summary>
+    [SupportedModules("Dnn.DynamicContentManager")]
+    [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.View)]
+    public class DataTypeController : DnnApiController
+    {
+
+        /// <summary>
+        /// GetDataTypes retrieves a page of DataTypes that satisfy the searchTerm
+        /// </summary>
+        /// <param name="searchTerm">The text to use to search for DataTypes</param>
+        /// <param name="pageIndex">The page to return, where 0 is the first page</param>
+        /// <param name="pageSize">The size of the page</param>
+        /// <returns></returns>
+        public HttpResponseMessage GetDataTypes(string searchTerm, int pageIndex, int pageSize)
+        {
+            var dataTypes = DataTypeManager.Instance.GetDataTypes();
+            return Request.CreateResponse(new {status = "Success"});
+        }
+    }
+}
