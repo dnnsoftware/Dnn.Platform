488674ea61a6ffb21483109f744d85359c86a4f4
Ben <ben.zhong@dnnsoftware.com>
2015-06-23 14:32:28 +0800

    CONTENT-7045: move ContentTypes and GetContentType into IFileContentTypeManager.

--- DNN Platform/Library/Services/FileSystem/FileManager.cs
+++ DNN Platform/Library/Services/FileSystem/FileManager.cs
@@ -70,43 +70,10 @@ namespace DotNetNuke.Services.FileSystem
 
         #region Properties
 
-        private IDictionary<string, string> _contentTypes;
-		private static readonly object _threadLocker = new object();
-
-        public virtual IDictionary<string, string> ContentTypes
-        {
-            get
-            {
-                if (_contentTypes == null)
-                {
-					lock (_threadLocker)
-	                {
-		                if (_contentTypes == null)
-		                {
-			                var listController = new ListController();
-			                var listEntries = listController.GetListEntryInfoItems("ContentTypes");
-			                if (listEntries == null || !listEntries.Any())
-			                {
-				                _contentTypes = GetDefaultContentTypes();
-			                }
-			                else
-			                {
-				                _contentTypes = new Dictionary<string, string>();
-								if (listEntries != null)
-								{
-									foreach (var contentTypeEntry in listEntries)
-									{
-										_contentTypes.Add(contentTypeEntry.Value, contentTypeEntry.Text);
-									}
-								}
-			                }
-		                }
-	                }
-                }
-
-                return _contentTypes;
-            }
-        }
+		public virtual IDictionary<string, string> ContentTypes
+		{
+			get { return FileContentTypeManager.Instance.ContentTypes; }
+		}
 
         private bool IgnoreWhiteList
         {
@@ -129,48 +96,6 @@ namespace DotNetNuke.Services.FileSystem
         #endregion
 
         #region Private Methods
-
-        private Dictionary<string, string> GetDefaultContentTypes()
-        {
-            var contentTypes = new Dictionary<string, string>();
-            contentTypes.Add("txt", "text/plain");
-            contentTypes.Add("htm", "text/html");
-            contentTypes.Add("html", "text/html");
-            contentTypes.Add("rtf", "text/richtext");
-            contentTypes.Add("jpg", "image/jpeg");
-            contentTypes.Add("jpeg", "image/jpeg");
-            contentTypes.Add("gif", "image/gif");
-            contentTypes.Add("bmp", "image/bmp");
-            contentTypes.Add("png", "image/png");
-            contentTypes.Add("ico", "image/x-icon");
-            contentTypes.Add("svg", "image/svg+xml");
-            contentTypes.Add("ttf", "font/ttf");
-            contentTypes.Add("eot", "application/vnd.ms-fontobject");
-            contentTypes.Add("woff", "application/font-woff");
-            contentTypes.Add("mp3", "audio/mpeg");
-            contentTypes.Add("wma", "audio/x-ms-wma");
-            contentTypes.Add("mpg", "video/mpeg");
-            contentTypes.Add("mpeg", "video/mpeg");
-            contentTypes.Add("avi", "video/avi");
-            contentTypes.Add("mp4", "video/mp4");
-            contentTypes.Add("wmv", "video/x-ms-wmv");
-            contentTypes.Add("pdf", "application/pdf");
-            contentTypes.Add("doc", "application/msword");
-            contentTypes.Add("dot", "application/msword");
-            contentTypes.Add("docx", "application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-            contentTypes.Add("dotx", "application/vnd.openxmlformats-officedocument.wordprocessingml.template");
-            contentTypes.Add("csv", "text/csv");
-            contentTypes.Add("xls", "application/x-msexcel");
-            contentTypes.Add("xlt", "application/x-msexcel");
-            contentTypes.Add("xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-            contentTypes.Add("xltx", "application/vnd.openxmlformats-officedocument.spreadsheetml.template");
-            contentTypes.Add("ppt", "application/vnd.ms-powerpoint");
-            contentTypes.Add("pps", "application/vnd.ms-powerpoint");
-            contentTypes.Add("pptx", "application/vnd.openxmlformats-officedocument.presentationml.presentation");
-            contentTypes.Add("ppsx", "application/vnd.openxmlformats-officedocument.presentationml.slideshow");
-
-            return contentTypes;
-        }
 
         private void AddFileToFolderProvider(Stream fileContent, string fileName, IFolderInfo destinationFolder, FolderProvider provider)
         {
@@ -828,10 +753,7 @@ namespace DotNetNuke.Services.FileSystem
         public virtual string GetContentType(string extension)
         {
 
-            if (string.IsNullOrEmpty(extension)) return "application/octet-stream";
-
-            var key = extension.TrimStart('.').ToLowerInvariant();
-            return ContentTypes.ContainsKey(key) ? ContentTypes[key] : "application/octet-stream";
+	        return FileContentTypeManager.Instance.GetContentType(extension);
         }
 
         /// <summary>
