27c2fc159f2c3f78a13ff07e662c4103c1ba0fc8
jbrinkman <joe@dnncorp.com>
2015-07-23 11:20:40 -0400

    Update project version for Beta 2.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,21 @@
-﻿/*
- * CKEditor Html Editor Provider for DotNetNuke
- * ========
- * http://dnnckeditor.codeplex.com/
- * Copyright (C) Ingo Herbote
- *
- * The software, this file and its contents are subject to the CKEditor Provider
- * License. Please read the license.txt file before using, installing, copying,
- * modifying or distribute this file or part of its contents. The contents of
- * this file is part of the Source Code of the CKEditor Provider.
- */
+﻿using System.Runtime.InteropServices;
+/*
+* CKEditor Html Editor Provider for DotNetNuke
+* ========
+* http://dnnckeditor.codeplex.com/
+* Copyright (C) Ingo Herbote
+*
+* The software, this file and its contents are subject to the CKEditor Provider
+* License. Please read the license.txt file before using, installing, copying,
+* modifying or distribute this file or part of its contents. The contents of
+* this file is part of the Source Code of the CKEditor Provider.
+*/
 
 using System.Reflection;
 
-[assembly: AssemblyDescription("CKEditor HTML Editor Provider for DotNetNuke 7.x")]
+[assembly: AssemblyDescription("CKEditor HTML Editor Provider for DNN")]
+[assembly: AssemblyTitle("CKEditor HTML Editor Provider for DNN")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: ComVisible(false)]
+
