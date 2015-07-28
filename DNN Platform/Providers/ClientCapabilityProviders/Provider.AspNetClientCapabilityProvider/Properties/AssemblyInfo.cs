aae8088b94f0ccca875952dcf8ca5d03e33c3dd5
George Alatrash <george.alatrash@dnnsoftware.com>
2015-07-28 12:52:09 -0700

    DNN-7144: removing 51Degrees dependency.
    I created a new "ClientDependencyProvider" that replaced 51D's which uses the ASP.NET "HttpBrowserCapabilities" class.
    To use it, there is a need to replace the 51D references in the distribution web.config file (which I didn't do as this should go into EVOQ codebase only and not Platform).
    
    Note: some features that EVOQ Analytic is depending on from 51D (e.g., operating system) are not available through this provider and there is a need for a decision about them.

---
+++ DNN Platform/Providers/ClientCapabilityProviders/Provider.AspNetClientCapabilityProvider/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+#region Usings
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("DotNetNuke.Providers.AspNetClientCapabilityProvider")]
+[assembly: AssemblyDescription("Open Source Web Application Framework")]
+[assembly: Guid("E4C6B3F6-406A-4FD5-996A-C0C2E89A2D84")]
