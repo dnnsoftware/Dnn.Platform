60cb253f8e18f595dd8af27f25a2993101b536e9
Charles NUrse <charlesnurse@gmail.com>
2015-08-19 12:28:18 -0700

    DNN-7428 Added sortable helper to fix the cell widths.

--- DNN Platform/Modules/Dnn.Modules.DynamicContentManager/ClientScripts/contentTypes.js
+++ DNN Platform/Modules/Dnn.Modules.DynamicContentManager/ClientScripts/contentTypes.js
@@ -318,6 +318,16 @@ dcc.contentFieldsViewModel = function(parentViewModel, config) {
 
     };
 
+    self.fixHelper = function (e, tr) {
+        var $originals = tr.children();
+        var $helper = tr.clone();
+        $helper.children().each(function (index) {
+            // Set helper cell sizes to match the original sizes
+            $(this).width($originals.eq(index).width());
+        });
+        return $helper;
+    },
+
     self.init = function() {
         dcc.pager().init(self);
     };
