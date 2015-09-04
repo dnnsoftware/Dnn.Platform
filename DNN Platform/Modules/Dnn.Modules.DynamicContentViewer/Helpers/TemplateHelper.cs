7d45cb9adf6a7ceba8b77732c6729d9e4d71f38e
Charles NUrse <charlesnurse@gmail.com>
2015-09-04 15:40:35 -0700

    DNN-7230 Create DCC Editor Framework - added support for caching and for default rednering when no template found

--- DNN Platform/Modules/Dnn.Modules.DynamicContentViewer/Helpers/TemplateHelper.cs
+++ DNN Platform/Modules/Dnn.Modules.DynamicContentViewer/Helpers/TemplateHelper.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq.Expressions;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.UI.WebControls;
 using Dnn.DynamicContent;
@@ -18,13 +19,54 @@ namespace Dnn.Modules.DynamicContentViewer.Helpers
 {
     internal class TemplateHelper
     {
-        private static string ExecuteTemplate(DnnHelper dnnHelper, string template, ViewDataDictionary viewData)
+        private static readonly Dictionary<string, Func<HtmlHelper, string>> DefaultDisplayActions =
+            new Dictionary<string, Func<HtmlHelper, string>>()
+            {
+                {"Boolean", DefaultDisplayTemplates.BooleanTemplate},
+                {"Bytes", DefaultDisplayTemplates.BytesTemplate},
+                {"DateTime", DefaultDisplayTemplates.StringTemplate},
+                {"Integer", DefaultDisplayTemplates.StringTemplate},
+                {"Float", DefaultDisplayTemplates.StringTemplate},
+                {"Guid", DefaultDisplayTemplates.StringTemplate},
+                {"String", DefaultDisplayTemplates.StringTemplate},
+                {"TimeSpan", DefaultDisplayTemplates.StringTemplate},
+                {"Uri", DefaultDisplayTemplates.UrlTemplate}
+            };
+
+        private static readonly Dictionary<string, Func<HtmlHelper, string>> DefaultEditorActions =
+            new Dictionary<string, Func<HtmlHelper, string>>()
+            {
+                {"String", DefaultEditorTemplates.StringInputTemplate},
+                {"Uri", DefaultEditorTemplates.UrlInputTemplate}
+            };
+
+        private static readonly string CacheItemId = Guid.NewGuid().ToString();
+
+        private static string ExecuteDefaultAction(DnnHelper dnnHelper, DataType dataType, ViewDataDictionary viewData, DataBoundControlMode mode)
         {
+            Dictionary<string, Func<HtmlHelper, string>> defaultActions = GetDefaultActions(mode);
+            var actionCache = GetActionCache(dnnHelper);
+
+            Func<HtmlHelper, string> defaultAction;
+            if (defaultActions.TryGetValue(dataType.UnderlyingDataType.ToString(), out defaultAction))
+            {
+                actionCache[dataType.Name] = new ActionCacheCodeItem { Action = defaultAction };
+                return defaultAction(MakeHtmlHelper(dnnHelper, viewData));
+            }
+
+            return String.Empty;
+        }
+
+        private static string ExecuteTemplate(DnnHelper dnnHelper, string dataType, string template, ViewDataDictionary viewData)
+        {
+            var actionCache = GetActionCache(dnnHelper);
             var viewContext = dnnHelper.ViewContext;
 
             ViewEngineResult viewEngineResult = ViewEngines.Engines.FindPartialView(dnnHelper.ViewContext, template);
             if (viewEngineResult.View != null)
             {
+                actionCache[dataType] = new ActionCacheViewItem { View = viewEngineResult.View };
+
                 using (StringWriter writer = new StringWriter(CultureInfo.InvariantCulture))
                 {
                     viewEngineResult.View.Render(new ViewContext(viewContext, viewEngineResult.View, viewData, viewContext.TempData, writer), writer);
@@ -33,6 +75,29 @@ namespace Dnn.Modules.DynamicContentViewer.Helpers
             }
 
             return String.Empty;
+        }
+
+        internal static Dictionary<string, ActionCacheItem> GetActionCache(DnnHelper dnnHelper)
+        {
+            HttpContextBase context = dnnHelper.ViewContext.HttpContext;
+            Dictionary<string, ActionCacheItem> result;
+
+            if (!context.Items.Contains(CacheItemId))
+            {
+                result = new Dictionary<string, ActionCacheItem>();
+                context.Items[CacheItemId] = result;
+            }
+            else
+            {
+                result = (Dictionary<string, ActionCacheItem>)context.Items[CacheItemId];
+            }
+
+            return result;
+        }
+
+        internal static Dictionary<string, Func<HtmlHelper, string>> GetDefaultActions(DataBoundControlMode mode)
+        {
+            return mode == DataBoundControlMode.ReadOnly ? DefaultDisplayActions : DefaultEditorActions;
         }
 
         private static string GetTemplate(string templateName, string rootPath, string rootMapPath, DataBoundControlMode mode)
@@ -94,6 +159,15 @@ namespace Dnn.Modules.DynamicContentViewer.Helpers
             return path;
         }
 
+        private static HtmlHelper MakeHtmlHelper(DnnHelper dnnHelper, ViewDataDictionary viewData)
+        {
+            var viewContext = dnnHelper.ViewContext;
+            var newHelper = new HtmlHelper(
+                new ViewContext(viewContext, viewContext.View, viewData, viewContext.TempData, viewContext.Writer),
+                new ViewDataContainer(viewData));
+            return newHelper;
+        }
+
         internal static MvcHtmlString TemplateFor<TModel>(DnnHelper<TModel> dnnHelper, string fieldName, string templateName, string htmlFieldName, DataBoundControlMode mode, object additionalViewData)
         {
             var contentItem = dnnHelper.ViewData.Model as DynamicContentItem;
@@ -112,39 +186,89 @@ namespace Dnn.Modules.DynamicContentViewer.Helpers
 
             var dataType = contentField.Definition.DataType;
 
-            var template = GetTemplate(templateName, dataType, mode, dnnHelper.PortalSettings);
+            var viewData = new ViewDataDictionary(dnnHelper.ViewData)
+                                        {
+                                            Model = contentField.Value
+                                        };
 
-            string htmlString;
-            if (String.IsNullOrEmpty(template))
+            if (additionalViewData != null)
             {
-                htmlString = contentField.ToString();
+                foreach (KeyValuePair<string, object> kvp in TypeHelper.ObjectToDictionary(additionalViewData))
+                {
+                    viewData[kvp.Key] = kvp.Value;
+                }
+            }
+
+            Dictionary<string, ActionCacheItem> actionCache = GetActionCache(dnnHelper);
+
+            ActionCacheItem cacheItem;
+
+            string htmlString = String.Empty;
+            if (actionCache.TryGetValue(dataType.Name, out cacheItem))
+            {
+                if (cacheItem != null)
+                {
+                    htmlString = cacheItem.Execute(dnnHelper, viewData);
+                }
             }
             else
             {
-                if (contentField.Value == null)
+                var template = GetTemplate(templateName, dataType, mode, dnnHelper.PortalSettings);
+
+                if (String.IsNullOrEmpty(template))
                 {
-                    htmlString = String.Empty;
+                    htmlString = ExecuteDefaultAction(dnnHelper, dataType, viewData, mode);
                 }
                 else
                 {
-                    var viewData = new ViewDataDictionary(dnnHelper.ViewData)
-                    {
-                        Model = contentField.Value
-                    };
-
-                    if (additionalViewData != null)
-                    {
-                        foreach (KeyValuePair<string, object> kvp in TypeHelper.ObjectToDictionary(additionalViewData))
-                        {
-                            viewData[kvp.Key] = kvp.Value;
-                        }
-                    }
-
-                    htmlString = ExecuteTemplate(dnnHelper, template, viewData);
+                    htmlString = contentField.Value == null 
+                                    ? String.Empty 
+                                    : ExecuteTemplate(dnnHelper, dataType.Name, template, viewData);
                 }
             }
 
             return MvcHtmlString.Create(htmlString);
+        }
+
+        internal abstract class ActionCacheItem
+        {
+            public abstract string Execute(DnnHelper dnnHelper, ViewDataDictionary viewData);
+        }
+
+        internal class ActionCacheCodeItem : ActionCacheItem
+        {
+            public Func<HtmlHelper, string> Action { get; set; }
+
+            public override string Execute(DnnHelper dnnHelper, ViewDataDictionary viewData)
+            {
+                return Action(MakeHtmlHelper(dnnHelper, viewData));
+            }
+        }
+
+        internal class ActionCacheViewItem : ActionCacheItem
+        {
+            public IView View { get; set; }
+
+            public override string Execute(DnnHelper dnnHelper, ViewDataDictionary viewData)
+            {
+                var viewContext = dnnHelper.ViewContext;
+
+                using (StringWriter writer = new StringWriter(CultureInfo.InvariantCulture))
+                {
+                    View.Render(new ViewContext(viewContext, View, viewData, viewContext.TempData, writer), writer);
+                    return writer.ToString();
+                }
+            }
+        }
+
+        private class ViewDataContainer : IViewDataContainer
+        {
+            public ViewDataContainer(ViewDataDictionary viewData)
+            {
+                ViewData = viewData;
+            }
+
+            public ViewDataDictionary ViewData { get; set; }
         }
     }
 }
