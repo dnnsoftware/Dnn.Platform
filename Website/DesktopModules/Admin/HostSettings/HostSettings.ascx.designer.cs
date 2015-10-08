f2b8a1ab14bf64039c69e138cbea8eaab519c1c7
Cathal Connolly <cathal@dnncorp.com>
2015-10-08 16:33:54 +0100

    DNN-7637 Add warning text to host settings that flags if oauth extension is not installed

--- Website/DesktopModules/Admin/HostSettings/HostSettings.ascx.designer.cs
+++ Website/DesktopModules/Admin/HostSettings/HostSettings.ascx.designer.cs
@@ -2290,6 +2290,15 @@ namespace DotNetNuke.Modules.Admin.Host {
         protected global::System.Web.UI.WebControls.CheckBox chkEnableOAuth;
         
         /// <summary>
+        /// plOAuthWarning control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label plOAuthWarning;
+        
+        /// <summary>
         /// plLogs control.
         /// </summary>
         /// <remarks>
