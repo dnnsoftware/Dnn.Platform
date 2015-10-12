15ef01fb49ef2e0d57fde0b8d4ad3fdba6df1bf6
Oliver Hine <oliver.hine@gmail.com>
2015-10-12 11:30:31 -0400

    fix issue with page template, skin and container dropdown lists loosing their values after a partial postback

--- DNN Platform/Admin Modules/Dnn.Modules.Tabs/ManageTabs.ascx.designer.cs
+++ DNN Platform/Admin Modules/Dnn.Modules.Tabs/ManageTabs.ascx.designer.cs
@@ -550,7 +550,7 @@ namespace Dnn.Modules.Tabs {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected dynamic CLControl1;
+        protected global::System.Web.UI.UserControl CLControl1;
         
         /// <summary>
         /// cmdUpdateLocalization control.
@@ -676,7 +676,7 @@ namespace Dnn.Modules.Tabs {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-		protected global::DotNetNuke.Web.UI.WebControls.DnnUrlControl ctlIcon;
+        protected global::System.Web.UI.UserControl ctlIcon;
         
         /// <summary>
         /// plIconLarge control.
@@ -694,7 +694,7 @@ namespace Dnn.Modules.Tabs {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-		protected global::DotNetNuke.Web.UI.WebControls.DnnUrlControl ctlIconLarge;
+        protected global::System.Web.UI.UserControl ctlIconLarge;
         
         /// <summary>
         /// plSkin control.
@@ -1207,7 +1207,7 @@ namespace Dnn.Modules.Tabs {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-		protected global::DotNetNuke.Web.UI.WebControls.DnnUrlControl ctlURL;
+        protected global::System.Web.UI.UserControl ctlURL;
         
         /// <summary>
         /// redirectRow control.
@@ -1270,6 +1270,6 @@ namespace Dnn.Modules.Tabs {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-		protected global::DotNetNuke.UI.UserControls.ModuleAuditControl ctlAudit;
+        protected global::System.Web.UI.UserControl ctlAudit;
     }
 }
