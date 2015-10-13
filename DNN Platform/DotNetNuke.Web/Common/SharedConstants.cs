e1c3032679f2d4d04c53a092fbd6e7bd53f754ed
kanm2015 <kan.ma@dnnsoftware.com>
2015-10-13 13:45:04 -0700

    DNN-7614:  Added new non-static class DynamicSharedConstants for dynamic language selection

--- DNN Platform/DotNetNuke.Web/Common/SharedConstants.cs
+++ DNN Platform/DotNetNuke.Web/Common/SharedConstants.cs
@@ -35,4 +35,31 @@ namespace DotNetNuke.Web.Common
             Unspecified = "<" + Localization.GetString("None_Specified", Localization.SharedResourceFile) + ">";
         }
     }
+
+    public class DynamicSharedConstants
+    {
+        public static string RootFolder
+        {
+            get
+            {
+                return Localization.GetString("RootFolder.Name", Localization.SharedResourceFile);
+            }
+        }
+
+        public static string HostRootFolder
+        {
+            get
+            {
+                return Localization.GetString("HostRootFolder.Name", Localization.SharedResourceFile);
+            }
+        }
+
+        public static string Unspecified
+        {
+            get
+            {
+                return "<" + Localization.GetString("None_Specified", Localization.SharedResourceFile) + ">";
+            }
+        }
+    }
 }
