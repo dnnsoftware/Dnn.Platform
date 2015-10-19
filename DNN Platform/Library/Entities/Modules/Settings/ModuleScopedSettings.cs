8db788fbeef857806cebe6d692fa0b3b75a9f02a
Peter Donker <peter@bring2mind.net>
2015-10-19 11:46:43 +0200

    Support for better settings management in modules

---
+++ DNN Platform/Library/Entities/Modules/Settings/ModuleScopedSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace DotNetNuke.Entities.Modules.Settings
+{
+    public class ModuleScopedSettings : StringBasedSettings
+    {
+        public ModuleScopedSettings(int moduleId, Hashtable moduleSettings)
+            : base(
+                name => moduleSettings[name] as string,
+                (name, value) => new ModuleController().UpdateModuleSetting(moduleId, name, value)
+                ) { }
+    }
+}
---
+++ DNN Platform/Library/Entities/Modules/Settings/PortalScopedSettings.cs
@@ -0,0 +1,14 @@
+﻿using DotNetNuke.Entities.Portals;
+
+namespace DotNetNuke.Entities.Modules.Settings
+{
+    public class PortalScopedSettings : StringBasedSettings
+    {
+        public PortalScopedSettings(int portalId)
+            : base(
+                name => PortalController.GetPortalSetting(name, portalId, ""),
+                (name, value) => PortalController.UpdatePortalSetting(portalId, name, value, true)
+                )
+        { }
+    }
+}
