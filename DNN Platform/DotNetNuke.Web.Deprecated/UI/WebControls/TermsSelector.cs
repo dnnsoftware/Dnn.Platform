6724dabea6411ab36bb8d7fec608883a61cfa527
Oliver Hine <oliver.hine@gmail.com>
2015-10-19 10:50:03 -0400

    restore TermsSelector code

--- DNN Platform/DotNetNuke.Web.Deprecated/UI/WebControls/TermsSelector.cs
+++ DNN Platform/DotNetNuke.Web.Deprecated/UI/WebControls/TermsSelector.cs
@@ -26,12 +26,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Script.Serialization;
 using System.Web.UI;
+using System.Web.UI.WebControls;
 using DotNetNuke.Common.Utilities;
 using DotNetNuke.Entities.Content.Common;
 using DotNetNuke.Entities.Content.Taxonomy;
 using DotNetNuke.Framework;
 using DotNetNuke.Framework.JavaScriptLibraries;
 using DotNetNuke.UI.Utilities;
+using DotNetNuke.Web.Client.ClientResourceManagement;
 using Telerik.Web.UI;
 
 #endregion
@@ -49,13 +51,13 @@ namespace DotNetNuke.Web.UI.WebControls
 
         #region Public Properties
 
-		public int PortalId { get; set; }
+        public int PortalId { get; set; }
 
-		public bool IncludeSystemVocabularies { get; set; }
+        public bool IncludeSystemVocabularies { get; set; }
 
-		public bool IncludeTags { get; set; }
+        public bool IncludeTags { get; set; }
 
-		public List<Term> Terms { get; set; }
+        public List<Term> Terms { get; set; }
 
         #endregion
 
@@ -69,57 +71,65 @@ namespace DotNetNuke.Web.UI.WebControls
 
             JavaScript.RequestRegistration(CommonJs.jQueryMigrate);
 
-			OnClientDropDownOpened = "webcontrols.termsSelector.OnClientDropDownOpened";
-			if (!string.IsNullOrEmpty(CssClass))
-			{
-				CssClass = string.Format("{0} TermsSelector", CssClass);
-			}
-			else
-			{
-				CssClass = "TermsSelector";
-			}
+            OnClientDropDownOpened = "webcontrols.termsSelector.OnClientDropDownOpened";
+            if (!string.IsNullOrEmpty(CssClass))
+            {
+                CssClass = string.Format("{0} TermsSelector", CssClass);
+            }
+            else
+            {
+                CssClass = "TermsSelector";
+            }
         }
 
-		protected override void OnLoad(EventArgs e)
-		{
-			base.OnLoad(e);
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
 
-			if(Page.IsPostBack)
-			{
-				if(Terms == null)
-				{
-					Terms = new List<Term>();
-				}
-				else
-				{
-					Terms.Clear();
-				}
-				
-				if (!string.IsNullOrEmpty(SelectedValue))
-				{
-					foreach (var id in SelectedValue.Split(','))
-					{
-						var termId = Convert.ToInt32(id.Trim());
-						var term = Util.GetTermController().GetTerm(termId);
-						if (term != null)
-						{
-							Terms.Add(term);
-						}
-					}
+            if (Page.IsPostBack)
+            {
+                if (Terms == null)
+                {
+                    Terms = new List<Term>();
+                }
+                else
+                {
+                    Terms.Clear();
+                }
 
-					//clear the append item by client side
-					if(Items.Count > 1)
-					{
-						Items.Remove(1);
-					}
-				}
-			}
+                if (!string.IsNullOrEmpty(SelectedValue))
+                {
+                    foreach (var id in SelectedValue.Split(','))
+                    {
+                        var termId = Convert.ToInt32(id.Trim());
+                        var term = Util.GetTermController().GetTerm(termId);
+                        if (term != null)
+                        {
+                            Terms.Add(term);
+                        }
+                    }
 
-			Page.ClientScript.RegisterClientScriptResource(GetType(), "DotNetNuke.Web.UI.WebControls.Resources.TermsSelector.js");
+                    //clear the append item by client side
+                    if (Items.Count > 1)
+                    {
+                        Items.Remove(1);
+                    }
+                }
+            }
 
-			ClientAPI.RegisterClientVariable(Page, "TermsSelectorCallback",
-				ClientAPI.GetCallbackEventReference(this, "'[PARAMS]'", "webcontrols.termsSelector.itemDataLoaded", "this", "webcontrols.termsSelector.itemDataLoadError"), true);
-		}
+
+            if (!Page.IsPostBack)
+            {
+                Page.ClientScript.RegisterClientScriptResource(GetType(), "DotNetNuke.Web.UI.WebControls.Resources.TermsSelector.js");
+
+                ClientResourceManager.RegisterStyleSheet(Page,
+                    Page.ClientScript.GetWebResourceUrl(GetType(), "DotNetNuke.Web.UI.WebControls.Resources.TermsSelector.css"));
+
+                ClientAPI.RegisterClientVariable(Page, "TermsSelectorCallback",
+                    ClientAPI.GetCallbackEventReference(this, "'[PARAMS]'", "webcontrols.termsSelector.itemDataLoaded", "this",
+                        "webcontrols.termsSelector.itemDataLoadError"), true);
+            }
+        }
 
         protected override void OnPreRender(EventArgs e)
         {
@@ -128,9 +138,9 @@ namespace DotNetNuke.Web.UI.WebControls
             {
                 Attributes.Add("SelectedTerms", String.Join(",", Terms.Select(t => t.TermId.ToString()).ToArray()));
             }
-			Attributes.Add("IncludeSystemVocabularies", IncludeSystemVocabularies.ToString().ToLowerInvariant());
-			Attributes.Add("IncludeTags", IncludeTags.ToString().ToLowerInvariant());
-			Attributes.Add("PortalId", PortalId.ToString());
+            Attributes.Add("IncludeSystemVocabularies", IncludeSystemVocabularies.ToString().ToLowerInvariant());
+            Attributes.Add("IncludeTags", IncludeTags.ToString().ToLowerInvariant());
+            Attributes.Add("PortalId", PortalId.ToString());
         }
 
         #endregion
@@ -138,30 +148,30 @@ namespace DotNetNuke.Web.UI.WebControls
         #region Private Template Class
 
         public class TreeViewTemplate : ITemplate
-		{
-			#region Private Fields
+        {
+            #region Private Fields
 
-			private RadComboBoxItem _container;
+            private RadComboBoxItem _container;
             private TermsSelector _termsSelector;
 
             private DnnTreeView _tree;
 
-			#endregion
+            #endregion
 
-			#region ITemplate Members
+            #region ITemplate Members
 
-			public void InstantiateIn(Control container)
+            public void InstantiateIn(Control container)
             {
-                _container = (RadComboBoxItem) container;
-                _termsSelector = (TermsSelector) container.Parent;
+                _container = (RadComboBoxItem)container;
+                _termsSelector = (TermsSelector)container.Parent;
 
                 _tree = new DnnTreeView();
-	            _tree.ID = string.Format("{0}_TreeView", _termsSelector.ID);
+                _tree.ID = string.Format("{0}_TreeView", _termsSelector.ID);
                 _tree.CheckBoxes = true;
-				_tree.EnableViewState = false;
+                _tree.EnableViewState = false;
 
-				//bind client-side events
-				_tree.OnClientNodeChecked = "webcontrols.termsSelector.OnClientNodeChecked";
+                //bind client-side events
+                _tree.OnClientNodeChecked = "webcontrols.termsSelector.OnClientNodeChecked";
 
                 _container.Controls.Add(_tree);
             }
@@ -172,68 +182,68 @@ namespace DotNetNuke.Web.UI.WebControls
 
         #endregion
 
-		#region IClientAPICallbackEventHandler Implementation
+        #region IClientAPICallbackEventHandler Implementation
 
-		public string RaiseClientAPICallbackEvent(string eventArgument)
-		{
-			var parameters = eventArgument.Split('-');
-			PortalId = Convert.ToInt32(parameters[1]);
-			IncludeTags = Convert.ToBoolean(parameters[2]);
-			IncludeSystemVocabularies = Convert.ToBoolean(parameters[3]);
-			var terms = GetTerms();
-			terms.Insert(0, new { clientId = parameters[0]});
-			var serializer = new JavaScriptSerializer();
-			return serializer.Serialize(terms);
-		}
+        public string RaiseClientAPICallbackEvent(string eventArgument)
+        {
+            var parameters = eventArgument.Split('-');
+            PortalId = Convert.ToInt32(parameters[1]);
+            IncludeTags = Convert.ToBoolean(parameters[2]);
+            IncludeSystemVocabularies = Convert.ToBoolean(parameters[3]);
+            var terms = GetTerms();
+            terms.Insert(0, new { clientId = parameters[0] });
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(terms);
+        }
 
-		#endregion
+        #endregion
 
-		#region Private Methods
+        #region Private Methods
 
-		private ArrayList GetTerms()
-		{
-			var vocabRep = Util.GetVocabularyController();
-			var terms = new ArrayList();
-			var vocabularies = from v in vocabRep.GetVocabularies() where v.ScopeType.ScopeType == "Application" || (v.ScopeType.ScopeType == "Portal" && v.ScopeId == PortalId) select v;
+        private ArrayList GetTerms()
+        {
+            var vocabRep = Util.GetVocabularyController();
+            var terms = new ArrayList();
+            var vocabularies = from v in vocabRep.GetVocabularies() where v.ScopeType.ScopeType == "Application" || (v.ScopeType.ScopeType == "Portal" && v.ScopeId == PortalId) select v;
 
-			foreach (Vocabulary v in vocabularies)
-			{
-				if (v.IsSystem)
-				{
-					if (IncludeSystemVocabularies || (IncludeTags && v.Name == "Tags"))
-					{
-						AddTerms(v, terms);
-					}
-				}
-				else
-				{
-					AddTerms(v, terms);
-				}
-			}
+            foreach (Vocabulary v in vocabularies)
+            {
+                if (v.IsSystem)
+                {
+                    if (IncludeSystemVocabularies || (IncludeTags && v.Name == "Tags"))
+                    {
+                        AddTerms(v, terms);
+                    }
+                }
+                else
+                {
+                    AddTerms(v, terms);
+                }
+            }
 
-			return terms;
-		}
+            return terms;
+        }
 
-		private void AddTerms(Vocabulary v, ArrayList terms)
-		{
-			ITermController termRep = Util.GetTermController();
+        private void AddTerms(Vocabulary v, ArrayList terms)
+        {
+            ITermController termRep = Util.GetTermController();
 
-			//Add a dummy parent term if simple vocabulary
-			if (v.Type == VocabularyType.Simple)
-			{
-				terms.Add(new { termId = -v.VocabularyId, name = v.Name, parentTermId = Null.NullInteger});
-			}
-			foreach (Term t in termRep.GetTermsByVocabulary(v.VocabularyId))
-			{
-				if (v.Type == VocabularyType.Simple)
-				{
-					t.ParentTermId = -v.VocabularyId;
-				}
-				terms.Add(new { termId = t.TermId, name = t.Name, parentTermId = t.ParentTermId });
-			}
+            //Add a dummy parent term if simple vocabulary
+            if (v.Type == VocabularyType.Simple)
+            {
+                terms.Add(new { termId = -v.VocabularyId, name = v.Name, parentTermId = Null.NullInteger });
+            }
+            foreach (Term t in termRep.GetTermsByVocabulary(v.VocabularyId))
+            {
+                if (v.Type == VocabularyType.Simple)
+                {
+                    t.ParentTermId = -v.VocabularyId;
+                }
+                terms.Add(new { termId = t.TermId, name = t.Name, parentTermId = t.ParentTermId });
+            }
 
-		}
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
