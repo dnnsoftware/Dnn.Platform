b803f8ccdd2ec8364ad6d6eb79a3ead27d4eea10
Miguel Gatmaytan <miguel.gatmaytan@dnnsoftware.com>
2015-10-26 14:18:58 -0700

    bug/DNN-7738 - Reverted CSS changes, added custom knockout binding handler for truncated text and added the tooltip functionality.

--- DNN Platform/Modules/Dnn.Modules.DynamicContentManager/ClientScripts/Manager.js
+++ DNN Platform/Modules/Dnn.Modules.DynamicContentManager/ClientScripts/Manager.js
@@ -1,4 +1,18 @@
-﻿function Manager($, ko, codeEditor, settings, resx){
+﻿ko.bindingHandlers.truncatedText = {
+    update: function (element, valueAccessor, allBindingsAccessor) {
+        var originalText = ko.utils.unwrapObservable(valueAccessor()),
+            // 10 is a default maximum length
+            length = ko.utils.unwrapObservable(allBindingsAccessor().maxTextLength) || 30,
+            truncatedText = originalText.length ? originalText.substring(0, length) + "..." : originalText;
+        // updating text binding handler to show truncatedText
+        ko.bindingHandlers.text.update(element, function () {
+            return truncatedText;
+        });
+    }
+};
+
+
+function Manager($, ko, codeEditor, settings, resx){
     var $rootElement;
     var activePanel;
 
