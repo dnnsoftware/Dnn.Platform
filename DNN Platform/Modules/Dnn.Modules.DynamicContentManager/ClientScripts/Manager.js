5b4a1cb72a1061027ef4105bc31d2e93e3465892
Miguel Gatmaytan <miguel.gatmaytan@dnnsoftware.com>
2015-10-28 07:58:21 -0700

    bug/DNN-7738 - Reverted truncated text on the javascript and added CSS logic to add the same functionality.

--- DNN Platform/Modules/Dnn.Modules.DynamicContentManager/ClientScripts/Manager.js
+++ DNN Platform/Modules/Dnn.Modules.DynamicContentManager/ClientScripts/Manager.js
@@ -1,18 +1,4 @@
-﻿ko.bindingHandlers.truncatedText = {
-    update: function (element, valueAccessor, allBindingsAccessor) {
-        var originalText = ko.utils.unwrapObservable(valueAccessor()),
-            // 10 is a default maximum length
-            length = ko.utils.unwrapObservable(allBindingsAccessor().maxTextLength) || 30,
-            truncatedText = originalText.length ? originalText.substring(0, length) + "..." : originalText;
-        // updating text binding handler to show truncatedText
-        ko.bindingHandlers.text.update(element, function () {
-            return truncatedText;
-        });
-    }
-};
-
-
-function Manager($, ko, codeEditor, settings, resx){
+﻿function Manager($, ko, codeEditor, settings, resx){
     var $rootElement;
     var activePanel;
 
