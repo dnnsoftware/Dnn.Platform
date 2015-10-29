eb594460c792c6570af2bbe0dec00ffa8e11e804
Peter Donker <peter@bring2mind.net>
2015-10-29 12:15:51 +0100

    Added HTML module settings class

---
+++ DNN Platform/Modules/HTML/Components/HtmlModuleSettings.cs
@@ -0,0 +1,18 @@
+﻿using DotNetNuke.Common.Utilities;
+using DotNetNuke.Entities.Modules.Settings;
+using System.Web.Caching;
+
+namespace DotNetNuke.Modules.Html.Components
+{
+    public class HtmlModuleSettings
+    {
+        [ModuleSetting(Prefix = "HtmlText_", Default = false)]
+        public bool ReplaceTokens { get; set; }
+
+        [ModuleSetting(Prefix = "HtmlText_", Default = true)]
+        public bool UseDecorate { get; set; }
+
+        [ModuleSetting(Prefix = "HtmlText_", Default = 100)]
+        public int SearchDescLength { get; set; }
+    }
+}
