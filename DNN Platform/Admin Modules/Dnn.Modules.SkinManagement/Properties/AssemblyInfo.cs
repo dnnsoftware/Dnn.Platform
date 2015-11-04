47bf90f125cf7aa7a72ff0142bcc26fc8251954d
Francesco Rivola <francesco.rivola@gmail.com>
2015-11-04 16:10:56 +0100

    DNN-6637 - fixed AssemblyInfo version to 8.0.0

--- DNN Platform/Admin Modules/Dnn.Modules.SkinManagement/Properties/AssemblyInfo.cs
+++ DNN Platform/Admin Modules/Dnn.Modules.SkinManagement/Properties/AssemblyInfo.cs
@@ -1,35 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dnn.Modules.SkinManagement")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyDescription("Skin Management Module for Open Source Web Application Framework")]
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+//The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("aa459fc0-371b-4e83-91d4-120fa3a6d4f5")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("01.01.00")]
-[assembly: AssemblyFileVersion("01.01.00")]
