48e94d6414e3aed6a81d874ca9e7442c962169ec
Angel Grueso <angel.grueso@xepient.com>
2015-11-13 17:21:12 +0100

    DNN-7449 - Modified the setting scope in HTML Token Replace manager. It needed a upper level

--- DNN Platform/Library/Services/Tokens/HtmlTokenReplace.cs
+++ DNN Platform/Library/Services/Tokens/HtmlTokenReplace.cs
@@ -26,7 +26,7 @@ namespace DotNetNuke.Services.Tokens
     public class HtmlTokenReplace : TokenReplace
     {
         public HtmlTokenReplace(Page page)
-            : base(Scope.NoSettings)
+            : base(Scope.DefaultSettings)
         {
             PropertySource["css"] = new CssPropertyAccess(page);
             PropertySource["js"] = new JavaScriptPropertyAccess(page);
