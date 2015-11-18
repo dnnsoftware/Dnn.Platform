2da15d51c87b6ee1a04d111655c0cefbb499b0dc
Peter Donker <peter@bring2mind.net>
2015-11-18 23:22:53 +0100

    Added Client Dependency Remove controls that can be used in a skin to remove specific dependencies

---
+++ DNN Platform/DotNetNuke.Web.Client/Controls/DnnCssRemove.cs
@@ -0,0 +1,12 @@
+﻿using DotNetNuke.Web.Client.Controls;
+
+namespace DotNetNuke.Web.Client.ClientResourceManagement
+{
+    public class DnnCssRemove : ClientResourceRemove
+    {
+        public DnnCssRemove()
+        {
+            DependencyType = ClientDependency.Core.ClientDependencyType.Css;
+        }
+    }
+}
---
+++ DNN Platform/DotNetNuke.Web.Client/Controls/DnnJsRemove.cs
@@ -0,0 +1,12 @@
+﻿using DotNetNuke.Web.Client.Controls;
+
+namespace DotNetNuke.Web.Client.ClientResourceManagement
+{
+    public class DnnJsRemove : ClientResourceRemove
+    {
+        public DnnJsRemove()
+        {
+            DependencyType = ClientDependency.Core.ClientDependencyType.Javascript;
+        }
+    }
+}
