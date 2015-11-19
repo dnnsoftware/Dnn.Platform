4a28f9cafb9434ae2431e3f96811bffaba2d9246
Francisco Lucas <francisco.lucas@xepient.com>
2015-11-19 11:40:06 +0100

    DNN-7141 - replaced copyrights and deleted some comments

--- DNN Platform/HttpModules/OutputCaching/OutputCacheModule.cs
+++ DNN Platform/HttpModules/OutputCaching/OutputCacheModule.cs
@@ -1,8 +1,22 @@
 #region Copyright
+// 
 // DotNetNuke® - http://www.dotnetnuke.com
 // Copyright (c) 2002-2015
 // by DotNetNuke Corporation
-// All Rights Reserved
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and 
+// to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+// of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+// CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+// DEALINGS IN THE SOFTWARE.
 #endregion
 
 using System;
@@ -18,6 +32,9 @@ using DotNetNuke.Services.OutputCache;
 
 namespace DotNetNuke.HttpModules.OutputCaching
 {
+    /// <summary>
+    /// Manages the output cache for a request
+    /// </summary>
     public class OutputCacheModule : IHttpModule
     {
         private const string ContextKeyResponseFilter = "OutputCache:ResponseFilter";
@@ -196,9 +213,6 @@ namespace DotNetNuke.HttpModules.OutputCaching
                 cached = true;
             }
 
-/*
-                IsHandledPageRequest = true;
-*/
             _app.Context.Items[ContextKeyTabId] = tabId;
 
             if (cached != true)
