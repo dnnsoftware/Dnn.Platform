99412434897b57937067ff1c4c0ca5476779774b
Brian Dukes <bdukes@engagesoftware.com>
2015-11-25 13:06:30 -0600

    Update whitespace and XML comments for ClientDependencyInclude

--- DNN Platform/Components/ClientDependency/Source/Controls/ClientDependencyInclude.cs
+++ DNN Platform/Components/ClientDependency/Source/Controls/ClientDependencyInclude.cs
@@ -6,24 +6,24 @@ using System.Web.UI;
 namespace ClientDependency.Core.Controls
 {
     public abstract class ClientDependencyInclude : Control, IClientDependencyFile, IRequiresHtmlAttributesParsing
-	{
+    {
         protected ClientDependencyInclude()
-		{
+        {
             Priority = Constants.DefaultPriority;
             Group = Constants.DefaultGroup;
-			PathNameAlias = "";
+            PathNameAlias = "";
             HtmlAttributes = new Dictionary<string, string>();
             AddTag = true;
             Name = "";
             Version = "";
-		}
+        }
 
         protected ClientDependencyInclude(IClientDependencyFile file)
-		{
-			Priority = file.Priority;
-			PathNameAlias = file.PathNameAlias;
-			FilePath = file.FilePath;
-			DependencyType = file.DependencyType;
+        {
+            Priority = file.Priority;
+            PathNameAlias = file.PathNameAlias;
+            FilePath = file.FilePath;
+            DependencyType = file.DependencyType;
             Group = file.Group;
             HtmlAttributes = new Dictionary<string, string>();
             AddTag = true;
@@ -34,62 +34,46 @@ namespace ClientDependency.Core.Controls
 
         public ClientDependencyType DependencyType { get; internal set; }
 
-		public string FilePath { get; set; }
+        public string FilePath { get; set; }
         public string PathNameAlias { get; set; }
         public int Priority { get; set; }
         public int Group { get; set; }
         public bool AddTag { get; set; }
 
-        /// <summary>
-        /// Name of a framework such as jQuery, Bootstrap, Angular, etc.
-        /// </summary>
+        /// <summary>Name of the script (e.g. <c>jQuery</c>, <c>Bootstrap</c>, <c>Angular</c>, etc.</summary>
         public string Name { get; set; }
 
-        /// <summary>
-        /// Version of this resource if it is a framework
-        /// Note this field is only used when Framework is specified
-        /// </summary>
+        /// <summary>Version of this resource if it is a named resources. Note this field is only used when <see cref="Name" /> is specified</summary>
         public string Version { get; set; }
 
-        /// <summary>
-        /// Force this version to be used. Meant for skin designers that wish to override
-        /// choices made by module developers or the framework.
-        /// </summary>
+        /// <summary>Force this version to be used. Meant for skin designers that wish to override choices made by module developers or the framework.</summary>
         public bool ForceVersion { get; set; }
 
-		/// <summary>
-		/// This can be empty and will use default provider
-		/// </summary>
-		public string ForceProvider { get; set; }
+        /// <summary>This can be empty and will use default provider</summary>
+        public string ForceProvider { get; set; }
 
         /// <summary>
         /// If the resources is an external resource then normally it will be rendered as it's own download unless
         /// this is set to true. In that case the system will download the external resource and include it in the local bundle.
         /// </summary>
-		public bool ForceBundle { get; set; }
+        public bool ForceBundle { get; set; }
 
-        /// <summary>
-        /// Used to store additional attributes in the HTML markup for the item
-        /// </summary>
-        /// <remarks>
-        /// Mostly used for CSS Media, but could be for anything
-        /// </remarks>
+        /// <summary>Used to store additional attributes in the HTML markup for the item</summary>
+        /// <remarks>Mostly used for CSS Media, but could be for anything</remarks>
         public IDictionary<string, string> HtmlAttributes { get; private set; }
 
-        /// <summary>
-        /// Used to set the HtmlAttributes on this class via a string which is parsed
-        /// </summary>
-        /// <remarks>
-        /// The syntax for the string must be: key1:value1,key2:value2   etc...
-        /// </remarks>
+        /// <summary>Used to set the HtmlAttributes on this class via a string which is parsed</summary>
+        /// <remarks>The syntax for the string must be: <c>key1:value1,key2:value2</c> etc.</remarks>
         public string HtmlAttributesAsString { get; set; }
 
-		protected override void OnPreRender(EventArgs e)
-		{
-			base.OnPreRender(e);
-			if (string.IsNullOrEmpty(FilePath))
-				throw new NullReferenceException("Both File and Type properties must be set");
-		}
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            if (string.IsNullOrEmpty(FilePath))
+            {
+                throw new NullReferenceException("Both File and Type properties must be set");
+            }
+        }
 
         protected override void Render(HtmlTextWriter writer)
         {
@@ -128,5 +112,5 @@ namespace ClientDependency.Core.Controls
                 return hashCode;
             }
         }
-	}
+    }
 }
