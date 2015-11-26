64f8658fa96e6a1e141b154d7847992f6d6a2a4a
jbrinkman <joe@dnncorp.com>
2015-11-26 09:54:59 -0500

    Cleanup broken images and fix filepath for CSS

--- DNN Platform/DotNetNuke.Web.Client/ClientResourceManager.cs
+++ DNN Platform/DotNetNuke.Web.Client/ClientResourceManager.cs
@@ -242,7 +242,7 @@ namespace DotNetNuke.Web.Client.ClientResourceManagement
 
         public static void RegisterDefaultStylesheet(Page page, string filePath)
         {
-            RegisterStyleSheet(page, filePath, (int)FileOrder.Css.DefaultCss, DefaultCssProvider, "dnndefault", "08.00.00");
+            RegisterStyleSheet(page, filePath, (int)FileOrder.Css.DefaultCss, DefaultCssProvider, "dnndefault", "8.0.0");
         }
 
         public static void RegisterFeatureStylesheet(Page page, string filePath)
