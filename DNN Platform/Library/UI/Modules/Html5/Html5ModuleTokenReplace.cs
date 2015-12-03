8c5c90ae56a72679aee1483dd61007b030d107df
Francisco Lucas <francisco.lucas@xepient.com>
2015-12-03 15:37:02 +0100

    DNN-8037 -  Added hash to create unnecessary object using reflection

--- DNN Platform/Library/UI/Modules/Html5/Html5ModuleTokenReplace.cs
+++ DNN Platform/Library/UI/Modules/Html5/Html5ModuleTokenReplace.cs
@@ -20,6 +20,7 @@
 #endregion
 
 using System;
+using System.Collections;
 using System.Web.UI;
 using DotNetNuke.Framework;
 using DotNetNuke.Services.Tokens;
@@ -32,6 +33,8 @@ namespace DotNetNuke.UI.Modules.Html5
     public class Html5ModuleTokenReplace : HtmlTokenReplace
     {
         private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(Html5ModuleTokenReplace));
+
+        private static Hashtable _businessControllers = new Hashtable();
 
         public Html5ModuleTokenReplace(Page page, string html5File, ModuleInstanceContext moduleContext, ModuleActionCollection moduleActions)
             : base(page)
@@ -48,25 +51,43 @@ namespace DotNetNuke.UI.Modules.Html5
 
             // DNN-7750
             var bizClass = moduleContext.Configuration.DesktopModule.BusinessControllerClass;
-            if (!string.IsNullOrEmpty(bizClass))
+            
+            var businessController = GetBusinessController(bizClass);
+            if (businessController != null)
             {
-                try
+                var tokens = businessController.GetTokens(page, moduleContext);
+                foreach (var token in tokens)
                 {
-                    var businessController = Reflection.CreateObject(bizClass, bizClass) as ICustomTokenProvider;
-                    if (businessController != null)
-                    {
-                        var tokens = businessController.GetTokens(page, moduleContext);
-                        foreach (var token in tokens)
-                        {
-                            PropertySource.Add(token.Key, token.Value);
-                        }
-                    }
-                }
-                catch (Exception exc)
-                {
-                    Logger.Error(exc);
+                    PropertySource.Add(token.Key, token.Value);
                 }
             }
+        }
+
+        private ICustomTokenProvider GetBusinessController(string bizClass)
+        {
+            if (string.IsNullOrEmpty(bizClass))
+            {
+                return null;
+            }
+
+            if (_businessControllers.ContainsKey(bizClass))
+            {
+                return _businessControllers[bizClass] as ICustomTokenProvider;
+            }
+
+            try
+            {
+                var controller = Reflection.CreateObject(bizClass, bizClass) as ICustomTokenProvider;
+                _businessControllers.Add(bizClass, controller);
+
+                return controller;
+            }
+            catch (Exception exc)
+            {
+                Logger.Error(exc);
+            }
+           
+            return null;
         }
     }
 }
