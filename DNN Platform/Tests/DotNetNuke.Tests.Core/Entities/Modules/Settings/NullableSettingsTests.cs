2b7ed7642a206d2c305dfbc11b42e9b183f2294c
Brian Dukes <bdukes@engagesoftware.com>
2015-12-07 10:36:01 -0600

    DNN-8091 Add string property to nullable setting tests

--- DNN Platform/Tests/DotNetNuke.Tests.Core/Entities/Modules/Settings/NullableSettingsTests.cs
+++ DNN Platform/Tests/DotNetNuke.Tests.Core/Entities/Modules/Settings/NullableSettingsTests.cs
@@ -2,25 +2,23 @@
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using DotNetNuke.Common.Utilities;
 using DotNetNuke.Entities.Modules.Settings;
-using DotNetNuke.Entities.Portals;
-using Moq;
 using NUnit.Framework;
 
 namespace DotNetNuke.Tests.Core.Entities.Modules.Settings
 {
-    using System.Collections;
-    using System.Globalization;
-
-    using DotNetNuke.Entities.Modules;
-
     [TestFixture]
     public class NullableSettingsTests : BaseSettingsTests
     {
         public class MyNullableSettings
         {
+            [ModuleSetting]
+            public string StringProperty { get; set; } = "Default Value";
+
             [PortalSetting]
             public int? IntegerProperty { get; set; }
 
@@ -36,78 +34,81 @@ namespace DotNetNuke.Tests.Core.Entities.Modules.Settings
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("ar-JO")]
-        public void SaveSettings_CallsUpdatePortalSetting_WithRightParameters_ar_JO(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void SaveSettings_CallsUpdateSetting_WithRightParameters_ar_JO(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.SaveSettings_CallsUpdateSetting_WithRightParameters(integerValue, datetimeValue, timeSpanValue);
+            SaveSettings_CallsUpdateSetting_WithRightParameters(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("ca-ES")]
-        public void SaveSettings_CallsUpdatePortalSetting_WithRightParameters_ca_ES(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void SaveSettings_CallsUpdateSetting_WithRightParameters_ca_ES(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.SaveSettings_CallsUpdateSetting_WithRightParameters(integerValue, datetimeValue, timeSpanValue);
+            SaveSettings_CallsUpdateSetting_WithRightParameters(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("zh-CN")]
-        public void SaveSettings_CallsUpdatePortalSetting_WithRightParameters_zh_CN(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void SaveSettings_CallsUpdateSetting_WithRightParameters_zh_CN(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.SaveSettings_CallsUpdateSetting_WithRightParameters(integerValue, datetimeValue, timeSpanValue);
+            SaveSettings_CallsUpdateSetting_WithRightParameters(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("en-US")]
-        public void SaveSettings_CallsUpdatePortalSetting_WithRightParameters_en_US(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void SaveSettings_CallsUpdateSetting_WithRightParameters_en_US(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.SaveSettings_CallsUpdateSetting_WithRightParameters(integerValue, datetimeValue, timeSpanValue);
+            SaveSettings_CallsUpdateSetting_WithRightParameters(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("fr-FR")]
-        public void SaveSettings_CallsUpdatePortalSetting_WithRightParameters_fr_FR(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void SaveSettings_CallsUpdateSetting_WithRightParameters_fr_FR(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.SaveSettings_CallsUpdateSetting_WithRightParameters(integerValue, datetimeValue, timeSpanValue);
+            SaveSettings_CallsUpdateSetting_WithRightParameters(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("he-IL")]
-        public void SaveSettings_CallsUpdatePortalSetting_WithRightParameters_he_IL(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void SaveSettings_CallsUpdateSetting_WithRightParameters_he_IL(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.SaveSettings_CallsUpdateSetting_WithRightParameters(integerValue, datetimeValue, timeSpanValue);
+            SaveSettings_CallsUpdateSetting_WithRightParameters(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("ru-RU")]
-        public void SaveSettings_CallsUpdatePortalSetting_WithRightParameters_ru_RU(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void SaveSettings_CallsUpdateSetting_WithRightParameters_ru_RU(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.SaveSettings_CallsUpdateSetting_WithRightParameters(integerValue, datetimeValue, timeSpanValue);
+            SaveSettings_CallsUpdateSetting_WithRightParameters(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("tr-TR")]
-        public void SaveSettings_CallsUpdatePortalSetting_WithRightParameters_tr_TR(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void SaveSettings_CallsUpdateSetting_WithRightParameters_tr_TR(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.SaveSettings_CallsUpdateSetting_WithRightParameters(integerValue, datetimeValue, timeSpanValue);
+            SaveSettings_CallsUpdateSetting_WithRightParameters(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
-        private void SaveSettings_CallsUpdateSetting_WithRightParameters(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        private void SaveSettings_CallsUpdateSetting_WithRightParameters(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
             //Arrange
             var moduleInfo = GetModuleInfo;
             var settings = new MyNullableSettings
             {
+                StringProperty = stringValue,
                 IntegerProperty = integerValue,
                 DateTimeProperty = datetimeValue,
                 TimeSpanProperty = timeSpanValue,
             };
 
+            var expectedStringValue = stringValue ?? string.Empty;
+            MockModuleController.Setup(pc => pc.UpdateModuleSetting(ModuleId, "StringProperty", expectedStringValue));
             var integerString = integerValue?.ToString() ?? string.Empty;
             MockPortalController.Setup(pc => pc.UpdatePortalSetting(PortalId, "IntegerProperty", integerString, true, Null.NullString));
             var dateTimeString = datetimeValue?.ToString("o", CultureInfo.InvariantCulture) ?? string.Empty;
@@ -160,74 +161,75 @@ namespace DotNetNuke.Tests.Core.Entities.Modules.Settings
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("ar-JO")]
-        public void GetSettings_GetsValuesFrom_PortalSettings_ar_JO(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void GetSettings_GetsValues_FromCorrectSettings_ar_JO(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.GetSettings_GetsValues_FromCorrectSettings(integerValue, datetimeValue, timeSpanValue);
+            GetSettings_GetsValues_FromCorrectSettings(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("ca-ES")]
-        public void GetSettings_GetsValuesFrom_PortalSettings_ca_ES(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void GetSettings_GetsValues_FromCorrectSettings_ca_ES(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.GetSettings_GetsValues_FromCorrectSettings(integerValue, datetimeValue, timeSpanValue);
+            GetSettings_GetsValues_FromCorrectSettings(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("zh-CN")]
-        public void GetSettings_GetsValuesFrom_PortalSettings_zh_CN(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void GetSettings_GetsValues_FromCorrectSettings_zh_CN(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.GetSettings_GetsValues_FromCorrectSettings(integerValue, datetimeValue, timeSpanValue);
+            GetSettings_GetsValues_FromCorrectSettings(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("en-US")]
-        public void GetSettings_GetsValuesFrom_PortalSettings_en_US(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void GetSettings_GetsValues_FromCorrectSettings_en_US(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.GetSettings_GetsValues_FromCorrectSettings(integerValue, datetimeValue, timeSpanValue);
+            GetSettings_GetsValues_FromCorrectSettings(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("fr-FR")]
-        public void GetSettings_GetsValuesFrom_PortalSettings_fr_FR(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void GetSettings_GetsValues_FromCorrectSettings_fr_FR(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.GetSettings_GetsValues_FromCorrectSettings(integerValue, datetimeValue, timeSpanValue);
+            GetSettings_GetsValues_FromCorrectSettings(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("he-IL")]
-        public void GetSettings_GetsValuesFrom_PortalSettings_he_IL(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void GetSettings_GetsValues_FromCorrectSettings_he_IL(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.GetSettings_GetsValues_FromCorrectSettings(integerValue, datetimeValue, timeSpanValue);
+            GetSettings_GetsValues_FromCorrectSettings(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("ru-RU")]
-        public void GetSettings_GetsValuesFrom_PortalSettings_ru_RU(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void GetSettings_GetsValues_FromCorrectSettings_ru_RU(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.GetSettings_GetsValues_FromCorrectSettings(integerValue, datetimeValue, timeSpanValue);
+            GetSettings_GetsValues_FromCorrectSettings(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
         [Test]
         [TestCaseSource(nameof(NullableCases))]
         [SetCulture("tr-TR")]
-        public void GetSettings_GetsValuesFrom_PortalSettings_tr_TR(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        public void GetSettings_GetsValues_FromCorrectSettings_tr_TR(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
-            this.GetSettings_GetsValues_FromCorrectSettings(integerValue, datetimeValue, timeSpanValue);
+            GetSettings_GetsValues_FromCorrectSettings(stringValue, integerValue, datetimeValue, timeSpanValue);
         }
 
-        private void GetSettings_GetsValues_FromCorrectSettings(int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
+        private void GetSettings_GetsValues_FromCorrectSettings(string stringValue, int? integerValue, DateTime? datetimeValue, TimeSpan? timeSpanValue)
         {
             //Arrange
+            var expectedStringValue = stringValue ?? string.Empty;
             var moduleInfo = GetModuleInfo;
-            var portalSettings = new Dictionary<string, string> { { "IntegerProperty", integerValue?.ToString() ?? string.Empty }, };
-            var moduleSettings = new Hashtable { { "DateTimeProperty", datetimeValue?.ToString("o", CultureInfo.InvariantCulture) ?? string.Empty }, };
-            var tabModuleSettings = new Hashtable { { "TimeSpanProperty", timeSpanValue?.ToString("c", CultureInfo.InvariantCulture) ?? string.Empty }, };
+            var portalSettings = new Dictionary<string, string> { ["IntegerProperty"] = integerValue?.ToString() ?? string.Empty, };
+            var moduleSettings = new Hashtable { ["DateTimeProperty"] = datetimeValue?.ToString("o", CultureInfo.InvariantCulture) ?? string.Empty, ["StringProperty"] = expectedStringValue, };
+            var tabModuleSettings = new Hashtable { ["TimeSpanProperty"] = timeSpanValue?.ToString("c", CultureInfo.InvariantCulture) ?? string.Empty, };
 
             MockPortalSettings(moduleInfo, portalSettings);
             MockModuleSettings(moduleInfo, moduleSettings);
@@ -239,6 +241,7 @@ namespace DotNetNuke.Tests.Core.Entities.Modules.Settings
             var settings = settingsRepository.GetSettings(moduleInfo);
 
             //Assert
+            Assert.AreEqual(expectedStringValue, settings.StringProperty, "The retrieved string property value is not equal to the stored one");
             Assert.AreEqual(integerValue, settings.IntegerProperty, "The retrieved integer property value is not equal to the stored one");
             Assert.AreEqual(datetimeValue, settings.DateTimeProperty, "The retrieved datetime property value is not equal to the stored one");
             Assert.AreEqual(timeSpanValue, settings.TimeSpanProperty, "The retrieved timespan property value is not equal to the stored one");
@@ -247,8 +250,9 @@ namespace DotNetNuke.Tests.Core.Entities.Modules.Settings
 
         public readonly object[] NullableCases =
         {
-            new object[] { null, null, null, },
-            new object[] { -1, DateTime.UtcNow, TimeSpan.FromMilliseconds(3215648), },
+            new object[] { null, null, null, null, },
+            new object[] { "", -1, DateTime.UtcNow, TimeSpan.FromMilliseconds(3215648), },
+            new object[] { "lorem ipsum", 456, DateTime.Now, DateTime.Today - DateTime.Now, },
         };
     }
 }
