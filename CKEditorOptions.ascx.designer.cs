e1d1fb0551751be88487a30361539a8cd1f2ee30
Pedro Berná <pedro.berna@xepient.com>
2015-12-14 10:50:27 +0100

    Revert "This is a manuel rever commit of 21ab619785189043dd0598f7c5a15335e037d816"
    This reverts commit 977027f799336df5d86b5a96fa0e72c94bf78b40.

--- CKEditorOptions.ascx.designer.cs
+++ CKEditorOptions.ascx.designer.cs
@@ -823,15 +823,6 @@ namespace DNNConnect.CKEditorProvider {
         protected global::System.Web.UI.WebControls.Button btnOk;
         
         /// <summary>
-        /// btnCancel control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink btnCancel;
-        
-        /// <summary>
         /// CopyToAllChild control.
         /// </summary>
         /// <remarks>
