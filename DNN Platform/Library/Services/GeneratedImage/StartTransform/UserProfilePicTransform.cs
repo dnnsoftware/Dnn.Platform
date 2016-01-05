42cc9112702bd6c867ad700b3b84fbcdfe12e37c
Amarjit Dhunna <amarjit.dhunna@dnnsoftware.com>
2016-01-05 11:30:03 -0800

    DNN-8207 Removed unnecessary references.

--- DNN Platform/Library/Services/GeneratedImage/StartTransform/UserProfilePicTransform.cs
+++ DNN Platform/Library/Services/GeneratedImage/StartTransform/UserProfilePicTransform.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 using DotNetNuke.Common;
 using DotNetNuke.Entities.Portals;
 using DotNetNuke.Entities.Users;
