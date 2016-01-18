3f6f63907838d7957526fc5044be799c031c0936
Brian Dukes <bdukes@engagesoftware.com>
2016-01-18 11:27:10 -0600

    Localize "Shared Module" indicator in action menu
    DNN-8355

--- Website/admin/Menus/ModuleActions/ModuleActions.js
+++ Website/admin/Menus/ModuleActions/ModuleActions.js
@@ -384,7 +384,7 @@
                 }
 
                 if (isShared) {
-                    buildSharedMenu(menuRoot, "Shared Module", "dnn_shared");
+                    buildSharedMenu(menuRoot, opts.sharedText, "dnn_shared");
                 }
                 watchResize(moduleId);
             }
