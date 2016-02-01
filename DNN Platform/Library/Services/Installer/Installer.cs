3c0cd9d785944045cea12cad1870d4b388744c59
Ben <ben.zhong@dnnsoftware.com>
2016-02-01 12:22:35 +0800

    DNN-8048: check PackageId value to identity whether the package already installed.

--- DNN Platform/Library/Services/Installer/Installer.cs
+++ DNN Platform/Library/Services/Installer/Installer.cs
@@ -206,7 +206,7 @@ namespace DotNetNuke.Services.Installer
 				//Check if package is valid
                 if (installer.Package.IsValid)
                 {
-                    if (installer.Package.InstallerInfo.Installed || installer.Package.InstallerInfo.RepairInstall)
+                    if (installer.Package.InstallerInfo.PackageID > Null.NullInteger || installer.Package.InstallerInfo.RepairInstall)
                     {
                         clearClientCache = true;
                     }
