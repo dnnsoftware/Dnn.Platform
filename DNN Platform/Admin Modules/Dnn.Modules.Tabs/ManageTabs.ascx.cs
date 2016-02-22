77970a37ef8403c9704bc462edd9c75973087fb4
Ben <ben.zhong@dnnsoftware.com>
2016-02-22 10:23:00 +0800

    DNN-8306: save module permissions after module saved.

--- DNN Platform/Admin Modules/Dnn.Modules.Tabs/ManageTabs.ascx.cs
+++ DNN Platform/Admin Modules/Dnn.Modules.Tabs/ManageTabs.ascx.cs
@@ -1005,16 +1005,26 @@ namespace Dnn.Modules.Tabs
                                 {
                                     newModule.ModuleID = Null.NullInteger;
                                     ModuleController.Instance.InitialModulePermission(newModule, newModule.TabID, 0);
-                                    
-                                    //copy permissions from source module
                                     newModule.InheritViewPermissions = objModule.InheritViewPermissions;
-                                    foreach (ModulePermissionInfo permission in objModule.ModulePermissions)
-                                    {
-                                        newModule.ModulePermissions.Add(permission, true);
-                                    }
                                 }
 
                                 newModule.ModuleID = ModuleController.Instance.AddModule(newModule);
+
+                                //copy permissions from source module
+                                foreach (ModulePermissionInfo permission in objModule.ModulePermissions)
+                                {
+                                    newModule.ModulePermissions.Add(new ModulePermissionInfo
+                                    {
+                                        ModuleID = newModule.ModuleID,
+                                        PermissionID = permission.PermissionID,
+                                        RoleID = permission.RoleID,
+                                        UserID = permission.UserID,
+                                        PermissionKey = permission.PermissionKey,
+                                        AllowAccess = permission.AllowAccess
+                                    }, true);
+                                }
+
+                                ModulePermissionController.SaveModulePermissions(newModule);
 
                                 if (optCopy.Checked)
                                 {
