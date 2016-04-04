e5c3b37b59fba2837e40ec64e291b1970d21e86a
kanm2015 <kan.ma@dnnsoftware.com>
2016-04-04 14:39:09 -0700

    DNN-7554: Fixed the null appPath issue in GetRouteName methods

--- DNN Platform/DotNetNuke.Web/Api/PortalAliasRouteManager.cs
+++ DNN Platform/DotNetNuke.Web/Api/PortalAliasRouteManager.cs
@@ -46,10 +46,13 @@ namespace DotNetNuke.Web.Api
         {
             var alias = portalAlias.HTTPAlias;
             string appPath = TestableGlobals.Instance.ApplicationPath;
-            int i = alias.IndexOf(appPath, StringComparison.OrdinalIgnoreCase);
-            if (i > 0)
+            if (!string.IsNullOrEmpty(appPath))
             {
-                alias = alias.Remove(i, appPath.Length);
+                int i = alias.IndexOf(appPath, StringComparison.OrdinalIgnoreCase);
+                if (i > 0)
+                {
+                    alias = alias.Remove(i, appPath.Length);
+                }
             }
             return GetRouteName(moduleFolderName, routeName, CalcAliasPrefixCount(alias));
         }
