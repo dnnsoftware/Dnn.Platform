598c9fe40576f5611befeba0cceb2d2dcf4d1734
kanm2015 <kan.ma@dnnsoftware.com>
2016-10-21 16:25:59 -0700

    PAE-17: Fixed UI not rendering issue in IE 10

--- src/Modules/Manage/Dnn.PersonaBar.AdminLogs/AdminLogs.Web/src/globals/application.js
+++ src/Modules/Manage/Dnn.PersonaBar.AdminLogs/AdminLogs.Web/src/globals/application.js
@@ -1,11 +1,12 @@
 import utilities from "../utils";
 const adminLogs = {
-    init(initCallback) {
+    init() {
         // This setting is required and define the public path 
         // to allow the web application to download assets on demand 
         // eslint-disable-next-line no-undef
         // __webpack_public_path__ = options.publicPath;
-        let options = window.dnn[initCallback]();
+        let options = window.dnn.initAdminLogs();
+        
         utilities.init(options.utility);
         utilities.moduleName = options.moduleName;
 
