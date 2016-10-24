f89d2685a74c0296b7dd064dadf9b7c8dea3c93f
Pedro Berná <pedro.berna@xepient.com>
2016-10-24 16:33:29 +0200

    AP-202 Added UI skeleton

--- src/Modules/Settings/Dnn.PersonaBar.Servers/Servers.Web/src/components/Tabs/Web/index.jsx
+++ src/Modules/Settings/Dnn.PersonaBar.Servers/Servers.Web/src/components/Tabs/Web/index.jsx
@@ -1,9 +1,56 @@
 import React, {Component } from "react";
+import GridSystem from "dnn-grid-system";
+import InfoBlock from "../../common/InfoBlock";
+
+import "../style.less";
+
+import Localization from "../../../localization";
 
 export default class Web extends Component {
     render() {
-        return <div>
-            <h1>Web</h1>
+        return <div className="dnn-servers-info-tab">
+            <GridSystem>
+                <div>
+                    <InfoBlock label={Localization.get("ServerInfo_OSVersion")} 
+                        tooltip={Localization.get("ServerInfo_OSVersion.Help")} 
+                        text={"Place holder"} />   
+
+                    <InfoBlock label={Localization.get("ServerInfo_IISVersion")} 
+                        tooltip={Localization.get("ServerInfo_IISVersion.Help")} 
+                        text={"Place holder"} /> 
+
+                    <InfoBlock label={Localization.get("ServerInfo_Framework")} 
+                        tooltip={Localization.get("ServerInfo_Framework.Help")} 
+                        text={"Place holder"} /> 
+
+                    <div className="tooltipAdjustment">
+                        <InfoBlock label={Localization.get("ServerInfo_Identity")} 
+                            tooltip={Localization.get("ServerInfo_Identity.Help")} 
+                            text={"Place holder"} />
+                    </div> 
+
+                    <InfoBlock label={Localization.get("ServerInfo_HostName")} 
+                        tooltip={Localization.get("ServerInfo_HostName.Help")} 
+                        text={"Place holder"} /> 
+                </div>
+                <div>
+                    <InfoBlock label={Localization.get("ServerInfo_PhysicalPath")} 
+                        tooltip={Localization.get("ServerInfo_PhysicalPath.Help")} 
+                        text={"Place holder"} /> 
+
+                    <InfoBlock label={Localization.get("ServerInfo_Url")} 
+                        tooltip={Localization.get("ServerInfo_Url.Help")} 
+                        text={"Place holder"} /> 
+
+                    <InfoBlock label={Localization.get("ServerInfo_RelativePath")} 
+                        tooltip={Localization.get("ServerInfo_RelativePath.Help")} 
+                        text={"Place holder"} /> 
+
+                    <InfoBlock label={Localization.get("ServerInfo_ServerTime")} 
+                        tooltip={Localization.get("ServerInfo_ServerTime.Help")} 
+                        text={"Place holder"} />
+                </div>
+            </GridSystem>
         </div>;
     }
 }
