cb5dc8309a6a5cc22c61c2a17ed06334c73632fb
Pedro Berná <pedro.berna@xepient.com>
2016-10-25 11:39:06 +0200

    AP-202 Added Restart Application and Clear Cache buttons on top

--- src/Modules/Settings/Dnn.PersonaBar.Servers/Servers.Web/src/components/App.jsx
+++ src/Modules/Settings/Dnn.PersonaBar.Servers/Servers.Web/src/components/App.jsx
@@ -4,32 +4,23 @@ import Button from "dnn-button";
 import SocialPanelHeader from "dnn-social-panel-header";
 import Body from "./Body";
 import PersonaBarPage from "dnn-persona-bar-page";
-import {visiblePanel as VisiblePanelActions } from "../actions";
-class App extends Component {
-    constructor() {
-        super();
-    }
+import localization from "../localization";
 
-    navigateMap(page, event) {
-        event.preventDefault();
-        const {props} = this;
-        props.dispatch(VisiblePanelActions.selectPanel(page));
-    }
+const restartAppButtonStyle = {
+    "margin-right": "10px"
+};
+
+class App extends Component { 
     render() {
-        const {props} = this;
         return (
             <div className="servers-app personaBar-mainContainer">
-                <PersonaBarPage isOpen={props.selectedPage === 0}>
+                <PersonaBarPage isOpen={true}>
                     <SocialPanelHeader title="Servers">
-                        <Button type="primary" onClick={this.navigateMap.bind(this, 1) }>Primary Action</Button>
+                        <Button type="secondary" size="large">{localization.get("clearCacheButtonLabel")}</Button>
+                        <Button type="secondary" size="large" style={restartAppButtonStyle}>{localization.get("restartApplicationButtonLabel")}</Button>                        
                     </SocialPanelHeader>
                     <Body />
-                </PersonaBarPage>
-                <PersonaBarPage isOpen={props.selectedPage === 1}>
-                    <SocialPanelHeader title="Pane 2">
-                        <Button type="primary" onClick={this.navigateMap.bind(this, 0) }>Go back</Button>
-                    </SocialPanelHeader>
-                </PersonaBarPage>
+                </PersonaBarPage>                
             </div>
         );
     }
