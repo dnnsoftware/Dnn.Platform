cbf06b1ae746039f82623b65700357cfdfd9f18c
Juan Sánchez <jsmontesinos@gmail.com>
2016-10-25 14:35:15 +0100

    AP-36 - Show more fields on settings page

--- src/Modules/Content/Dnn.PersonaBar.Pages/Pages.Web/src/containers/PageSettings.js
+++ src/Modules/Content/Dnn.PersonaBar.Pages/Pages.Web/src/containers/PageSettings.js
@@ -1,12 +1,11 @@
 import React, {Component, PropTypes } from "react";
-import { bindActionCreators } from "redux";
-import { connect } from "react-redux";
 import Tabs from "dnn-tabs";
 import Localization from "../localization";
-import GridSystem from "dnn-grid-system";
-import EditableField from "dnn-editable-field";
 import SocialPanelBody from "dnn-social-panel-body";
 import SingleLineInputWithError from "dnn-single-line-input-with-error";
+import MultiLineInputWithError from "dnn-multi-line-input-with-error";
+import GridSystem from "dnn-grid-system";
+import GridCell from "dnn-grid-cell";
 
 class PageSettings extends Component {
 
@@ -17,36 +16,40 @@ class PageSettings extends Component {
             <SocialPanelBody>
                 <Tabs tabHeaders={[Localization.get("Details"), Localization.get("Permissions")]}>
                     <div>
-                        <SingleLineInputWithError
-                            label={Localization.get("Name")}
-                            tooltipMessage={Localization.get("page_name_tooltip")}
-                            value={selectedPage.name}/>
-                        <SingleLineInputWithError
-                            label={Localization.get("Title")}
-                            tooltipMessage={Localization.get("page_title_tooltip")}
-                            value={selectedPage.title}/>
+                        <GridSystem>
+                            
+                                <SingleLineInputWithError
+                                    label={Localization.get("Name")}
+                                    tooltipMessage={Localization.get("page_name_tooltip")}
+                                    value={selectedPage.name}/>
+                                <SingleLineInputWithError
+                                    label={Localization.get("Title")}
+                                    tooltipMessage={Localization.get("page_title_tooltip")}
+                                    value={selectedPage.title}/>
+                            
+                        </GridSystem>
+                        <GridCell>
+                            <MultiLineInputWithError
+                                label={Localization.get("Description")}
+                                value={selectedPage.description} />
+                        </GridCell>
+                        <GridCell>
+                            <MultiLineInputWithError
+                                label={Localization.get("Keywords")}
+                                value={selectedPage.keywords} />
+                        </GridCell>
+                        <GridCell>
+                            <SingleLineInputWithError
+                                label={Localization.get("Tags")}
+                                value={selectedPage.tags} />
+                            <SingleLineInputWithError
+                                label={Localization.get("URL")}
+                                value={selectedPage.url}
+                                enabled={false} />
+                        </GridCell>
                     </div>
                     <div>
-                        <EditableField
-                            label={Localization.get("Description")}
-                            value={selectedPage.description}
-                            editable={true} />
-                    </div>
-                    <div>
-                        <EditableField
-                            label={Localization.get("Keywords")}
-                            value={selectedPage.keywords}
-                            editable={true} />
-                    </div>
-                    <div>
-                        <EditableField
-                            label={Localization.get("Tags")}
-                            value={selectedPage.tags}
-                            editable={true} />
-                        <EditableField
-                            label={Localization.get("URL")}
-                            value={selectedPage.url}
-                            editable={false} />
+                        <p>Permission stuff</p>
                     </div>
                 </Tabs>
             </SocialPanelBody>
