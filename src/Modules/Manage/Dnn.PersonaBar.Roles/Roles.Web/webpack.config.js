b12a9069937202cf7699091dc163d8d8f8c6009f
Miguel Gatmaytan <miguel.gatmaytan@dnnsoftware.com>
2016-11-02 15:06:39 -0700

    AP-51 - Bumped package of dropdown.
    Also fixed roles webpack config, as well as fixed list view style on anchor.

--- src/Modules/Manage/Dnn.PersonaBar.Roles/Roles.Web/webpack.config.js
+++ src/Modules/Manage/Dnn.PersonaBar.Roles/Roles.Web/webpack.config.js
@@ -2,62 +2,48 @@
 const I18nPlugin = require("i18n-webpack-plugin");
 const packageJson = require("./package.json");
 const isProduction = process.env.NODE_ENV === "production";
-const languages = {
-    "en": null
-    // TODO: create locallizaton files per language 
-    // "de": require("./localizations/de.json"),
-    // "es": require("./localizations/es.json"),
-    // "fr": require("./localizations/fr.json"),
-    // "it": require("./localizations/it.json"),
-    // "nl": require("./localizations/nl.json")
-};
+module.exports = {
+    entry: "./src/main.jsx",
+    output: {
+        path: "../admin/personaBar/scripts/bundles/",
+        filename: "roles-bundle.js",
+        publicPath: isProduction ? "" : "http://localhost:8080/dist/"
+    },
 
-module.exports = Object.keys(languages).map(function (language) {
-    return {
-        entry: "./src/main.jsx",
-        output: {
-            path: "./dist/",
-            filename: "bundle-" + language + ".js",
-            publicPath: isProduction ? "" : "http://localhost:8080/dist/"
-        },
+    module: {
+        loaders: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
+            { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+            { test: /\.(ttf|woff)$/, loader: "url-loader?limit=8192" },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            { test: /\.(gif|png)$/, loader: "url-loader?mimetype=image/png" },
+            { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
+        ],
 
-        module: {
-            loaders: [
-                { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
-                { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-                { test: /\.(ttf|woff)$/, loader: "url-loader?limit=8192" },
-                { test: /\.css$/, loader: "style-loader!css-loader" },
-                { test: /\.(gif|png)$/, loader: "url-loader?mimetype=image/png" },
-                { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
-                { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
-            ],
+        preLoaders: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader" }
+        ]
+    },
 
-            preLoaders: [
-                { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader" }
-            ]
-        },
+    resolve: {
+        extensions: ["", ".js", ".json", ".jsx"]
+    },
 
-        resolve: {
-            extensions: ["", ".js", ".json", ".jsx"]
-        },
+    externals: require("dnn-webpack-externals"),
 
-        externals: require("dnn-webpack-externals"),
-
-        plugins: isProduction ? [
-            new webpack.optimize.UglifyJsPlugin(),
-            new webpack.optimize.DedupePlugin(),
-            new I18nPlugin(languages[language]),
+    plugins: isProduction ? [
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.DefinePlugin({
+            VERSION: JSON.stringify(packageJson.version),
+            "process.env": {
+                "NODE_ENV": JSON.stringify("production")
+            }
+        })
+    ] : [
             new webpack.DefinePlugin({
-                VERSION: JSON.stringify(packageJson.version),
-                "process.env": {
-                    "NODE_ENV": JSON.stringify("production")
-                }
+                VERSION: JSON.stringify(packageJson.version)
             })
-        ] : [
-                new I18nPlugin(languages["en"]),
-                new webpack.DefinePlugin({
-                    VERSION: JSON.stringify(packageJson.version)
-                })
-            ]
-    };
-});
+        ]
+};
