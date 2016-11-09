d73687fe98186862428df6488f88539d7aa16bfd
Ben <zyhfish@163.com>
2016-11-09 12:07:28 +0800

    AP-27: add extension.

--- src/Modules/Settings/Dnn.PersonaBar.SiteSettings/SiteSettings.Web/src/components/moreSettings/index.jsx
+++ src/Modules/Settings/Dnn.PersonaBar.SiteSettings/SiteSettings.Web/src/components/moreSettings/index.jsx
@@ -5,6 +5,7 @@ import {
     pagination as PaginationActions
 } from "../../actions";
 import Button from "dnn-button";
+import GridCell from "dnn-grid-cell";
 import "./style.less";
 import util from "../../utils";
 import resx from "../../resources";
@@ -15,11 +16,92 @@ class MoreSettingsPanelBody extends Component {
         super();
     }
 
+    getMicroservicesExtensions(){
+        let extensions = window.dnn && window.dnn.personaBarExtensions && window.dnn.personaBarExtensions.siteSettings
+                            ? window.dnn.personaBarExtensions.siteSettings : [];
+
+        return extensions.filter( e => {
+            return e.type === "microservices-settings";
+        });
+    }
+
+    onCancel(){
+        let extensions = this.getMicroservicesExtensions();
+        for(let i  = 0;i < extensions.length; i++){
+            let ext = extensions[i].name;
+            if(this.refs[ext] && typeof this.refs[ext].cancel === "function"){
+                this.refs[ext].cancel();
+            }
+        }
+    }
+
+    onSave(){
+        let extensions = this.getMicroservicesExtensions();
+        for(let i  = 0;i < extensions.length; i++){
+            let ext = extensions[i].name;
+            if(this.refs[ext] && typeof this.refs[ext].save === "function"){
+                this.refs[ext].save();
+            }
+        }
+    }
+
+    renderExtensions(){
+        const {props, state} = this;
+
+        let extensions = this.getMicroservicesExtensions();
+
+        if(!extensions.length){
+            return null;
+        }
+
+        return <GridCell>
+            <div className="sectionTitle">{resx.get("MicroServices")}</div>
+            <div className="messageBox">{resx.get("MicroServicesDescription")}</div>
+            {
+                extensions.map(function(ext){
+                    if(typeof ext.init === "function"){
+                        ext.init({utility: util.utilities});
+                    }
+
+                    let ExtensionComponent = ext.extension;
+                    return <GridCell columnSize="50">
+                        <ExtensionComponent ref={ext.name} />
+                    </GridCell>;
+                })
+            }
+        </GridCell>;
+    }
+
+    renderActions(){
+        const {props, state} = this;
+
+        let extensions = this.getMicroservicesExtensions();
+
+        if(!extensions.length){
+            return null;
+        }
+
+        return <div className="buttons-box">
+            <Button
+                type="secondary"
+                onClick={this.onCancel.bind(this)}>
+                {resx.get("Cancel")}
+            </Button>
+            <Button
+                type="primary"
+                onClick={this.onSave.bind(this)}>
+                {resx.get("Save")}
+            </Button>
+        </div>;
+    }
+
     /* eslint-disable react/no-danger */
     render() {
         const {props, state} = this;
+
         return (
             <div className={styles.moreSettings}>
+                {this.renderExtensions()}
                 <div className="sectionTitle">{resx.get("HtmlEditor")}</div>
                 <div className="htmlEditorWrapper">
                     <div className="htmlEditorWrapper-left">
@@ -33,6 +115,7 @@ class MoreSettingsPanelBody extends Component {
                         </Button>
                     </div>
                 </div>
+                {this.renderActions()}
             </div>
         );
     }
