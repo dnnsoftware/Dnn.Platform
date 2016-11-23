133318a5bf9e3554712348b00df58691b9ed4b47
Ben <zyhfish@163.com>
2016-11-24 06:20:43 +0800

    Revert "AP-179: remove taxonomy package."
    This reverts commit 0d3e11e802af28150ba724ecff25b3beda60be94.

---
+++ DNN Platform/Modules/Taxonomy/CreateVocabulary.ascx
@@ -0,0 +1,10 @@
+﻿<%@ Control Language="C#" AutoEventWireup="false" CodeBehind="CreateVocabulary.ascx.cs" Inherits="DotNetNuke.Modules.Taxonomy.Views.CreateVocabulary" %>
+<%@ Register TagPrefix="dnn" Assembly="DotNetNuke.Web" Namespace="DotNetNuke.Web.UI.WebControls" %>
+<%@ Register TagPrefix="dnn" TagName="EditVocabularyControl" Src="Controls/EditVocabularyControl.ascx" %>
+<div class="dnnForm dnnCreateVocab dnnClear">
+    <dnn:EditVocabularyControl ID="editVocabularyControl" runat="server" IsAddMode="true" />
+    <ul class="dnnActions dnnClear">
+        <li><asp:LinkButton ID="saveVocabulary" runat="server" resourceKey="saveVocabulary" CssClass="dnnPrimaryAction" /></li>
+        <li><asp:HyperLink ID="cancelCreate" runat="server" resourceKey="cancelCreate" CssClass="dnnSecondaryAction" /></li>
+    </ul>
+</div>
---
+++ DNN Platform/Modules/Taxonomy/Scripts/TaxonomyEditor.js
@@ -0,0 +1,62 @@
+﻿(function ($) {
+	$.fn.termComplete = function (options) {
+		options = $.extend({}, $.fn.termComplete.defaultOptions, options);
+		this.each(function () {
+			var instance = $(this);
+			instance.autocomplete({
+				source: function (request, response) {
+					var vocabularyId = instance.data("vocabularyid") ? instance.data("vocabularyid") : -1;
+					var termId = instance.data("termid") ? instance.data("termid") : -1;
+					var parentId = options.getParent() ? options.getParent() : -1;
+					$.ajax({
+						url: options.serviceFramework.getServiceRoot('Taxonomy') + 'Services/Search?vocabularyId=' + vocabularyId + "&termId=" + termId + "&parentId=" + parentId,
+						beforeSend: options.serviceFramework.setModuleHeaders,
+						data: {
+							termName: request.term
+						},
+						success: function (data) {
+							updateButtonStatus(instance);
+							response(data);
+						}
+					});
+				},
+				close: function() {
+					updateButtonStatus(instance);
+				}
+			});
+			
+			instance.on("termVal", function (e) {
+				updateButtonStatus(instance);
+			});
+		});
+
+		function updateButtonStatus(instance) {
+			var vocabularyId = instance.data("vocabularyid") ? instance.data("vocabularyid") : -1;
+			var termId = instance.data("termid") ? instance.data("termid") : -1;
+			var parentId = options.getParent() ? options.getParent() : -1;
+			$.ajax({
+				url: options.serviceFramework.getServiceRoot('Taxonomy') + 'Services/Exist?vocabularyId=' + vocabularyId + "&termId=" + termId + "&parentId=" + parentId + "&termName=" + instance.val(),
+				beforeSend: options.serviceFramework.setModuleHeaders,
+				success: function (data) {
+					if (data) {
+						options.saveButton.addClass("dnnActionDisabled");
+					} else {
+						options.saveButton.removeClass("dnnActionDisabled");
+					}
+				}
+			});
+		}
+
+		options.saveButton.click(function() {
+			return !$(this).hasClass("dnnActionDisabled");
+		});
+		
+		return this;
+	};
+	
+	$.fn.termComplete.defaultOptions = {
+		serviceFramework: null,
+		getParent: null,
+		saveButton: null
+	};
+})(jQuery);
