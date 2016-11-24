210f07446326f451bc9dc195427370dcd8b180bb
Pedro Berná <pedro.berna@xepient.com>
2016-11-24 13:52:18 +0100

    PAE-105 Added tooltip to social panel header

--- SocialPanelHeader/src/SocialPanelHeader.jsx
+++ SocialPanelHeader/src/SocialPanelHeader.jsx
@@ -1,15 +1,16 @@
 import React, {PropTypes} from "react";
 import "./style.less";
 
-const SocialPanelHeader = ({title, children}) => (
+const SocialPanelHeader = ({title, children, tooltip}) => (
     <div className="dnn-social-panel-header socialpanelheader">
-        <h3>{title}</h3>
+        <h3 title={tooltip}>{title}</h3>
         {children}
     </div>
 );
 
 SocialPanelHeader.propTypes = {
     title: PropTypes.string,
-    children: PropTypes.node
+    children: PropTypes.node,
+    tooltip: PropTypes.string
 };
 export default SocialPanelHeader;
