20bae51dd95be4ffb03ae80e1e81482e9c691872
Miguel Gatmaytan <miguel.gatmaytan@dnnsoftware.com>
2016-11-28 14:59:41 -0800

    AP-310 - Fixed overflow on site list.

--- src/Modules/Manage/Dnn.PersonaBar.Sites/Sites.Web/src/_exportables/src/Components/ListView/PortalListItem/MainLabel.jsx
+++ src/Modules/Manage/Dnn.PersonaBar.Sites/Sites.Web/src/_exportables/src/Components/ListView/PortalListItem/MainLabel.jsx
@@ -1,13 +1,14 @@
 import React, {PropTypes} from "react";
 import GridCell from "dnn-grid-cell";
+import TextOverflowWrapper from "dnn-text-overflow-wrapper";
 
 /* eslint-disable react/no-danger */
 const MainLabel = ({label, portalAliases}) => (
     <GridCell className="portal-name-info">
-        <label>{label}</label>
+        <TextOverflowWrapper text={label} maxWidth={220}/>
         <GridCell>
         {portalAliases.map((alias)=>{
-            return <a href={alias.link} target="_blank">{alias.url}</a>;
+            return <TextOverflowWrapper href={alias.link} target="_blank" text={alias.url} isAnchor={true} maxWidth={220}/>;
         })}
         </GridCell>
     </GridCell>
