0c79ecc794e56d45a05d1f5799d2f864ce99b2de
Pavel Terenin <pavel.terenin@dnnsoftware.com>
2016-11-29 12:24:07 -0800

    NO JIRA - update externals to 1.0.8

--- src/Modules/Settings/Dnn.PersonaBar.TaskScheduler/TaskScheduler.Web/src/components/scheduler/schedulerRow/index.jsx
+++ src/Modules/Settings/Dnn.PersonaBar.TaskScheduler/TaskScheduler.Web/src/components/scheduler/schedulerRow/index.jsx
@@ -1,5 +1,5 @@
 import React, {Component, PropTypes } from "react";
-import Collapse from "react-collapse";
+import Collapse from "dnn-collapsible";
 import "./style.less";
 import { EditIcon } from "dnn-svg-icons";
 import ItemHistory from "../itemHistory";
