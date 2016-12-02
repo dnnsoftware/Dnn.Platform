e26e600ff8b19071258b281224df5810dd9ff4c8
Ash Prasad <ashishpd@hotmail.com>
2016-12-01 17:48:09 -0800

    AP-347 Removed Facebook and Gravity Skins

--- Website/Portals/_default/Skins/Gravity/js/doubletaptogo.min.js
+++
@@ -1,8 +0,0 @@
-/*
-	By Osvaldas Valutis, www.osvaldas.info
-	Available for use under the MIT License
-*/
-
-
-
-;(function(e,t,n,r){e.fn.doubleTapToGo=function(r){if(!("ontouchstart"in t)&&!navigator.msMaxTouchPoints&&!navigator.userAgent.toLowerCase().match(/windows phone os 7/i))return false;this.each(function(){var t=false;e(this).on("click",function(n){var r=e(this);if(r[0]!=t[0]){n.preventDefault();t=r}});e(n).on("click touchstart MSPointerDown",function(n){var r=true,i=e(n.target).parents();for(var s=0;s<i.length;s++)if(i[s]==t[0])r=false;if(r)t=false})});return this}})(jQuery,window,document);
