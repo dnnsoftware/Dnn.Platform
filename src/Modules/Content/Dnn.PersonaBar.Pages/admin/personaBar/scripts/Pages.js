b12ffe98d8d5160bbb2f591b535ad8c216f80945
Antonio D. Gonzalez <antonio.gonzalez@xepient.com>
2017-01-05 15:51:18 +0100

    DNN-9055 Changes after code review

--- src/Modules/Content/Dnn.PersonaBar.Pages/admin/personaBar/scripts/Pages.js
+++ src/Modules/Content/Dnn.PersonaBar.Pages/admin/personaBar/scripts/Pages.js
@@ -18,8 +18,8 @@
                 utilities: util,
                 moduleName: "Pages",
                 config: config,
-                viewName: (params && params.viewName) ? params.viewName : null,
-                viewParams: (params && params.viewParams) ? params.viewParams : null,
+                viewName: params.viewName,
+                viewParams: params.viewParams,
                 settings: params.settings
             };
         };
@@ -33,8 +33,8 @@
 
     var load = function (params, callback) {
         window.dnn.pages.load({
-            viewName: (params && params.viewName) ? params.viewName : null,
-            viewParams: (params && params.viewParams) ? params.viewParams : null,
+            viewName: params.viewName,
+            viewParams: params.viewParams,
             settings: params.settings
         });
         if (window.dnn.pages.pageHierarchyManager &&
