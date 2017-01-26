aedb86c4b93566fef876fd0d31112c7bddbbee0a
Karthick Sundararajan <sky.karthick@gmail.com>
2017-01-26 13:52:33 +0100

    (DNN-9223) BugFix - /sitemap.aspx does not produce alternate links in multilingual website

--- DNN Platform/Library/Services/Sitemap/SitemapBuilder.cs
+++ DNN Platform/Library/Services/Sitemap/SitemapBuilder.cs
@@ -362,17 +362,17 @@ namespace DotNetNuke.Services.Sitemap
             writer.WriteElementString("changefreq", sitemapUrl.ChangeFrequency.ToString().ToLower());
             writer.WriteElementString("priority", sitemapUrl.Priority.ToString("F01", CultureInfo.InvariantCulture));
 
-            //if (sitemapUrl.AlternateUrls != null)
-            //{
-            //    foreach (AlternateUrl alternate in sitemapUrl.AlternateUrls)
-            //    {
-            //        writer.WriteStartElement("link", "http://www.w3.org/1999/xhtml");
-            //        writer.WriteAttributeString("rel", "alternate");
-            //        writer.WriteAttributeString("hreflang", alternate.Language);
-            //        writer.WriteAttributeString("href", alternate.Url);
-            //        writer.WriteEndElement();
-            //    }
-            //}
+            if (sitemapUrl.AlternateUrls != null)
+            {
+                foreach (AlternateUrl alternate in sitemapUrl.AlternateUrls)
+                {
+                    writer.WriteStartElement("link", "http://www.w3.org/1999/xhtml");
+                    writer.WriteAttributeString("rel", "alternate");
+                    writer.WriteAttributeString("hreflang", alternate.Language);
+                    writer.WriteAttributeString("href", alternate.Url);
+                    writer.WriteEndElement();
+                }
+            }
             writer.WriteEndElement();
         }
 
