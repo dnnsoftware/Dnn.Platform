2b113f2d52a35ce866913eac395a6feca558f2c6
Kelly Ford <kford@dnndev.com>
2017-01-31 10:42:41 -0700

    Implemented get-portal command

---
+++ src/Modules/Prompt/Dnn.PersonaBar.Prompt/Commands/Portal/GetPortal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using DotNetNuke;
+using DotNetNuke.Entities;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Entities.Tabs;
+using DotNetNuke.Entities.Users;
+using Dnn.PersonaBar.Prompt.Attributes;
+using Dnn.PersonaBar.Prompt.Common;
+using Dnn.PersonaBar.Prompt.Interfaces;
+using Dnn.PersonaBar.Prompt.Models;
+using System.Text;
+
+namespace Dnn.PersonaBar.Prompt.Commands.Portal
+{
+    [ConsoleCommand("get-portal", "Retrieves information about the current portal", new string[] { "id" })]
+    public class GetPortal : BaseConsoleCommand, IConsoleCommand
+{
+
+    public string ValidationMessage { get; private set; }
+    public int? PortalIdFlagValue { get; private set; }
+
+    public void Init(string[] args, PortalSettings portalSettings, UserInfo userInfo, int activeTabId)
+    {
+        base.Initialize(args, portalSettings, userInfo, activeTabId);
+        StringBuilder sbErrors = new StringBuilder();
+
+        // default usage: return current page if nothing else specified
+        if (args.Length == 1)
+        {
+            // currently prompt only works on current portal
+            PortalIdFlagValue = PortalId;
+        }
+        else
+        {
+            sbErrors.Append("The get-portal command does not take any arguments or flags; ");
+        }
+
+
+        ValidationMessage = sbErrors.ToString();
+    }
+
+    public bool IsValid()
+    {
+        return string.IsNullOrEmpty(ValidationMessage);
+    }
+
+    public ConsoleResultModel Run()
+    {
+        PortalController pc = new PortalController();
+        List<PortalInfoModel> lst = new List<PortalInfoModel>();
+
+        if (PortalIdFlagValue.HasValue)
+        {
+            lst.Add(PortalInfoModel.FromDnnPortalInfo(PortalSettings));
+        }
+
+        return new ConsoleResultModel(string.Empty) { data = lst };
+    }
+
+
+}
+}
---
+++ src/Modules/Prompt/Dnn.PersonaBar.Prompt/Models/PortalInfoModel.cs
@@ -0,0 +1,66 @@
+﻿using static DotNetNuke.Common.Globals;
+
+namespace Dnn.PersonaBar.Prompt.Models
+{
+    public class PortalInfoModel
+    {
+        public int PortalId;
+        public string PortalName;
+        public int CdfVersion;
+        public string RegistrationMode;
+        public string DefaultPortalAlias;
+        public int PageCount;
+        // command link
+        public string __PageCount;
+        public int UserCount;
+        // command link
+        public string __UserCount;
+        public string SiteTheme;
+        public string AdminTheme;
+        public string Container;
+        public string AdminContainer;
+
+        public string Language;
+        public static PortalInfoModel FromDnnPortalInfo(DotNetNuke.Entities.Portals.PortalSettings portal)
+        {
+            PortalInfoModel pim = new PortalInfoModel
+            {
+                PortalId = portal.PortalId,
+                PortalName = portal.PortalName,
+                CdfVersion = portal.CdfVersion,
+                DefaultPortalAlias = portal.DefaultPortalAlias,
+                PageCount = portal.Pages,
+                __PageCount = "list-pages",
+                UserCount = portal.Users,
+                __UserCount = "list-users",
+                SiteTheme = Utilities.FormatSkinName(portal.DefaultPortalSkin),
+                AdminTheme = Utilities.FormatSkinName(portal.DefaultAdminSkin),
+                Container = Utilities.FormatContainerName(portal.DefaultPortalContainer),
+                AdminContainer = Utilities.FormatContainerName(portal.DefaultAdminContainer),
+                Language = portal.DefaultLanguage
+            };
+
+            switch (portal.UserRegistration)
+            {
+                case (int)PortalRegistrationType.NoRegistration:
+                    pim.RegistrationMode = "None";
+                    break;
+                case (int)PortalRegistrationType.PrivateRegistration:
+                    pim.RegistrationMode = "Private";
+                    break;
+                case (int)PortalRegistrationType.PublicRegistration:
+                    pim.RegistrationMode = "Public";
+                    break;
+                case (int)PortalRegistrationType.VerifiedRegistration:
+                    pim.RegistrationMode = "Verified";
+                    break;
+                default:
+                    pim.RegistrationMode = "Unknown";
+                    break;
+            }
+
+            return pim;
+        }
+
+    }
+}
