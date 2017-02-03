c9b754bd75fdc692e186633ee3a222f1810f0e1b
Pedro Berná <pedro.berna@xepient.com>
2017-02-03 12:35:02 +0100

    SCM-472 Added support to enabled property to dnn-tags control

--- Tags/src/Tags.jsx
+++ Tags/src/Tags.jsx
@@ -116,7 +116,7 @@ export default class Tags extends Component {
 
     render() {
         const Tags = this.props.tags.map((tag, index) => {
-            return <Tag tag={tag} key={index} onRemove={this.removeTagByName.bind(this, tag) }/>;
+            return <Tag tag={tag} key={index} onRemove={this.removeTagByName.bind(this, tag) } enabled={this.props.enabled} />;
         });
         const inputStyle = {
             width: this.state.inputWidth,
@@ -124,8 +124,14 @@ export default class Tags extends Component {
         };
         const placeholderText =  this.state.tags.length ? "" : "Add Tags";
 
-        let className = "dnn-uicommon-tags-field-input" +
+        const className = "dnn-uicommon-tags-field-input" +
             (this.state.tagInputActive ? " active " : "");
+
+        const opts = {};
+
+        if(!this.props.enabled) {
+            opts["disabled"] = "disabled";
+        }
         
         return (
             <div className={className} 
@@ -143,6 +149,7 @@ export default class Tags extends Component {
                         onChange={this.onChange.bind(this) }
                         onFocus={this.onTagFocus.bind(this) }
                         onBlur={this.onTagBlur.bind(this) }
+                        {...opts}
                        />
                 </div>                
             </div>
@@ -153,5 +160,10 @@ export default class Tags extends Component {
 Tags.propTypes = {
     tags: PropTypes.array.isRequired,
     onUpdateTags: PropTypes.func.isRequired,
-    style: PropTypes.object
+    style: PropTypes.object,
+    enabled: PropTypes.bool.isRequired
+};
+
+Tags.defaultProps = {
+    enabled: true
 };
