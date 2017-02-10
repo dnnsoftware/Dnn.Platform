ea048f8dad13b7b51496e55836a59df4f163b3fb
Ben <zyhfish@163.com>
2017-02-10 14:06:50 +0800

    DNN-9352: set AddUser always be true when not authenticated.

--- DNN Platform/Library/Entities/Modules/UserUserControlBase.cs
+++ DNN Platform/Library/Entities/Modules/UserUserControlBase.cs
@@ -191,6 +191,12 @@ namespace DotNetNuke.Entities.Modules
 
         #endregion
 
+        #region "Properties"
+
+        protected override bool AddUser => !Request.IsAuthenticated || base.AddUser;
+
+        #endregion
+
         #region Nested type: BaseUserEventArgs
 
         /// -----------------------------------------------------------------------------
