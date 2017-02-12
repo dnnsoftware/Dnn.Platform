75c980c0f09dd43bbb12c51cddfe2e918484d37d
Ben <zyhfish@163.com>
2017-02-13 07:48:47 +0800

    Update version to 1.3.0

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -30,4 +30,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
