5fbf0dfeece79bd4e09c37d9dde2161c78fb8822
Ben <zyhfish@163.com>
2017-02-16 08:39:09 +0800

    Update sp parameters order to compatible in upgrade process.

--- src/Dnn.PersonaBar.Library/Data/DataService.cs
+++ src/Dnn.PersonaBar.Library/Data/DataService.cs
@@ -11,9 +11,10 @@ namespace Dnn.PersonaBar.Library.Data
         public int SavePersonaBarMenu(string identifier, string moduleName, string folderName, string controller, string resourceKey, string path,
             string link, string cssClass, string iconFile, int parentId, int order, bool allowHost, bool enabled, int currentUserId)
         {
-            return DataProvider.ExecuteScalar<int>("PersonaBar_SavePersonaBarMenu", identifier, moduleName, folderName, controller, resourceKey, path,
-                Null.GetNull(link, DBNull.Value), Null.GetNull(cssClass, DBNull.Value), Null.GetNull(iconFile, DBNull.Value),
-                Null.GetNull(parentId, DBNull.Value), order, allowHost, enabled, currentUserId);
+            return DataProvider.ExecuteScalar<int>("PersonaBar_SavePersonaBarMenu", 
+                identifier, moduleName, folderName, controller, resourceKey, path,
+                Null.GetNull(link, DBNull.Value), Null.GetNull(cssClass, DBNull.Value),
+                Null.GetNull(parentId, DBNull.Value), order, allowHost, enabled, currentUserId, Null.GetNull(iconFile, DBNull.Value));
         }
 
         public IDataReader GetPersonaBarMenu()
