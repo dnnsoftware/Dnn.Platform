adeebcb20749d1f491376bfa18cefe40b0445c13
AMARJIT DHUNNA <AMARJIT.DHUNNA@DNNSOFTWARE.COM>
2017-03-07 13:34:42 -0800

    DNN-9475: Implemented profile properties export/import

---
+++ DNN Platform/Modules/DnnExportImport/Components/Dto/ProfileProperties/ExportProfileProperty.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using DotNetNuke.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace Dnn.ExportImport.Components.Dto.ProfileProperties
+{
+    [JsonObject]
+    [Serializable]
+    [TableName("ProfilePropertyDefinition")]
+    [PrimaryKey("PropertyDefinitionID")]
+    public class ExportProfileProperty : BasicExportImportDto
+    {
+        [ColumnName("PropertyDefinitionID")]
+        [JsonProperty(PropertyName = "PropertyDefinitionID")]
+        public int PropertyDefinitionId { get; set; }
+
+        [ColumnName("PortalID")]
+        [JsonProperty(PropertyName = "PortalID")]
+        public int? PortalId { get; set; }
+
+        [ColumnName("ModuleDefID")]
+        [JsonProperty(PropertyName = "ModuleDefID")]
+        public int ModuleDefId { get; set; }
+
+        public bool Deleted { get; set; }
+        public int? DataType { get; set; }
+        public string DefaultValue { get; set; }
+        public string PropertyCategory { get; set; }
+        public string PropertyName { get; set; }
+        public int Length { get; set; }
+        public bool Required { get; set; }
+        public string ValidationExpression { get; set; }
+        public int ViewOrder { get; set; }
+        public bool Visible { get; set; }
+
+        [ColumnName("CreatedByUserID")]
+        [JsonProperty(PropertyName = "CreatedByUserID")]
+        public int? CreatedByUserId { get; set; }
+
+        [IgnoreColumn]
+        public string CreatedByUserName { get; set; } //This could be used to find "CreatedByUserId"
+        public DateTime? CreatedOnDate { get; set; }
+
+        [ColumnName("LastModifiedByUserID")]
+        [JsonProperty(PropertyName = "LastModifiedByUserID")]
+        public int? LastModifiedByUserId { get; set; }
+
+        [IgnoreColumn]
+        public string LastModifiedByUserName { get; set; } //This could be used to find "LastModifiedByUserId"
+        public DateTime? LastModifiedOnDate { get; set; }
+        public int DefaultVisibility { get; set; }
+        public bool ReadOnly { get; set; }
+    }
+}
---
+++ DNN Platform/Modules/DnnExportImport/Components/Services/ProfilePropertiesService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Linq;
+using Dnn.ExportImport.Components.Dto;
+using Dnn.ExportImport.Components.Dto.ProfileProperties;
+using Dnn.ExportImport.Components.Entities;
+using Dnn.ExportImport.Components.Interfaces;
+using DotNetNuke.Data;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.Entities.Profile;
+
+namespace Dnn.ExportImport.Components.Services
+{
+    public class ProfilePropertiesService : IPortable2
+    {
+        private int _progressPercentage;
+        public string Category => "PROFILEPROPERTIES";
+        public uint Priority => 5;
+        public bool CanCancel => false;
+        public bool CanRollback => false;
+
+        public int ProgressPercentage
+        {
+            get { return _progressPercentage; }
+            private set
+            {
+                if (value < 0) value = 0;
+                else if (value > 100) value = 100;
+                _progressPercentage = value;
+            }
+        }
+
+        public void ExportData(ExportImportJob exportJob, IExportImportRepository repository)
+        {
+            ProgressPercentage = 0;
+            var profileProperties =
+                CBO.FillCollection<ExportProfileProperty>(
+                    DataProvider.Instance().GetPropertyDefinitionsByPortal(exportJob.PortalId)).ToList();
+            ProgressPercentage = 50;
+            repository.CreateItems(profileProperties, null);
+            ProgressPercentage = 100;
+        }
+
+        public void ImportData(ExportImportJob importJob, ExportDto exporteDto, IExportImportRepository repository)
+        {
+            ProgressPercentage = 0;
+            var profileProperties = repository.GetAllItems<ExportProfileProperty>().ToList();
+
+            foreach (var profileProperty in profileProperties)
+            {
+                using (var db = DataContext.Instance())
+                {
+                    var existingProfileProperty = CBO.FillObject<ExportProfileProperty>(DataProvider.Instance()
+                        .GetPropertyDefinitionByName(importJob.PortalId, profileProperty.PropertyName));
+                    var modifiedById = Common.Util.GetUserIdOrName(importJob, profileProperty.LastModifiedByUserId,
+                        profileProperty.LastModifiedByUserName);
+
+                    if (existingProfileProperty != null)
+                    {
+                        switch (exporteDto.CollisionResolution)
+                        {
+                            case CollisionResolution.Overwrite:
+                                ProcessUpdateProfileProperty(db, profileProperty, existingProfileProperty,
+                                    existingProfileProperty.CreatedByUserId, modifiedById);
+                                break;
+                            case CollisionResolution.Ignore: //Just ignore the record
+                            //TODO: Log that profile property was ignored.
+                            case CollisionResolution.Duplicate: //Just ignore the record
+                                //TODO: Log that profile property was ignored as duplicate not possible for profile properties.
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException(exporteDto.CollisionResolution.ToString());
+                        }
+                    }
+                    else
+                    {
+                        var createdById = Common.Util.GetUserIdOrName(importJob, profileProperty.CreatedByUserId,
+                            profileProperty.CreatedByUserName);
+
+                        ProcessCreateProfileProperty(importJob, db, profileProperty, createdById, modifiedById);
+                    }
+                }
+            }
+        }
+
+        private static void ProcessCreateProfileProperty(ExportImportJob importJob, IDataContext db,
+            ExportProfileProperty profileProperty, int createdById, int modifiedById)
+        {
+            profileProperty.PropertyDefinitionId = 0;
+            profileProperty.PortalId = importJob.PortalId;
+            profileProperty.CreatedOnDate = DateTime.UtcNow;
+            profileProperty.CreatedByUserId = createdById;
+            profileProperty.LastModifiedOnDate = DateTime.UtcNow;
+            profileProperty.LastModifiedByUserId = modifiedById;
+            var repProfileProperty = db.GetRepository<ExportProfileProperty>();
+            repProfileProperty.Insert(profileProperty);
+        }
+
+        private static void ProcessUpdateProfileProperty(IDataContext db,
+            ExportProfileProperty profileProperty, ExportProfileProperty existingProfileProperty,
+            int? createdById, int modifiedById)
+        {
+            profileProperty.PortalId = existingProfileProperty.PortalId;
+            profileProperty.CreatedOnDate = existingProfileProperty.CreatedOnDate;
+            profileProperty.CreatedByUserId = createdById;
+            profileProperty.LastModifiedOnDate = DateTime.UtcNow;
+            profileProperty.LastModifiedByUserId = modifiedById;
+            var repProfileProperty = db.GetRepository<ExportProfileProperty>();
+            repProfileProperty.Update(profileProperty);
+        }
+    }
+}
