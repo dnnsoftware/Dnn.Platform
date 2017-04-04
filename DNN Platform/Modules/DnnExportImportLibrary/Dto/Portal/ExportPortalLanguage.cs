45d9c566d3233aad26fc7ad3cc2d592c96cf4d46
AMARJIT DHUNNA <AMARJIT.DHUNNA@DNNSOFTWARE.COM>
2017-04-04 14:57:56 -0700

    DNN-9455: Moved portal entities.
    Removed petapoco

--- DNN Platform/Modules/DnnExportImportLibrary/Dto/Portal/ExportPortalLanguage.cs
+++ DNN Platform/Modules/DnnExportImportLibrary/Dto/Portal/ExportPortalLanguage.cs
@@ -20,54 +20,28 @@
 #endregion
 
 using System;
-using Dnn.ExportImport.Dto;
-using DotNetNuke.ComponentModel.DataAnnotations;
-using Newtonsoft.Json;
 
-namespace Dnn.ExportImport.Components.Dto.Portal
+namespace Dnn.ExportImport.Dto.Portal
 {
-    [JsonObject]
-    [Serializable]
-    [TableName("PortalLocalization")]
-    [PrimaryKey("PortalID,CultureCode", AutoIncrement = false)]
-
-    public class ExportPortalLocalization : BasicExportImportDto
+    public class ExportPortalLanguage : BasicExportImportDto
     {
-        [ColumnName("PortalID")]
-        [JsonProperty(PropertyName = "PortalID")]
-        public int PortalId { get; set; }
+        public int PortalLanguageId { get; set; }
 
         public string CultureCode { get; set; }
-        public string PortalName { get; set; }
-        public string LogoFile { get; set; }
-        public string FooterText { get; set; }
-        public string Description { get; set; }
-        public string KeyWords { get; set; }
-        public string BackgroundFile { get; set; }
-        public int? HomeTabId { get; set; }
-        public int? LoginTabId { get; set; }
-        public int? UserTabId { get; set; }
-        public int? AdminTabId { get; set; }
-        public int? SplashTabId { get; set; }
 
-        [ColumnName("CreatedByUserID")]
-        [JsonProperty(PropertyName = "CreatedByUserID")]
+        public int PortalId { get; set; }
+
+        public int LanguageId { get; set; }
+
         public int? CreatedByUserId { get; set; }
 
-        [IgnoreColumn]
         public string CreatedByUserName { get; set; } //This could be used to find "CreatedByUserId"
         public DateTime? CreatedOnDate { get; set; }
 
-        [ColumnName("LastModifiedByUserID")]
-        [JsonProperty(PropertyName = "LastModifiedByUserID")]
         public int? LastModifiedByUserId { get; set; }
 
-        [IgnoreColumn]
         public string LastModifiedByUserName { get; set; } //This could be used to find "LastModifiedByUserId"
         public DateTime? LastModifiedOnDate { get; set; }
-        public int? RegisterTabId { get; set; }
-        public int? SearchTabId { get; set; }
-        public int? Custom404TabId { get; set; }
-        public int? Custom500TabId { get; set; }
+        public bool IsPublished { get; set; }
     }
 }
--- DNN Platform/Modules/DnnExportImportLibrary/Dto/Portal/ExportPortalSetting.cs
+++ DNN Platform/Modules/DnnExportImportLibrary/Dto/Portal/ExportPortalSetting.cs
@@ -20,50 +20,27 @@
 #endregion
 
 using System;
-using Dnn.ExportImport.Dto;
-using DotNetNuke.ComponentModel.DataAnnotations;
-using Newtonsoft.Json;
 
-namespace Dnn.ExportImport.Components.Dto.Portal
+namespace Dnn.ExportImport.Dto.Portal
 {
-    [JsonObject]
-    [Serializable]
-    [TableName("PortalLanguages")]
-    [PrimaryKey("PortalLanguageID")]
-
-    public class ExportPortalLanguage : BasicExportImportDto
+    public class ExportPortalSetting : BasicExportImportDto
     {
-        [ColumnName("PortalLanguageID")]
-        [JsonProperty(PropertyName = "PortalLanguageID")]
-        public int PortalLanguageId { get; set; }
+        public int PortalSettingId { get; set; }
 
-        [JsonIgnore]
-        [IgnoreColumn]
-        public string CultureCode { get; set; }
-
-        [ColumnName("PortalID")]
-        [JsonProperty(PropertyName = "PortalID")]
         public int PortalId { get; set; }
 
-        [ColumnName("LanguageID")]
-        [JsonProperty(PropertyName = "LanguageID")]
-        public int LanguageId { get; set; }
+        public string SettingName { get; set; }
+        public string SettingValue { get; set; }
 
-        [ColumnName("CreatedByUserID")]
-        [JsonProperty(PropertyName = "CreatedByUserID")]
         public int? CreatedByUserId { get; set; }
 
-        [IgnoreColumn]
         public string CreatedByUserName { get; set; } //This could be used to find "CreatedByUserId"
         public DateTime? CreatedOnDate { get; set; }
 
-        [ColumnName("LastModifiedByUserID")]
-        [JsonProperty(PropertyName = "LastModifiedByUserID")]
         public int? LastModifiedByUserId { get; set; }
 
-        [IgnoreColumn]
         public string LastModifiedByUserName { get; set; } //This could be used to find "LastModifiedByUserId"
         public DateTime? LastModifiedOnDate { get; set; }
-        public bool IsPublished { get; set; }
+        public string CultureCode { get; set; }
     }
 }
