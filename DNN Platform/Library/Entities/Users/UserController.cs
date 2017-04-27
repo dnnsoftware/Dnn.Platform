2eca06514cff2f36f9cd1b9a9ff0cf68d6ddbcbc
Kan Ma <kan.ma@dnnsoftware.com>
2017-04-26 19:49:31 -0700

    DNN-9728: got rid of unwanted change

--- DNN Platform/Library/Entities/Users/UserController.cs
+++ DNN Platform/Library/Entities/Users/UserController.cs
@@ -1,6 +1,6 @@
-#region Copyright
+﻿#region Copyright
 // 
-// DotNetNuke?- http://www.dotnetnuke.com
+// DotNetNuke®- http://www.dotnetnuke.com
 // Copyright (c) 2002-2017
 // by DotNetNuke Corporation
 // 
