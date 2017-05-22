ec5474b51f74216aa4ad8cb7009f0fc56ac5513e
Ben <zyhfish@163.com>
2017-05-22 11:28:01 +0800

    DNN-8478: hide profile binding scripts when no property rendered.

--- Website/DesktopModules/Admin/ViewProfile/ViewProfile.ascx.designer.cs
+++ Website/DesktopModules/Admin/ViewProfile/ViewProfile.ascx.designer.cs
@@ -47,5 +47,14 @@ namespace DotNetNuke.Modules.Admin.ViewProfile {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink editLink;
+        
+        /// <summary>
+        /// pnlScripts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel pnlScripts;
     }
 }
