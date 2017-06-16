f614e16cea806f5aa2ea47a5397551119d8e1ac6
Adam Nierzad <adam.nierzad@cantarus.com>
2017-06-16 14:42:44 +0100

    Created APIUser data structure and associated controller and database script.

---
+++ PolyDeploy/DataAccess/Models/APIUser.cs
@@ -0,0 +1,36 @@
+﻿using DotNetNuke.ComponentModel.DataAnnotations;
+using System;
+
+namespace Cantarus.Modules.PolyDeploy.DataAccess.Models
+{   
+    [TableName("Cantarus_PolyDeploy_APIUsers")]
+    [PrimaryKey("APIUserID")]
+    internal class APIUser
+    {
+        public int APIUserId { get; set; }
+        public string Name { get; set; }
+        public string APIKey { get; set; }
+        public string EncryptionKey { get; set; }
+
+        public APIUser(string name)
+        {
+            if(name == null || name.Length < 1)
+            {
+                throw new Exception("Unable to create new APIUser without a name.");
+            }
+
+            Name = name;
+            APIKey = GenerateKey();
+            EncryptionKey = GenerateKey();
+        }
+
+        private static string GenerateKey()
+        {
+            // Get new guid as string.
+            string guidString = Guid.NewGuid().ToString();
+
+            // Remove hyphens, uppercase and return.
+            return guidString.Replace("-", null).ToUpper();
+        }
+    }
+}
