7ce37060a14b59428cccc9f1cadc84bf9b32887d
Pavel Terenin <pavel.terenin@dnnsoftware.com>
2017-06-20 10:37:16 -0700

    DNN-9753 - add loclization for switcher in pages

--- src/Modules/Content/Dnn.PersonaBar.Pages/Pages.Web/src/components/Seo/Seo.jsx
+++ src/Modules/Content/Dnn.PersonaBar.Pages/Pages.Web/src/components/Seo/Seo.jsx
@@ -24,56 +24,58 @@ class Seo extends Component {
     getSitemapPriorityOptions() {
         const options = [];
         for (let i = 0; i <= 1; i += 0.1) {
-            const v = i.toFixed(1)/1;
-            options.push({value: v, label: v});
-        } 
+            const v = i.toFixed(1) / 1;
+            options.push({ value: v, label: v });
+        }
 
         return options;
     }
-    
+
     render() {
         const {page, onChangeField} = this.props;
-        
+
         const editing = page.tabId !== 0;
-        
+
         return (
             <div className={styles.seoContainer}>
                 {editing &&
-                <PageUrls pageUrls={page.pageUrls} 
-                    siteAliases={page.siteAliases} 
-                    pageHasParent={page.hasParent}
-                    primaryAliasId={page.primaryAliasId} />} 
-            
+                    <PageUrls pageUrls={page.pageUrls}
+                        siteAliases={page.siteAliases}
+                        pageHasParent={page.hasParent}
+                        primaryAliasId={page.primaryAliasId} />}
+
                 <GridSystem>
                     <GridCell className="left-column">
                         <MultiLineInputWithError
-                            label={Localization.get("PageHeaderTags")}
-                            tooltipMessage={Localization.get("PageHeaderTags_tooltip")} 
+                            label={Localization.get("PageHeaderTags") }
+                            tooltipMessage={Localization.get("PageHeaderTags_tooltip") }
                             value={page.pageHeadText}
-                            onChange={this.onChangeField.bind(this, "pageHeadText")} />
+                            onChange={this.onChangeField.bind(this, "pageHeadText") } />
                     </GridCell>
                     <GridCell className="right-column">
                         <GridCell>
                             <Label
                                 labelType="block"
-                                tooltipMessage={Localization.get("SitemapPriority_tooltip")}
-                                label={Localization.get("SitemapPriority")} />
-                            <Dropdown options={this.getSitemapPriorityOptions()}
-                                value={page.sitemapPriority} 
-                                onSelect={this.onSitemapPrioritySelected.bind(this)} 
+                                tooltipMessage={Localization.get("SitemapPriority_tooltip") }
+                                label={Localization.get("SitemapPriority") } />
+                            <Dropdown options={this.getSitemapPriorityOptions() }
+                                value={page.sitemapPriority}
+                                onSelect={this.onSitemapPrioritySelected.bind(this) }
                                 withBorder={true} />
                         </GridCell>
                         <GridCell className="new-section">
                             <Label
                                 labelType="inline"
-                                tooltipMessage={Localization.get("AllowIndexing_tooltip")}
-                                label={Localization.get("AllowIndexing")}
+                                tooltipMessage={Localization.get("AllowIndexing_tooltip") }
+                                label={Localization.get("AllowIndexing") }
                                 />
                             <Switch
-                                labelHidden={true}
+                                labelHidden={false}
+                                onText={Localization.get("On") }
+                                offText={Localization.get("Off") }
                                 value={page.allowIndex}
-                                onChange={onChangeField.bind(this, "allowIndex")} />
-                    </GridCell>
+                                onChange={onChangeField.bind(this, "allowIndex") } />
+                        </GridCell>
                     </GridCell>
                 </GridSystem>
             </div>
