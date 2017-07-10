9201fafea5e94bffc095e62d72ac2c1f6e87693b
AMARJIT DHUNNA <AMARJIT.DHUNNA@DNNSOFTWARE.COM>
2017-07-10 09:32:50 -0700

    CONTENT-8120: Comment the Region declaration in RecycleBin model javascript file

--- src/Modules/Content/Dnn.PersonaBar.Recyclebin/admin/personaBar/scripts/RecycleBin.ViewModel.js
+++ src/Modules/Content/Dnn.PersonaBar.Recyclebin/admin/personaBar/scripts/RecycleBin.ViewModel.js
@@ -1,4 +1,4 @@
-﻿#region Copyright
+﻿//#region Copyright
 // 
 // DotNetNuke® - http://www.dotnetnuke.com
 // Copyright (c) 2002-2017
@@ -17,7 +17,7 @@
 // THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
 // CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 // DEALINGS IN THE SOFTWARE.
-#endregion
+//#endregion
 
 /***
  * @class DnnPageRecycleBin
