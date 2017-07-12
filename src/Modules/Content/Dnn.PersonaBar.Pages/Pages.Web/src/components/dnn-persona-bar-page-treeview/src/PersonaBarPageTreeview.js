a7e79455dffcae25b7be9475905ab27905ea1d8f
daniel ram <danieldram@gmail.com>
2017-07-12 15:48:48 -0700

    DNN-9995 - Base treeview and treeviewinteractor underway!

--- src/Modules/Content/Dnn.PersonaBar.Pages/Pages.Web/src/components/dnn-persona-bar-page-treeview/src/PersonaBarPageTreeview.js
+++ src/Modules/Content/Dnn.PersonaBar.Pages/Pages.Web/src/components/dnn-persona-bar-page-treeview/src/PersonaBarPageTreeview.js
@@ -1,22 +1,46 @@
 import React, {Component} from "react";
 import GridCell from "dnn-grid-cell";
+import { PropTypes } from "prop-types";
 
 export class PersonaBarPageTreeview extends Component {
-    renderCollapseExpand() {
+
+
+    render_tree(childListItems){
+        const {getChildListItems} = this.props;
         return (
-            <span>
-                [COLLAPSE EXPAND]
-            </span>
+             <PersonaBarPageTreeview getChildListItems={getChildListItems} listItems={childListItems} />
         );
+    }
+
+    toggleParentState(item) {
+
+    }
+
+    render_li() {
+        const {listItems, getChildListItems} = this.props;
+        return listItems.map((item)=>{
+            return (
+                <li>
+                    <span onClick={()=>getChildListItems(item.id)}>{item.name}</span>
+                    {item.childListItems && item.isOpen ? this.render_tree(item.childListItems) : null}
+                </li>
+            );
+        });
     }
 
     render() {
         return (
-            <GridCell columnSize={30}  style={{marginTop:"120px", backgroundColor:"#aaa"}} >
-                {this.renderCollapseExpand()}
-            </GridCell>
+            <ul>
+                {this.render_li()}
+            </ul>
         );
     }
 
 }
 
+PersonaBarPageTreeview.propTypes = {
+    listItems: PropTypes.array.isRequired,
+    getChildListItems: PropTypes.func.isRequired,
+    icons: PropTypes.object.isRequired,
+    onSelect: PropTypes.func.isRequired
+};
--- src/Modules/Content/Dnn.PersonaBar.Pages/Pages.Web/src/components/dnn-persona-bar-page-treeview/src/PersonaBarPageTreeviewInteractor.js
+++ src/Modules/Content/Dnn.PersonaBar.Pages/Pages.Web/src/components/dnn-persona-bar-page-treeview/src/PersonaBarPageTreeviewInteractor.js
@@ -1,42 +1,131 @@
-import React, {Component} from "react";
-import {PersonaBarPageTreeview} from "./PersonaBarPageTreeview";
+import React, { Component } from "react";
+import { PersonaBarPageTreeview } from "./PersonaBarPageTreeview";
+import { PropTypes } from "prop-types";
+import Promise from "promise";
+import GridCell from "dnn-grid-cell";
 
 export class PersonaBarPageTreeviewInteractor extends Component {
 
-    constructor(){
+    constructor() {
         super();
+        this.state = {
+            isTreeviewExpanded: false
+        };
         this.origin = window.origin;
-        //http://auto.engage14-162.com/API/PersonaBar/EvoqPages/GetPageList?searchKey=
-
-        this.state = {};
-        this.origin = window.origin;
-        this.url = `${window.origin}/API/PersonaBar/${window.dnn.pages.apiController}/GetPageList?searchKey=`;
-        this.GET(this.url, this.setState.bind(this));
-
+        this.getRootListItems();
     }
 
     GET(url, setState) {
-        function reqListener () {
-           const pageList =  JSON.parse(this.responseText);
-           setState({pageList: pageList}, function(){
-               console.log(this.state);
-           });
-        }
-
-        const xhr= new XMLHttpRequest();
-        xhr.addEventListener("load", reqListener);
-        xhr.open("GET", url);
-        xhr.send();
+        return new Promise((resolve, reject) => {
+            function reqListener() {
+                const data = JSON.parse(this.responseText);
+                resolve(data);
+            }
+            const xhr = new XMLHttpRequest();
+            xhr.addEventListener("load", reqListener);
+            xhr.open("GET", url);
+            xhr.send();
+        });
     }
 
-    requestRootPageList(){
 
+    _traverse(comparator) {
+        let listItems = this.state.pageList.concat();
+        const cachedChildListItems = [];
+        cachedChildListItems.push(listItems);
+        const condition = cachedChildListItems.length > 0;
+
+        const loop = () => {
+            const childItem = cachedChildListItems.length ? cachedChildListItems.shift() : null;
+            const left = () => childItem.forEach(item => {
+                comparator(item, listItems);
+                Array.isArray(item.childListItems) ? cachedChildListItems.push(item.childListItems) : null;
+                condition ? loop() : exit();
+            });
+            const right = () => null;
+            childItem ? left() : right();
+        };
+
+        const exit = () => null;
+
+        loop();
+        return;
+    }
+
+    getRootListItems() {
+        const url = `${window.origin}/API/PersonaBar/${window.dnn.pages.apiController}/GetPageList?searchKey=`;
+        this.GET(url)
+            .then((data) => this.setState({ pageList: data }));
+    }
+
+    toggleParentCollapsedState(id){
+        const pageList = this.state.pageList.concat();
+
+        this._traverse((item, listItem)=>{
+            (item.id===id)  ? item.isOpen = !item.isOpen : null;
+            this.setState({pageList:listItem}, ()=>{
+                console.log(this.state);
+            });
+        });
+    }
+
+    getChildListItems(id) {
+
+        const url = `${window.origin}/API/PersonaBar/${window.dnn.pages.apiController}/GetPageList?parentId=${id}`;
+
+        this.GET(url)
+            .then((childListItems)=> {
+                this._traverse((item, listItems) => {
+                    const left = () => item.childListItems=childListItems;
+                    const right = () => null;
+                    (item.id === id ) ? left() : right();
+                    this.setState({pageList:listItems});
+                });
+                this.toggleParentCollapsedState(id);
+            });
+    }
+
+    addNewPageData(pageData) {
+        const pageListArray = this.state.pageList.concat();
+        const parentId = pageData.parentId;
+    }
+
+
+    toggleTreeview() {
+        this.setState({isTreeviewExpanded: !this.state.isTreeviewExpanded});
+        console.log(this.state);
+    }
+
+    render_treeview() {
+        return (
+            <span>
+                { this.state.isTreeviewExpanded ?
+                    <PersonaBarPageTreeview
+                        listItems={this.state.pageList}
+                        getChildListItems={this.getChildListItems.bind(this)} />
+                : null}
+            </span>
+        );
+    }
+
+    render_collapseExpand() {
+        return (
+            <span onClick={this.toggleTreeview.bind(this)}>
+                [COLLAPSE EXPAND]
+            </span>
+        );
     }
 
     render() {
-
         return (
-            <PersonaBarPageTreeview />
+            <GridCell columnSize={30} style={{ marginTop: "120px", backgroundColor: "#aaa" }} >
+                {this.render_collapseExpand()}
+                {this.render_treeview()}
+            </GridCell>
         );
     }
 }
+
+PersonaBarPageTreeviewInteractor.propTypes = {
+    OnSelect: PropTypes.func.isRequired
+};
