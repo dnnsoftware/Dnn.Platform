453aa000067494b09947ff33581eb32b1190d866
Adam Nierzad <adam.nierzad@cantarus.com>
2017-07-13 14:54:13 +0100

    Initial DeployClient.

---
+++ DeployClient/API.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace DeployClient
+{
+    class API
+    {
+        private static string APIKey = Properties.Settings.Default.APIKey;
+
+        private static HttpClient BuildClient()
+        {
+            HttpClient client = new HttpClient();
+
+            client.BaseAddress = new Uri(new Uri(Properties.Settings.Default.TargetUri), "DesktopModules/PolyDeploy/API/");
+            client.DefaultRequestHeaders.Add("x-api-key", Properties.Settings.Default.APIKey);
+
+            return client;
+        }
+
+        public static async Task<Dictionary<string, dynamic>> CIInstall(List<KeyValuePair<string, Stream>> streams)
+        {
+            string endpoint = "CI/Install";
+
+            using (HttpClient client = BuildClient())
+            {
+                MultipartFormDataContent form = new MultipartFormDataContent();
+
+                foreach (KeyValuePair<string, Stream> keyValuePair in streams)
+                {
+                    form.Add(new StreamContent(keyValuePair.Value), "none", keyValuePair.Key);
+                }
+
+                HttpResponseMessage response = await client.PostAsync(endpoint, form);
+
+                string json = await response.Content.ReadAsStringAsync();
+
+                JavaScriptSerializer jsonSer = new JavaScriptSerializer();
+
+                return jsonSer.Deserialize<Dictionary<string, dynamic>>(json);
+            }
+        }
+    }
+}
---
+++ DeployClient/Program.cs
@@ -0,0 +1,151 @@
+﻿using Cantarus.Libraries.Encryption;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+
+namespace DeployClient
+{
+    class Program
+    {
+        enum ExitCode : int
+        {
+            Success = 0,
+            Error = 1,
+            NoModulesFound = 2,
+            UserExit = 3,
+            InstallFailure = 4
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Output start.
+                WriteLine("*** Polly Deployment Client ***");
+                WriteLine();
+
+                // Get the properties we need.
+                string targetUri = Properties.Settings.Default.TargetUri;
+                string apiKey = Properties.Settings.Default.APIKey;
+                string encryptionKey = Properties.Settings.Default.EncryptionKey;
+
+                // Do we have a target uri.
+                if (targetUri.Equals(string.Empty))
+                {
+                    throw new Exception("No target uri has been set.");
+                }
+
+                // Do we have an api key?
+                if (apiKey.Equals(string.Empty))
+                {
+                    throw new Exception("No api key has been set.");
+                }
+
+                // Do we have an encryption key?
+                if (encryptionKey.Equals(string.Empty))
+                {
+                    throw new Exception("No encryption key has been set.");
+                }
+
+                // Output identifying module archives.
+                WriteLine("Identifying module archives...");
+
+                // Read zip files in current directory.
+                string currentDirectory = Directory.GetCurrentDirectory();
+                List<string> zipFiles = new List<string>(Directory.GetFiles(currentDirectory, "*.zip"));
+
+                // Is there something to do?
+                if (zipFiles.Count <= 0)
+                {
+                    // No, exit.
+                    WriteLine("No module archives found.");
+                    WriteLine("Exiting.");
+                    ReadLine();
+                    Environment.Exit((int)ExitCode.NoModulesFound);
+                }
+
+                // Inform user of modules found.
+                WriteLine(string.Format("Found {0} module archives in {1}:", zipFiles.Count, currentDirectory));
+
+                foreach (string zipFile in zipFiles)
+                {
+                    WriteLine(string.Format("\t{0}. {1}", zipFiles.IndexOf(zipFile) + 1, Path.GetFileName(zipFile)));
+                }
+                WriteLine();
+
+                // Prompt to continue.
+                WriteLine("Would you like to continue? (y/n)");
+
+                // Continue?
+                if (!Confirm())
+                {
+                    // No, exit.
+                    WriteLine("Exiting.");
+                    Environment.Exit((int)ExitCode.UserExit);
+                }
+                WriteLine();
+
+                // Inform user of encryption.
+                WriteLine("Starting encryption...");
+
+                List<KeyValuePair<string, Stream>> encryptedStreams = new List<KeyValuePair<string, Stream>>();
+
+                foreach (string zipFile in zipFiles)
+                {
+                    using (FileStream fs = new FileStream(zipFile, FileMode.Open))
+                    {
+                        encryptedStreams.Add(new KeyValuePair<string, Stream>(Path.GetFileName(zipFile), Crypto.Encrypt(fs, Properties.Settings.Default.EncryptionKey)));
+                    }
+                    WriteLine(string.Format("\tEncrypting {0}", Path.GetFileName(zipFile)));
+                }
+
+                Dictionary<string, dynamic> results = API.CIInstall(encryptedStreams).Result;
+
+                ArrayList installed = results.ContainsKey("Installed") ? results["Installed"] : null;
+                ArrayList failed = results.ContainsKey("Failed") ? results["Failed"] : null;
+
+                if (failed.Count > 0)
+                {
+                    WriteLine(string.Format("{0} module archives failed to install.", failed.Count));
+                    ReadLine();
+                    Environment.Exit((int)ExitCode.InstallFailure);
+                }
+
+                foreach (KeyValuePair<string, Stream> keyValuePair in encryptedStreams)
+                {
+                    keyValuePair.Value.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteLine(ex.Message);
+                ReadLine();
+                Environment.Exit((int)ExitCode.Error);
+            }
+        }
+
+        private static void WriteLine(string message = "")
+        {
+            Console.WriteLine(message);
+        }
+
+        private static string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        private static bool Confirm()
+        {
+            char ch = Console.ReadKey(true).KeyChar;
+
+            if (ch.Equals('y') || ch.Equals('Y'))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
