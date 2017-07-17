4ccdcfd059b279585ba550bcaa83c7fa574e2702
amarjit dhunna <amarjit.dhunna@dnnsoftware.com>
2017-07-17 15:30:52 -0700

    DNN-10074: Add json properties

--- src/Dnn.PersonaBar.Library/Prompt/Models/PagingInfo.cs
+++ src/Dnn.PersonaBar.Library/Prompt/Models/PagingInfo.cs
@@ -1,9 +1,14 @@
-﻿namespace Dnn.PersonaBar.Library.Prompt.Models
+﻿using Newtonsoft.Json;
+
+namespace Dnn.PersonaBar.Library.Prompt.Models
 {
     public class PagingInfo
     {
+        [JsonProperty(PropertyName = "pageNo")]
         public int PageNo { get; set; }
+        [JsonProperty(PropertyName = "pageSize")]
         public int PageSize { get; set; }
+        [JsonProperty(PropertyName = "totalPages")]
         public int TotalPages { get; set; }
     }
 }
