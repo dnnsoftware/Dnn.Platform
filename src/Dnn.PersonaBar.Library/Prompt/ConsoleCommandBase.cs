5ee5a47c259cc775c5b1473ea578cd9423aa99ee
amarjit dhunna <amarjit.dhunna@dnnsoftware.com>
2017-07-26 16:49:25 -0700

    DNN-10046: Use common method to get value for flags

--- src/Dnn.PersonaBar.Library/Prompt/ConsoleCommandBase.cs
+++ src/Dnn.PersonaBar.Library/Prompt/ConsoleCommandBase.cs
@@ -1,19 +1,111 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
 using Dnn.PersonaBar.Library.Prompt.Models;
 using DotNetNuke.Entities.Portals;
 using DotNetNuke.Entities.Users;
+using DotNetNuke.Services.Localization;
 
 namespace Dnn.PersonaBar.Library.Prompt
 {
     public abstract class ConsoleCommandBase : IConsoleCommand
     {
+        protected abstract string LocalResourceFile { get; }
         protected PortalSettings PortalSettings { get; private set; }
         protected UserInfo User { get; private set; }
         protected int PortalId { get; private set; }
         protected int TabId { get; private set; }
         protected string[] Args { get; private set; }
         protected Hashtable Flags { get; private set; }
+        #region Protected Methods
+        protected bool HasFlag(string flagName)
+        {
+            flagName = NormalizeFlagName(flagName);
+            return Flags.ContainsKey(flagName);
+        }
 
+        protected bool IsFlag(object input)
+        {
+            var inputVal = Convert.ToString(input);
+            return !string.IsNullOrEmpty(inputVal) && inputVal.StartsWith("--");
+        }
+
+        protected string LocalizeString(string key)
+        {
+            return Localization.GetString(key, LocalResourceFile);
+        }
+        protected void AddMessage(string message)
+        {
+            ValidationMessage += message;
+        }
+        protected virtual T GetFlagValue<T>(string flag, string fieldName, T defaultVal, bool required = false,
+            bool checkmain = false, bool checkpositive = false)
+        {
+            const string resourceFile = "~/DesktopModules/admin/Dnn.PersonaBar/App_LocalResources/SharedResources.resx";
+            dynamic value = null;
+            try
+            {
+                if (HasFlag(flag))
+                {
+                    value = Flag<T>(flag, defaultVal);
+                }
+                else
+                {
+                    if (checkmain && Args.Length >= 2 && !IsFlag(Args[1]))
+                    {
+                        var tc = TypeDescriptor.GetConverter(typeof(T));
+                        value = tc.ConvertFrom(Args[1]);
+                    }
+                    else if (!required)
+                    {
+                        value = defaultVal;
+                    }
+                    else
+                    {
+                        ValidationMessage += Localization.GetString(
+                            checkmain ? "Promp_MainFlagIsRequired" : "Prompt_FlagIsRequired",
+                            resourceFile)?.Replace("[0]", fieldName).Replace("[1]", flag);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                ValidationMessage +=
+                    Localization.GetString("Prompt_InvalidType", resourceFile)?
+                        .Replace("[0]", fieldName)
+                        .Replace("[1]", GetTypeName(typeof(T)));
+            }
+            if (checkpositive &&
+                (typeof(T) == typeof(int) || typeof(T) == typeof(long) || typeof(T) == typeof(int?) ||
+                 typeof(T) == typeof(long?)) && value != null && Convert.ToInt32(value) <= 0)
+            {
+                ValidationMessage += Localization.GetString("Promp_PositiveValueRequired", resourceFile)?.Replace("[0]", fieldName);
+            }
+            return value ?? defaultVal;
+        }
+        #endregion
+
+        #region Public Methods
+        public virtual void Init(string[] args, PortalSettings portalSettings, UserInfo userInfo, int activeTabId)
+        {
+            Args = args;
+            PortalSettings = portalSettings;
+            User = userInfo;
+            PortalId = portalSettings.PortalId;
+            TabId = activeTabId;
+            ValidationMessage = "";
+            ParseFlags();
+        }
+
+        public abstract ConsoleResultModel Run();
+
+        public virtual bool IsValid()
+        {
+            return string.IsNullOrEmpty(ValidationMessage);
+        }
+        #endregion
+
+        #region Private Methods
         private void ParseFlags()
         {
             Flags = new Hashtable();
@@ -47,27 +139,37 @@ namespace Dnn.PersonaBar.Library.Prompt
             }
         }
 
-        protected string Flag(string flagName, string defValue = "")
+        private object Flag<T>(string flagName, T defValue)
         {
             flagName = NormalizeFlagName(flagName);
             if (!Flags.ContainsKey(flagName)) return defValue;
             var retVal = Flags[flagName];
             if (retVal == null || (string)retVal == "")
                 return defValue;
-            return (string)retVal;
+            var tc = TypeDescriptor.GetConverter(typeof(T));
+            return tc.ConvertFrom(retVal);
         }
 
-        protected bool HasFlag(string flagName)
+       
+        #endregion
+
+        #region Helper Methods
+        private static string GetTypeName(Type type)
         {
-            flagName = NormalizeFlagName(flagName);
-            return Flags.ContainsKey(flagName);
+            if (type.FullName.ToLowerInvariant().Contains("int"))
+            {
+                return "Integer";
+            }
+            if (type.FullName.ToLowerInvariant().Contains("double"))
+            {
+                return "Double";
+            }
+            if (type.FullName.ToLowerInvariant().Contains("bool"))
+            {
+                return "Boolean";
+            }
+            return "";
         }
-
-        protected bool IsFlag(string input)
-        {
-            return !string.IsNullOrEmpty(input) && input.StartsWith("--");
-        }
-
         private static string NormalizeFlagName(string flagName)
         {
             if (flagName == null)
@@ -76,25 +178,8 @@ namespace Dnn.PersonaBar.Library.Prompt
                 flagName = flagName.Substring(2);
             return flagName.ToLower().Trim();
         }
+        #endregion
 
-        public virtual void Init(string[] args, PortalSettings portalSettings, UserInfo userInfo, int activeTabId)
-        {
-            Args = args;
-            PortalSettings = portalSettings;
-            User = userInfo;
-            PortalId = portalSettings.PortalId;
-            TabId = activeTabId;
-            ValidationMessage = "";
-            ParseFlags();
-        }
-
-        public abstract ConsoleResultModel Run();
-
-        public virtual bool IsValid()
-        {
-            return string.IsNullOrEmpty(ValidationMessage);
-        }
-
-        public virtual string ValidationMessage { get; protected set; }
+        public string ValidationMessage { get; private set; }
     }
 }
