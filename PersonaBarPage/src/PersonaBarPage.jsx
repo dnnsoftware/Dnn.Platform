207cff88438482c29473e93e757270079f533466
Kan Ma <kan.ma@dnnsoftware.com>
2017-08-11 15:23:19 -0700

    DNN-10077: added additional property for full width

--- PersonaBarPage/src/PersonaBarPage.jsx
+++ PersonaBarPage/src/PersonaBarPage.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./style.less";
 
-const getClassName = function (className, isOpen) {
-    return "dnn-persona-bar-page" + (isOpen ? " show " : " ") + className;
+const getClassName = function (className, isOpen, fullWidth) {
+    return "dnn-persona-bar-page" + (isOpen ? " show " : " ") + (fullWidth ? " full-width " : " ") + className;
 };
 
-const PersonaBarPage = ({className, isOpen, children}) => (
-    <div className={getClassName(className, isOpen)}>
+const PersonaBarPage = ({className, isOpen, children, fullWidth}) => (
+    <div className={getClassName(className, isOpen, fullWidth)}>
         {children}
     </div> 
 );
@@ -14,7 +14,12 @@ const PersonaBarPage = ({className, isOpen, children}) => (
 PersonaBarPage.propTypes = {
     isOpen: React.PropTypes.bool.isRequired, 
     className: React.PropTypes.string,
-    children: React.PropTypes.node
+    children: React.PropTypes.node,
+    fullWidth: React.PropTypes.bool
+};
+
+PersonaBarPage.defaultProps = {
+    fullWidth: false
 };
 
 export default PersonaBarPage;
