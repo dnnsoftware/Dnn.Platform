227eda74d64c399b0daab3607eda99ee7f9941b0
George Alatrash <george.alatrash@dnnsoftware.com>
2017-08-16 17:33:57 -0700

    NOJIRA: Added CombineSymbols.ps1 script.

---
+++ Build/BuildScripts/CombineSymbols.ps1
@@ -0,0 +1,38 @@
+Param(
+    [parameter(Mandatory=$true)][string]$checkoutDir,
+    [parameter(Mandatory=$false)][string]$symbolsDir = "Dnn.Symbols"
+	)
+
+$baseFolder   = ([System.IO.FileInfo]"$checkoutDir\$symbolsDir").FullName
+$binFolder    = ([System.IO.FileInfo]"$baseFolder\bin").FullName
+$resourcePath = ([System.IO.FileInfo]"$baseFolder\Resources.zip").FullName
+
+if (Test-Path "$binFolder")
+{
+    Remove-Item "$binFolder" -Recurse
+}
+    
+if (Test-Path "$resourcePath")
+{
+    Remove-Item "$resourcePath"
+}
+    
+$artifactsPath = ([System.IO.FileInfo]"$checkoutDir\Artifacts\").FullName
+if (!(Test-Path $artifactsPath -PathType Container))
+{
+    md $artifactsPath
+}
+    
+# extract all resources files to bin folder
+Get-ChildItem Resources.zip -Recurse | Foreach-Object {
+    Expand-Archive -LiteralPath "$($_.FullName)" -DestinationPath "$baseFolder"
+}
+
+# compress all files in bin folder
+Compress-Archive -Path "$binFolder" -CompressionLevel Optimal -DestinationPath "$resourcePath"
+
+Get-ChildItem *Symbols.zip |  Foreach-Object {
+    # add teh resources file back to the symbols
+    Compress-Archive -LiteralPath "$resourcePath" -Update -CompressionLevel Optimal -DestinationPath "$($_.FullName)"
+    Copy-Item "$($_.FullName)" "$artifactsPath"
+}
