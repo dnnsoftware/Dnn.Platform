862ce9d02ea976d7c98af723d350d5998b75a4f9
George Alatrash <george.alatrash@dnnsoftware.com>
2017-08-17 10:56:31 -0700

    AUTO-563: Added CombineSymbols.ps1 script.

--- Build/BuildScripts/CombineSymbols.ps1
+++ Build/BuildScripts/CombineSymbols.ps1
@@ -7,32 +7,56 @@ $baseFolder   = ([System.IO.FileInfo]"$checkoutDir\$symbolsDir").FullName
 $binFolder    = ([System.IO.FileInfo]"$baseFolder\bin").FullName
 $resourcePath = ([System.IO.FileInfo]"$baseFolder\Resources.zip").FullName
 
-if (Test-Path "$binFolder")
-{
-    Remove-Item "$binFolder" -Recurse
-}
-    
-if (Test-Path "$resourcePath")
-{
-    Remove-Item "$resourcePath"
-}
-    
-$artifactsPath = ([System.IO.FileInfo]"$checkoutDir\Artifacts\").FullName
-if (!(Test-Path $artifactsPath -PathType Container))
-{
-    md $artifactsPath
-}
-    
-# extract all resources files to bin folder
-Get-ChildItem Resources.zip -Recurse | Foreach-Object {
-    Expand-Archive -Force -LiteralPath "$($_.FullName)" -DestinationPath "$baseFolder"
-}
+Write-Host "This script will Combine symbols under $baseFolder"
 
-# compress all files in bin folder
-Compress-Archive -Force -Path "$binFolder" -CompressionLevel Optimal -DestinationPath "$resourcePath"
+Push-Location
+Set-Location $baseFolder
+ 
+try
+{
+    if (Test-Path "$binFolder")
+    {
+        Write-Host "Deleting $binFolder"
+        Remove-Item "$binFolder" -Recurse
+    }
 
-Get-ChildItem *Symbols.zip |  Foreach-Object {
-    # add teh resources file back to the symbols
-    Compress-Archive -Force -LiteralPath "$resourcePath" -Update -CompressionLevel Optimal -DestinationPath "$($_.FullName)"
-    Copy-Item "$($_.FullName)" "$artifactsPath"
+    if (Test-Path "$resourcePath")
+    {
+        Write-Host "Deleting $resourcePath"
+        Remove-Item "$resourcePath"
+    }
+
+    $artifactsPath = ([System.IO.FileInfo]"$checkoutDir\Artifacts\").FullName
+    if (!(Test-Path $artifactsPath -PathType Container))
+    {
+        Write-Host "Creating $artifactsPath"
+        md $artifactsPath
+    }
+
+    # extract all resources files to bin folder
+    Get-ChildItem "Resources.zip" -Recurse | Foreach-Object {
+        Write-Host "Extrachting $($_.FullName)"
+        Expand-Archive -Force -LiteralPath "$($_.FullName)" -DestinationPath "$baseFolder"
+    }
+
+    if (Test-Path "$binFolder")
+    {
+        # compress all files in bin folder
+        Write-Host "Compressing content of $binFolder"
+        Compress-Archive -Force -Path "$binFolder" -CompressionLevel Optimal -DestinationPath "$resourcePath"
+
+        $symbolsZip = (Get-ChildItem "*Symbols.zip").FullName
+        Write-Host "Adding $resourcePath to $symbolsZip"
+        Compress-Archive -LiteralPath "$resourcePath" -Update -CompressionLevel Optimal -DestinationPath "$symbolsZip"
+        Write-Host "Copying $symbolsZip to $artifactsPath"
+        Copy-Item "$symbolsZip" "$artifactsPath"
+    }
+    else
+    {
+       Write-Host "BIN folder doesn't exist: $binFolder"
+    }
+}
+finally
+{
+    Pop-Location
 }
