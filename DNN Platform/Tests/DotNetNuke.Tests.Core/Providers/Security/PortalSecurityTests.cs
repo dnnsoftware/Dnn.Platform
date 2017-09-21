8e1c85a4418b188cafccc1682e2c5e5ee004c77e
Ali Azizoğlu <azizoglua@hotmail.com>
2017-09-21 15:17:40 +0300

    revert from gitignore

---
+++ DNN Platform/Tests/DotNetNuke.Tests.Core/Providers/Security/PortalSecurityTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetNuke.Tests.Core.Providers.Security
+{
+    public class PortalSecurityTests
+    {
+        //sending and getting mathces with script tag still contain or no
+
+    }
+}
