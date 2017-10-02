4046cf2fb60ec1ef328f5f9755b34b8c1ca9ff29
emac80 <emac80@gmail.com>
2017-10-02 18:29:52 +0200

    SCM-539 Implementation of arrows keys events for TAGS selection dialog

--- Tags/src/Suggestions.jsx
+++ Tags/src/Suggestions.jsx
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from "react";
+import React, {Component, PropTypes} from "react";
 import {Scrollbars} from "react-custom-scrollbars";
 
 const style = {
@@ -10,42 +10,67 @@ const style = {
 };
 
 export default class Suggestions extends Component {
+
+    onComponent
+
     onSelectSuggestion(tag) {
-        if (typeof (this.props.onSelectSuggestion) === "function") {
-            this.props.onSelectSuggestion(tag);    
-        }
+        this.props.onSelectSuggestion(tag);
     }
 
     onScrollUpdate(scroll) {
         if (typeof (this.props.onScrollUpdate) === "function") {
-            this.props.onScrollUpdate(scroll);    
+            this.props.onScrollUpdate(scroll);
         }
     }
 
     getSuggestions() {
-        return this.props.suggestions.map((suggestion, index) => {
-            return <div className="suggestion" key={index} onClick={this.onSelectSuggestion.bind(this, suggestion.value)}>{suggestion.value}</div>;
+        let suggestions = this.props.suggestions.map((suggestion, index) => {
+            let className = this.props.selectedIndex > -1 && index === this.props.selectedIndex ? 'selected' : '';
+            return <div className={`suggestion ${className}`} key={index}
+                        onClick={this.onSelectSuggestion.bind(this, suggestion.value)}>{suggestion.value}</div>;
         });
+        return suggestions;
     }
 
     render() {
         const suggestions = this.getSuggestions();
 
+        const selectedIndex = this.props.selectedIndex;
         return (<div className="suggestions">
-                <Scrollbars style={style.list}
+            <Scrollbars ref={(sb) => {
+                if(sb) {
+                    const items = document.getElementsByClassName('suggestion');
+                    if(items) {
+                        const viewHeight = sb.getClientHeight();
+                        const h = items[0].clientHeight;
+                        const selectedItemScroll = h * selectedIndex;
+                        const currentScroll = sb.getScrollTop();
+                        setTimeout(() => {
+                            if(selectedItemScroll < currentScroll) {
+                                sb.scrollTop(currentScroll - viewHeight);
+                            } else if (selectedItemScroll + h > (currentScroll + viewHeight )) {
+                                sb.scrollTop(selectedItemScroll + h + currentScroll + viewHeight);
+                            }
+                        });
+                    }
+                }
+            }}
+                        style={style.list}
                         onUpdate={this.onScrollUpdate.bind(this)}>
-                    {suggestions}
-                </Scrollbars>
-            </div>);
+                {suggestions}
+            </Scrollbars>
+        </div>);
     }
 }
 
 Suggestions.propTypes = {
     onSelectSuggestion: PropTypes.func.isRequired,
     suggestions: PropTypes.array,
-    onScrollUpdate: PropTypes.func
+    onScrollUpdate: PropTypes.func,
+    selectedIndex: PropTypes.number.isRequired
 };
 
 Suggestions.defaultProps = {
-    suggestions: []
+    suggestions: [],
+    selectedIndex:-1
 };
