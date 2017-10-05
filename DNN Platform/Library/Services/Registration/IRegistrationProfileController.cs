e199fdfd7925e14969355413ea35e9b10368d658
Mohtshum <mohtisham.zubair@crossover.com>
2017-10-05 23:37:28 +0500

    DNN-10299:  SECURITY: Admin Security settings issues - Remove from DotNetNuke.Web to Library project as that to be used in validation for CONTENT-7615 - Put in Registration folder under Library.Sesrvices need feedback on this

---
+++ DNN Platform/Library/Services/Registration/IRegistrationProfileController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DotNetNuke.Services.Registration
+{
+    public interface IRegistrationProfileController
+    {
+        IEnumerable<string> Search(int portalId, string searchValue);
+    }
+}
---
+++ DNN Platform/Library/Services/Registration/RegistrationProfileController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using DotNetNuke.Framework;
+using DotNetNuke.Common.Lists;
+using DotNetNuke.Entities.Profile;
+using System.Linq;
+
+namespace DotNetNuke.Services.Registration
+{
+    public class RegistrationProfileController : ServiceLocator<IRegistrationProfileController, RegistrationProfileController>, IRegistrationProfileController
+    {
+        public IEnumerable<string> Search(int portalId, string searchTerm)
+        {
+            var controller = new ListController();
+
+            ListEntryInfo imageType = controller.GetListEntryInfo("DataType", "Image");
+
+            List<string> results = new List<string>();
+            foreach (var definition in ProfileController.GetPropertyDefinitionsByPortal(portalId)
+                                        .Cast<ProfilePropertyDefinition>()
+                                        .Where(definition => definition.DataType != imageType.EntryID))
+            {
+                AddProperty(results, definition.PropertyName, searchTerm);
+            }
+
+            AddProperty(results, "Email", searchTerm);
+            AddProperty(results, "DisplayName", searchTerm);
+            AddProperty(results, "Username", searchTerm);
+            AddProperty(results, "Password", searchTerm);
+            AddProperty(results, "PasswordConfirm", searchTerm);
+            AddProperty(results, "PasswordQuestion", searchTerm);
+            AddProperty(results, "PasswordAnswer", searchTerm);
+
+            return results;            
+        }
+
+        private void AddProperty(List<string> results, string field, string searchTerm)
+        {         
+            if (field.ToLowerInvariant().Contains(searchTerm.ToLowerInvariant().Trim()))
+            {
+                results.Add(field);
+            }
+        }
+
+        protected override Func<IRegistrationProfileController> GetFactory()
+        {
+            return () => new RegistrationProfileController();
+        }
+    }
+}
