425515b6e0d02cce4be7204b6c68128b6ddb15cb
emac80 <emac80@gmail.com>
2017-10-18 11:41:07 +0200

    SCM-762 Review of Babel, Sourcemap, Eslint, Dependencies, removed Webpack dependency for DayPicker common component

--- DayPicker/src/DayPicker.jsx
+++ DayPicker/src/DayPicker.jsx
@@ -1,12 +1,9 @@
-import React, {Component, PropTypes} from "react";
-import Picker, { WeekdayPropTypes, DateUtils } from "react-day-picker";
+import React from "react";
+import Picker from "react-day-picker";
 import "./style.less";
 
-
 const DayPicker = () => {
-    return(<div className="dnn-day-picker"> <Picker /></div>)
+    return (<div className="dnn-day-picker"> <Picker /></div>);
 };
 
-
 export default DayPicker;
-
