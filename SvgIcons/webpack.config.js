d566e4cded98878c0b9b8672641461dce850d915
Emanuele Colonnelli <emanuele.colonnelli@mean2.me>
2017-10-23 15:12:13 +0200

    SCM-762 Review SvgIcons settings. Webpack reintroduced to optimize SVG files management.

---
+++ SvgIcons/webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    entry: "./src/SvgIcons.jsx",
+    output: {
+        path: "./lib/",
+        filename: "SvgIcons.js",
+        libraryTarget: "umd",
+        library: "SvgIcons"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(svg)$/, exclude: /node_modules/,
+                loader: "raw-loader"
+            },
+            { 
+                test: /\.(js|jsx)$/, exclude: /node_modules/,
+                loader: "babel-loader",
+                query: {
+                    presets: ["react", "es2015"]
+                } 
+            }
+        ],
+        preLoaders: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader"}
+        ]
+    },
+    externals: {
+        "react": "react"
+    },
+    resolve: {
+        extensions: ["", ".js", ".json", ".jsx"] 
+    }
+};
