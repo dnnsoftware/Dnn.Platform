7abe2f730794009924a7b2fdeaea7adcc505ffb1
Ricardo Cavalcante <ricardo.cavalcante@crossover.com>
2017-11-05 02:15:40 -0200

    Alter webpack to new packaging system on dependencies

--- src/Modules/UI/Dnn.PersonaBar.UI/admin/personaBar/Bundle.Web/webpack.config.js
+++ src/Modules/UI/Dnn.PersonaBar.UI/admin/personaBar/Bundle.Web/webpack.config.js
@@ -21,9 +21,17 @@ module.exports = {
 
     module: {
         loaders: [
-            { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
+            { test: /\.(js|jsx)$/ ,exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
             { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
-            { test: /\.(ttf|woff)$/, loader: "url-loader?limit=8192" }
+            { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
+            
+            { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            
+            { test: /\.(gif|png)$/, loader: "url-loader?mimetype=image/png" }
+        
+
+
         ],
 
         preLoaders: [
