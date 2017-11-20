c9f880e0ef9bc60a974459577ea2d54607a9f0d4
Kan Ma <kan.ma@dnnsoftware.com>
2017-11-20 14:16:15 -0800

    DNN-10215: update to allow comma separated emails

--- src/Modules/Manage/Dnn.PersonaBar.AdminLogs/AdminLogs.Web/src/components/AdminLog/EmailPanel/index.jsx
+++ src/Modules/Manage/Dnn.PersonaBar.AdminLogs/AdminLogs.Web/src/components/AdminLog/EmailPanel/index.jsx
@@ -74,7 +74,7 @@ class EmailPanel extends Component {
 
     validateEmail(value) {
         const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(value);
+        return value.split(",").every(v => re.test(v));
     }
 
     onEmailValueChange(key, event) {
