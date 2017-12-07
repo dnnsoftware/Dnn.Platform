70328d61f87695a2738b7a01851c16951815a39b
Kan Ma <kan.ma@dnnsoftware.com>
2017-12-07 10:07:20 -0800

    DNN-10547: updated code per review

--- DNN Platform/Library/Services/Mail/Mail.cs
+++ DNN Platform/Library/Services/Mail/Mail.cs
@@ -332,15 +332,15 @@ namespace DotNetNuke.Services.Mail
             {
                 subject = Localize.GetSystemMessage(locale, settings, subject, user, Localize.GlobalResourceFile, custom, "", settings.AdministratorId);
                 body = Localize.GetSystemMessage(locale, settings, body, user, Localize.GlobalResourceFile, custom, "", settings.AdministratorId);
+
+                var fromUser = (UserController.GetUserByEmail(settings.PortalId, settings.Email) != null) ?
+                    String.Format("{0} < {1} >", UserController.GetUserByEmail(settings.PortalId, settings.Email).DisplayName, settings.Email) : settings.Email;
+                SendEmail(fromUser, UserController.GetUserById(settings.PortalId, toUser).Email, subject, body);
             }
             catch (Exception exc)
             {
                 Exceptions.Exceptions.LogException(exc);
             }
-
-            var fromUser = (UserController.GetUserByEmail(settings.PortalId, settings.Email)!=null)?
-                String.Format("{0} < {1} >", UserController.GetUserByEmail(settings.PortalId, settings.Email).DisplayName, settings.Email) : settings.Email;
-            SendEmail(fromUser, UserController.GetUserById(settings.PortalId, toUser).Email, subject, body);
 
             return Null.NullString;
         }
