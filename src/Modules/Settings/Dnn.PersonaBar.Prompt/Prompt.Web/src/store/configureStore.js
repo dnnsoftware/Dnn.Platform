ffcd6a080457e0999fbf13acb066014f9d250a3c
Emanuele Colonnelli <emanuele.colonnelli@mean2.me>
2017-12-28 09:46:06 +0100

    DNN-10634 Fixed

--- src/Modules/Settings/Dnn.PersonaBar.Prompt/Prompt.Web/src/store/configureStore.js
+++ src/Modules/Settings/Dnn.PersonaBar.Prompt/Prompt.Web/src/store/configureStore.js
@@ -3,15 +3,18 @@ import thunkMiddleware from "redux-thunk";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import reducers from "../reducers/promptReducers";
 import DevTools from "../containers/DevTools";
+import { IS_DEV} from "../globals/promptInit";
 
 export default function configureStore(initialState) {
-    const store = createStore(
-        reducers,
-        initialState,
-        compose(
-            applyMiddleware(thunkMiddleware, reduxImmutableStateInvariant()),    // TODO: apply only for development
-            DevTools.instrument()
-        )
-    );
-    return store;
+    if (IS_DEV) {
+        return createStore(
+            reducers,
+            initialState,
+            compose(applyMiddleware(thunkMiddleware, reduxImmutableStateInvariant()), DevTools.instrument()));
+    } else {
+        return createStore(
+            reducers,
+            initialState,
+            compose(applyMiddleware(thunkMiddleware), DevTools.instrument()));
+    }
 }
