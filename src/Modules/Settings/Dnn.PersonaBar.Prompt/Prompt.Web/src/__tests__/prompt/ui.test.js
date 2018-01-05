44acdfa6e1459d4463af6489a0cec9d068d1cde8
Emanuele Colonnelli <emanuele.colonnelli@mean2.me>
2018-01-05 11:15:22 +0100

    DNN-10618 Unit tests covering Redux reducers behaviors and component renderings. Snapshots verified and included.

--- src/Modules/Settings/Dnn.PersonaBar.Prompt/Prompt.Web/src/__tests__/prompt/ui.test.js
+++ src/Modules/Settings/Dnn.PersonaBar.Prompt/Prompt.Web/src/__tests__/prompt/ui.test.js
@@ -1,9 +1,18 @@
+import Command from "../../components/Command";
+
+jest.mock("../../components/Prompt.less", () => jest.fn());
+jest.mock("../../utils/helpers");
+jest.mock("../../localization/Localization");
+
 import React from "react";
-import Enzyme, {shallow, render} from "enzyme";
+import Enzyme, {mount, shallow, render} from "enzyme";
 import {Output} from "../../components/Output";
 import reducer from "../../reducers/promptReducers";
 import types from "../../constants/actionTypes";
 import Adapter from "enzyme-adapter-react-15";
+import DataTable from "../../components/DataTable";
+import {formatLabel} from "../../utils/helpers";
+import Help from "../../components/Help";
 
 const initialState = {
     commandList: null,
@@ -25,33 +34,7 @@ const initialState = {
     error: null
 };
 
-jest.mock("../../components/Prompt.less", () => jest.fn());
-jest.mock("../../utils/helpers");
-jest.mock("../../localization/Localization");
-
 Enzyme.configure({adapter: new Adapter()});
-
-const getProps = (state) => {
-    return {
-        output: state.output,
-        data: state.data,
-        paging: state.pagingInfo,
-        isHtml: state.isHtml,
-        reload: state.reload,
-        style: state.style,
-        fieldOrder: state.fieldOrder,
-        commandList: state.commandList,
-        isError: state.isError,
-        clearOutput: state.clearOutput,
-        isHelp: state.isHelp,
-        name: state.name,
-        description: state.description,
-        options: state.options,
-        resultHtml: state.resultHtml,
-        error: state.error,
-        nextPageCommand: state.nextPageCommand
-    };
-};
 
 describe("UI snapshots for state change", () => {
 
@@ -65,7 +48,7 @@ describe("UI snapshots for state change", () => {
         expect(out).toMatchSnapshot();
     });
 
-    it(`Should handle ${types.RETRIEVED_COMMAND_LIST}`, () => {
+    it(`Should render Output component simulating a ${types.RETRIEVED_COMMAND_LIST} action with mocked data`, () => {
 
         const commands = [
             {
@@ -90,24 +73,25 @@ describe("UI snapshots for state change", () => {
             }
         });
 
-        const out = render(<Output {...props} IsPaging={jest.fn()} busy={jest.fn()} scrollToBottom={jest.fn()} className="test"/>);
+        const out = render(<Output {...props} IsPaging={jest.fn()} busy={jest.fn()} scrollToBottom={jest.fn()}
+                                   className="test"/>);
 
         expect(out).toMatchSnapshot();
     });
 
-    it(`Should handle ${types.EXECUTED_COMMAND}, test data got from list-tasks command`, () => {
+    it(`Should render Output component simultaing a ${types.EXECUTED_COMMAND} action with mocked data`, () => {
         const commands = [
             {
-                ScheduleId:20,
-                FriendlyName:"Analytics Engage",
-                NextStart:"2018-01-03 18:25",
-                Enabled:true
+                ScheduleId: 20,
+                FriendlyName: "Analytics Engage",
+                NextStart: "2018-01-03 18:25",
+                Enabled: true
             },
             {
-                ScheduleId:22,
-                FriendlyName:"Content Personalization Data Clean",
-                NextStart:"2018-01-04 12:46",
-                Enabled:true
+                ScheduleId: 22,
+                FriendlyName: "Content Personalization Data Clean",
+                NextStart: "2018-01-04 12:46",
+                Enabled: true
             }
         ];
 
@@ -131,8 +115,117 @@ describe("UI snapshots for state change", () => {
             data
         });
 
-        const out = render(<Output {...props} IsPaging={jest.fn()} busy={jest.fn()} scrollToBottom={jest.fn()} className="test"/>);
+        const out = render(<Output {...props} IsPaging={jest.fn()} busy={jest.fn()} scrollToBottom={jest.fn()}
+                                   className="test"/>);
 
         expect(out).toMatchSnapshot();
+    });
+
+    it(`Should render DataTable according to mocked rows and columns`, () => {
+
+        const rows = [
+            {
+                Email: "host@change.me",
+                IsAuthorized: true,
+                IsDeleted: false,
+                IsLockedOut: false,
+                LastLogin: "2018-01-04",
+                UserId: 1,
+                Username: "host",
+                __Email: "get-user 'host@change.me'",
+                __UserId: "get-user 1",
+                __Username: "get-user 'host'"
+            }
+        ];
+
+        const columns = ["UserId", "Username", "Email", "LastLogin", "IsDeleted", "IsAuthorized", "IsLockedOut"];
+
+        const cssClass = "test-ui-class";
+
+        const wrapper = render(<DataTable rows={rows} columns={columns} cssClass={cssClass}/>);
+
+        expect(wrapper.find("th")).toHaveLength(7);
+        expect(wrapper.find("tr")).toHaveLength(2);
+        expect(wrapper.find("th").first().text()).toEqual(formatLabel(columns[0]));
+        expect(wrapper).toMatchSnapshot();
+
+    });
+
+    it("Should render Command component according to mocked commandsList", () => {
+
+        const commandsList = [
+            {
+                Category: "General Commands",
+                Description: "Clears the Prompt console. <code>cls</code> is a shortcut for <a href=\"#clear-screen\"><code>clear-screen</code></a>",
+                Key: "CLS",
+                Name: "cls",
+                Version: "1.5.0.0"
+            },
+            {
+                Category: "General Commands",
+                Description: "Lists all the commands.",
+                Key: "LIST-COMMANDS",
+                Name: "list-commands",
+                Version: "1.5.0.0"
+            },
+            {
+                Category: "General Commands",
+                Description: "Clears history of commands used in current session",
+                Key: "CLEAR-HISTORY",
+                Name: "clear-history",
+                Version: "1.5.0.0"
+            },
+            {
+                Category: "General Commands",
+                Description: "Sets the DNN View Mode. This has the same effect as clicking the appropriate options in the DNN Control Bar.",
+                Key: "SET-MODE",
+                Name: "set-mode",
+                Version: "1.5.0.0"
+            }
+        ];
+
+        const wrapper = render(<Command commandList={commandsList} IsPaging={jest.fn()}/>);
+
+        const commandsTableRows = wrapper.find("table > tbody > tr");
+
+        expect(commandsTableRows).toHaveLength(commandsList.length);
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    it("Should render Help component according to set properties", () => {
+
+        const props = {
+            IsPaging: jest.fn(),
+            style: "test-style",
+            isError: false,
+            error: "",
+            name: "test-name",
+            description: "test-description",
+            options: [
+                {
+                    Flag: "test-flag",
+                    Type: "test-type",
+                    Required: false,
+                    DefaultValue: "",
+                    Description: "test-description"
+                },
+                {
+                    Flag: "test-flag2",
+                    Type: "test-type2",
+                    Required: false,
+                    DefaultValue: "",
+                    Description: "test-description2"
+                }
+            ],
+            resultHtml: "<h1>TEST-HTML</h1>"
+        };
+
+        const wrapper = mount(<Help {...props}/>);
+        const heading = wrapper.find("section > h3");
+        const anchor = wrapper.find("section > a").find({name: "test-name"});
+
+        expect(heading.text()).toEqual("test-name");
+        expect(anchor).toHaveLength(1);
+        expect(wrapper.find(DataTable)).toHaveLength(1);
     });
 });
