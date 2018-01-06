9bc56a8a7a0ef88c00c571def5730ff227063526
Emanuele Colonnelli <emanuele.colonnelli@mean2.me>
2018-01-06 11:38:04 +0100

    DNN-10666 Improved webpack.config.js to support source mapping, required to properly test dnn-* components (and all external libraries)

--- src/Modules/UI/Dnn.PersonaBar.UI/admin/personaBar/Bundle.Web/webpack.config.js
+++ src/Modules/UI/Dnn.PersonaBar.UI/admin/personaBar/Bundle.Web/webpack.config.js
@@ -11,6 +11,11 @@ const languages = {
     // "nl": require("./localizations/nl.json")
 };
 
+const sourceMapLoader = { test: /\.js$/, loader: "source-map-loader", enforce: "pre" };
+const eslintLoader = { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader" };
+
+const preLoaders = isProduction === true ? [eslintLoader] : [eslintLoader, sourceMapLoader];
+
 module.exports = {
     entry: "./src/main.jsx",
     output: {
@@ -20,20 +25,13 @@ module.exports = {
     },
 
     module: {
-        loaders: [
-            { test: /\.(js|jsx)$/ , exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
-            { test: /\.(less|css)$/, loader: "style-loader!css-loader!less-loader" },
-            { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },     
-            { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
-            { test: /\.(gif|png)$/, loader: "url-loader?mimetype=image/png" }
-
-        ],
-
-        preLoaders: [
-            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: "eslint-loader" }
-        ]
+        loaders:[{ test: /\.(js|jsx)$/ , exclude: /node_modules/, loaders: ["react-hot-loader", "babel-loader"] },
+                { test: /\.(less|css)$/, loader: "style-loader!css-loader!less-loader" },
+                { test: /\.woff(2)?(\?v=[0-9].[0-9].[0-9])?$/, loader: "url-loader?mimetype=application/font-woff" },
+                { test: /\.(ttf|eot|svg)(\?v=[0-9].[0-9].[0-9])?$/, loader: "file-loader?name=[name].[ext]" },
+                { test: /\.(gif|png)$/, loader: "url-loader?mimetype=image/png" }],
+        preLoaders
     },
-
     resolve: {
         extensions: ["", ".js", ".json", ".jsx"]
     },
@@ -52,3 +50,8 @@ module.exports = {
             })
         ]
 };
+
+if(!isProduction) {
+    module.exports.devtool = "source-map";
+    module.exports.debug = true;
+}
