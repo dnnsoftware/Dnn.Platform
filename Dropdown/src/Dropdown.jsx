e41a20cf14cf660c31c092178ff64cb19046d9e7
Emanuele Colonnelli <emanuele.colonnelli@mean2.me>
2018-01-10 11:58:49 +0100

    SCM-960 Added test case for decode function, updated snapshots, updated obsolete dependencies

--- Dropdown/src/Dropdown.jsx
+++ Dropdown/src/Dropdown.jsx
@@ -1,4 +1,5 @@
-import React, {PropTypes, Component} from "react";
+import React, { Component} from "react";
+import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 import Collapse from "react-collapse";
 import Scrollbars from "react-custom-scrollbars";
