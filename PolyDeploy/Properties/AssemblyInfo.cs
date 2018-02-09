d21ce61c80ee9fdbff357a5c97a9b0b5c1a11d71
Adam Nierzad <adam.nierzad@cantarus.com>
2018-02-09 16:10:46 +0000

    Incremented versions where they were mismatched.

--- PolyDeploy/Properties/AssemblyInfo.cs
+++ PolyDeploy/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("00.05.00.*")]
+[assembly: AssemblyVersion("00.06.00.*")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
