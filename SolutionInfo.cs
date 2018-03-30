7c0fd4a6ada2da748d2def6db8aed9095aff2f53
emanuele.colonnelli <emanuele.colonnelli@crossover.com>
2018-03-30 12:36:27 +0700

    DNN-12099 code review

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -44,4 +44,4 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("9.2.0.367")]
-[assembly: AssemblyFileVersion("9.2.0.367")]
+[assembly: AssemblyFileVersion("9.2.0.0")]
