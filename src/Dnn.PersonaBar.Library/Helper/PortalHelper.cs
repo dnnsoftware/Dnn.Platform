38a8067adabd1f4ce6d6edffa12ffeeb40b6f055
Mohtshum <mohtisham.zubair@crossover.com>
2018-05-02 22:29:19 +0500

    DNN-20025: Make PurgePage class testable - Introduce a new class ContentVerifier : IContentVerifier, which takes IPortalGroupController and IPortalController as dependencies - Add static ContentVerifier field in PortalHelper - Make PortalHelper.IsContentExistsForRequestedPortal forward calls to ContentVerifier - Mark PortalHelper obsolete

--- src/Dnn.PersonaBar.Library/Helper/PortalHelper.cs
+++ src/Dnn.PersonaBar.Library/Helper/PortalHelper.cs
@@ -1,20 +1,21 @@
 ﻿using DotNetNuke.Entities.Portals;
+using System;
 using System.Linq;
 
 namespace Dnn.PersonaBar.Library.Helper
 {
     public class PortalHelper
     {
+        static readonly IContentVerifier contentVerifier = new ContentVerifier(PortalController.Instance, PortalGroupController.Instance);
+
+        [Obsolete("Deprecated in 9.2.1. Use IContentVerifier.IsContentExistsForRequestedPortal")]
         public static bool IsContentExistsForRequestedPortal(int contentPortalId, PortalSettings portalSettings, bool checkForSiteGroup = false)
-        {
-            var currentPortal = PortalController.Instance.GetCurrentPortalSettings();
-            return contentPortalId == portalSettings.PortalId 
-                || portalSettings == currentPortal 
-                || (checkForSiteGroup && IsRequestForSiteGroup(contentPortalId, portalSettings.PortalId));                
+        {        
+            return contentVerifier.IsContentExistsForRequestedPortal(contentPortalId, portalSettings, checkForSiteGroup);            
         }
 
-        private static bool IsRequestForSiteGroup(int portalId, int portalIdSiteGroup)
-        {
+        private bool IsRequestForSiteGroup(int portalId, int portalIdSiteGroup)
+        {            
             const int NO_SITE_GROUPID = -1;
             var isSiteGroupPage = false;
             var portal = PortalController.Instance.GetPortal(portalIdSiteGroup);
