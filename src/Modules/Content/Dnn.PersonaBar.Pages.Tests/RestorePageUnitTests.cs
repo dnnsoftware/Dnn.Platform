66ca60ebee44c6c9db9e6638f23022a9c3a4d470
Mohtshum <mohtisham.zubair@crossover.com>
2018-05-25 10:44:29 +0500

    DNN-20662: Make restore-page testable

---
+++ src/Modules/Content/Dnn.PersonaBar.Pages.Tests/RestorePageUnitTests.cs
@@ -0,0 +1,127 @@
+﻿using NUnit.Framework;
+using Moq;
+using Dnn.PersonaBar.Library.Prompt;
+using Dnn.PersonaBar.Recyclebin.Components;
+using Dnn.PersonaBar.Recyclebin.Components.Prompt.Commands;
+using DotNetNuke.Entities.Portals;
+using DotNetNuke.Entities.Tabs;
+using Dnn.PersonaBar.Library.Helper;
+using Dnn.PersonaBar.Library.Prompt.Models;
+
+namespace Dnn.PersonaBar.Pages.Tests
+{
+    [TestFixture]
+    public class RestorePageUnitTests
+    {
+        TabInfo _tab;
+        string _message;
+        PortalSettings _portalSettings;
+        IConsoleCommand _restorePage;
+
+        Mock<ITabController> _tabControllerMock;
+        Mock<IRecyclebinController> _recyclebinControllerMock;
+        Mock<IContentVerifier> _contentVerifierMock;
+        
+        int _tabId = 91;
+        int _testPortalId = 1;
+
+        [SetUp]
+        public void RunBeforeAnyTest()
+        {
+            _message = string.Empty;
+
+            _tab = new TabInfo();
+            _tab.TabID = _tabId;
+            _tab.PortalID = _testPortalId;
+
+            _portalSettings = new PortalSettings();
+            _portalSettings.PortalId = _testPortalId;
+
+            _tabControllerMock = new Mock<ITabController>();
+            _recyclebinControllerMock = new Mock<IRecyclebinController>();
+            _contentVerifierMock = new Mock<IContentVerifier>();
+        }
+
+        [Test]
+        public void Run_RestorePage_WithValidCommand_ShouldReturnSuccessResponse()
+        {
+            // Arrange            
+            _tabControllerMock.SetReturnsDefault(_tab);
+            _contentVerifierMock.SetReturnsDefault(true);
+            _recyclebinControllerMock.Setup(r => r.RestoreTab(_tab, out _message));
+
+            SetupCommand();
+
+            // Act
+            var result = _restorePage.Run();
+
+            // Assert
+            Assert.IsFalse(result.IsError);
+            Assert.AreEqual(1, result.Records);
+            Assert.IsFalse(result is ConsoleErrorResultModel);
+        }    
+
+        [Test]
+        public void Run_RestorePage_WithValidCommandForNonExistingTab_ShouldReturnErrorResponse()
+        {
+            // Arrange
+            _tab = null;
+
+            _tabControllerMock.SetReturnsDefault(_tab);
+
+            SetupCommand();
+
+            // Act
+            var result = _restorePage.Run();
+
+            // Assert
+            Assert.IsTrue(result.IsError);
+            Assert.IsTrue(result is ConsoleErrorResultModel);
+        }
+
+        [Test]
+        public void Run_RestorePage_WithValidCommandForRequestedPortalNotAllowed_ShouldReturnErrorResponse()
+        {
+            // Arrange
+            _tabControllerMock.SetReturnsDefault(_tab);
+            _contentVerifierMock.SetReturnsDefault(false);
+
+            SetupCommand();
+
+            // Act
+            var result = _restorePage.Run();
+
+            // Assert
+            Assert.IsTrue(result.IsError);
+            Assert.IsTrue(result is ConsoleErrorResultModel);
+        }
+
+        [Test]
+        public void Run_RestorePage_WithValidCommandForNotRestoreTab_ShouldReturnErrorResponse()
+        {
+            // Arrange
+            _message = "Tab not found";
+
+            _tabControllerMock.SetReturnsDefault(_tab);
+            _contentVerifierMock.SetReturnsDefault(true);
+            _recyclebinControllerMock.Setup(r => r.RestoreTab(_tab, out _message));
+
+            SetupCommand();
+
+            // Act
+            var result = _restorePage.Run();
+
+            // Assert
+            Assert.IsTrue(result.IsError);
+            Assert.IsTrue(result is ConsoleErrorResultModel);
+        }
+
+        private void SetupCommand()
+        {
+            _restorePage = new RestorePage(_tabControllerMock.Object, _recyclebinControllerMock.Object, _contentVerifierMock.Object);
+
+            var args = new[] { "restore-page", _tabId.ToString() };
+            _restorePage.Initialize(args, _portalSettings, null, 0);
+        }
+    }
+}
--- src/Modules/Content/Dnn.PersonaBar.Recyclebin/Components/Prompt/Commands/RestorePage.cs
+++ src/Modules/Content/Dnn.PersonaBar.Recyclebin/Components/Prompt/Commands/RestorePage.cs
@@ -21,24 +21,28 @@ namespace Dnn.PersonaBar.Recyclebin.Components.Prompt.Commands
 
         [FlagParameter("id", "Prompt_RestorePage_FlagId", "Integer")]
         private const string FlagId = "id";
+
+        private readonly ITabController _tabController;
         private readonly IContentVerifier _contentVerifier;
+        private readonly IRecyclebinController _recyclebinController;
 
         private int PageId { get; set; }
         private string PageName { get; set; }
         private int ParentId { get; set; }
 
-        public RestorePage() : this(new ContentVerifier())
+        public RestorePage() : this(TabController.Instance, RecyclebinController.Instance, new ContentVerifier())
         {
         }
 
-        public RestorePage(IContentVerifier contentVerifier)
+        public RestorePage(ITabController tabController, IRecyclebinController recyclebinController, IContentVerifier contentVerifier)
         {
+            this._tabController = tabController;
+            this._recyclebinController = recyclebinController;
             this._contentVerifier = contentVerifier;
         }
 
         public override void Init(string[] args, PortalSettings portalSettings, UserInfo userInfo, int activeTabId)
         {
-            
             PageId = GetFlagValue(FlagId, "Page Id", -1, false, true);
             ParentId = GetFlagValue(FlagParentId, "Parent Id", -1);
             PageName = GetFlagValue(FlagName, "Page Name", string.Empty);
@@ -55,17 +59,17 @@ namespace Dnn.PersonaBar.Recyclebin.Components.Prompt.Commands
 
             if (PageId > 0)
             {
-                tab = TabController.Instance.GetTab(PageId, PortalId);
+                tab = _tabController.GetTab(PageId, PortalId);
                 if (tab == null)
-                {                  
+                {
                     return new ConsoleErrorResultModel(message);
                 }
             }
             else if (!string.IsNullOrEmpty(PageName))
             {
-                tab = ParentId > 0 
-                    ? TabController.Instance.GetTabByName(PageName, PortalId, ParentId) 
-                    : TabController.Instance.GetTabByName(PageName, PortalId);
+                tab = ParentId > 0
+                    ? _tabController.GetTabByName(PageName, PortalId, ParentId)
+                    : _tabController.GetTabByName(PageName, PortalId);
 
                 if (tab == null)
                 {
@@ -83,8 +87,18 @@ namespace Dnn.PersonaBar.Recyclebin.Components.Prompt.Commands
                 return new ConsoleErrorResultModel(message);
             }
 
-            RecyclebinController.Instance.RestoreTab(tab, out message);
-            return string.IsNullOrEmpty(message) ? new ConsoleResultModel(string.Format(LocalizeString("Prompt_PageRestoredSuccessfully"), tab.TabID, tab.TabName)) { Records = 1 } : new ConsoleErrorResultModel(message);
+            _recyclebinController.RestoreTab(tab, out message);
+
+            return string.IsNullOrEmpty(message)
+                ? new ConsoleResultModel(
+                    string.Format(
+                        LocalizeString("Prompt_PageRestoredSuccessfully"),
+                        tab.TabID,
+                        tab.TabName
+                        )
+                    )
+                { Records = 1 }
+                : new ConsoleErrorResultModel(message);
         }
     }
 }
