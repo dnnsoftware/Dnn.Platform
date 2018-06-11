7226acc2f4a9d540d908e9db2931a8495fcd78e1
Mohtisham <mohtisham.zubair@crossover.com>
2018-06-11 11:05:59 +0500

    DNN-20873:  delete-user not working across portals and for cross-port… (#542)
    * DNN-20873:  delete-user not working across portals and for cross-portals api calls working regardless of API calls
    
    * DNN-20875: UnitTest added for Delete-User
    
    * DNN-208&3: Arrange namespace
    
    * DNN-20873: PortalId is getting changed by MasterId of SiteGroup, so to avoid any issue use local variable

--- src/Modules/Manage/Dnn.PersonaBar.Users/Components/Prompt/Commands/DeleteUser.cs
+++ src/Modules/Manage/Dnn.PersonaBar.Users/Components/Prompt/Commands/DeleteUser.cs
@@ -19,17 +19,19 @@ namespace Dnn.PersonaBar.Users.Components.Prompt.Commands
         private const string FlagNotify = "notify";
 
         private IUserValidator _userValidator;
+        private IUserControllerWrapper _userControllerWrapper;
 
         private int UserId { get; set; }
         private bool Notify { get; set; }
 
-        public DeleteUser() : this(new UserValidator())
+        public DeleteUser() : this(new UserValidator(), new UserControllerWrapper())
         {
         }
 
-        public DeleteUser(IUserValidator userValidator)
+        public DeleteUser(IUserValidator userValidator, IUserControllerWrapper userControllerWrapper)
         {
             this._userValidator = userValidator;
+            this._userControllerWrapper = userControllerWrapper;
         }
 
         public override void Init(string[] args, PortalSettings portalSettings, UserInfo userInfo, int activeTabId)
@@ -49,28 +51,27 @@ namespace Dnn.PersonaBar.Users.Components.Prompt.Commands
                 return errorResultModel;
             }
 
-            var userModels = new List<UserModel> { new UserModel(userInfo) };
+            var userModels = new List<UserModel> { new UserModel(userInfo) };            
 
             if (userInfo.IsDeleted)
             {
                 return new ConsoleErrorResultModel(LocalizeString("Prompt_UserAlreadyDeleted"));
             }
 
-            if (!UserController.DeleteUser(ref userInfo, Notify, false))
+            var validPortalId = userInfo.PortalID;
+
+            if (!_userControllerWrapper.DeleteUserAndClearCache(ref userInfo, Notify, false))
             {
                 return new ConsoleErrorResultModel(LocalizeString("Prompt_UserDeletionFailed"))
                 {
                     Data = userModels
                 };
             }
-            // We must clear User cache or else, when the user is 'removed' (so it can't be restored), you 
-            // will not be able to create a new user with the same username -- even though no user with that username
-            // exists.
-            DotNetNuke.Common.Utilities.DataCache.ClearUserCache(PortalId, userInfo.Username);
-            // attempt to retrieve the user from the dB
-            userInfo = UserController.GetUserById(PortalId, userInfo.UserID);
+
+            // attempt to retrieve the user from the dB 
+            userInfo = _userControllerWrapper.GetUserById(validPortalId, userInfo.UserID);
             userModels = new List<UserModel> { new UserModel(userInfo) };
-            return new ConsoleResultModel(LocalizeString("UserDeleted")) { Data = userModels, Records = userModels.Count };
+            return new ConsoleResultModel(LocalizeString("UserDeleted")) { Data = userModels, Records = userModels.Count };            
         }
     }
 }
