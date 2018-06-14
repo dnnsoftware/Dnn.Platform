ae0fc753eae016a2bd0a49627050efc1130995f8
iJungleboy <daniel.mettler@2sic.com>
2018-06-14 08:49:55 +0200

    fixes as requested in https://github.com/dnnsoftware/Dnn.Platform/pull/2118

--- DNN Platform/Library/Services/Zip/SharpZipLibRedirect.cs
+++ DNN Platform/Library/Services/Zip/SharpZipLibRedirect.cs
@@ -2,6 +2,16 @@
 using System.Linq;
 using System.Reflection;
 
+// DNN uses SharpZipLib as the zip system of choice.Because of this, it is bundled with DNN.
+//
+// This folder here contains a fix for a breaking upgrade in DNN 9.2 regarding the ZIP.
+// The class in this folder will be used by.net when an assembly is missing.
+// It will then check if .net was looking for SharpZipLib, and if necessary,
+// redirect it to the correct (new) assembly.
+//
+// Once this remapping has been completed, this code will not be used again until the next
+// restart of the DNN application. 
+
 namespace DotNetNuke.Services.Zip
 {
     /// <summary>
@@ -12,9 +22,9 @@ namespace DotNetNuke.Services.Zip
     /// final solution taken from https://raw.githubusercontent.com/2sic/2sxc/master/2sxc%20Dnn/Dnn920/SharpZipLibRedirect.cs
     /// read the readme.md in this folder for more info
     /// </remarks>
-    public class SharpZipLibRedirect
+    internal class SharpZipLibRedirect
     {
-        public static bool AlreadyRun { get; private set; }
+        internal static bool AlreadyRun { get; private set; }
 
         private const string OldName = "SharpZipLib";
         private const string NewName = "ICSharpCode.SharpZipLib";
@@ -23,9 +33,12 @@ namespace DotNetNuke.Services.Zip
         /// Registration call - should only be called once
         /// Has extra security to prevent it from running multiple times
         /// </summary>
-        public static void RegisterSharpZipLibRedirect()
+        internal static void RegisterSharpZipLibRedirect()
         {
-            if (AlreadyRun) return;
+            if (AlreadyRun)
+            {
+                return;
+            }
             // stop any further attempts to access this
             AlreadyRun = true;
 
@@ -37,14 +50,18 @@ namespace DotNetNuke.Services.Zip
             catch
             {
                 /* ignore */
+                // this is to ensure that if anything at all goes wrong when registering this, it won't break DNN.
             }
         }
 
         private static Assembly Handler(object sender, ResolveEventArgs args)
         {
             // only check for access to old SharpZipLib
-            if (!args.Name.StartsWith(OldName))
+            // requires startswith, because the full string is something like "SharpZipLib, Version=0.81.0.1407, Culture=neutral, PublicKeyToken=null"
+            if (!args.Name.StartsWith(OldName, StringComparison.OrdinalIgnoreCase))
+            {
                 return null;
+            }
 
             // If the original was not found, it should find the new one - otherwise it will be null
             var alreadyLoadedAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().Name == NewName);
