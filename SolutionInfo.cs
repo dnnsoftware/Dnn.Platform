84a7aa27dbcb845970620675b3652cb8ddae5eb3
emanuele.colonnelli <emanuele.colonnelli@crossover.com>
2018-08-07 14:09:48 +0700

    Code review

--- SolutionInfo.cs
+++ SolutionInfo.cs
@@ -43,5 +43,5 @@ using System.Reflection;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("9.2.2.14")]
-[assembly: AssemblyFileVersion("9.2.2.14")]
+[assembly: AssemblyVersion("9.2.2.0")]
+[assembly: AssemblyFileVersion("9.2.2.0")]
