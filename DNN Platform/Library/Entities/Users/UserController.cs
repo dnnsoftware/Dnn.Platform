2eb4e4b5b2d3dce648dd1d2f888e624adcf55b42
Mohtshum <mohtisham.zubair@crossover.com>
2018-09-09 16:30:20 +0500

    Changes for optimization to clear User cache

--- DNN Platform/Library/Entities/Users/UserController.cs
+++ DNN Platform/Library/Entities/Users/UserController.cs
@@ -1045,8 +1045,8 @@ namespace DotNetNuke.Entities.Users
                     //send email notification to portal administrator that the user was removed from the portal
                     SendDeleteEmailNotifications(user, portalSettings);
                 }
-                
-                CachingProvider.Instance().Remove(string.Format(DataCache.PortalUserCountCacheKey, user.PortalID));
+                                
+                DataCache.ClearPortalUserCountCache(user.PortalID);
                 DataCache.ClearUserCache(user.PortalID, user.Username);
 
                 //also clear current portal's cache if the user is a host user
