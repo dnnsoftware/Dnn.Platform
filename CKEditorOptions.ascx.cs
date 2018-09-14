1584b4265d799828df1b4afa1b148451ac2bfab4
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2018-09-14 11:00:47 +0200

    remove dupe method

--- CKEditorOptions.ascx.cs
+++ CKEditorOptions.ascx.cs
@@ -3483,29 +3483,6 @@ namespace DNNConnect.CKEditorProvider
         }
 
         /// <summary>
-        /// Adds the other toolbar roles.
-        /// </summary>
-        /// <param name="listToolbarRoles">The list toolbar roles.</param>
-        /// <param name="roleName">Name of the role.</param>
-        /// <param name="value">The value.</param>
-        private void AddOtherToolbarRoles(List<ToolbarRoles> listToolbarRoles, string roleName, string value)
-        {
-            if (IsAllInstances)
-            {
-                listToolbarRoles.AddRange(
-                    from PortalInfo portal in PortalController.Instance.GetPortals()
-                    select RoleController.Instance.GetRoleByName(portal.PortalID, roleName)
-                    into objRole
-                    select new ToolbarRoles { RoleId = objRole.RoleID, Toolbar = value });
-            }
-            else
-            {
-                var objRole = RoleController.Instance.GetRoleByName(_portalSettings.PortalId, roleName);
-                listToolbarRoles.Add(new ToolbarRoles { RoleId = objRole.RoleID, Toolbar = value });
-            }
-        }
-
-        /// <summary>
         /// Gets default upload size limits for each existent role
         /// </summary>
         /// <param name="portalRoles"></param>
