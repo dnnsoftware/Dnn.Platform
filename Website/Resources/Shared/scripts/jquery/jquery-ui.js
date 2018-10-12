ee3182d246e0ffeceed0f83b4841a9c73d821eb5
Mikhail Bigun <mikhail.bigun@ignitetech.com>
2018-10-12 10:49:22 +0300

    Revert "DNN-24620 Member Directory: Basic Search: GetSuggestions / AutoComplete adding text to page"
    This reverts commit 47070ca2a53e8fd0aff0c98352f3175a21d02053.

--- Website/Resources/Shared/scripts/jquery/jquery-ui.js
+++ Website/Resources/Shared/scripts/jquery/jquery-ui.js
@@ -8283,13 +8283,12 @@ $.widget( "ui.autocomplete", {
 			}
 		} );
 
-		var liveRegionContainer = this.element.closest("div.dnnForm") || this.document[0].body;
 		this.liveRegion = $( "<div>", {
 			role: "status",
 			"aria-live": "assertive",
 			"aria-relevant": "additions"
 		} )
-			.appendTo( liveRegionContainer );
+			.appendTo( this.document[ 0 ].body );
 
 		this._addClass( this.liveRegion, null, "ui-helper-hidden-accessible" );
 
