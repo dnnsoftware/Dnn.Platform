630043d7bb603de742fc868ddcd3610b2aa415db
Daniel Valadas <info@danielvaladas.com>
2018-10-18 11:53:14 -0400

    DOCS: API docs on Application and Collections (#2404)
    * API docs on Application and Collections
    
    * Update DNN Platform/Library/Collections/CollectionExtensions.cs
    
    * Update DNN Platform/Library/Collections/CollectionExtensions.cs
    
    * Update DNN Platform/Library/Collections/CollectionExtensions.cs
    
    * Update DNN Platform/Library/Collections/PagedList.cs
    
    * Update DNN Platform/Library/Collections/PagedList.cs
    
    * Update DNN Platform/Library/Collections/PagedList.cs
    
    * Update DNN Platform/Library/Collections/PagedSelector.cs
    
    * Update DNN Platform/Library/Collections/PagingExtensions.cs

--- DNN Platform/Library/Collections/CollectionExtensions.cs
+++ DNN Platform/Library/Collections/CollectionExtensions.cs
@@ -34,12 +34,21 @@ using DotNetNuke.Instrumentation;
 
 namespace DotNetNuke.Collections
 {
+    /// <summary>
+    /// Provides a collection of useful extensions to collections
+    /// </summary>
     public static class CollectionExtensions
     {
         private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(CollectionExtensions));
+
         /// <summary>
-        /// This method is extracted from the FriendlyUrlSettings class
+        /// Converts a string with multiple key-value pairs into a Dictionary, if there are duplicated keys in your string 
+        /// only the first found key is kept since keys must be unique in a Dictionary
         /// </summary>
+        /// <param name="stringOfPairs">The string containing the key-value pairs</param> 
+        /// <param name="pairsSeparator">The separator used to separate each key-value pair</param>
+        /// <param name="pairSeparator">The separator used between the each key and value</param>
+        /// <returns>Dictionary with a string key and a string value</returns> 
         public static Dictionary<String, String> CreateDictionaryFromString(string stringOfPairs, char pairsSeparator, char pairSeparator)
         {
             var dictionary = new Dictionary<String, String>();
@@ -68,6 +77,13 @@ namespace DotNetNuke.Collections
             return dictionary;
         }
 
+        /// <summary>
+        /// Converts a Dictionary with a string key and a string value into a plain string separated by your choice of separators
+        /// </summary>
+        /// <param name="dictionary">The Dictionary to convert</param>
+        /// <param name="pairsSeparator">The separator to use between key-value pairs</param>
+        /// <param name="pairSeparator">The separator to use between each key and value</param>
+        /// <returns>The dictionary's contents as a <see cref="string" /></returns> 
         public static string DictionaryToString(this Dictionary<String, String> dictionary, string pairsSeparator, string pairSeparator)
         {
             return String.Join(pairsSeparator, dictionary.Select(pair => pair.Key + pairSeparator + pair.Value));
