4f02653c5f002fea9b112e364e9bd7065c194651
Matt Rutledge <mattrutledge@believegroup.com>
2018-11-09 22:00:56 -0700

    Add more stories.

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,12 +4,16 @@ import { action } from "@storybook/addon-actions";
 import BackToLink from "../BackToLink";
 import Button from "../Button";
 import Checkbox from "../Checkbox";
+import Collapsible from "../Collapsible";
 
 storiesOf("BackToLink", module)
-    .add("with text", () => <BackToLink onClick={action("clicked")}>Hello BackToLink</BackToLink>); 
+    .add("with text", () => <BackToLink onClick={action("clicked")} text="Hello BackToLink"></BackToLink>); 
 
 storiesOf("Button", module)
     .add("with text", () => <Button onClick={action("clicked")}>Hello Button</Button>); 
     
 storiesOf("Checkbox", module)
-    .add("with text", () => <Checkbox onClick={action("clicked")} label="Hello Checkbox"></Checkbox>); 
+    .add("with text", () => <Checkbox value={false} onChange={action("clicked")} label="Hello Checkbox"></Checkbox>); 
+
+storiesOf("Collapsible", module)
+    .add("with text", () => <Collapsible isOpened={false}><p>Test</p></Collapsible>); 
