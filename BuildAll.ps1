4464e92c021b96ef023a9d3f287836c7ce157c6b
Matt Rutledge <mattrutledge@believegroup.com>
2018-11-13 14:10:43 -0700

    Add versioning and publish scripts

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -1,7 +1,2 @@
 yarn install
-
-cd WebpackExternals
-yarn install
-cd ../
-
 yarn run build
---
+++ PublishAll.ps1
@@ -0,0 +1,10 @@
+# This script should only be run when releasing a new version to npm from VSTS.
+# Bump the version and publish
+npm version patch -m "Releasing version %s of the DNN React Common Library"
+npm publish --access public
+
+cd WebpackExternals
+npm version patch -m "Releasing version %s of the DNN Webpack Externals"
+npm publish --access public
+
+cd ../
