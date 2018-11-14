750665f1a9e14ae21088803ecf3912e52d20f9b1
Matt Rutledge <mattrutledge@believegroup.com>
2018-11-14 10:47:12 -0700

    Fix prop-types ref error

--- src/TreeControlInteractor/icons/arrow-icon/_arrow_icon.jsx
+++ src/TreeControlInteractor/icons/arrow-icon/_arrow_icon.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 const arrow_svg = require("!raw-loader!./arrow_bullet.svg");
 import { global } from "../../global";
