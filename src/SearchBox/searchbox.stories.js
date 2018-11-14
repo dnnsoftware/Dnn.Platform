9ba75e8b11940d96fba8ee32033ee58287561743
Matt Rutledge <mattrutledge@believegroup.com>
2018-11-14 11:45:57 -0700

    Add select story

--- src/SearchBox/searchbox.stories.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import SearchBox from "./index";
-
-storiesOf("SearchBox", module).add("with content", () => <SearchBox placeholder="Search..." onSearch={action("OnSearch")} maxLength={50} iconStyle={{ right: 0 }} inputDisabled={false} />);
