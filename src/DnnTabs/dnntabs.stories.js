7534d3a43134da2af1d80a70141a3d1c748d56e3
Matt Rutledge <mattrutledge@believegroup.com>
2018-11-14 10:35:24 -0700

    Fix lint issue

--- src/DnnTabs/dnntabs.stories.js
+++ src/DnnTabs/dnntabs.stories.js
@@ -5,7 +5,7 @@ import DnnTabs from "./index";
 import "react-tabs/style/react-tabs.less";
 
 storiesOf("DnnTabs", module).add("with content", () => <DnnTabs tabHeaders={["Tab 1", "Tab 2", "Tab 3"]} className="dnn-tabs" onSelect={action("selected")}>
-        <div>Tab 1</div>
-        <div>Tab 2</div>
-        <div>Tab 3</div>
+    <div>Tab 1</div>
+    <div>Tab 2</div>
+    <div>Tab 3</div>
 </DnnTabs>);
