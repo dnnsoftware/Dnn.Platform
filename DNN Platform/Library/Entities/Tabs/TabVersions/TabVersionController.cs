e7913945b32e723c949b3659fc44a41e6c9e1050
emanuele.colonnelli <emanuele.colonnelli@crossover.com>
2018-11-24 14:42:46 +0700

    Code review

--- DNN Platform/Library/Entities/Tabs/TabVersions/TabVersionController.cs
+++ DNN Platform/Library/Entities/Tabs/TabVersions/TabVersionController.cs
@@ -52,7 +52,7 @@ namespace DotNetNuke.Entities.Tabs.TabVersions
             var tabVersions = CBO.Instance.GetCachedObject<List<TabVersion>>(new CacheItemArgs(tabCacheKey,
                                                                     DataCache.TabVersionsCacheTimeOut,
                                                                     DataCache.TabVersionsCachePriority),
-                                                            c => CBO.FillCollection<TabVersion>(Provider.GetTabVersions(tabId)), false);
+                                                            c => CBO.FillCollection<TabVersion>(Provider.GetTabVersions(tabId)));
 
             return tabVersions;
         }
