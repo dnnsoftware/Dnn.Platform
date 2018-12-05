8d842e016f8fd666fdb1d2ab9e0c78b236192564
valadas <info@danielvaladas.com>
2018-12-05 17:00:04 -0500

    Updated to latest common components and fixed some build warnings

--- Extensions/Settings/Dnn.PersonaBar.Licensing/Licensing.Web/src/components/body/index.jsx
+++ Extensions/Settings/Dnn.PersonaBar.Licensing/Licensing.Web/src/components/body/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import PersonaBarPageBody from "dnn-persona-bar-page-body";
+import { PersonaBarPageBody } from "@dnnsoftware/dnn-react-common";
 import {
     pagination as PaginationActions,
     licensing as LicensingActions
