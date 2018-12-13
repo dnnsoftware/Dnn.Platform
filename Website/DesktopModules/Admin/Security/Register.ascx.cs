7f18472f3225a5f0d65b4af9fc00be8dcabbb95c
Ben <zyhfish@163.com>
2018-12-13 13:03:28 +0800

    DNN-27566: end the response after redirect.

--- Website/DesktopModules/Admin/Security/Register.ascx.cs
+++ Website/DesktopModules/Admin/Security/Register.ascx.cs
@@ -30,6 +30,7 @@ using System.Collections.Specialized;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.UI;
 
@@ -245,8 +246,14 @@ namespace DotNetNuke.Modules.Admin.Users
 			//Verify that the current user has access to this page
 			if (PortalSettings.UserRegistration == (int)Globals.PortalRegistrationType.NoRegistration && Request.IsAuthenticated == false)
 			{
-				Response.Redirect(Globals.NavigateURL("Access Denied"), false);
-				Context.ApplicationInstance.CompleteRequest();
+			    try
+			    {
+			        Response.Redirect(Globals.NavigateURL("Access Denied"), true);
+			    }
+			    catch (ThreadAbortException)
+			    {
+                    //do nothing here.
+			    }
 			}
 
 			cancelLink.NavigateUrl = closeLink.NavigateUrl = GetRedirectUrl(false);
