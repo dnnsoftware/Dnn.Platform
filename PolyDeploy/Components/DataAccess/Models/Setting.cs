0fdacadc8c9840b6381fab84925ad6073587e58a
Adam Nierzad <adam.nierzad@cantarus.com>
2019-01-25 13:08:35 +0000

    Created Setting model and supporting data controller.

---
+++ PolyDeploy/Components/DataAccess/Models/Setting.cs
@@ -0,0 +1,14 @@
+﻿using DotNetNuke.ComponentModel.DataAnnotations;
+
+namespace Cantarus.Modules.PolyDeploy.Components.DataAccess.Models
+{
+    [TableName("Cantarus_PolyDeploy_Settings")]
+    [PrimaryKey("SettingID")]
+    public class Setting
+    {
+        public int SettingId { get; set; }
+        public string Group { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
