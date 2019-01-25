f929087faf2ebff3c80a2cccecd5b84e38380cfc
Adam Nierzad <adam.nierzad@cantarus.com>
2019-01-25 13:10:04 +0000

    Created SettingManager.

---
+++ PolyDeploy/Components/Exceptions/SettingNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cantarus.Modules.PolyDeploy.Components.Exceptions
+{
+    internal class SettingNotFoundException : Exception
+    {
+        public static SettingNotFoundException Create(string group, string key)
+        {
+            return new SettingNotFoundException($"Setting in group '{group}' with key '{key}' was not found.");
+        }
+
+        public SettingNotFoundException(string message) : base(message) { }
+    }
+}
---
+++ PolyDeploy/Components/SettingManager.cs
@@ -0,0 +1,50 @@
+﻿using Cantarus.Modules.PolyDeploy.Components.DataAccess.DataControllers;
+using Cantarus.Modules.PolyDeploy.Components.DataAccess.Models;
+using Cantarus.Modules.PolyDeploy.Components.Exceptions;
+
+namespace Cantarus.Modules.PolyDeploy.Components
+{
+    internal class SettingManager
+    {
+        private static SettingDataController SettingDC = new SettingDataController();
+
+        public static Setting GetSetting(string group, string key)
+        {
+            Setting setting = SettingDC.GetSetting(group, key);
+
+            if (setting == null)
+            {
+                throw SettingNotFoundException.Create(group, key);
+            }
+
+            return setting;
+        }
+
+        public static void SetSetting(string group, string key, string value)
+        {
+            // Retrieve setting.
+            Setting setting = SettingDC.GetSetting(group, key);
+
+            // Does it already exist?
+            if (setting == null)
+            {
+                // No, create it.
+                setting = new Setting()
+                {
+                    Group = group,
+                    Key = key,
+                    Value = value
+                };
+
+                SettingDC.Create(setting);
+            }
+            else
+            {
+                // Yes, Update it.
+                setting.Value = value;
+
+                SettingDC.Update(setting);
+            }
+        }
+    }
+}
