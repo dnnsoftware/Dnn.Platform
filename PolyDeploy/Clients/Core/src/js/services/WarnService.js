d5faf88e8110cc4a95ae81c2d42644da70ffb788
Adam Nierzad <adam.nierzad@cantarus.com>
2019-01-29 14:31:57 +0000

    Created WarnService for monitoring the status of warnings.

---
+++ PolyDeploy/Clients/Core/src/js/services/WarnService.js
@@ -0,0 +1,56 @@
+﻿module.exports = ['SettingDataService',
+    function (SettingDataService) {
+
+        var warnings = {
+
+            // IP Whitelist Disabled.
+            whitelistDisabled: {
+                key: 'WHITELIST_STATE',
+                active: false,
+                message: 'IP Whitelisting is currently disabled.',
+                test: function () {
+                    var self = this;
+
+                    // Retrive state.
+                    SettingDataService.whitelist.getState()
+                        .then(function (result) {
+                            self.active = !result;
+                        });
+                }
+            }
+        };
+
+        // For each warning.
+        angular.forEach(warnings, function (warning) {
+
+            // Is there a test available?
+            if (warning.test) {
+
+                // Perform test.
+                warning.test();
+
+                // Is there a key?
+                if (warning.key) {
+
+                    // Subscribe for updates.
+                    SettingDataService.subscribe(warning.key,
+                        function () {
+                            warning.test();
+                        });
+                }
+            }
+        });
+
+        // Update the state of the passed warning key.
+        function updateWarnState(warn, state) {
+
+            // Key exists?
+            if (warnings[warn]) {
+                warnings[warn].state = state;
+            }
+        }
+
+        return {
+            warnings: warnings
+        };
+    }];
