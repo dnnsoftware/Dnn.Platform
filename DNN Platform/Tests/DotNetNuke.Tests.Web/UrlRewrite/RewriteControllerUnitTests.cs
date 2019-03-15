090e2dab32900e8f97811bff690f50c5ea7a9838
Mhtshum <43787378+Mhtshum@users.noreply.github.com>
2019-03-15 13:23:43 +0500

    - Add cases for domain names as well

--- DNN Platform/Tests/DotNetNuke.Tests.Web/UrlRewrite/RewriteControllerUnitTests.cs
+++ DNN Platform/Tests/DotNetNuke.Tests.Web/UrlRewrite/RewriteControllerUnitTests.cs
@@ -9,6 +9,10 @@ namespace DotNetNuke.Tests.Web.UrlRewrite
         [TestCase("http://www.dnn.com/en-us/aboutus", "en-us", "http://www.dnn.com/aboutus")]
         [TestCase("http://www.dnn.com/en-Us/aboutus", "en-Us", "http://www.dnn.com/aboutus")]
         [TestCase("http://www.dnn.com/aboutus", "en-Us", "http://www.dnn.com/aboutus")]
+        [TestCase("https://dnn.com/en", "en-Us", "https://dnn.com/en")]
+        [TestCase("https://www.dnn.com/en", "en-Us", "https://www.dnn.com/en")]
+        [TestCase("http://en.dnn.com/", "en-Us", "http://en.dnn.com/")]
+        [TestCase("http://www.a.b.c.com", "en-Us", "http://www.a.b.c.com")]
         public void RemoveLanguageCodeFromAbsoluteURL(string absoluteURL, string languageCode, string expectedURL)
         {
             // Act
