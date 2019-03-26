7c3b89e0fdb07823880859279c0ccf405a65fc45
Daniel Aguilera <daniel.aguilera@aurea.com>
2019-03-26 00:39:04 -0300

    Increased data provider timeout during install

--- DNN Platform/Library/Services/Upgrade/Upgrade.cs
+++ DNN Platform/Library/Services/Upgrade/Upgrade.cs
@@ -796,7 +796,7 @@ namespace DotNetNuke.Services.Upgrade
             string script = FileSystemUtils.ReadFile(scriptFile);
 
             // execute SQL installation script
-            string exceptions = DataProvider.Instance().ExecuteScript(script);
+            string exceptions = DataProvider.Instance().ExecuteScript(script, 300);
 
             //add installer logging
             if (string.IsNullOrEmpty(exceptions))
