c22d1c544a07dd998dc929b3ce62245e20369838
Oliver Hine <oliver.hine@gmail.com>
2019-04-26 19:43:22 -0400

    update url to documentation (#441)

--- Library/Dnn.PersonaBar.UI/admin/personaBar/scripts/serversummary.js
+++ Library/Dnn.PersonaBar.UI/admin/personaBar/scripts/serversummary.js
@@ -16,7 +16,7 @@ define(['jquery', 'knockout', 'jquery.hoverIntent.min'], function ($, ko) {
                         '<li class="server-name" data-bind="visible: ServerName.length > 0"><span data-bind="html: ServerName"></span><label data-bind="html: resx.ServerName"></label></li>' +
                         '<li class="license-info" data-bind="visible: visibleCheck(\'LicenseVisible\')"><a href="http://www.dnnsoftware.com/products/" data-bind="html: resx.UpgradeLicense" target="_blank"></a><label data-bind="html: resx.LicenseKey"></label></li>' +
                         '<li class="separator"></li>' +
-                        '<li class="doc-center"><a href="http://www.dnnsoftware.com/docs/" data-bind="html: resx.Documentation, visible: visibleCheck(\'DocCenterVisible\')" target="_blank"></a></li>' +
+                        '<li class="doc-center"><a href="https://dnndocs.com/" data-bind="html: resx.Documentation, visible: visibleCheck(\'DocCenterVisible\')" target="_blank"></a></li>' +
                         '<li id="Logout" class="logout" data-bind="html: resx.nav_Logout"></li>' +
                     '</ul>' +
                 '</div>');
