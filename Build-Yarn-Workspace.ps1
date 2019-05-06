43b991a011681672223bbc5d1cbb32f07a1b43ef
Oliver Hine <oliver@nbrane.com>
2019-05-06 10:32:29 -0400

    add yarn build script

---
+++ Build-Yarn-Workspace.ps1
@@ -0,0 +1,21 @@
+
+yarn install
+
+yarn workspace @dnnsoftware/dnn-react-common build
+yarn workspace admin_logs build
+yarn workspace extensions build
+yarn workspace licensing build
+yarn workspace pages build
+yarn workspace prompt build
+yarn workspace roles build
+yarn workspace security_settings build
+yarn workspace seo build
+yarn workspace servers build
+yarn workspace site_import_export build
+yarn workspace boilerplate build
+yarn workspace site_settings build
+yarn workspace task_scheduler build
+yarn workspace themes build
+yarn workspace users build
+yarn workspace dnn-users-exportables build
+yarn workspace taxonomy build
