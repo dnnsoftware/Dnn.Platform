71f9c856cadd74eca03c994c1eb673939f2dd19a
Andrew Hoefling <andrew@hoefling.me>
2019-05-11 21:18:59 -0400

    Fixed access modifier and added XML Comments

--- DNN Platform/DotNetNuke.Web.Mvc/DnnMvcControllerFactory.cs
+++ DNN Platform/DotNetNuke.Web.Mvc/DnnMvcControllerFactory.cs
@@ -5,7 +5,12 @@ using System.Web.Routing;
 
 namespace DotNetNuke.Web.Mvc
 {
-    class DnnMvcControllerFactory : DefaultControllerFactory, IControllerFactory
+    /// <summary>
+    /// DNN Specific MVC Controller Factory that attempts to use 
+    /// Dependency Injection to include all dependencies on the
+    /// Controller.
+    /// </summary>
+    public class DnnMvcControllerFactory : DefaultControllerFactory, IControllerFactory
     {
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
