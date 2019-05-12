addecbd9ff6decead1cd4ab7e75d68b7eb073196
Andrew Hoefling <andrew@hoefling.me>
2019-05-11 21:16:32 -0400

    Added documentation to Module Pipeline

--- DNN Platform/DotNetNuke.ModulePipeline/ModuleControlPipeline.cs
+++ DNN Platform/DotNetNuke.ModulePipeline/ModuleControlPipeline.cs
@@ -15,8 +15,18 @@ using System.Web.UI;
 
 namespace DotNetNuke.ModulePipeline
 {
-
+    /// <summary>
+    /// The Module Pipeline that determines which Module pattern
+    /// to invoke based on the input module type.
+    /// </summary>
     public class ModuleControlPipeline
+        // MULTI-TARGETTING PIPELINE
+        // -------------------------
+        // This file multi-targets .NET Framework and .NET Standard,
+        // which is needed as DNN migrates to .NET Core. The 'NET472'
+        // pre-processor directives are to fully support Legacy DNN.
+        // As the Pipeline is upgraded to be more complaint with 
+        // .NET Standard 2.0 use the apprioprate pre-processor directives.
 #if NET472
         : IModuleControlPipeline
 #endif
