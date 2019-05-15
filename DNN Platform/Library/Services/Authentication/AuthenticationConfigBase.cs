ef60e320c4ff73e88f8fc0191a9fd7fdc7140cb4
Andrew Hoefling <andrew@hoefling.me>
2019-05-15 14:27:22 -0400

    Added DependencyProvider to Authentication Base Classes

--- DNN Platform/Library/Services/Authentication/AuthenticationConfigBase.cs
+++ DNN Platform/Library/Services/Authentication/AuthenticationConfigBase.cs
@@ -22,7 +22,7 @@
 
 using System;
 using System.ComponentModel;
-
+using DotNetNuke.Common;
 using DotNetNuke.Common.Utilities;
 
 #endregion
@@ -38,7 +38,22 @@ namespace DotNetNuke.Services.Authentication
     [Serializable]
     public abstract class AuthenticationConfigBase
     {
+        /// <summary>
+        /// Gets or sets the Dependency Provider to resolve registered 
+        /// services with the container.
+        /// </summary>
+        /// <value>
+        /// The Dependency Service.
+        /// </value>
+        protected IServiceProvider DependencyProvider { get; }
+
+        public AuthenticationConfigBase()
+        {
+            DependencyProvider = Globals.DependencyProvider;
+        }
+
         protected AuthenticationConfigBase(int portalID)
+            : this()
         {
             PortalID = portalID;
         }
