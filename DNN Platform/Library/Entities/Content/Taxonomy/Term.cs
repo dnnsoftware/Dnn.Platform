6352862f7a8d084d9ce74bb4ccccee5539f7f0f2
berkarslan-xo <berkarslan.cs@aurea.com>
2019-08-19 14:14:09 +0300

    DNN-29557 - Infinite while loop fix

--- DNN Platform/Library/Entities/Content/Taxonomy/Term.cs
+++ DNN Platform/Library/Entities/Content/Taxonomy/Term.cs
@@ -176,9 +176,9 @@ namespace DotNetNuke.Entities.Content.Taxonomy
             }
             set
             {
-                while (HtmlUtils.IsUrlEncoded(value))
+                if (HtmlUtils.IsUrlEncoded(value))
                     value = System.Net.WebUtility.UrlDecode(value);
-                while (HtmlUtils.ContainsEntity(value))
+                if (HtmlUtils.ContainsEntity(value))
                     value = System.Net.WebUtility.HtmlDecode(value);
                 _name = Security.InputFilter(value, PortalSecurity.FilterFlag.NoMarkup);
             }
