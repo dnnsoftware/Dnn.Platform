b245ed2a90e6966576d5ce4d0cb6004af2f58527
David Poindexter <dpoindexter@nvisionative.com>
2019-09-29 19:39:09 -0400

    Remove disposal of MemoryStream for email attachments sent using SendtokenizedBuilkEmail

--- DNN Platform/Library/Services/Mail/SendTokenizedBulkEmail.cs
+++ DNN Platform/Library/Services/Mail/SendTokenizedBulkEmail.cs
@@ -347,24 +347,22 @@ namespace DotNetNuke.Services.Mail
 			foreach (var attachment in _attachments)
 			{
 				var buffer = new byte[4096];
-                using (var memoryStream = new MemoryStream())
+                var memoryStream = new MemoryStream();
+                while (true)
                 {
-                    while (true)
+                    var read = attachment.ContentStream.Read(buffer, 0, 4096);
+                    if (read <= 0)
                     {
-                        var read = attachment.ContentStream.Read(buffer, 0, 4096);
-                        if (read <= 0)
-                        {
-                            break;
-                        }
-                        memoryStream.Write(buffer, 0, read);
+                        break;
                     }
-
-                    var newAttachment = new Attachment(memoryStream, attachment.ContentType);
-                    newAttachment.ContentStream.Position = 0;
-                    attachments.Add(newAttachment);
-                    //reset original position
-                    attachment.ContentStream.Position = 0;
+                    memoryStream.Write(buffer, 0, read);
                 }
+
+                var newAttachment = new Attachment(memoryStream, attachment.ContentType);
+                newAttachment.ContentStream.Position = 0;
+                attachments.Add(newAttachment);
+                //reset original position
+                attachment.ContentStream.Position = 0;
 			}
 
 			return attachments;
