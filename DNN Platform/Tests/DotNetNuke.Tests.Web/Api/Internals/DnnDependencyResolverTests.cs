9c9051cab95537684f4d8641e5cffc3aabbc79d6
Andrew Hoefling <andrew@hoefling.me>
2019-10-23 18:31:47 -0400

    Added more unit tests for the DnnDependencyResolver to cover GetServices (multiple) and BeginScope

--- DNN Platform/Tests/DotNetNuke.Tests.Web/Api/Internals/DnnDependencyResolverTests.cs
+++ DNN Platform/Tests/DotNetNuke.Tests.Web/Api/Internals/DnnDependencyResolverTests.cs
@@ -1,7 +1,8 @@
 ﻿using DotNetNuke.Web.Api.Internal;
-using Moq;
+using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using System;
+using System.Linq;
 using System.Web.Http.Dependencies;
 
 namespace DotNetNuke.Tests.Web.Api.Internals
@@ -9,23 +10,28 @@ namespace DotNetNuke.Tests.Web.Api.Internals
     [TestFixture]
     public class DnnDependencyResolverTests
     {
+        private IServiceProvider _serviceProvider;
         private IDependencyResolver _dependencyResolver;
 
         [TestFixtureSetUp]
         public void FixtureSetUp()
         {
-            var mockServiceProvider = new Mock<IServiceProvider>();
-            mockServiceProvider
-                .Setup(x => x.GetService(typeof(ITestService)))
-                .Returns(new TestService());
+            var services = new ServiceCollection();
+            services.AddScoped(typeof(ITestService), typeof(TestService));
 
-            _dependencyResolver = new DnnDependencyResolver(mockServiceProvider.Object);
+            _serviceProvider = services.BuildServiceProvider();
+            _dependencyResolver = new DnnDependencyResolver(_serviceProvider);
         }
 
         [TestFixtureTearDown]
         public void FixtureTearDown()
         {
             _dependencyResolver = null;
+
+            if (_serviceProvider is IDisposable disposable)
+                disposable.Dispose();
+
+            _serviceProvider = null;
         }
 
         [Test]
@@ -50,8 +56,52 @@ namespace DotNetNuke.Tests.Web.Api.Internals
             Assert.AreEqual(expected.GetType(), actual.GetType());
         }
 
+        [Test]
+        public void GetTestServices()
+        {
+            var expected = new TestService();
+            var actual = _dependencyResolver.GetServices(typeof(ITestService)).ToArray();
+
+            Assert.NotNull(actual);
+            Assert.AreEqual(1, actual.Length);
+            Assert.AreEqual(expected.GetType(), actual[0].GetType());
+        }
+
+        [Test]
+        public void BeginScope()
+        {
+            var actual = _dependencyResolver.BeginScope();
+
+            Assert.NotNull(actual);
+            Assert.IsInstanceOf<DnnDependencyResolver>(actual);
+        }
+
+        [Test]
+        public void BeginScope_GetService()
+        {
+            var scope = _dependencyResolver.BeginScope();
+
+            var expected = new TestService();
+            var actual = scope.GetService(typeof(ITestService));
+
+            Assert.NotNull(actual);
+            Assert.AreEqual(expected.GetType(), actual.GetType());
+        }
+
+        [Test]
+        public void BeginScope_GetServices()
+        {
+            var scope = _dependencyResolver.BeginScope();
+
+            var expected = new TestService();
+            var actual = scope.GetServices(typeof(ITestService)).ToArray();
+
+            Assert.NotNull(actual);
+            Assert.AreEqual(1, actual.Length);
+            Assert.AreEqual(expected.GetType(), actual[0].GetType());
+        }
+
         private interface ITestService { }
         private class TestService : ITestService { }
-
     }
 }
