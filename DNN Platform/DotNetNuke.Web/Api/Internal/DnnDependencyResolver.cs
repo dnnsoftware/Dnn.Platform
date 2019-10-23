0f1946d0002cf96c5be93316554d3b86c90da1a2
Andrew Hoefling <andrew@hoefling.me>
2019-10-22 22:54:04 -0400

    Removed DnnHttpControllerActivator and added DependencyResolver implementation using the IServiceProvider

---
+++ DNN Platform/DotNetNuke.Web/Api/Internal/DnnDependencyResolver.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Web.Http.Dependencies;
+
+namespace DotNetNuke.Web.Api.Internal
+{
+    internal class DnnDependencyResolver : IDependencyResolver
+    {
+        private readonly IServiceProvider _serviceProvider;
+        public DnnDependencyResolver(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            return new DnnDependencyResolver(_serviceProvider.CreateScope().ServiceProvider);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _serviceProvider.GetService(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _serviceProvider.GetServices(serviceType);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // I don't think we should dispose anything
+        }
+    }
+}
