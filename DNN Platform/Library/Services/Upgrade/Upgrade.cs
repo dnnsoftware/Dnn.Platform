ce3feb7b435e1d140d209efdec0e5f57533db507
donker <peter@bring2mind.net>
2019-10-30 23:13:03 +0100

    Fix to installer and the dependencies of the HTML and DA modules

--- DNN Platform/Library/Services/Upgrade/Upgrade.cs
+++ DNN Platform/Library/Services/Upgrade/Upgrade.cs
@@ -4920,7 +4920,7 @@ namespace DotNetNuke.Services.Upgrade
         /// <returns></returns>
         public static IDictionary<string, PackageInfo> GetInstallPackages()
         {
-            var packageTypes = new string[] { "Module", "Skin", "Container", "JavaScriptLibrary", "Language", "Provider", "AuthSystem", "Package" };
+            var packageTypes = new string[] { "Library", "Module", "Skin", "Container", "JavaScriptLibrary", "Language", "Provider", "AuthSystem", "Package" };
             var invalidPackages = new List<string>();
 
             var packages = new Dictionary<string, PackageInfo>();
