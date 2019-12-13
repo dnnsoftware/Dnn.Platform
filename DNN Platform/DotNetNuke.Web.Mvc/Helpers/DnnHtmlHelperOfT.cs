0f3a8af5d65fe2bc071f9bcef0d94f2e260fccf7
donker <peter@bring2mind.net>
2019-12-13 11:30:31 +0100

    Sweep 1

--- DNN Platform/DotNetNuke.Web.Mvc/Helpers/DnnHtmlHelperOfT.cs
+++ DNN Platform/DotNetNuke.Web.Mvc/Helpers/DnnHtmlHelperOfT.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) DNN Software. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace DotNetNuke.Web.Mvc.Helpers
