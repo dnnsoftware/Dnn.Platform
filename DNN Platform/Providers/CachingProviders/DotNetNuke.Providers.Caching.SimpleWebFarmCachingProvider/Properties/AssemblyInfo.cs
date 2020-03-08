c5f60b96056f43ec561eceb8f46362684504ce82
Peter Donker <peter@bring2mind.net>
2020-03-08 15:32:09 +0100

    Fixes to build process (#3618)
    * Fixes to build process
    
    * Last fix

--- DNN Platform/Providers/CachingProviders/DotNetNuke.Providers.Caching.SimpleWebFarmCachingProvider/Properties/AssemblyInfo.cs
+++ DNN Platform/Providers/CachingProviders/DotNetNuke.Providers.Caching.SimpleWebFarmCachingProvider/Properties/AssemblyInfo.cs
@@ -1,36 +1,15 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// 
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// 
+#region Usings
+
+using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+#endregion
+
 [assembly: AssemblyTitle("DotNetNuke.Providers.Caching.SimpleWebFarmCachingProvider")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNetNuke.Providers.Caching.SimpleWebFarmCachingProvider")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: AssemblyDescription("Open Source Web Application Framework")]
 [assembly: Guid("2c25580c-a971-4f0b-9f70-436a35c2473e")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
