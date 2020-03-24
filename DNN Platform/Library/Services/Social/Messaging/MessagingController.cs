2aab772c4bcc723ffb7af65ff97400e39f2d19bc
Ben <zyhfish@163.com>
2020-03-24 13:02:07 +0800

    check message attachment's permission.
    Fix #3644.

--- DNN Platform/Library/Services/Social/Messaging/MessagingController.cs
+++ DNN Platform/Library/Services/Social/Messaging/MessagingController.cs
@@ -16,7 +16,9 @@ using DotNetNuke.Entities.Portals.Internal;
 using DotNetNuke.Framework;
 using DotNetNuke.Security;
 using DotNetNuke.Entities.Users;
+using DotNetNuke.Security.Permissions;
 using DotNetNuke.Security.Roles;
+using DotNetNuke.Services.FileSystem;
 using DotNetNuke.Services.Social.Messaging.Data;
 using DotNetNuke.Services.Social.Messaging.Exceptions;
 using DotNetNuke.Services.Social.Messaging.Internal;
@@ -193,7 +195,10 @@ namespace DotNetNuke.Services.Social.Messaging
             {
                 foreach (var attachment in fileIDs.Select(fileId => new MessageAttachment { MessageAttachmentID = Null.NullInteger, FileID = fileId, MessageID = message.MessageID }))
                 {
-                    _dataService.SaveMessageAttachment(attachment, UserController.Instance.GetCurrentUserInfo().UserID);
+                    if (CanViewFile(attachment.FileID))
+                    {
+                        _dataService.SaveMessageAttachment(attachment, UserController.Instance.GetCurrentUserInfo().UserID);
+                    }
                 }
             }
 
@@ -274,5 +279,17 @@ namespace DotNetNuke.Services.Social.Messaging
         }
 
         #endregion
+
+        private bool CanViewFile(int fileId)
+        {
+            var file = FileManager.Instance.GetFile(fileId);
+            if (file == null)
+            {
+                return false;
+            }
+
+            var folder = FolderManager.Instance.GetFolder(file.FolderId);
+            return folder != null && FolderPermissionController.Instance.CanViewFolder(folder);
+        }
     }
 }
