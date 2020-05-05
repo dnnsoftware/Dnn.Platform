aaa79836194804af3df3a1363d731dcac3ab81f1
Mitchel Sellers <msellers@iowacomputergurus.com>
2020-05-05 12:52:48 -0500

    Standardization of File Headers

--- DNN Platform/Modules/ResourceManager/Components/SettingsManager.cs
+++ DNN Platform/Modules/ResourceManager/Components/SettingsManager.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+using System.Collections;
 using DotNetNuke.Entities.Modules;
 using DotNetNuke.Entities.Portals;
 using DotNetNuke.Entities.Users;
