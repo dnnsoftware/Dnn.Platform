224db034b676fa75f491e2f0bb4f48b58a38839d
Peter Donker <peter@bring2mind.net>
2020-05-28 21:58:27 +0200

    More licenses

--- DNN Platform/Library/Entities/Modules/Prompt/AddModule.cs
+++ DNN Platform/Library/Entities/Modules/Prompt/AddModule.cs
@@ -1,4 +1,7 @@
-﻿using DotNetNuke.Abstractions.Portals;
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+using DotNetNuke.Abstractions.Portals;
 using DotNetNuke.Abstractions.Prompt;
 using DotNetNuke.Abstractions.Users;
 using DotNetNuke.Common.Utilities;
