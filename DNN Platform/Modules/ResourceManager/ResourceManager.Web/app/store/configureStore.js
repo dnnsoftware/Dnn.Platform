42a4a11014ddc5552c127f6f468feedbeafc3d0f
Peter Donker <peter@bring2mind.net>
2020-05-31 20:10:05 +0200

    Fix store and downgrade react-dropzone as it required react 16.8

--- DNN Platform/Modules/ResourceManager/ResourceManager.Web/app/store/configureStore.js
+++ DNN Platform/Modules/ResourceManager/ResourceManager.Web/app/store/configureStore.js
@@ -1,21 +1,21 @@
 import { createStore, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
+import thunkMiddleware from "redux-thunk";
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import rootReducer from "../reducers/rootReducer";
+import DevTools from "../containers/DevTools";
+
+/* eslint-disable no-undef */
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 export default function configureStore(initialState) {
-
-    // eslint-disable-next-line no-undef
-    const middleware = process.env.NODE_ENV !== "production" ?
-        [require("redux-immutable-state-invariant")(), thunk] :
-        [thunk];
-
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    
     const store = createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware(...middleware))
+        compose(
+            IS_PRODUCTION ?
+                applyMiddleware(thunkMiddleware) :
+                applyMiddleware(thunkMiddleware, reduxImmutableStateInvariant()), DevTools.instrument()
+        )
     );
-
     return store;
 }
