7a3deee92b71db7abe7fda770af0ae331784c866
Peter Donker <peter@bring2mind.net>
2020-06-03 09:34:28 +0200

    Fix random replacement of http with https in resources (issue #3599)

--- DNN Platform/Library/Entities/Portals/PortalSettings.cs
+++ DNN Platform/Library/Entities/Portals/PortalSettings.cs
@@ -699,15 +699,19 @@ namespace DotNetNuke.Entities.Portals
             var isPublic = true;
             switch (lowerPropertyName)
             {
+                case "scheme":
+                    propertyNotFound = false;
+                    result = SSLEnabled ? "https" : "http";
+                    break;
                 case "url":
                     propertyNotFound = false;
                     result = PropertyAccess.FormatString(PortalAlias.HTTPAlias, format);
                     break;
-                case "fullurl": //return portal alias with protocol
+                case "fullurl": //return portal alias with protocol - note this depends on HttpContext
                     propertyNotFound = false;
                     result = PropertyAccess.FormatString(Globals.AddHTTP(PortalAlias.HTTPAlias), format);
                     break;
-                case "passwordreminderurl": //if regsiter page defined in portal settings, then get that page url, otherwise return home page.
+                case "passwordreminderurl": //if regsiter page defined in portal settings, then get that page url, otherwise return home page. - note this depends on HttpContext
                     propertyNotFound = false;
                     var reminderUrl = Globals.AddHTTP(PortalAlias.HTTPAlias);
                     if (RegisterTabId > Null.NullInteger)
