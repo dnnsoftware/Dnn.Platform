4c22c25d79e33fcd43c6967421737cb03643e6ca
Christopher Wolf <ChristopherWolf.87@live.de>
2020-06-07 13:52:12 +0200

    Clean up and extend command line arguments
    Cleaned-up CommandlineOptions class and added short names for options.
    Furthermore, added option "packages-directory" to defines a directory,
    from which install packages will be read.

--- DeployClient/CommandlineOptions.cs
+++ DeployClient/CommandlineOptions.cs
@@ -1,44 +1,52 @@
 ﻿using CommandLine;
 using CommandLine.Text;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DeployClient
 {
     internal class CommandLineOptions
     {
-        [Option("silent",
+        [Option('s',
+            "silent",
             DefaultValue = false,
             HelpText = "Whether to not print any output")]
         public bool IsSilent { get; set; }
 
-        [Option("no-prompt",
+        [Option('n',
+            "no-prompt",
             DefaultValue = false,
             HelpText = "Whether to prompt the user for confirmation")]
         public bool NoPrompt { get; set; }
 
-        [Option("target-uri", 
+        [Option('u',
+            "target-uri", 
             Required = false, 
             HelpText = "The URL to deploy the packages to.",
             DefaultValue = null)]
         public string TargetUri { get; set; }
 
-        [Option("api-key",
+        [Option('a',
+            "api-key",
             Required = false,
             HelpText = "The API Key to use",
             DefaultValue = null)]
         public string APIKey { get; set; }
 
-        [Option("encryption-key",
+        [Option('e',
+            "encryption-key",
             Required = false,
             HelpText = "The Encryption Key to use",
             DefaultValue = null)]
         public string EncryptionKey { get; set; }
 
-        [Option("installation-status-timeout",
+        [Option('d',
+            "packages-directory",
+            Required = false,
+            HelpText = "Defines the directory that contains the to install packages.",
+            DefaultValue = null)]
+        public string PackagesDirectoryPath { get; set; }
+
+        [Option('t',
+            "installation-status-timeout",
             Required = false,
             HelpText = "The number of seconds to ignore 404 errors when checking installation status",
             DefaultValue = 60)]
