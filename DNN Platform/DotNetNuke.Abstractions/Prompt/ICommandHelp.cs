39299ca2bf868d625f79ac80177c0f65c78835b3
David Poindexter <dpoindexter@nvisionative.com>
2020-07-12 19:55:38 -0400

    Move using statements inside namespace for DotNetNuke.Abstractions project

--- DNN Platform/DotNetNuke.Abstractions/Prompt/ICommandHelp.cs
+++ DNN Platform/DotNetNuke.Abstractions/Prompt/ICommandHelp.cs
@@ -1,10 +1,10 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
-using System.Collections.Generic;
-
 namespace DotNetNuke.Abstractions.Prompt
 {
+    using System.Collections.Generic;
+
     /// <summary>
     /// This is used to send the result back to the client when a user asks help for a command
     /// </summary>
