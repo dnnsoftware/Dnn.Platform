8b47e26af5de24525d1d31801168f2c6e17b4948
David Poindexter <dpoindexter@nvisionative.com>
2020-07-12 19:55:38 -0400

    Move using statements inside namespace for DotNetNuke.Abstractions project

--- DNN Platform/DotNetNuke.Abstractions/Portals/IPortalAliasInfo.cs
+++ DNN Platform/DotNetNuke.Abstractions/Portals/IPortalAliasInfo.cs
@@ -1,12 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
-using System.Data;
-using System.Xml;
-using System.Xml.Schema;
-
 namespace DotNetNuke.Abstractions.Portals
 {
+    using System.Data;
+    using System.Xml;
+    using System.Xml.Schema;
+
     public interface IPortalAliasInfo
     {
         //BrowserTypes BrowserType { get; set; }
