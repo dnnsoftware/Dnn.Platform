5234baa48a6b57edf7c020131463435e58203849
Berk Arslan <berk.arslan@aurea.com>
2020-07-17 14:44:32 +0300

    DNN-33330 - UTs are provided

---
+++ DNN Platform/Tests/DotNetNuke.Tests.Core/Entities/Tabs/TabChangeTrackerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Tabs;
+using DotNetNuke.Framework;
+using Moq;
+using NUnit.Framework;
+
+namespace DotNetNuke.Tests.Core.Entities.Tabs
+{
+    [TestFixture]
+    public class TabChangeTrackerTests
+    {
+        [Test]
+        public void TrackModuleModification_WithSharedModule_ThrowsException()
+        {
+            // Arrange
+            var tabChangeTracker = new TabChangeTracker();
+            var mockedModuleController = new Mock<IModuleController>();
+            mockedModuleController
+                .Setup(s => s.IsSharedModule(It.IsAny<ModuleInfo>()))
+                .Returns(true);
+            ServiceLocator<IModuleController, ModuleController>.SetTestableInstance(mockedModuleController.Object);
+
+            // Act & Assert
+            var exception = Assert.Throws<InvalidOperationException>(() => tabChangeTracker.TrackModuleModification(null, 1, 0));
+            Assert.AreEqual(true, exception.Data?[TabChangeTracker.IsModuleDoesNotBelongToPage]);
+        }
+    }
+}
