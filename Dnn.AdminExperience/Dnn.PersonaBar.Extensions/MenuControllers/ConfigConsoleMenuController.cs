d8a80f21eb331966aed8785ed71801814e0820f1
Daniel Valadas <info@danielvaladas.com>
2020-07-27 01:16:57 -0400

    Resolves 9.6.2 logging issue.
    Closes #3906
    
    Apparently some files where deleted as part of #3843
    
    This PR brings those files back and also fixes stylecop warnings. Some class names had to be changed both for clarity and to respect stylecop rule that the file name should match the class name. Because of that change, the dnn manifest was also changed so it all works with the new names.

--- Dnn.AdminExperience/Dnn.PersonaBar.Extensions/MenuControllers/ConfigConsoleMenuController.cs
+++ Dnn.AdminExperience/Dnn.PersonaBar.Extensions/MenuControllers/ConfigConsoleMenuController.cs
@@ -2,7 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
 
-namespace Dnn.PersonaBar.Prompt.MenuControllers
+namespace Dnn.PersonaBar.ConfigConsole.MenuControllers
 {
     using System.Collections.Generic;
 
@@ -11,9 +11,9 @@ namespace Dnn.PersonaBar.Prompt.MenuControllers
     using DotNetNuke.Entities.Users;
 
     /// <summary>
-    /// Controls the prompt menu.
+    /// Controls the config console menu.
     /// </summary>
-    public class PromptMenuController : IMenuItemController
+    public class ConfigConsoleMenuController : IMenuItemController
     {
         /// <inheritdoc/>
         public void UpdateParameters(MenuItem menuItem)
