b1884dcf51181b5b6aaeeed01e89177ea788529a
Gerard Smit <gsmit1996@gmail.com>
2020-09-04 22:05:12 +0200

    Use request scope in PortalModuleBase
    Fixes #4046

---
+++ DNN Platform/Library/Common/ServiceScopeContainer.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+namespace DotNetNuke.Common
+{
+    using System;
+    using System.Web;
+    using DotNetNuke.Common.Extensions;
+    using Microsoft.Extensions.DependencyInjection;
+
+    internal struct ServiceScopeContainer : IDisposable
+    {
+        public ServiceScopeContainer(IServiceScope serviceScope, bool shouldDispose)
+        {
+            ServiceScope = serviceScope;
+            ShouldDispose = shouldDispose;
+        }
+
+        /// <summary>
+        ///     Service scope that the container holds.
+        /// </summary>
+        public IServiceScope ServiceScope { get; }
+
+        /// <summary>
+        ///     True if the <see cref="ServiceScope"/> should be disposed.
+        /// </summary>
+        public bool ShouldDispose { get; }
+
+        /// <summary>
+        ///     Get the service scope from <see cref="HttpContext"/> or create a new scope from <see cref="Globals.DependencyProvider"/> when the scope doesn't exists.
+        /// </summary>
+        public static ServiceScopeContainer GetRequestOrCreateScope()
+        {
+            var requestScope = HttpContext.Current?.GetScope();
+
+            if (requestScope != null)
+            {
+                return new ServiceScopeContainer(requestScope, false);
+            }
+
+            return new ServiceScopeContainer(Globals.DependencyProvider.CreateScope(), true);
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            if (ShouldDispose)
+            {
+                ServiceScope.Dispose();
+            }
+        }
+    }
+}
---
+++ DNN Platform/Tests/DotNetNuke.Tests.Web/Api/Internals/ServiceContainerTests.cs
@@ -0,0 +1,67 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+namespace DotNetNuke.Tests.Web.Api.Internals
+{
+    using System.Web;
+    using DotNetNuke.Abstractions;
+    using DotNetNuke.Abstractions.Application;
+    using DotNetNuke.Common;
+    using DotNetNuke.Common.Extensions;
+    using DotNetNuke.Tests.Instance.Utilities.HttpSimulator;
+    using Moq;
+    using Microsoft.Extensions.DependencyInjection;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ServiceContainerTests
+    {
+        [TestFixtureSetUp]
+        public void FixtureSetUp()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddTransient<INavigationManager>(container => Mock.Of<INavigationManager>());
+            serviceCollection.AddTransient<IApplicationStatusInfo>(container => new DotNetNuke.Application.ApplicationStatusInfo(Mock.Of<IApplicationInfo>()));
+            
+            Globals.DependencyProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            Globals.DependencyProvider = null;
+        }
+
+        [Test]
+        public void CreateScopeWhenRequestDoesNotExists()
+        {
+            Assert.Null(HttpContext.Current?.GetScope());
+
+            var container = ServiceScopeContainer.GetRequestOrCreateScope();
+
+            Assert.True(container.ShouldDispose);
+        }
+
+        [Test]
+        public void UseRequestScopeWhenPossible()
+        {
+            using (var simulator = new HttpSimulator())
+            {
+                simulator.SimulateRequest();
+
+                var scope = Globals.DependencyProvider.CreateScope();
+
+                HttpContext.Current.SetScope(scope);
+
+                Assert.NotNull(HttpContext.Current?.GetScope());
+
+                var container = ServiceScopeContainer.GetRequestOrCreateScope();
+
+                Assert.False(container.ShouldDispose);
+                Assert.AreEqual(scope, container.ServiceScope);
+            }
+        }
+    }
+}
