a446852a76e06bb1678707d9091b669e69a6cdfa
Andrew Hoefling <andrew@hoefling.me>
2020-09-07 21:06:10 -0400

    Added ISaveSettingsService to abstractions project

---
+++ DNN Platform/DotNetNuke.Abstractions/Settings/ISaveSettingsService.cs
@@ -0,0 +1,42 @@
+﻿namespace DotNetNuke.Abstractions.Settings
+{
+    /// <summary>
+    /// The Save Settings service is used by implementations of
+    /// <see cref="ISettingsService"/>. It is not currently designed
+    /// to be injected via Dependency Injection. Please use the
+    /// <see cref="ISettingsService"/>.
+    /// </summary>
+    public interface ISaveSettingsService
+    {
+        /// <summary>
+        /// Update the current setting with a new value. If there is no
+        /// setting key found it will create a new setting.
+        /// </summary>
+        /// <param name="key">The setting key to update.</param>
+        /// <param name="value">The new setting value.</param>
+        /// <param name="clearCache">If true the cache will be cleared.</param>
+        void Update(string key, string value, bool clearCache);
+
+        /// <summary>
+        /// Update the current setting with a new encrypted value.
+        /// If there is no setting key found it will create a
+        /// new setting.
+        /// </summary>
+        /// <param name="key">The setting key to update.</param>
+        /// <param name="value">The new setting value.</param>
+        /// <param name="passPhrase">The encryption salt key or pass phrase.</param>
+        /// <param name="clearCache">If true the cache will be cleared.</param>
+        void UpdateEncrypted(string key, string value, string passPhrase, bool clearCache);
+
+        /// <summary>
+        /// Update the current setting with a new encrypted value.
+        /// If there is no setting key found it will create a
+        /// new setting. This function will use the default encryption
+        /// salt key or (pass phrase).
+        /// </summary>
+        /// <param name="key">The setting key to update.</param>
+        /// <param name="value">The new setting value.</param>
+        /// <param name="clearCache">If true the cache will be cleared.</param>
+        void UpdateEncrypted(string key, string value, bool clearCache);
+    }
+}
