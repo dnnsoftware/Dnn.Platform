9950d0b8314e72341dfda2f2f12921e58d4cbfa4
Daniel Valadas <info@danielvaladas.com>
2020-09-19 23:12:47 -0400

    Adjusted nuget references to project references

--- DNN Platform/Providers/HtmlEditorProviders/DNNConnect.CKE/Properties/AssemblyInfo.cs
+++ DNN Platform/Providers/HtmlEditorProviders/DNNConnect.CKE/Properties/AssemblyInfo.cs
@@ -1,23 +1,8 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-/*
-* CKEditor Provider for DNN
-* ========
-* https://github.com/DNN-Connect/CKEditorProvider
-* Copyright (C) DNN Connect
-*
-* The software, this file and its contents are subject to the CKEditorProvider
-* License. Please read the license.txt file before using, installing, copying,
-* modifying or distribute this file or part of its contents. The contents of
-* this file is part of the Source Code of the CKEditor Provider.
-*/
-
 [assembly: AssemblyDescription("CKEditor HTML Editor Provider for DNN")]
 [assembly: AssemblyTitle("CKEditor HTML Editor Provider for DNN")]
-[assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyCompany("DNN-Connect")]
-[assembly: AssemblyCopyright("2019 DNN-Connect")]
-
