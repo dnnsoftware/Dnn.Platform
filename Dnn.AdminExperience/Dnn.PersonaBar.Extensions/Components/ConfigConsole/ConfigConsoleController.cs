9d745bf12718c09d5694a485f0b6b6c5a09cd972
Will Strohl <hismightiness@users.noreply.github.com>
2020-10-09 17:55:39 -0700

    Adds support for editing the robots.txt on a website (#4165)
    * Adds support for editing the robots.txt on a website
    
    Resolves #3103
    
    * Moved robots.txt get/update methods & made private
    
    * Applied suggested updates from code review

--- Dnn.AdminExperience/Dnn.PersonaBar.Extensions/Components/ConfigConsole/ConfigConsoleController.cs
+++ Dnn.AdminExperience/Dnn.PersonaBar.Extensions/Components/ConfigConsole/ConfigConsoleController.cs
@@ -1,23 +1,33 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
+
 namespace Dnn.PersonaBar.ConfigConsole.Components
 {
     using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Threading;
     using System.Xml;
 
     using DotNetNuke.Application;
     using DotNetNuke.Common;
     using DotNetNuke.Common.Utilities;
+    using DotNetNuke.Instrumentation;
 
     public class ConfigConsoleController
     {
+        private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(ConfigConsoleController));
+        private const string CONFIG_EXT = ".config";
+        private const string ROBOTS_EXT = "robots.txt";  // in multi-portal instances, there may be multiple robots.txt files (e.g., site1.com.robots.txt, site2.com.robots.txt, etc.)
+
         public IEnumerable<string> GetConfigFilesList()
         {
-            var files = Directory.GetFiles(Globals.ApplicationMapPath, "*.config");
+            var files = Directory
+                .EnumerateFiles(Globals.ApplicationMapPath) 
+                .Where(file => file.ToLower().EndsWith(CONFIG_EXT, StringComparison.InvariantCultureIgnoreCase) || file.ToLower().EndsWith(ROBOTS_EXT, StringComparison.InvariantCultureIgnoreCase))
+                .ToList();
             IEnumerable<string> fileList = (from file in files select Path.GetFileName(file));
             return fileList;
         }
@@ -26,15 +36,24 @@ namespace Dnn.PersonaBar.ConfigConsole.Components
         {
             this.ValidateFilePath(configFile);
 
-            var configDoc = Config.Load(configFile);
-            using (var txtWriter = new StringWriter())
+            if (configFile.EndsWith(CONFIG_EXT, StringComparison.InvariantCultureIgnoreCase))
             {
-                using (var writer = new XmlTextWriter(txtWriter))
+                var configDoc = Config.Load(configFile);
+                using (var txtWriter = new StringWriter())
                 {
-                    writer.Formatting = Formatting.Indented;
-                    configDoc.WriteTo(writer);
+                    using (var writer = new XmlTextWriter(txtWriter))
+                    {
+                        writer.Formatting = Formatting.Indented;
+                        configDoc.WriteTo(writer);
+                    }
+
+                    return txtWriter.ToString();
                 }
-                return txtWriter.ToString();
+            }
+            else
+            {
+                var doc = File.ReadAllText(Path.Combine(Globals.ApplicationMapPath, "\\", configFile));
+                return doc;
             }
         }
 
@@ -42,9 +61,16 @@ namespace Dnn.PersonaBar.ConfigConsole.Components
         {
             this.ValidateFilePath(fileName);
 
-            var configDoc = new XmlDocument { XmlResolver = null };
-            configDoc.LoadXml(fileContent);
-            Config.Save(configDoc, fileName);
+            if (fileName.EndsWith(CONFIG_EXT, StringComparison.InvariantCultureIgnoreCase))
+            {
+                var configDoc = new XmlDocument {XmlResolver = null};
+                configDoc.LoadXml(fileContent);
+                Config.Save(configDoc, fileName);
+            }
+            else
+            {
+                SaveNonConfig(fileContent, fileName);
+            }
         }
 
         public void MergeConfigFile(string fileContent)
@@ -77,6 +103,61 @@ namespace Dnn.PersonaBar.ConfigConsole.Components
             {
                 throw new ArgumentException("Invalid File Path");
             }
+        }
+
+        private static string SaveNonConfig(string document, string filename)
+        {
+            var retMsg = string.Empty;
+            try
+            {
+                var strFilePath = Path.Combine(Globals.ApplicationMapPath, "\\", filename);
+                var existingFileAttributes = FileAttributes.Normal;
+                if (File.Exists(strFilePath))
+                {
+                    // save current file attributes
+                    existingFileAttributes = File.GetAttributes(strFilePath);
+
+                    // change to normal ( in case it is flagged as read-only )
+                    File.SetAttributes(strFilePath, FileAttributes.Normal);
+                }
+
+                // Attempt a few times in case the file was locked; occurs during modules' installation due
+                // to application restarts where IIS can overlap old application shutdown and new one start.
+                const int maxRetires = 4;
+                const double miltiplier = 2.5;
+                for (var retry = maxRetires; retry >= 0; retry--)
+                {
+                    try
+                    {
+                        // save the config file
+                        File.WriteAllText(strFilePath, document);
+
+                        break;
+                    }
+                    catch (IOException exc)
+                    {
+                        if (retry == 0)
+                        {
+                            Logger.Error(exc);
+                            retMsg = exc.Message;
+                        }
+
+                        // try incremental delay; maybe the file lock is released by then
+                        Thread.Sleep((int)(miltiplier * (maxRetires - retry + 1)) * 1000);
+                    }
+                }
+
+                // reset file attributes
+                File.SetAttributes(strFilePath, existingFileAttributes);
+            }
+            catch (Exception exc)
+            {
+                // the file permissions may not be set properly
+                Logger.Error(exc);
+                retMsg = exc.Message;
+            }
+
+            return retMsg;
         }
     }
 }
