05d426fdfeb31929b2333fa20ebab36f4ea563f0
Daniel Valadas <info@danielvaladas.com>
2020-10-26 08:54:44 -0400

    Corrected a typo in IGlobals.cs

--- DNN Platform/Library/Common/Internal/IGlobals.cs
+++ DNN Platform/Library/Common/Internal/IGlobals.cs
@@ -31,7 +31,7 @@ namespace DotNetNuke.Common.Internal
         /// </summary>
         /// <param name="fileNamePath">The folder the absolute path.</param>
         /// <param name="portalId">Portal Id.</param>
-        /// <returns>The subforlder path.</returns>
+        /// <returns>The subfolder path.</returns>
         string GetSubFolderPath(string fileNamePath, int portalId);
 
         /// <summary>
