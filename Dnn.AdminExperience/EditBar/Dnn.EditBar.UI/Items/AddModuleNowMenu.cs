7f9f5468211857950a3d0a73f114f03a839b093e
Peter Donker <peter@bring2mind.net>
2020-11-13 12:52:43 +0100

    Hook in a new control in the edit bar to allow for quick addition of a module to a page.

---
+++ Dnn.AdminExperience/EditBar/Dnn.EditBar.UI/Items/AddModuleNowMenu.cs
@@ -0,0 +1,81 @@
+﻿namespace Dnn.EditBar.UI.Items
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Dnn.EditBar.Library;
+    using Dnn.EditBar.Library.Items;
+    using DotNetNuke.Entities.Modules;
+    using DotNetNuke.Entities.Portals;
+    using DotNetNuke.Services.Personalization;
+    using DotNetNuke.Web.Components.Controllers;
+
+    [Serializable]
+    public class AddModuleNowMenu : BaseMenuItem
+    {
+        public override string Name { get; } = "AddModuleNow";
+
+        public override string Text
+        {
+            get
+            {
+                return "Add Module Now";
+            }
+        }
+
+        public override string CssClass
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public override bool CustomLayout => true;
+
+        public override string Template
+        {
+            get
+            {
+                var portalSettings = PortalSettings.Current;
+                if (portalSettings == null)
+                {
+                    return string.Empty;
+                }
+
+                var moduleList = ControlBarController.Instance.GetCategoryDesktopModules(portalSettings.PortalId, "All", string.Empty).Select(m =>
+                {
+                    var selected = m.Value.FriendlyName == "HTML" ? " selected" : string.Empty;
+                    return $"<option value=\"{m.Value.DesktopModuleID}\"{selected}>{m.Value.FriendlyName}</option>";
+                });
+                var panes = string.Empty;
+                foreach (string paneName in portalSettings.ActiveTab.Panes)
+                {
+                    var selected = paneName == "ContentPane" ? " selected" : string.Empty;
+                    panes += $"<option value=\"{paneName}\"{selected}>{paneName}</options>";
+                }
+
+                return $"<div><select id=\"menu-AddModuleNow-module\">{string.Join(string.Empty, moduleList)}</select><select id=\"menu-AddModuleNow-pane\">{panes}</select><button href=\"javascript: void(0);\">{this.Text}</button></div>";
+            }
+        }
+
+        public override string Parent { get; } = Constants.LeftMenu;
+
+        public override string Loader { get; } = "AddModuleNow";
+
+        public override int Order { get; } = 7;
+
+        public override bool Visible()
+        {
+            var portalSettings = PortalSettings.Current;
+            if (portalSettings == null)
+            {
+                return false;
+            }
+
+            return Personalization.GetUserMode() == PortalSettings.Mode.Edit
+                && ControlBarController.Instance.GetCategoryDesktopModules(portalSettings.PortalId, "All", string.Empty).Any();
+        }
+    }
+}
---
+++ Dnn.AdminExperience/EditBar/Dnn.EditBar.UI/editBar/scripts/AddModuleNow.js
@@ -0,0 +1,36 @@
+﻿define(["jquery"], function ($) {
+  "use strict";
+  var menuItem, util;
+
+  var init = function (menu, utility, params, callback) {
+    menuItem = menu;
+    util = utility;
+
+    if (typeof callback === "function") {
+      callback();
+    }
+
+    $('#menu-AddModuleNow button').click(function () {
+      util.sf.moduleRoot = "internalservices";
+      util.sf.controller = "controlBar";
+      util.sf.post(
+        "AddModule",
+        {
+          Visibility: 0,
+          Position: "BOTTOM",
+          Module: $("#menu-AddModuleNow-module").val(),
+          Pane: $("#menu-AddModuleNow-pane").val(),
+          AddExistingModule: false,
+          CopyModule: false,
+        },
+        function done() {
+          window.top.location.reload();
+        }
+      );
+    });
+  };
+
+  return {
+    init: init,
+  };
+});
