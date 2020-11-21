383a18f5711572f17e7bcae3dd2658cd2ccd7c39
Brian Dukes <bdukes@engagesoftware.com>
2020-11-21 08:54:55 -0600

    Revert to older version of GitVersion

--- Build/Program.cs
+++ Build/Program.cs
@@ -30,7 +30,7 @@ public class Program : IFrostingStartup
         var module = new NuGetModule(new CakeConfiguration(new Dictionary<string, string>()));
         module.Register(services);
         
-        services.UseTool(new Uri("nuget:?package=GitVersion.CommandLine&version=5.5.1"));
+        services.UseTool(new Uri("nuget:?package=GitVersion.CommandLine&version=5.0.1"));
         services.UseTool(new Uri("nuget:?package=Microsoft.TestPlatform&version=16.8.0"));
         services.UseTool(new Uri("nuget:?package=NUnitTestAdapter&version=2.3.0"));
     }
