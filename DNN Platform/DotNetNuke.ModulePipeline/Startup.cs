3089b7230cf756ced56ca2791735e14ec22f8af3
Brian Dukes <bdukes@engagesoftware.com>
2020-12-21 15:50:14 -0600

    Enable warnings as errors for ModulePipeline (#4378)

--- DNN Platform/DotNetNuke.ModulePipeline/Startup.cs
+++ DNN Platform/DotNetNuke.ModulePipeline/Startup.cs
@@ -1,15 +1,16 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
-
 namespace DotNetNuke.ModulePipeline
 {
     using DotNetNuke.DependencyInjection;
     using DotNetNuke.UI.Modules;
     using Microsoft.Extensions.DependencyInjection;
 
+    /// <inheritdoc/>
     public class Startup : IDnnStartup
     {
+        /// <inheritdoc/>
         public void ConfigureServices(IServiceCollection services)
         {
             // MULTI-TARGETTING PIPELINE
