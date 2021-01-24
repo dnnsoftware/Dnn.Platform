192113224ca1866e6793f14fbc2788166cb82c5e
Daniel Valadas <info@danielvaladas.com>
2021-01-24 14:48:19 -0500

    Create fr.js

---
+++ DNN Platform/Providers/HtmlEditorProviders/DNNConnect.CKE/js/ckeditor/4.15.1/plugins/dnnpages/lang/fr.js
@@ -0,0 +1,7 @@
+CKEDITOR.plugins.setLang('dnnpages', 'en', {
+    dnnpages: 'Page du site',
+    fileToBig: 'Le fichier est trop grand pour être télévresé.',
+	localPageLabel : 'Page qui provient de votre site.',
+	localPageTitle: 'Choisissez la page de votre site pour laquelle vous désirez créer un lien',
+	portalUrl: 'Fichier du site ou autre URL'
+});
