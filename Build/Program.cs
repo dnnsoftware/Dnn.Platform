bb87ca979cc6c9204942ba9da10dcc0f85b2d0d6
Brian Dukes <bdukes@engagesoftware.com>
2021-01-30 14:37:22 -0600

    Update to Cake 1.0.0-RC3

--- Build/Program.cs
+++ Build/Program.cs
@@ -10,10 +10,10 @@ public class Program
             .UseContext<Context>()
             .UseLifetime<Lifetime>()
             .UseWorkingDirectory("..")
-            .UseTool(new Uri("nuget:?package=GitVersion.CommandLine&version=5.0.1"))
-            .UseTool(new Uri("nuget:?package=Microsoft.TestPlatform&version=16.8.0"))
-            .UseTool(new Uri("nuget:?package=NUnitTestAdapter&version=2.3.0"))
-            .UseTool(new Uri("nuget:?package=NuGet.CommandLine&version=5.8.0"))
+            .InstallTool(new Uri("nuget:?package=GitVersion.CommandLine&version=5.0.1"))
+            .InstallTool(new Uri("nuget:?package=Microsoft.TestPlatform&version=16.8.0"))
+            .InstallTool(new Uri("nuget:?package=NUnitTestAdapter&version=2.3.0"))
+            .InstallTool(new Uri("nuget:?package=NuGet.CommandLine&version=5.8.0"))
             .Run(args);
     }
 }
