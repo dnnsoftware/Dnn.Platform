1343411d47e56f7335ab2cfda13055f780f93b79
Daniel Aguilera <daniel.aguilera@aurea.com>
2021-02-11 12:00:45 -0300

    Added missing using statement

--- DNN Platform/Modules/DDRMenu/MenuNode.cs
+++ DNN Platform/Modules/DDRMenu/MenuNode.cs
@@ -6,6 +6,7 @@ namespace DotNetNuke.Web.DDRMenu
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Xml;
     using System.Xml.Schema;
     using System.Xml.Serialization;
