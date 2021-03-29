a0ea2f461567a316e7464aba6221c93329c40cdc
W Welch <wwelchj@gmail.com>
2021-03-28 21:22:02 -0400

    Fix using statement placement in ICommand*.
    Fixes #4042.

--- DNN Platform/DotNetNuke.Abstractions/Prompt/ICommand.cs
+++ DNN Platform/DotNetNuke.Abstractions/Prompt/ICommand.cs
@@ -2,10 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
 
-using System;
-
 namespace DotNetNuke.Abstractions.Prompt
 {
+    using System;
+
     /// <summary>
     /// This is used to retrieve and keep a list of all commands found in the installation.
     /// </summary>
--- DNN Platform/DotNetNuke.Abstractions/Prompt/ICommandOption.cs
+++ DNN Platform/DotNetNuke.Abstractions/Prompt/ICommandOption.cs
@@ -2,10 +2,10 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
 
-using System;
-
 namespace DotNetNuke.Abstractions.Prompt
 {
+    using System;
+
     /// <summary>
     /// This is used in the ICommandHelp to send a list of command parameters to the client for explanatory help.
     /// </summary>
