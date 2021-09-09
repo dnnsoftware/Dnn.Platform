c60ddebc3cddbf97328ef519627dd785e9c122d9
Berk Arslan <berk.arslan@aurea.com>
2021-09-09 16:02:04 +0300

    Issue-4823 - UTs added

---
+++ DNN Platform/Tests/DotNetNuke.Tests.Core/Framework/ReflectionTests.cs
@@ -0,0 +1,36 @@
+﻿namespace DotNetNuke.Tests.Core.Framework
+{
+    using System.Text;
+    using DotNetNuke.Framework;
+    using NUnit.Framework;
+
+    public class ReflectionTests
+    {
+        [Test]
+        public void CreateInstance_WithArgs_WorksCorrectly()
+        {
+            // Arrange
+            var typeToCreate = typeof(StringBuilder);
+            var argToPass = new object[] { 1 };
+
+            // Act
+            var result = Reflection.CreateInstance(typeToCreate, argToPass) as StringBuilder;
+
+            // Assert
+            Assert.AreEqual(1, result.Capacity);
+        }
+
+        [Test]
+        public void CreateInstance_WithoutArgs_WorksCorrectly()
+        {
+            // Arrange
+            var typeToCreate = typeof(StringBuilder);
+
+            // Act
+            var result = Reflection.CreateInstance(typeToCreate) as StringBuilder;
+
+            // Assert
+            Assert.AreEqual(16, result.Capacity);
+        }
+    }
+}
