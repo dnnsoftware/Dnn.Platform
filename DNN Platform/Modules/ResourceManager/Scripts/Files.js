1bb46d6b4b35b3b87780390cf808f8c350cdc0cd
Daniel <info@danielvaladas.com>
2022-02-18 00:51:15 -0500

    Initial attempt at PersonaBar integration

---
+++ DNN Platform/Modules/ResourceManager/Scripts/Files.js
@@ -0,0 +1,28 @@
+﻿'use strict';
+define(['jquery'],
+    function ($) {
+        function loadScript() {
+            var url = "Modules/ResourceManager/Scripts/dnn-resourcemanager/dnn-resource-manager.esm.js";
+            //var url = "http://localhost:8099/ListingManager-bundle.js"; //Use this for dev
+            $.ajax({
+                dataType: "script",
+                cache: true,
+                url: url
+            });
+        }
+        return {
+            init: function (wrapper, util, params, callback) {
+                loadScript();
+            },
+
+            initMobile: function (wrapper, util, params, callback) {
+                this.init(wrapper, util, params, callback);
+            },
+
+            load: function (params, callback) {
+            },
+
+            loadMobile: function (params, callback) {
+            }
+        };
+    });
