03e727036ba14629c907083c44783cf1e3d1d825
Daniel <info@danielvaladas.com>
2022-02-27 22:03:17 -0500

    Implemented file search
    Implemented file search

--- DNN Platform/Modules/ResourceManager/ResourceManager.Web/src/components/dnn-rm-top-bar/dnn-rm-top-bar.tsx
+++ DNN Platform/Modules/ResourceManager/ResourceManager.Web/src/components/dnn-rm-top-bar/dnn-rm-top-bar.tsx
@@ -1,4 +1,6 @@
 import { Component, Host, h, } from '@stencil/core';
+import state from '../../store/store';
+import { ItemsClient } from "../../services/ItemsClient";
 @Component({
   tag: 'dnn-rm-top-bar',
   styleUrl: 'dnn-rm-top-bar.scss',
@@ -6,10 +8,41 @@ import { Component, Host, h, } from '@stencil/core';
 })
 export class DnnRmTopBar {
 
+  private itemsClient: ItemsClient;
+
+  constructor(){
+    this.itemsClient = new ItemsClient(state.moduleId);
+  }
+
+  private handleSearchChanged(e: CustomEvent<any>): void {
+    if (e.detail != ""){
+      state.itemsSearchTerm = e.detail;
+      this.itemsClient.search(state.currentItems.folder.folderId, e.detail, 0)
+      .then(data =>{
+        state.currentItems = {
+          ...state.currentItems,
+          totalCount: data.totalCount,
+          items: data.items,
+        };
+        state.lastSearchRequestedPage = 1;
+      })
+      .catch(reason => console.error(reason));
+    }
+    else
+    {
+      this.itemsClient.getFolderContent(state.currentItems.folder.folderId, 0)
+      .then(data => {
+        state.lastSearchRequestedPage = 1;
+        state.itemsSearchTerm = undefined;
+        state.currentItems = data;
+      })
+    }
+  }
+
   render() {
     return (
       <Host>
-        <dnn-searchbox></dnn-searchbox>
+        <dnn-searchbox onQueryChanged={e => this.handleSearchChanged(e)}></dnn-searchbox>
       </Host>
     );
   }
