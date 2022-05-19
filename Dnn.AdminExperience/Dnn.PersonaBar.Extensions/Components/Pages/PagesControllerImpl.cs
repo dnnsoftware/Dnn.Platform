4f81cc3912725fa83ab4d3733ff092dac84b4155
Daniel Bolef <dbolef@engagesoftware.com>
2022-05-19 15:16:46 -0500

    Added SQL for PortalPermission
    Undid some changes no longer needed
    Added SQL for PortalPermission Table and related SPs
    Platform/Website/Providers/DataProviders/SqlDataProvider/09.11.00.SqlDataProvider
    Dnn.AdminExperience/Dnn.PersonaBar.Extensions/Components/Pages/PagesControllerImpl.cs
    Dnn.AdminExperience/Dnn.PersonaBar.Extensions/Services/PagesController.cs

--- Dnn.AdminExperience/Dnn.PersonaBar.Extensions/Components/Pages/PagesControllerImpl.cs
+++ Dnn.AdminExperience/Dnn.PersonaBar.Extensions/Components/Pages/PagesControllerImpl.cs
@@ -912,10 +912,6 @@ namespace Dnn.PersonaBar.Pages.Components
                     permissions.UserPermissions = permissions.UserPermissions.OrderBy(p => p.DisplayName).ToList();
                 }
             }
-            else if (pageId == 0)
-            {
-                return this.GetPermissionsData(PortalController.Instance.GetCurrentSettings().HomeTabId);
-            }
 
             return permissions;
         }
