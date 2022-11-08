6b264dba6061121495e0a020b384cf5b8d02701b
Daniel <info@danielvaladas.com>
2022-11-08 15:09:54 -0500

    Fixed an infinite loop in schedule item.
    The NextStart property was setting itself instead of the privete nextStart value which made it go into an infinite loop and caused the instance to crash.
    
    I also added docs to public methods while I was there.

--- DNN Platform/Library/Services/Scheduling/ScheduleItem.cs
+++ DNN Platform/Library/Services/Scheduling/ScheduleItem.cs
@@ -11,6 +11,9 @@ namespace DotNetNuke.Services.Scheduling
     using DotNetNuke.Entities;
     using DotNetNuke.Entities.Modules;
 
+    /// <summary>
+    /// Represents one item in the scheduler.
+    /// </summary>
     [Serializable]
     public class ScheduleItem : BaseEntityInfo, IHydratable
     {
@@ -40,16 +43,34 @@ namespace DotNetNuke.Services.Scheduling
             this.ScheduleStartDate = Null.NullDate;
         }
 
+        /// <summary>
+        /// Gets or sets the the event this item attaches to.
+        /// </summary>
         public string AttachToEvent { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether cath-up is enabled.
+        /// </summary>
         public bool CatchUpEnabled { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the item is enabled.
+        /// </summary>
         public bool Enabled { get; set; }
 
+        /// <summary>
+        /// Gets or sets the schedule start date.
+        /// </summary>
         public DateTime ScheduleStartDate { get; set; }
 
+        /// <summary>
+        /// Gets or sets the friednly name for the item.
+        /// </summary>
         public string FriendlyName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the next start date.
+        /// </summary>
         public virtual DateTime NextStart
         {
             get
@@ -64,32 +85,68 @@ namespace DotNetNuke.Services.Scheduling
 
             set
             {
-                this.NextStart = value;
+                this.nextStart = value;
             }
         }
 
+        /// <summary>
+        /// Gets or sets the object dependencies.
+        /// </summary>
         public string ObjectDependencies { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating how many history items to keep.
+        /// </summary>
         public int RetainHistoryNum { get; set; }
 
+        /// <summary>
+        /// Gets or sets the retry time lapse value.
+        /// </summary>
         public int RetryTimeLapse { get; set; }
 
+        /// <summary>
+        /// Gets or sets the unit of measure for the retry time lapse value.
+        /// </summary>
         public string RetryTimeLapseMeasurement { get; set; }
 
+        /// <summary>
+        /// Gets or sets the ID of the scheduled item.
+        /// </summary>
         public int ScheduleID { get; set; }
 
+        /// <summary>
+        /// Gets or sets the servers this task should run on.
+        /// </summary>
         public string Servers { get; set; }
 
+        /// <summary>
+        /// Gets or sets the recurrence time lapse value.
+        /// </summary>
         public int TimeLapse { get; set; }
 
+        /// <summary>
+        /// Gets or sets the unit of measure for the recurrence time lapse value.
+        /// </summary>
         public string TimeLapseMeasurement { get; set; }
 
+        /// <summary>
+        /// Gets or sets the full type name.
+        /// </summary>
         public string TypeFullName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the process group.
+        /// </summary>
         public int ProcessGroup { get; set; }
 
+        /// <summary>
+        /// Gets or sets the <see cref="ScheduleSource"/>.
+        /// </summary>
         public ScheduleSource ScheduleSource { get; set; }
 
+        /// <summary>
+        /// Gets or sets the ID of the running thread.
+        /// </summary>
         public int ThreadID { get; set; }
 
         /// <inheritdoc/>
@@ -112,6 +169,11 @@ namespace DotNetNuke.Services.Scheduling
             this.FillInternal(dr);
         }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether the item has object dependencies.
+        /// </summary>
+        /// <param name="strObjectDependencies">A string representing the name of the object dependencies.</param>
+        /// <returns>A value indicating whether the item has object dependencies.</returns>
         public bool HasObjectDependencies(string strObjectDependencies)
         {
             if (strObjectDependencies.IndexOf(",") > -1)
@@ -135,11 +197,21 @@ namespace DotNetNuke.Services.Scheduling
             return false;
         }
 
+        /// <summary>
+        /// Adds a schedule item setting value.
+        /// </summary>
+        /// <param name="key">The setting key.</param>
+        /// <param name="value">The value of the setting.</param>
         public void AddSetting(string key, string value)
         {
             this.scheduleItemSettings.Add(key, value);
         }
 
+        /// <summary>
+        /// Gets a specific setting.
+        /// </summary>
+        /// <param name="key">The key of the setting to get.</param>
+        /// <returns>The value of the setting.</returns>
         public virtual string GetSetting(string key)
         {
             if (this.scheduleItemSettings == null)
@@ -157,6 +229,10 @@ namespace DotNetNuke.Services.Scheduling
             }
         }
 
+        /// <summary>
+        /// Gets all the item settings.
+        /// </summary>
+        /// <returns>An <see cref="Hashtable"/> of all the settings.</returns>
         public virtual Hashtable GetSettings()
         {
             this.scheduleItemSettings = SchedulingProvider.Instance().GetScheduleItemSettings(this.ScheduleID);
