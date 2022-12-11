ad47b6695ebdc889fb828934d5592c6c98ae1b55
Daniel <info@danielvaladas.com>
2022-12-10 22:17:33 -0500

    Change build to run all npm build before backend builds.
    This makes `yarn install` and `yarn build` before any project builds. It still uses lerna as before.
    
    - I confirmed that a failed install makes the build fail.
    - I confirmed that a failed npm build makes the build fail.
    - I confirmed that it fixed the resource manager scripts missing from the final build.
    
    The drawback of this approach is that one can no longer build only the persona bar in release mode without using cake. But for dev/debug, the current documented way with `yarn start --scope ...` is still valid.

---
+++ Build/Tasks/BuildNpmPackages.cs
@@ -0,0 +1,32 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+namespace DotNetNuke.Build.Tasks
+{
+    using Cake.Common.IO;
+    using Cake.Core;
+    using Cake.Core.IO;
+    using Cake.Core.Tooling;
+    using Cake.Frosting;
+    using Cake.Yarn;
+
+    /// <summary>
+    /// Builds the npm packages for the entire solution.
+    /// </summary>
+    public sealed class BuildNpmPackages : FrostingTask<Context>
+    {
+        /// <inheritdoc/>
+        public override void Run(Context context)
+        {
+            var yarn = new YarnRunner(
+                context.FileSystem,
+                context.Environment,
+                context.ProcessRunner,
+                context.Tools);
+            yarn.Install(c => c
+                .WithArgument("--no-immutable")
+                .WithWorkingDirectory(context.Directory("./")));
+            yarn.RunScript("build");
+        }
+    }
+}
