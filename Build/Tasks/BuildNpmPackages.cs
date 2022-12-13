f13c95fc456c39d1920998ac9c4c285fb9499595
Brian Dukes <bdukes@engagesoftware.com>
2022-12-13 14:40:30 -0600

    Remove now-unused Yarn.MsBuild package

--- Build/Tasks/BuildNpmPackages.cs
+++ Build/Tasks/BuildNpmPackages.cs
@@ -4,29 +4,20 @@
 namespace DotNetNuke.Build.Tasks
 {
     using Cake.Common.IO;
-    using Cake.Core;
-    using Cake.Core.IO;
     using Cake.Core.Tooling;
     using Cake.Frosting;
     using Cake.Yarn;
 
-    /// <summary>
-    /// Builds the npm packages for the entire solution.
-    /// </summary>
+    /// <summary>Builds the npm packages for the entire solution.</summary>
     public sealed class BuildNpmPackages : FrostingTask<Context>
     {
         /// <inheritdoc/>
         public override void Run(Context context)
         {
-            var yarn = new YarnRunner(
-                context.FileSystem,
-                context.Environment,
-                context.ProcessRunner,
-                context.Tools);
-            yarn.Install(c => c
+            context.Yarn().Install(c => c
                 .WithArgument("--no-immutable")
                 .WithWorkingDirectory(context.Directory("./")));
-            yarn.RunScript("build");
+            context.Yarn().RunScript("build");
         }
     }
 }
