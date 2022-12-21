607e4a02dc61123909f4f6fae7a3c8cf6fcd27b2
Brian Dukes <bdukes@engagesoftware.com>
2022-12-21 15:16:32 -0600

    Fix SA1303 Const field names should begin with upper-case letter

--- DNN Platform/Library/Services/OutputCache/Providers/MemoryProvider.cs
+++ DNN Platform/Library/Services/OutputCache/Providers/MemoryProvider.cs
@@ -7,6 +7,7 @@ namespace DotNetNuke.Services.OutputCache.Providers
     using System;
     using System.Collections;
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using System.Linq;
     using System.Text;
@@ -18,6 +19,9 @@ namespace DotNetNuke.Services.OutputCache.Providers
     /// </summary>
     public class MemoryProvider : OutputCachingProvider
     {
+        [SuppressMessage("StyleCop.CSharp.NamingRules", "SA1303:ConstFieldNamesMustBeginWithUpperCaseLetter", Justification = "Breaking Change")]
+
+        // ReSharper disable once InconsistentNaming
         protected const string cachePrefix = "DNN_OUTPUT:";
         private static System.Web.Caching.Cache runtimeCache;
 
