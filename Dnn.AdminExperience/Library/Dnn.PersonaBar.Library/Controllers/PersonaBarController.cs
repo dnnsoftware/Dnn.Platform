bf0e7f9ffc1882007bfe543c706f7c5f15cbb4af
Brian Dukes <bdukes@engagesoftware.com>
2023-02-14 15:03:21 -0600

    Enable DI for IMenuItemController

--- Dnn.AdminExperience/Library/Dnn.PersonaBar.Library/Controllers/PersonaBarController.cs
+++ Dnn.AdminExperience/Library/Dnn.PersonaBar.Library/Controllers/PersonaBarController.cs
@@ -11,10 +11,12 @@ namespace Dnn.PersonaBar.Library.Controllers
     using Dnn.PersonaBar.Library.Containers;
     using Dnn.PersonaBar.Library.Permissions;
     using Dnn.PersonaBar.Library.Repository;
+    using DotNetNuke.Common;
     using DotNetNuke.Entities.Portals;
     using DotNetNuke.Entities.Users;
     using DotNetNuke.Framework;
     using DotNetNuke.Instrumentation;
+    using Microsoft.Extensions.DependencyInjection;
     using Newtonsoft.Json;
 
     using MenuItem = Dnn.PersonaBar.Library.Model.MenuItem;
@@ -25,10 +27,22 @@ namespace Dnn.PersonaBar.Library.Controllers
         private static readonly DnnLogger Logger = DnnLogger.GetClassLogger(typeof(PersonaBarController));
 
         private readonly IPersonaBarRepository personaBarRepository;
+        private readonly IServiceScopeFactory serviceScopeFactory;
 
+        /// <summary>Initializes a new instance of the <see cref="PersonaBarController"/> class.</summary>
+        [Obsolete("Deprecated in DotNetNuke 10.0.0. Please use overload with IServiceScopeFactory. Scheduled removal in v12.0.0.")]
         public PersonaBarController()
+            : this(Globals.DependencyProvider.GetRequiredService<IServiceScopeFactory>(), PersonaBarRepository.Instance)
         {
-            this.personaBarRepository = PersonaBarRepository.Instance;
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="PersonaBarController"/> class.</summary>
+        /// <param name="serviceScopeFactory">The service scope factory.</param>
+        /// <param name="personaBarRepository">The Persona Bar repository.</param>
+        public PersonaBarController(IServiceScopeFactory serviceScopeFactory, IPersonaBarRepository personaBarRepository)
+        {
+            this.serviceScopeFactory = serviceScopeFactory;
+            this.personaBarRepository = personaBarRepository;
         }
 
         /// <inheritdoc/>
@@ -62,7 +76,8 @@ namespace Dnn.PersonaBar.Library.Controllers
             {
                 try
                 {
-                    var menuController = this.GetMenuItemController(menuItem);
+                    using var scope = this.serviceScopeFactory.CreateScope();
+                    var menuController = GetMenuItemController(scope, menuItem);
                     visible = menuController == null || menuController.Visible(menuItem);
                 }
                 catch (Exception ex)
@@ -78,7 +93,50 @@ namespace Dnn.PersonaBar.Library.Controllers
         /// <inheritdoc/>
         protected override Func<IPersonaBarController> GetFactory()
         {
-            return () => new PersonaBarController();
+            return Globals.DependencyProvider.GetRequiredService<IPersonaBarController>;
+        }
+
+        private static void AddPermissions(MenuItem menuItem, IDictionary<string, object> settings)
+        {
+            var portalSettings = PortalSettings.Current;
+            if (!settings.ContainsKey("permissions") && portalSettings != null)
+            {
+                var menuPermissions = MenuPermissionController.GetPermissions(menuItem.MenuId)
+                    .Where(p => p.PermissionKey != "VIEW");
+                var portalId = portalSettings.PortalId;
+                var permissions = new Dictionary<string, bool>();
+                foreach (var permission in menuPermissions)
+                {
+                    var key = permission.PermissionKey;
+                    var hasPermission = MenuPermissionController.HasMenuPermission(portalId, menuItem, key);
+                    permissions.Add(key, hasPermission);
+                }
+
+                settings.Add("permissions", permissions);
+            }
+        }
+
+        private static IMenuItemController GetMenuItemController(IServiceScope scope, MenuItem menuItem)
+        {
+            var identifier = menuItem.Identifier;
+            var controller = menuItem.Controller;
+
+            if (string.IsNullOrEmpty(controller))
+            {
+                return null;
+            }
+
+            try
+            {
+                var cacheKey = $"PersonaBarMenuController_{identifier}";
+                var controllerType = Reflection.CreateType(controller, cacheKey, useCache: true);
+                return ActivatorUtilities.GetServiceOrCreateInstance(scope.ServiceProvider, controllerType) as IMenuItemController;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                return null;
+            }
         }
 
         private bool GetPersonaBarMenuWithPermissionCheck(PortalSettings portalSettings, UserInfo user, IList<MenuItem> filterItems, IList<MenuItem> menuItems)
@@ -111,7 +169,7 @@ namespace Dnn.PersonaBar.Library.Controllers
                         ParentId = menuItem.ParentId,
                     };
 
-                    this.UpdateParamters(cloneItem);
+                    this.UpdateParameters(cloneItem);
                     cloneItem.Settings = this.GetMenuSettings(menuItem);
 
                     var filtered = this.GetPersonaBarMenuWithPermissionCheck(portalSettings, user, cloneItem.Children, menuItem.Children);
@@ -130,9 +188,10 @@ namespace Dnn.PersonaBar.Library.Controllers
             return menuFiltered;
         }
 
-        private void UpdateParamters(MenuItem menuItem)
+        private void UpdateParameters(MenuItem menuItem)
         {
-            var menuController = this.GetMenuItemController(menuItem);
+            using var scope = this.serviceScopeFactory.CreateScope();
+            var menuController = GetMenuItemController(scope, menuItem);
             try
             {
                 menuController?.UpdateParameters(menuItem);
@@ -148,9 +207,11 @@ namespace Dnn.PersonaBar.Library.Controllers
             IDictionary<string, object> settings;
             try
             {
-                var menuController = this.GetMenuItemController(menuItem);
+                using var scope = this.serviceScopeFactory.CreateScope();
+                var menuController = GetMenuItemController(scope, menuItem);
                 settings = menuController?.GetSettings(menuItem) ?? new Dictionary<string, object>();
-                this.AddPermissions(menuItem, settings);
+
+                AddPermissions(menuItem, settings);
             }
             catch (Exception ex)
             {
@@ -159,48 +220,6 @@ namespace Dnn.PersonaBar.Library.Controllers
             }
 
             return JsonConvert.SerializeObject(settings);
-        }
-
-        private void AddPermissions(MenuItem menuItem, IDictionary<string, object> settings)
-        {
-            var portalSettings = PortalSettings.Current;
-            if (!settings.ContainsKey("permissions") && portalSettings != null)
-            {
-                var menuPermissions = MenuPermissionController.GetPermissions(menuItem.MenuId)
-                    .Where(p => p.PermissionKey != "VIEW");
-                var portalId = portalSettings.PortalId;
-                var permissions = new Dictionary<string, bool>();
-                foreach (var permission in menuPermissions)
-                {
-                    var key = permission.PermissionKey;
-                    var hasPermission = MenuPermissionController.HasMenuPermission(portalId, menuItem, key);
-                    permissions.Add(key, hasPermission);
-                }
-
-                settings.Add("permissions", permissions);
-            }
-        }
-
-        private IMenuItemController GetMenuItemController(MenuItem menuItem)
-        {
-            var identifier = menuItem.Identifier;
-            var controller = menuItem.Controller;
-
-            if (string.IsNullOrEmpty(controller))
-            {
-                return null;
-            }
-
-            try
-            {
-                var cacheKey = $"PersonaBarMenuController_{identifier}";
-                return Reflection.CreateObject(controller, cacheKey) as IMenuItemController;
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex);
-                return null;
-            }
         }
     }
 }
