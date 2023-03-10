452cf869b4cc0d923554633a4e8cf1e15d94ed0c
Brian Dukes <bdukes@engagesoftware.com>
2023-03-10 07:59:26 -0600

    Enable DI for IExtensionController

--- Dnn.AdminExperience/Library/Dnn.PersonaBar.UI/Services/MenuExtensionsController.cs
+++ Dnn.AdminExperience/Library/Dnn.PersonaBar.UI/Services/MenuExtensionsController.cs
@@ -1,16 +1,13 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information
-
 namespace Dnn.PersonaBar.UI.Services
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using System.Net;
     using System.Net.Http;
-    using System.Web;
     using System.Web.Http;
 
     using Dnn.PersonaBar.Library;
@@ -18,16 +15,36 @@ namespace Dnn.PersonaBar.UI.Services
     using Dnn.PersonaBar.Library.Controllers;
     using Dnn.PersonaBar.Library.Model;
     using Dnn.PersonaBar.Library.Repository;
+
+    using DotNetNuke.Common;
     using DotNetNuke.Framework;
     using DotNetNuke.Instrumentation;
     using DotNetNuke.Services.Exceptions;
+
+    using Microsoft.Extensions.DependencyInjection;
 
     [MenuPermission(Scope = ServiceScope.Regular)]
     public class MenuExtensionsController : PersonaBarApiController
     {
         private static readonly DnnLogger Logger = DnnLogger.GetClassLogger(typeof(MenuExtensionsController));
+        private readonly IServiceProvider serviceProvider;
+
+        /// <summary>Initializes a new instance of the <see cref="MenuExtensionsController"/> class.</summary>
+        [Obsolete("Deprecated in DotNetNuke 10.0.0. Please use overload with IServiceProvider. Scheduled removal in v12.0.0.")]
+        public MenuExtensionsController()
+            : this(Globals.GetCurrentServiceProvider())
+        {
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="MenuExtensionsController"/> class.</summary>
+        /// <param name="serviceProvider">The DI container.</param>
+        public MenuExtensionsController(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
 
         /// <summary>Retrieve a list of extensions for menu.</summary>
+        /// <param name="menu">The menu identifier.</param>
         /// <returns>A response with a collection of extension info.</returns>
         [HttpGet]
         public HttpResponseMessage GetExtensions(string menu)
@@ -35,7 +52,6 @@ namespace Dnn.PersonaBar.UI.Services
             try
             {
                 var menuItem = PersonaBarRepository.Instance.GetMenuItem(menu);
-
                 if (menuItem != null)
                 {
                     var extensions = PersonaBarExtensionRepository.Instance.GetExtensions(menuItem.MenuId)
@@ -83,24 +99,22 @@ namespace Dnn.PersonaBar.UI.Services
         private IDictionary<string, object> GetExtensionSettings(PersonaBarExtension extension)
         {
             var extensionController = this.GetExtensionController(extension);
-            var settings = extensionController?.GetSettings(extension);
-            return settings;
+            return extensionController?.GetSettings(extension);
         }
 
         private IExtensionController GetExtensionController(PersonaBarExtension extension)
         {
-            var identifier = extension.Identifier;
-            var controller = extension.Controller;
-
-            if (string.IsNullOrEmpty(controller))
+            var controllerTypeName = extension.Controller;
+            if (string.IsNullOrEmpty(controllerTypeName))
             {
                 return null;
             }
 
             try
             {
-                var cacheKey = $"PersonaBarExtensionController_{identifier}";
-                return Reflection.CreateObject(controller, cacheKey) as IExtensionController;
+                var cacheKey = $"PersonaBarExtensionController_{extension.Identifier}";
+                var controllerType = Reflection.CreateType(controllerTypeName, cacheKey, useCache: true);
+                return ActivatorUtilities.GetServiceOrCreateInstance(this.serviceProvider, controllerType) as IExtensionController;
             }
             catch (Exception ex)
             {
