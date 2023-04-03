9163ea7c20f0d242455f023af6fd0301198ccbb8
Brian Dukes <bdukes@engagesoftware.com>
2023-04-03 13:56:50 -0500

    Move PolyDeploy.Encryption to DotNetNuke.BulkInstall.Encryption

---
+++ DotNetNuke.BulkInstall/DotNetNuke.BulkInstall.Encryption/CryptoUtilities.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+namespace DotNetNuke.BulkInstall.Encryption
+{
+    using System.Security.Cryptography;
+    using System.Text;
+
+    /// <summary>
+    /// Provides useful utility methods which may not easily be grouped
+    /// elsewhere.
+    /// </summary>
+    public static class CryptoUtilities
+    {
+        /// <summary>
+        /// Generates a byte array of the length specified filled with random
+        /// bytes.
+        /// </summary>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        public static byte[] GenerateRandomBytes(int length)
+        {
+            // Create a new byte array of the size required.
+            var bytes = new byte[length];
+
+            using RNGCryptoServiceProvider rngCsp = new();
+            // Fill it with random bytes.
+            rngCsp.GetBytes(bytes);
+
+            return bytes;
+        }
+
+        public static string SHA256HashString(string value)
+        {
+            var bytes = SHA256HashBytes(value);
+
+            return bytes.Aggregate("", (current, part) => $"{current}{part:X2}");
+        }
+
+        /// <summary>
+        /// Hashes the passed value using the SHA256 algorithm.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte[] SHA256HashBytes(string value)
+        {
+            // Convert string to byte array.
+            var bytes = Encoding.UTF8.GetBytes(value);
+
+            using SHA256 sha = new SHA256Managed();
+            // Hash bytes.
+            var hashedBytes = sha.ComputeHash(bytes);
+
+            return hashedBytes;
+        }
+    }
+}
