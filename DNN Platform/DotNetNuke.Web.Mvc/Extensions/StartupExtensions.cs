126978968dad72d6a34f08778c6f91c811542bb4
Brian Dukes <bdukes@engagesoftware.com>
2023-06-23 15:35:57 -0500

    Log all loader errors at once, at warning level

--- DNN Platform/DotNetNuke.Web.Mvc/Extensions/StartupExtensions.cs
+++ DNN Platform/DotNetNuke.Web.Mvc/Extensions/StartupExtensions.cs
@@ -3,37 +3,31 @@
 // See the LICENSE file in the project root for more information
 namespace DotNetNuke.Web.Mvc.Extensions
 {
-    using System;
     using System.Linq;
 
     using DotNetNuke.DependencyInjection.Extensions;
     using DotNetNuke.Instrumentation;
     using DotNetNuke.Web.Mvc.Framework.Controllers;
+
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.DependencyInjection.Extensions;
 
-    /// <summary>
-    /// Adds DNN MVC Controller Specific startup extensions to simplify the
-    /// <see cref="Startup"/> Class.
-    /// </summary>
+    /// <summary>Adds DNN MVC Controller Specific startup extensions to simplify the <see cref="Startup"/> Class.</summary>
     public static class StartupExtensions
     {
         private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(StartupExtensions));
 
-        /// <summary>
-        /// Configures all of the <see cref="DnnController"/>'s to be used
-        /// with the Service Collection for Dependency Injection.
-        /// </summary>
-        /// <param name="services">
-        /// Service Collection used to registering services in the container.
-        /// </param>
+        /// <summary>Configures all of the <see cref="DnnController"/>'s to be used with the Service Collection for Dependency Injection.</summary>
+        /// <param name="services">Service Collection used to registering services in the container.</param>
         public static void AddMvcControllers(this IServiceCollection services)
         {
-            var controllerTypes = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(x => x.SafeGetTypes(Logger))
-                .Where(x => typeof(IDnnController).IsAssignableFrom(x)
-                    && x.IsClass
-                    && !x.IsAbstract);
+            var allTypes = TypeExtensions.SafeGetTypes();
+            allTypes.LogOtherExceptions(Logger);
+
+            var controllerTypes = allTypes.Types
+                .Where(
+                    type => typeof(IDnnController).IsAssignableFrom(type) &&
+                            type is { IsClass: true, IsAbstract: false });
             foreach (var controller in controllerTypes)
             {
                 services.TryAddTransient(controller);
