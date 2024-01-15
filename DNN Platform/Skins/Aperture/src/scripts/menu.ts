df383b7d231ecef5f22cd0c5a562ce03606e9461
David Poindexter <dpoindexter@nvisionative.com>
2024-01-15 00:46:06 -0500

    Aperture theme - mobile menu refinement

--- DNN Platform/Skins/Aperture/src/scripts/menu.ts
+++ DNN Platform/Skins/Aperture/src/scripts/menu.ts
@@ -12,6 +12,8 @@ export const navFunctions = (() => {
 				const target: HTMLAnchorElement | null = document.querySelector(anchorWithHash.hash);
 				// check if target exists
 				if (target !== null) {
+					// toggle mobile menu and overlay
+					hamburgerMenu.checked = !hamburgerMenu.checked;
 					// get target position
 					const targetTop = target.getBoundingClientRect().top;
 					// get window position
@@ -27,32 +29,37 @@ export const navFunctions = (() => {
 	});
 
 	// add event listeners to expands
-	const navExpand = document.querySelectorAll('.nav-expand');
-  
-	navExpand.forEach(item => {
-	  item.querySelector('.nav-expand-link')?.addEventListener('click', () => item.classList.add('active'));
-	  item.querySelector('.nav-back-link')?.addEventListener('click', () => item.classList.remove('active'));
+	const navExpand = [].slice.call(document.querySelectorAll('.nav-expand'));
+
+	navExpand.forEach((item: HTMLElement) => {
+		item.querySelector('.nav-expand-link')!.addEventListener('click', () => item.classList.add('active'));
+		item.querySelector('.nav-back-link')!.addEventListener('click', () => item.classList.remove('active'));
 	});
-  
+
 	// setup and add overlay
-	let overlay = document.createElement('div');
+	let overlay: HTMLDivElement = document.createElement('div');
 	overlay.setAttribute('id', 'body-overlay');
 	document.body.appendChild(overlay).classList.add('aperture-d-none');
-  
+
 	// target hamburger menu
-	const responsiveMenu = document.getElementById('nav-mobile');
-	const bodyOverlay = document.getElementById('body-overlay');
-  
-	responsiveMenu?.addEventListener('click', function() {
-	  // toggle menu
-	  document.body.classList.toggle('nav-is-toggled');
-	  bodyOverlay?.classList.toggle('aperture-d-none');
+	const responsiveMenuLinks: NodeListOf<HTMLAnchorElement> = document.querySelectorAll('#nav-mobile input[type^="checkbox"], #nav-mobile .nav-link:not(.nav-expand-link, .nav-back-link)');
+	const bodyOverlay: HTMLElement = document.getElementById('body-overlay')!;
+	const hamburgerMenu: HTMLInputElement = document.querySelector('#menuToggle input[type="checkbox"]')!;
+
+	responsiveMenuLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			// toggle menu
+			console.log('responsiveMenu clicked');
+			document.body.classList.toggle('nav-is-toggled');
+			bodyOverlay.classList.toggle('aperture-d-none');
+		});
 	});
-  
+
 	// if overlay is clicked
-	bodyOverlay?.addEventListener('click', function() {
-	  // toggle menu and overlay
-	  document.body.classList.toggle('nav-is-toggled');
-	  bodyOverlay?.classList.toggle('aperture-d-none');
+	bodyOverlay.addEventListener('click', () => {
+		// toggle menu and overlay
+		document.body.classList.toggle('nav-is-toggled');
+		hamburgerMenu.checked = !hamburgerMenu.checked;
+		bodyOverlay.classList.toggle('aperture-d-none');
 	});
-  });
+});
