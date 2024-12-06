3d9f3ff94aebfb0345a99f8bbdaffa9817a3efb8
Peter Donker <peter@bring2mind.net>
2024-12-06 11:20:18 +0100

    Deprecate DnnLogger

--- Dnn.AdminExperience/Library/Dnn.PersonaBar.Library/Controllers/PersonaBarController.cs
+++ Dnn.AdminExperience/Library/Dnn.PersonaBar.Library/Controllers/PersonaBarController.cs
@@ -22,7 +22,7 @@ namespace Dnn.PersonaBar.Library.Controllers
 
     public class PersonaBarController : ServiceLocator<IPersonaBarController, PersonaBarController>, IPersonaBarController
     {
-        private static readonly DnnLogger Logger = DnnLogger.GetClassLogger(typeof(PersonaBarController));
+        private static readonly ILog Logger = LoggerSource.Instance.GetLogger(typeof(PersonaBarController));
 
         private readonly IPersonaBarRepository personaBarRepository;
 
