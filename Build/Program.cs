52ee00c6c4a3768204862f1f460d4102e0646272
Brian Dukes <bdukes@engagesoftware.com>
2025-01-31 09:30:51 -0600

    Bump NuGet.CommandLine from 6.5.0 to 6.12.2

--- Build/Program.cs
+++ Build/Program.cs
@@ -30,7 +30,7 @@ namespace DotNetNuke.Build
                 .InstallTool(new Uri("dotnet:?package=GitVersion.Tool&version=6.1.0"))
                 .InstallTool(new Uri("nuget:?package=Microsoft.TestPlatform&version=" + MicrosoftTestPlatformVersion))
                 .InstallTool(new Uri("nuget:?package=NUnit3TestAdapter&version=" + NUnit3TestAdapterVersion))
-                .InstallTool(new Uri("nuget:?package=NuGet.CommandLine&version=6.5.0"))
+                .InstallTool(new Uri("nuget:?package=NuGet.CommandLine&version=6.12.2"))
                 .Run(args);
         }
     }
