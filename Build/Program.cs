2305465c65867eb195f838825db104c348f930ac
Brian Dukes <bdukes@engagesoftware.com>
2025-01-31 09:30:08 -0600

    Bump Microsoft.Test.Platform from 7.6.2 to 7.12.0

--- Build/Program.cs
+++ Build/Program.cs
@@ -12,7 +12,7 @@ namespace DotNetNuke.Build
     public class Program
     {
         /// <summary>The version of the Microsoft.TestPlatform NuGet package.</summary>
-        internal const string MicrosoftTestPlatformVersion = "17.6.2";
+        internal const string MicrosoftTestPlatformVersion = "17.12.0";
 
         /// <summary>The version of the NUnit3TestAdapter NuGet package.</summary>
         internal const string NUnit3TestAdapterVersion = "4.5.0";
