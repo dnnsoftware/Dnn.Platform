825ae25da7f3a92271a62a1e57f7becdddb325b2
Peter Donker <peter@bring2mind.net>
2025-03-24 13:12:11 +0100

    Ability to replace the urlrewriter

---
+++ DNN Platform/DotNetNuke.Web.MvcPipeline/HttpModules/MvcUrlRewriteModule.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using DotNetNuke.Web.MvcPipeline.Entities.Urls;
+
+namespace DotNetNuke.Web.MvcPipeline.HttpModules
+{
+    public class MvcUrlRewriteModule : IHttpModule
+    {
+        public void Dispose()
+        {
+        }
+
+        public void Init(HttpApplication context)
+        {
+            var mvcRewriter = new MvcAdvancedUrlRewriter();
+            context.BeginRequest += mvcRewriter.RewriteUrl;
+        }
+    }
+}
