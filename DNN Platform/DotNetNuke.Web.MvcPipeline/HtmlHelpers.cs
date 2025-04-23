f3dab1b3316855e18238fa407c23d385ba89af06
Sacha <sachatrauwaen@gmail.com>
2025-04-23 11:27:43 +0200

    add support for mvc modules

--- DNN Platform/DotNetNuke.Web.MvcPipeline/HtmlHelpers.cs
+++ DNN Platform/DotNetNuke.Web.MvcPipeline/HtmlHelpers.cs
@@ -5,21 +5,32 @@
 namespace DotNetNuke.Web.MvcPipeline
 {
     using System;
+    using System.Net.Http;
     using System.Web;
     using System.Web.Helpers;
     using System.Web.Mvc;
     using System.Web.Mvc.Html;
-
+    using System.Web.Routing;
+    using DotNetNuke.Common.Utilities;
     using DotNetNuke.Entities.Modules;
     using DotNetNuke.Framework;
+    using DotNetNuke.Services.Localization;
+    using DotNetNuke.UI.Modules;
 
     // using DotNetNuke.Framework.Models;
+    using DotNetNuke.Collections;
     using DotNetNuke.Web.MvcPipeline.Framework;
     using DotNetNuke.Web.MvcPipeline.Models;
     using DotNetNuke.Web.MvcPipeline.UI.Utilities;
+    using System.Linq;
+    using System.Reflection;
 
     public static partial class HtmlHelpers
     {
+
+        private const string ExcludedQueryStringParams = "tabid,mid,ctl,language,popup,action,controller";
+        private const string ExcludedRouteValues = "mid,ctl,popup";
+
         public static IHtmlString ViewComponent(this HtmlHelper htmlHelper, string controllerName, object model)
         {
             return htmlHelper.Action("Invoke", controllerName, model);
@@ -44,19 +55,113 @@ namespace DotNetNuke.Web.MvcPipeline
         {
             try
             {
-                return htmlHelper.Action(
-                    "Invoke",
-                    MvcUtils.GetControlControllerName(controlSrc),
-                    new ControlViewModel()
+                if (module.ModuleControl.ControlSrc.EndsWith(".mvc", System.StringComparison.OrdinalIgnoreCase))
+                {
+                    var controlKey = module.ModuleControl.ControlKey;
+                    var segments = module.ModuleControl.ControlSrc.Replace(".mvc", string.Empty).Split('/');
+
+                    var localResourceFile = string.Format(
+                        "~/DesktopModules/MVC/{0}/{1}/{2}.resx",
+                        module.DesktopModule.FolderName,
+                        Localization.LocalResourceDirectory,
+                        segments[0]);
+
+                    RouteValueDictionary values = new RouteValueDictionary();
+
+                    var queryString = htmlHelper.ViewContext.HttpContext.Request.QueryString;
+
+                    if (string.IsNullOrEmpty(controlKey))
                     {
-                        ModuleId = module.ModuleID,
-                        TabId = module.TabID,
-                        ModuleControlId = module.ModuleControlId,
-                        PanaName = module.PaneName,
-                        ContainerSrc = module.ContainerSrc,
-                        ContainerPath = module.ContainerPath,
-                        IconFile = module.IconFile,
-                    });
+                        controlKey = queryString.GetValueOrDefault("ctl", string.Empty);
+                    }
+
+                    var moduleId = Null.NullInteger;
+                    if (queryString["moduleid"] != null)
+                    {
+                        int.TryParse(queryString["moduleid"], out moduleId);
+                    }
+
+                    string routeNamespace = string.Empty;
+                    string routeControllerName;
+                    string routeActionName;
+                    if (segments.Length == 3)
+                    {
+                        routeNamespace = segments[0];
+                        routeControllerName = segments[1];
+                        routeActionName = segments[2];
+                    }
+                    else
+                    {
+                        routeControllerName = segments[0];
+                        routeActionName = segments[1];
+                    }
+                    string controllerName;
+                    string actionName;
+                    if (moduleId != module.ModuleID && string.IsNullOrEmpty(controlKey))
+                    {
+                        // Set default routeData for module that is not the "selected" module
+                        actionName = routeActionName;
+                        controllerName = routeControllerName;
+
+                        // routeData.Values.Add("controller", controllerName);
+                        // routeData.Values.Add("action", actionName);
+
+                        if (!string.IsNullOrEmpty(routeNamespace))
+                        {
+                            // routeData.DataTokens.Add("namespaces", new string[] { routeNamespace });
+                        }
+                    }
+                    else
+                    {
+                        var control = ModuleControlController.GetModuleControlByControlKey(controlKey, module.ModuleDefID);
+                        actionName = queryString.GetValueOrDefault("action", routeActionName);
+                        controllerName = queryString.GetValueOrDefault("controller", routeControllerName);
+
+                        // values.Add("controller", controllerName);
+                        // values.Add("action", actionName);
+
+                        foreach (var param in queryString.AllKeys)
+                        {
+                            if (!ExcludedQueryStringParams.Split(',').ToList().Contains(param.ToLowerInvariant()))
+                            {
+                                values.Add(param, queryString[param]);
+                            }
+                        }
+
+                        if (!string.IsNullOrEmpty(routeNamespace))
+                        {
+                            // routeData.DataTokens.Add("namespaces", new string[] { routeNamespace });
+                        }
+
+                        values.Add("ModuleId", module.ModuleID);
+                        values.Add("TabId", module.TabID);
+                        values.Add("ModuleControlId", module.ModuleControlId);
+                        values.Add("PanaName", module.PaneName);
+                        values.Add("ContainerSrc", module.ContainerSrc);
+                        values.Add("ContainerPath", module.ContainerPath);
+                        values.Add("IconFile", module.IconFile);
+                    }
+                    return htmlHelper.Action(
+                            actionName,
+                            controllerName,
+                            values);
+                }
+                else
+                {
+                    return htmlHelper.Action(
+                        "Invoke",
+                        MvcUtils.GetControlControllerName(controlSrc),
+                        new ControlViewModel()
+                        {
+                            ModuleId = module.ModuleID,
+                            TabId = module.TabID,
+                            ModuleControlId = module.ModuleControlId,
+                            PanaName = module.PaneName,
+                            ContainerSrc = module.ContainerSrc,
+                            ContainerPath = module.ContainerPath,
+                            IconFile = module.IconFile,
+                        });
+                }
             }
             catch (Exception ex)
             {
@@ -66,26 +171,7 @@ namespace DotNetNuke.Web.MvcPipeline
 
         public static IHtmlString Control(this HtmlHelper htmlHelper, ModuleInfo module)
         {
-            try
-            {
-                return htmlHelper.Action(
-                    "Invoke",
-                    MvcUtils.GetControlControllerName(module.ModuleControl.ControlSrc),
-                    new ControlViewModel()
-                    {
-                        ModuleId = module.ModuleID,
-                        TabId = module.TabID,
-                        ModuleControlId = module.ModuleControlId,
-                        PanaName = module.PaneName,
-                        ContainerSrc = module.ContainerSrc,
-                        ContainerPath = module.ContainerPath,
-                        IconFile = module.IconFile,
-                    });
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"{ex.Message} - {MvcUtils.GetControlControllerName(module.ModuleControl.ControlSrc)} - Invoke", ex);
-            }
+            return htmlHelper.Control(module.ModuleControl.ControlSrc, module);
         }
 
         public static IHtmlString CspNonce(this HtmlHelper htmlHelper)
