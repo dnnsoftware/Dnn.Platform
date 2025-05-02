09abe33badeaa2fc6956824de8b2249c317ba9f6
Sacha <sachatrauwaen@gmail.com>
2025-05-02 21:37:41 +0200

    fix skin build, issues when extension not provided

--- DNN Platform/Skins/Aperture/build.ts
+++ DNN Platform/Skins/Aperture/build.ts
@@ -54,17 +54,17 @@ const transpiledFiles: TranspiledFileConfig[] = [
 ];
 
 const copyFiles: StaticFileConfig[] = [
-  { src: "containers/*.ascx", dest: containersDist },
-  { src: "containers/Views/*", dest: containersDist + "/Views" },
-  { src: "menus/desktop/*", dest: skinDist + "/menus/desktop" },
-  { src: "menus/footer/*", dest: skinDist + "/menus/footer" },
-  { src: "menus/mobile/*", dest: skinDist + "/menus/mobile" },
-  { src: "partials/*", dest: skinDist + "/partials" },
-  { src: "src/fonts/*", dest: skinDist + "/fonts" },
-  { src: "src/images/*", dest: skinDist + "/images" },
-  { src: "Views/*", dest: skinDist + "/Views" },
-  { src: "Views/partials/*", dest: skinDist + "/Views/partials" },
-  { src: "*.{ascx,png,dnn,xml,txt}", dest: skinDist },
+    { src: "containers/*.ascx", dest: containersDist },
+    { src: "containers/Views/*", dest: containersDist + "/Views" },
+    { src: "menus/desktop/*", dest: skinDist + "/menus/desktop" },
+    { src: "menus/footer/*", dest: skinDist + "/menus/footer" },
+    { src: "menus/mobile/*", dest: skinDist + "/menus/mobile" },
+    { src: "partials/*", dest: skinDist + "/partials" },
+    { src: "src/fonts/*", dest: skinDist + "/fonts" },
+    { src: "src/images/*", dest: skinDist + "/images" },
+    { src: "Views/*.{cshtml,config}", dest: skinDist + "/Views" },
+    { src: "Views/partials/*.cshtml", dest: skinDist + "/Views/partials" },
+    { src: "*.{ascx,png,dnn,xml,txt}", dest: skinDist },
 ];
 
 /** Normalizes a path (windows vs linux, etc.) */
