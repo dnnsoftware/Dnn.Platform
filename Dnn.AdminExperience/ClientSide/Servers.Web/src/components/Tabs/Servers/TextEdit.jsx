ede9bc97b8b80e17b65bc543740ace3159f00a25
Daniel <info@danielvaladas.com>
2025-05-17 03:02:55 -0400

    Updated Servers module to use eslint v9

--- Dnn.AdminExperience/ClientSide/Servers.Web/src/components/Tabs/Servers/TextEdit.jsx
+++ Dnn.AdminExperience/ClientSide/Servers.Web/src/components/Tabs/Servers/TextEdit.jsx
@@ -1,38 +1,39 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { SingleLineInputWithError, IconButton } from "@dnnsoftware/dnn-react-common";
 
 function TextEdit(props) {
-  if (props.inEdit) {
-    return (
-      <SingleLineInputWithError
-        value={props.text}
-        type="Text"
-        onChange={(e) => props.onChange(e.target.value)}
-        error={!!props.error}
-        errorMessage={props.error}
-      />
-    );
-  } else {
-    return (
-      <div>
-        <span>{props.text}</span>
-        <IconButton
-          type="edit"
-          width="16"
-          height="16"
-          onClick={props.toggleEdit.bind(this)}
-        />
-      </div>
-    );
-  }
+    if (props.inEdit) {
+        return (
+            <SingleLineInputWithError
+                value={props.text}
+                type="Text"
+                onChange={(e) => props.onChange(e.target.value)}
+                error={!!props.error}
+                errorMessage={props.error}
+            />
+        );
+    } else {
+        return (
+            <div>
+                <span>{props.text}</span>
+                <IconButton
+                    type="edit"
+                    width="16"
+                    height="16"
+                    onClick={props.toggleEdit.bind(this)}
+                />
+            </div>
+        );
+    }
 }
 
 export default TextEdit;
 
 TextEdit.propTypes = {
-  text: PropTypes.string,
-  inEdit: PropTypes.bool,
-  toggleEdit: PropTypes.func,
-  onChange: PropTypes.func,
+    text: PropTypes.string,
+    inEdit: PropTypes.bool,
+    toggleEdit: PropTypes.func,
+    onChange: PropTypes.func,
+    error: PropTypes.string,
 };
--- Dnn.AdminExperience/ClientSide/Servers.Web/src/components/common/EditBlock.jsx
+++ Dnn.AdminExperience/ClientSide/Servers.Web/src/components/common/EditBlock.jsx
@@ -1,47 +1,47 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import {
-  Label,
-  InputGroup,
-  SingleLineInputWithError,
+    Label,
+    InputGroup,
+    SingleLineInputWithError,
 } from "@dnnsoftware/dnn-react-common";
 import GlobalIcon from "./GlobalIcon";
 
 export default class EditBlock extends Component {
-  render() {
-    const { props } = this;
+    render() {
+        const { props } = this;
 
-    return (
-      <InputGroup>
-        <Label
-          className="title"
-          tooltipMessage={props.tooltip}
-          label={props.label}
-          style={{ width: "auto" }}
-        />
-        {props.isGlobal && <GlobalIcon />}
-        <SingleLineInputWithError
-          value={props.value}
-          type={props.type}
-          onChange={props.onChange}
-          error={!!props.error}
-          errorMessage={props.error}
-        />
-      </InputGroup>
-    );
-  }
+        return (
+            <InputGroup>
+                <Label
+                    className="title"
+                    tooltipMessage={props.tooltip}
+                    label={props.label}
+                    style={{ width: "auto" }}
+                />
+                {props.isGlobal && <GlobalIcon />}
+                <SingleLineInputWithError
+                    value={props.value}
+                    type={props.type}
+                    onChange={props.onChange}
+                    error={!!props.error}
+                    errorMessage={props.error}
+                />
+            </InputGroup>
+        );
+    }
 }
 
 EditBlock.propTypes = {
-  label: PropTypes.string,
-  tooltip: PropTypes.string,
-  value: PropTypes.string,
-  isGlobal: PropTypes.bool.isRequired,
-  type: PropTypes.string,
-  onChange: PropTypes.func,
-  error: PropTypes.string,
+    label: PropTypes.string,
+    tooltip: PropTypes.string,
+    value: PropTypes.string,
+    isGlobal: PropTypes.bool.isRequired,
+    type: PropTypes.string,
+    onChange: PropTypes.func,
+    error: PropTypes.string,
 };
 
 EditBlock.defaultProps = {
-  isGlobal: false,
+    isGlobal: false,
 };
