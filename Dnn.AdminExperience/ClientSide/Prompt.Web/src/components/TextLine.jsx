eaf6ff4bee98aba0040a63b9987849d10cfa74a5
Brian Dukes <bdukes@engagesoftware.com>
2025-06-03 12:42:08 -0500

    Respect isHtml prop in primary output

--- Dnn.AdminExperience/ClientSide/Prompt.Web/src/components/TextLine.jsx
+++ Dnn.AdminExperience/ClientSide/Prompt.Web/src/components/TextLine.jsx
@@ -3,12 +3,13 @@ import PropTypes from "prop-types";
 import Html from "./Html";
 import DomKey from "../services/DomKey";
 
-const TextLine = ({txt, css}) => {
+const TextLine = ({txt, css, isHtml}) => {
     if (!txt) {
         return null;
     }
     const textLines = txt.split("\\n");
-    const rows = textLines.map((line) => line ? <span key={DomKey.get("textline")} className={css}><Html html={line} /></span> : null).reduce((prev,current) => {
+    const rows = textLines.map((line) => line ? <span key={DomKey.get("textline")} className={css}>{isHtml ? <Html html={line} /> : line}</span> : null)
+        .reduce((prev,current) => {
         if (current !== "" && current !== null && current !== undefined) {
             return [...prev,current];
         }
@@ -21,7 +22,8 @@ TextLine.defaultProps = { css: "dnn-prompt-cmd" };
 
 TextLine.propTypes = {
     txt: PropTypes.string.isRequired,
-    css: PropTypes.string
+    css: PropTypes.string,
+    isHtml: PropTypes.bool
 };
 
 export default TextLine;
