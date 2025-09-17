eb790e0f0113e074d694073b2b246c079671af70
Sacha <sachatrauwaen@gmail.com>
2025-09-17 14:04:08 +0200

    ad mvcmodelclass to db for html module

---
+++ DNN Platform/Modules/HTML/Controls/EditHTMLControl.cs
@@ -0,0 +1,155 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+namespace DotNetNuke.Modules.Html.Controls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Net.NetworkInformation;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using DotNetNuke.Abstractions;
+    using DotNetNuke.Common;
+    using DotNetNuke.Common.Utilities;
+    using DotNetNuke.ContentSecurityPolicy;
+    using DotNetNuke.Entities.Content.Workflow;
+    using DotNetNuke.Entities.Content.Workflow.Entities;
+    using DotNetNuke.Entities.Modules;
+    using DotNetNuke.Entities.Modules.Settings;
+    using DotNetNuke.Framework.JavaScriptLibraries;
+    using DotNetNuke.Modules.Html;
+    using DotNetNuke.Modules.Html.Components;
+    using DotNetNuke.Modules.Html.Models;
+    using DotNetNuke.Security;
+    using DotNetNuke.Services.Exceptions;
+    using DotNetNuke.Services.Localization;
+    using DotNetNuke.Web.Client.ClientResourceManagement;
+    using DotNetNuke.Web.MvcPipeline.Controllers;
+    using DotNetNuke.Web.MvcPipeline.ModuleControl;
+    using DotNetNuke.Web.MvcPipeline.ModuleControl.Razor;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class EditHTMLControl : RazorModuleControlBase
+    {
+        private readonly INavigationManager navigationManager;
+        private readonly HtmlTextController htmlTextController;
+        private readonly IWorkflowManager workflowManager = WorkflowManager.Instance;
+        private readonly IContentSecurityPolicy contentSecurityPolicy;
+
+        public EditHTMLControl(IContentSecurityPolicy csp)
+        {
+            this.navigationManager = Globals.DependencyProvider.GetRequiredService<INavigationManager>();
+            this.htmlTextController = new HtmlTextController(this.navigationManager);
+            this.contentSecurityPolicy = csp;
+        }
+
+        /*
+        public override string ControlPath => "~/DesktopModules/Html/";
+
+        public override string ID => "EditHTML";
+        */
+        public override IRazorModuleResult Invoke()
+        {
+            var model = new EditHtmlViewModel();
+            try
+            {
+                model.LocalResourceFile = this.LocalResourceFile;
+                model.ShowEditView = true;
+                model.ModuleId = this.ModuleId;
+                model.TabId = this.TabId;
+                model.PortalId = this.PortalId;
+                model.RedirectUrl = this.navigationManager.NavigateURL();
+                int workflowID = this.htmlTextController.GetWorkflow(this.ModuleId, this.TabId, this.PortalId).Value;
+
+                var htmlContentItemID = Null.NullInteger;
+                var htmlContent = this.htmlTextController.GetTopHtmlText(this.ModuleId, false, workflowID);
+
+                if (htmlContent != null)
+                {
+                    htmlContentItemID = htmlContent.ItemID;
+                    var html = System.Web.HttpUtility.HtmlDecode(htmlContent.Content);
+                    model.EditorContent = html;
+                }
+
+                var workflow = this.workflowManager.GetWorkflow(workflowID);
+                var workflowStates = workflow.States.ToList();
+                model.MaxVersions = this.htmlTextController.GetMaximumVersionHistory(this.PortalId);
+                var userCanEdit = this.UserInfo.IsSuperUser || PortalSecurity.IsInRole(this.PortalSettings.AdministratorRoleName);
+
+                model.PageSize = Math.Min(Math.Max(model.MaxVersions, 5), 10); // min 5, max 10
+
+                switch (workflow.WorkflowKey)
+                {
+                    case SystemWorkflowManager.DirectPublishWorkflowKey:
+                        model.CurrentWorkflowType = WorkflowType.DirectPublish;
+                        break;
+                    case SystemWorkflowManager.SaveDraftWorkflowKey:
+                        model.CurrentWorkflowType = WorkflowType.SaveDraft;
+                        break;
+                    case SystemWorkflowManager.ContentAprovalWorkflowKey:
+                        model.CurrentWorkflowType = WorkflowType.ContentApproval;
+                        break;
+                }
+
+                if (htmlContentItemID != -1)
+                {
+                    this.PopulateModelWithContent(model, htmlContent);
+                }
+                else
+                {
+                    this.PopulateModelWithInitialContent(model, workflowStates[0]);
+                }
+
+                model.ShowPublishOption = model.CurrentWorkflowType != WorkflowType.DirectPublish;
+                model.ShowCurrentVersion = model.CurrentWorkflowType != WorkflowType.DirectPublish;
+                model.ShowPreviewVersion = model.CurrentWorkflowType != WorkflowType.DirectPublish;
+                model.ShowHistoryView = false;
+                model.ShowMasterContentButton = false;
+            }
+            catch (Exception exc)
+            {
+                // Exceptions.ProcessModuleLoadException(this, exc);
+                throw new Exception("EditHTML", exc);
+            }
+
+            /*
+            this.RegisterScript("~/Resources/Shared/scripts/jquery/jquery.form.min.js");
+            this.RegisterStyleSheet("~/Portals/_default/Skins/_default/WebControlSkin/Default/GridView.default.css");
+            this.RegisterStyleSheet("~/DesktopModules/HTML/edit.css");
+            this.RegisterScript("~/DesktopModules/HTML/js/edit.js");
+            */
+            /*
+            MvcClientResourceManager.RegisterScript(this.ControllerContext, "~/Resources/Shared/scripts/jquery/jquery.form.min.js");
+            MvcClientResourceManager.RegisterStyleSheet(this.ControllerContext, "~/Portals/_default/Skins/_default/WebControlSkin/Default/GridView.default.css");
+            MvcClientResourceManager.RegisterStyleSheet(this.ControllerContext, "~/DesktopModules/HTML/edit.css");
+            MvcClientResourceManager.RegisterScript(this.ControllerContext, "~/DesktopModules/HTML/js/edit.js");
+            */
+
+            this.contentSecurityPolicy.StyleSource.AddInline();
+            this.contentSecurityPolicy.ScriptSource.AddSelf().AddInline();
+            this.contentSecurityPolicy.ImgSource.AddScheme("data:");
+            return this.View(model);
+        }
+
+        private void PopulateModelWithContent(EditHtmlViewModel model, HtmlTextInfo htmlContent)
+        {
+            model.CurrentWorkflowInUse = htmlContent.WorkflowName;
+            model.CurrentWorkflowState = htmlContent.StateName;
+            model.CurrentVersion = htmlContent.Version.ToString();
+
+            // model.Content = this.FormatContent(htmlContent.Content);
+        }
+
+        private void PopulateModelWithInitialContent(EditHtmlViewModel model, WorkflowState firstState)
+        {
+            // model.EditorContent = this.LocalizeString("AddContent");
+            model.CurrentWorkflowInUse = firstState.StateName;
+            model.ShowCurrentWorkflowState = false;
+            model.ShowCurrentVersion = false;
+        }
+    }
+}
---
+++ DNN Platform/Modules/HTML/Controls/MyWorkControl.cs
@@ -0,0 +1,104 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+namespace DotNetNuke.Modules.Html.Controls
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using DotNetNuke.Abstractions;
+    using DotNetNuke.Common;
+    using DotNetNuke.Entities.Content.Workflow.Entities;
+    using DotNetNuke.Entities.Modules;
+    using DotNetNuke.Entities.Modules.Actions;
+    using DotNetNuke.Entities.Modules.Settings;
+    using DotNetNuke.Framework.JavaScriptLibraries;
+    using DotNetNuke.Modules.Html;
+    using DotNetNuke.Modules.Html.Components;
+    using DotNetNuke.Modules.Html.Models;
+    using DotNetNuke.Security;
+    using DotNetNuke.Services.Exceptions;
+    using DotNetNuke.Services.Localization;
+    using DotNetNuke.Web.Client.ClientResourceManagement;
+    using DotNetNuke.Web.Mvc;
+    using DotNetNuke.Web.MvcPipeline.Controllers;
+    using DotNetNuke.Web.MvcPipeline.ModuleControl;
+    using DotNetNuke.Web.MvcPipeline.ModuleControl.Razor;
+    using DotNetNuke.Web.MvcPipeline.ModuleControl.Resources;
+    using DotNetNuke.Website.Controllers;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class MyWorkControl : RazorModuleControlBase, IActionable, IResourcable
+    {
+        private readonly INavigationManager navigationManager;
+
+        public MyWorkControl()
+        {
+            this.navigationManager = Globals.DependencyProvider.GetRequiredService<INavigationManager>();
+        }
+
+        public ModuleActionCollection ModuleActions
+        {
+            get
+            {
+                var actions = new ModuleActionCollection();
+                actions.Add(
+                    this.GetNextActionID(),
+                    Localization.GetString(ModuleActionType.AddContent, this.LocalResourceFile),
+                    ModuleActionType.AddContent,
+                    string.Empty,
+                    string.Empty,
+                    this.EditUrl(),
+                    false,
+                    SecurityAccessLevel.Edit,
+                    true,
+                    false);
+
+                return actions;
+            }
+        }
+
+        public ModuleResources ModuleResources
+        {
+            get
+            {
+                return new ModuleResources()
+                {
+                    StyleSheets = new List<ModuleStyleSheet>()
+                    {
+                        new ModuleStyleSheet()
+                        {
+                            FilePath = "~/DesktopModules/HTML/edit.css",
+                        },
+                        new ModuleStyleSheet()
+                        {
+                            FilePath = "~/Portals/_default/Skins/_default/WebControlSkin/Default/GridView.default.css",
+                        },
+                    },
+                };
+            }
+        }
+
+        public override IRazorModuleResult Invoke()
+        {
+            var objHtmlTextUsers = new HtmlTextUserController();
+            var lst = objHtmlTextUsers.GetHtmlTextUser(this.UserInfo.UserID).Cast<HtmlTextUserInfo>();
+
+            return this.View(new MyWorkModel()
+            {
+                LocalResourceFile = this.LocalResourceFile,
+                ModuleId = this.ModuleId,
+                TabId = this.TabId,
+                RedirectUrl = this.navigationManager.NavigateURL(),
+                HtmlTextUsers = lst.Select(u => new HtmlTextUserModel()
+                {
+                    Url = this.navigationManager.NavigateURL(u.TabID),
+                    ModuleID = u.ModuleID,
+                    ModuleTitle = u.ModuleTitle,
+                    StateName = u.StateName,
+                }).ToList(),
+            });
+        }
+    }
+}
