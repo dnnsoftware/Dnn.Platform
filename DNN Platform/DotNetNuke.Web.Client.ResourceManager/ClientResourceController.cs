2b18975bfc93c040896454076471d3dec47cb677
Peter Donker <peter@bring2mind.net>
2025-10-03 16:40:19 +0200

    Fix for empty providers

--- DNN Platform/DotNetNuke.Web.Client.ResourceManager/ClientResourceController.cs
+++ DNN Platform/DotNetNuke.Web.Client.ResourceManager/ClientResourceController.cs
@@ -126,7 +126,7 @@ namespace DotNetNuke.Web.Client.ResourceManager
             var sortedList = new List<string>();
             if (resourceType == ResourceType.Font || resourceType == ResourceType.All)
             {
-                foreach (var link in this.Fonts.Where(s => s.Provider == provider && !this.FontsToExclude.Contains(s.Name.ToLowerInvariant())).OrderBy(l => l.Priority))
+                foreach (var link in this.Fonts.Where(s => (s.Provider == provider || (s.Provider == string.Empty && provider == ClientResourceProviders.DefaultCssProvider)) && !this.FontsToExclude.Contains(s.Name.ToLowerInvariant())).OrderBy(l => l.Priority))
                 {
                     sortedList.Add(link.Render(this.hostSettings.CrmVersion, this.hostSettings.CdnEnabled, applicationPath));
                 }
@@ -134,7 +134,7 @@ namespace DotNetNuke.Web.Client.ResourceManager
 
             if (resourceType == ResourceType.Stylesheet || resourceType == ResourceType.All)
             {
-                foreach (var link in this.Stylesheets.Where(s => s.Provider == provider && !this.StylesheetsToExclude.Contains(s.Name.ToLowerInvariant())).OrderBy(l => l.Priority))
+                foreach (var link in this.Stylesheets.Where(s => (s.Provider == provider || (s.Provider == string.Empty && provider == ClientResourceProviders.DefaultCssProvider)) && !this.StylesheetsToExclude.Contains(s.Name.ToLowerInvariant())).OrderBy(l => l.Priority))
                 {
                     sortedList.Add(link.Render(this.hostSettings.CrmVersion, this.hostSettings.CdnEnabled, applicationPath));
                 }
@@ -142,7 +142,7 @@ namespace DotNetNuke.Web.Client.ResourceManager
 
             if (resourceType == ResourceType.Script || resourceType == ResourceType.All)
             {
-                foreach (var script in this.Scripts.Where(s => s.Provider == provider && !this.ScriptsToExclude.Contains(s.Name.ToLowerInvariant())).OrderBy(s => s.Priority))
+                foreach (var script in this.Scripts.Where(s => (s.Provider == provider || (s.Provider == string.Empty && provider == ClientResourceProviders.DefaultJsProvider)) && !this.ScriptsToExclude.Contains(s.Name.ToLowerInvariant())).OrderBy(s => s.Priority))
                 {
                     sortedList.Add(script.Render(this.hostSettings.CrmVersion, this.hostSettings.CdnEnabled, applicationPath));
                 }
