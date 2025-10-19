182d7075435eb4437152f836c49251fc8dec4240
Peter Donker <peter@bring2mind.net>
2025-10-19 11:54:28 +0200

    Add support for Typescript in servers.web project and hook in new tab

--- Dnn.AdminExperience/ClientSide/Servers.Web/src/components/Body/Body.jsx
+++ Dnn.AdminExperience/ClientSide/Servers.Web/src/components/Body/Body.jsx
@@ -8,6 +8,7 @@ import {
 import "./style.less";
 import Localization from "../../localization";
 import ApplicationTab from "../Tabs/Application";
+import UpgradesTab from "../Tabs/Upgrades";
 import WebTab from "../Tabs/Web";
 import DatabaseTab from "../Tabs/Database/Database";
 import ServersTab from "../Tabs/Servers/Servers";
@@ -20,8 +21,8 @@ import utils from "../../utils";
 class Body extends Component {
     constructor() {
         super();
-        this.handleSelect = this.handleSelect.bind(this);        
-    }   
+        this.handleSelect = this.handleSelect.bind(this);
+    }
 
     handleSelect(index) {
         const {props} = this;
@@ -31,11 +32,11 @@ class Body extends Component {
     insertElementsInArray(array, elements, propertyNameHasIndex, propertyNameHasValue) {
         for (let i = 0; i < elements.length; i++) {
             let index = this.getInteger(elements[i][propertyNameHasIndex]);
-            
+
             if (index || index === 0) {
                 index = Math.min(array.length, Math.max(0, index));
                 array.splice(index, 0, elements[i][propertyNameHasValue]);
-            }            
+            }
         }
     }
 
@@ -47,22 +48,22 @@ class Body extends Component {
             return parseInt(value.toString());
         }
         return value;
-    }  
-   
+    }
+
     render() {
-        const {props} = this;       
+        const {props} = this;
         const isHost = utils.isHostUser();
         let registeredTabs = application.getRegisteredServerTabs();
         if (!isHost) {
             registeredTabs = registeredTabs.filter(function (tab) {
                 return !tab.isHostOnlyVisible;
             });
-        }        
+        }
 
-        const systemInfoTabHeaders = isHost ? [Localization.get("tabApplicationTitle"), Localization.get("tabWebTitle"), Localization.get("tabDatabaseTitle"), Localization.get("tabServersTitle")] 
+        const systemInfoTabHeaders = isHost ? [Localization.get("tabApplicationTitle"), Localization.get("tabWebTitle"), Localization.get("tabDatabaseTitle"), Localization.get("tabServersTitle"), "Upgrades"]
             : [Localization.get("tabApplicationTitle")];
-        const systemInfoTabBody = isHost ? [<ApplicationTab key="first" />, <WebTab key="second" />, <DatabaseTab key="third" />, <ServersTab key="fourth" />]
-            :  [<ApplicationTab key="first" />]; 
+        const systemInfoTabBody = isHost ? [<ApplicationTab key="first" />, <WebTab key="second" />, <DatabaseTab key="third" />, <ServersTab key="fourth" />, <UpgradesTab key="fifth" />]
+            :  [<ApplicationTab key="first" />];
         const serverSettingsTabHeaders = isHost ? [Localization.get("tabSmtpServerTitle"), Localization.get("tabPerformanceTitle"), Localization.get("tabLogsTitle")]
             : [Localization.get("tabSmtpServerTitle")];
         const serverSettingsTabBody = isHost ? [<SmtpServerTab key="first" />, <PerformanceTab key="second" />, <LogsTab key="third" />]
@@ -70,7 +71,7 @@ class Body extends Component {
         const mainTabHeaders = [Localization.get("tabSystemInfoTitle"), Localization.get("tabServerSettingsTitle")];
         const mainTabBody = [
             <Tabs tabHeaders={systemInfoTabHeaders}
-                type="secondary" 
+                type="secondary"
                 key="first">
                 {systemInfoTabBody}
             </Tabs>,
@@ -79,7 +80,7 @@ class Body extends Component {
                 key="second">
                 {serverSettingsTabBody}
             </Tabs>];
-        
+
         this.insertElementsInArray(mainTabHeaders, registeredTabs.filter(
             function (tab) {
                 return !tab.parentIndex && tab.parentIndex !== 0;
@@ -109,13 +110,13 @@ class Body extends Component {
 
         return (
             <PersonaBarPageBody>
-                <Tabs className="dnn-servers-tab-panel" 
+                <Tabs className="dnn-servers-tab-panel"
                     onSelect={this.handleSelect}
                     selectedIndex={props.tabIndex}
                     tabHeaders={mainTabHeaders}>
-                    {mainTabBody}                   
+                    {mainTabBody}
                 </Tabs>
-                
+
             </PersonaBarPageBody>
         );
     }
@@ -123,7 +124,7 @@ class Body extends Component {
 
 Body.propTypes = {
     dispatch: PropTypes.func.isRequired,
-    tabIndex: PropTypes.number   
+    tabIndex: PropTypes.number
 };
 
 function mapStateToProps(state) {
---
+++ Dnn.AdminExperience/ClientSide/Servers.Web/src/components/Tabs/Upgrades.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./tabs.less";
+
+interface IProps {
+};
+
+const UpgradesTab: React.FC<IProps> = props => {
+    return (
+      <div className="dnn-servers-info-panel">
+        <h1>Upgrades</h1>
+      </div>
+    );
+}
+
+export default UpgradesTab;
