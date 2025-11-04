05aa59e90829e20e57b887798af753d9f3f4f05e
Peter Donker <peter@bring2mind.net>
2025-11-04 22:44:49 +0100

    Update DNN Platform/DotNetNuke.Web.Client/ClientResourceManager.cs
    Co-authored-by: Brian Dukes <bdukes@engagesoftware.com>

--- DNN Platform/DotNetNuke.Web.Client/ClientResourceManager.cs
+++ DNN Platform/DotNetNuke.Web.Client/ClientResourceManager.cs
@@ -23,7 +23,7 @@ namespace DotNetNuke.Web.Client.ClientResourceManagement
     using Microsoft.Extensions.DependencyInjection;
 
     /// <summary>Provides the ability to request that client resources (JavaScript and CSS) be loaded on the client browser.</summary>
-    [Obsolete("Deprecated in DotNetNuke 10.2.0. Please use IClientResourceController instead. Scheduled removal in v12.0.0.")]
+    [DnnDeprecated, 10, 2, 0, "Please use IClientResourceController instead.")]
     public partial class ClientResourceManager
     {
         /// <summary>The default css provider.</summary>
