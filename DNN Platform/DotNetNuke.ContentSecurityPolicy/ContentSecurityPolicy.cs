fd9026b8e052f453825953981d97d3763c893255
Sacha <sachatrauwaen@gmail.com>
2025-11-15 18:29:02 +0100

    fix blank line

--- DNN Platform/DotNetNuke.ContentSecurityPolicy/ContentSecurityPolicy.cs
+++ DNN Platform/DotNetNuke.ContentSecurityPolicy/ContentSecurityPolicy.cs
@@ -6,7 +6,6 @@ namespace DotNetNuke.ContentSecurityPolicy
 {
     using System.Collections.Generic;
     using System.Linq;
-    using DotNetNuke.Abstractions.ClientResources;
 
     /// <summary>
     /// Manages the entire Content Security Policy.
