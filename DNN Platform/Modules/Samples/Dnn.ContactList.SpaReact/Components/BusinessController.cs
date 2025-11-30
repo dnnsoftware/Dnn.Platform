1f7c7956465646abc49173c705f97045636437dc
Peter Donker <peter@bring2mind.net>
2025-11-30 14:56:51 +0100

    Create new project for SPA react demo

---
+++ DNN Platform/Modules/Samples/Dnn.ContactList.SpaReact/Components/BusinessController.cs
@@ -0,0 +1,36 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+using System.Collections.Generic;
+using System.Web.UI;
+using Dnn.ContactList.SpaReact.Components;
+using DotNetNuke.Entities.Modules;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Services.Tokens;
+using DotNetNuke.UI.Modules;
+
+namespace Dnn.ContactList.Spa.Components
+{
+    /// <summary>
+    /// Business controller for the Contact list SPA module.
+    /// This class needs to implement the interface ICustomTokenProvider in order to define custom tokens that
+    /// will be managed by the Token Replace Engine.
+    /// </summary>
+    public class BusinessController : ICustomTokenProvider
+    {
+        /// <summary>
+        /// Implementation of the interface ICustomTokenProvider
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="moduleContext"></param>
+        /// <returns></returns>
+        public IDictionary<string, IPropertyAccess> GetTokens(Page page, ModuleInstanceContext moduleContext)
+        {
+            var tokens = new Dictionary<string, IPropertyAccess>();
+            tokens["context"] = new ContextTokens(moduleContext, UserController.Instance.GetCurrentUserInfo());
+
+            return tokens;
+        }
+    }
+}
---
+++ DNN Platform/Modules/Samples/Dnn.ContactList.SpaReact/Components/ContextTokens.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+using System.Globalization;
+using DotNetNuke.Entities.Users;
+using DotNetNuke.Services.Tokens;
+using DotNetNuke.UI.Modules;
+using Newtonsoft.Json;
+
+namespace Dnn.ContactList.SpaReact.Components
+{
+    public class ContextTokens : IPropertyAccess
+    {
+        public CacheLevel Cacheability => CacheLevel.notCacheable;
+        private readonly SecurityContext security;
+        private readonly ModuleInstanceContext moduleContext;
+
+        public ContextTokens(ModuleInstanceContext moduleContext, UserInfo user)
+        {
+            this.moduleContext = moduleContext;
+            this.security = new SecurityContext(moduleContext.Configuration, user);
+        }
+
+        public string GetProperty(string propertyName, string format, CultureInfo formatProvider, UserInfo accessingUser, Scope accessLevel, ref bool propertyNotFound)
+        {
+            switch (propertyName.ToLower())
+            {
+                case "security":
+                    return JsonConvert.SerializeObject(this.security);
+                case "module":
+                    return JsonConvert.SerializeObject(new
+                    {
+                        this.moduleContext.ModuleId,
+                        this.moduleContext.TabId,
+                        this.moduleContext.TabModuleId,
+                        this.moduleContext.PortalId,
+                    });
+                default:
+                    propertyNotFound = true;
+                    return string.Empty;
+            }
+        }
+    }
+}
