665b1bb3269647ae4fdce55ad6779208f1e1a241
Peter Donker <peter@bring2mind.net>
2026-01-27 23:41:19 +0100

    Fix to Spa module container control for MVC pipeline to cater for virtual roots

--- DNN Platform/DotNetNuke.Web.MvcPipeline/ModuleControl/SpaModuleControl.cs
+++ DNN Platform/DotNetNuke.Web.MvcPipeline/ModuleControl/SpaModuleControl.cs
@@ -4,34 +4,18 @@
 
 namespace DotNetNuke.Web.MvcPipeline.ModuleControl
 {
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using System.Web;
     using System.Web.Mvc;
-    using System.Web.Mvc.Html;
-    using System.Web.Routing;
-    using System.Web.UI;
 
     using DotNetNuke.Abstractions.ClientResources;
     using DotNetNuke.Abstractions.Modules;
-    using DotNetNuke.Collections;
     using DotNetNuke.Common;
     using DotNetNuke.Common.Utilities;
-    using DotNetNuke.Entities.Modules;
     using DotNetNuke.Entities.Modules.Actions;
-    using DotNetNuke.Entities.Portals;
-    using DotNetNuke.Entities.Users;
-    using DotNetNuke.Framework;
-    using DotNetNuke.Instrumentation;
     using DotNetNuke.Services.Cache;
-    using DotNetNuke.Services.Localization;
-    using DotNetNuke.UI.Containers;
-    using DotNetNuke.UI.Modules;
+    using DotNetNuke.Services.ClientDependency;
     using DotNetNuke.UI.Modules.Html5;
-    using DotNetNuke.Web.Client.ClientResourceManagement;
     using DotNetNuke.Web.MvcPipeline.ModuleControl.Page;
     using Microsoft.Extensions.DependencyInjection;
 
@@ -81,21 +65,15 @@ namespace DotNetNuke.Web.MvcPipeline.ModuleControl
             if (!string.IsNullOrEmpty(this.Html5File))
             {
                 // Check if css file exists
-                var cssFile = Path.ChangeExtension(this.Html5File, ".css");
-                if (this.FileExists(cssFile))
-                {
-                    context.ClientResourceController.CreateStylesheet(cssFile).Register();
-                }
+                var cssFile = Globals.ApplicationPath + "/" + Path.ChangeExtension(this.Html5File, ".css");
+                context.ClientResourceController.RegisterStylesheet(cssFile, true);
             }
 
             if (!string.IsNullOrEmpty(this.Html5File))
             {
                 // Check if js file exists
-                var jsFile = Path.ChangeExtension(this.Html5File, ".js");
-                if (this.FileExists(jsFile))
-                {
-                    context.ClientResourceController.CreateScript(jsFile).Register();
-                }
+                var jsFile = Globals.ApplicationPath + "/" + Path.ChangeExtension(this.Html5File, ".js");
+                context.ClientResourceController.RegisterScript(jsFile, true);
             }
         }
 
@@ -129,23 +107,12 @@ namespace DotNetNuke.Web.MvcPipeline.ModuleControl
         private string GetFileContent(string filepath)
         {
             var cacheKey = string.Format(DataCache.SpaModulesContentHtmlFileCacheKey, filepath);
-            var absoluteFilePath = HttpContext.Current.Server.MapPath("/" + filepath);
+            var absoluteFilePath = HttpContext.Current.Server.MapPath(Globals.ApplicationPath + "/" + filepath);
             var cacheItemArgs = new CacheItemArgs(cacheKey, DataCache.SpaModulesHtmlFileTimeOut, DataCache.SpaModulesHtmlFileCachePriority)
             {
                 CacheDependency = new DNNCacheDependency(absoluteFilePath),
             };
             return CBO.GetCachedObject<string>(cacheItemArgs, c => GetFileContentInternal(absoluteFilePath));
-        }
-
-        private bool FileExists(string filepath)
-        {
-            var cacheKey = string.Format(DataCache.SpaModulesFileExistsCacheKey, filepath);
-            return CBO.GetCachedObject<bool>(
-                new CacheItemArgs(
-                    cacheKey,
-                    DataCache.SpaModulesHtmlFileTimeOut,
-                    DataCache.SpaModulesHtmlFileCachePriority),
-                c => File.Exists(HttpContext.Current.Server.MapPath("/" + filepath)));
         }
     }
 }
