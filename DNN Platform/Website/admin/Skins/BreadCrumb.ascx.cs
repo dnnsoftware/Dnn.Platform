8554455bf0ee9903486a3263cc3922dead1618c1
Daniel Valadas <info@danielvaladas.com>
2026-02-05 12:40:09 -0500

    Use <see cref /> in doc comment
    Co-authored-by: Brian Dukes <bdukes@engagesoftware.com>

--- DNN Platform/Website/admin/Skins/BreadCrumb.ascx.cs
+++ DNN Platform/Website/admin/Skins/BreadCrumb.ascx.cs
@@ -115,7 +115,7 @@ namespace DotNetNuke.UI.Skins.Controls
         public bool HideWithNoBreadCrumb { get; set; }
 
         /// <summary>Gets or sets a value indicating whether to take advantage of the enhanced markup
-        /// (remove extra wrapping elements). No effect when RenderingVersion is 2 or higher.
+        /// (remove extra wrapping elements). No effect when <see cref="SkinObjectBase.RenderingVersion" /> is 2 or higher.
         /// </summary>
         public bool CleanerMarkup { get; set; }
 
