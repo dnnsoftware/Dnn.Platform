cb544d2f6c8c24a4405e2e34e1c56b1eef652e72
Brian Dukes <bdukes@engagesoftware.com>
2026-02-11 12:16:31 -0600

    Add assembly version to WebControls

--- DNN Platform/Controls/DotNetNuke.WebControls/AssemblyInfo.vb
+++ DNN Platform/Controls/DotNetNuke.WebControls/AssemblyInfo.vb
@@ -21,10 +21,11 @@ Imports System.Web.UI
 <Assembly: Guid("1CDFE09D-9111-4ABE-8CF4-02D1FDC157A7")>
 <Assembly: CLSCompliant(True)>
 <Assembly: TagPrefix("DotNetNuke.UI.WebControls", "DNN")>
-<Assembly: AssemblyTrademark("DotNetNuke")>
-<Assembly: AssemblyCopyright("DotNetNuke is copyright 2002-2011 by DotNetNuke Corporation. All Rights Reserved.")>
-<Assembly: AssemblyProduct("http://www.dotnetnuke.com")>
-<Assembly: AssemblyCompany("DotNetNuke Corporation")>
-<Assembly: AssemblyDescription("Open Source Web Application Framework")>
-<Assembly: AssemblyTitle("DotNetNuke")>
+<Assembly: AssemblyTrademark("DNN")>
+<Assembly: AssemblyCopyright("DNN Platform is copyright 2002-2026 by .NET Foundation. All Rights Reserved.")>
+<Assembly: AssemblyProduct("https://dnncommunity.org")>
+<Assembly: AssemblyCompany(".NET Foundation")>
+<Assembly: AssemblyDescription("Web Controls for DNN Platform")>
+<Assembly: AssemblyTitle("DotNetNuke WebControls")>
+<Assembly: AssemblyVersion("10.02.03")>
 
