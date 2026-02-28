3bb17cfaf1f8320d13cec9a6b2eb306b1aef7499
Peter Donker <peter@bring2mind.net>
2026-02-28 20:47:06 +0100

    Merge code from previous Serilog solution to latest DNN version

---
+++ DNN Platform/DotNetNuke.Instrumentation/SerilogController.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+namespace DotNetNuke.Instrumentation;
+
+using System.Globalization;
+using System.IO;
+
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+/// <summary>
+/// Controller for Serilog functions.
+/// </summary>
+internal sealed class SerilogController
+{
+    /// <summary>
+    /// Sets up Serilog using the config file ~/Config/Serilog.config.
+    /// </summary>
+    /// <param name="hostMapPath">Path to the root of the DNN installation.</param>
+    internal static void AddSerilog(string hostMapPath)
+    {
+        var configFile = hostMapPath + "\\Config\\Serilog.config";
+        var config = new LoggerConfiguration()
+            .WriteTo.File(hostMapPath + "\\Portals\\_default\\Logs\\log.txt", rollingInterval: RollingInterval.Day, formatProvider: CultureInfo.InvariantCulture)
+            .MinimumLevel.Debug();
+        if (File.Exists(configFile))
+        {
+            config = new LoggerConfiguration()
+                .ReadFrom.Configuration(new ConfigurationBuilder()
+                    .AddJsonFile(configFile, optional: false, reloadOnChange: true)
+                    .Build());
+        }
+
+        Log.Logger = config.CreateLogger();
+    }
+}
---
+++ DNN Platform/DotNetNuke.Instrumentation/StartupExtensions.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+namespace DotNetNuke.Instrumentation;
+
+using System;
+
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+/// <summary>
+/// The startup extensions to add Serilog to the project.
+/// </summary>
+public static class StartupExtensions
+{
+    /// <summary>
+    /// Add Serilog to the project.
+    /// </summary>
+    /// <param name="services">The IServiceCollection.</param>
+    /// <param name="hostMapPath">The path to the root of the DotNetNuke website. This is needed to find the correct directory to write the log files to.</param>
+    public static void AddSerilog(this IServiceCollection services, string hostMapPath)
+    {
+        Environment.SetEnvironmentVariable("BASEDIR", hostMapPath);
+        SerilogController.AddSerilog(hostMapPath);
+        services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(null, true));
+    }
+}
