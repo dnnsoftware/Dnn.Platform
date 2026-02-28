9e7b17e6da2bce31ed792101ea561111a6464d31
Peter Donker <peter@bring2mind.net>
2026-02-28 22:16:57 +0100

    Add obsolete annotations

--- DNN Platform/DotNetNuke.Instrumentation/ILoggerSource.cs
+++ DNN Platform/DotNetNuke.Instrumentation/ILoggerSource.cs
@@ -5,8 +5,11 @@ namespace DotNetNuke.Instrumentation
 {
     using System;
 
+    using DotNetNuke.Internal.SourceGenerators;
+
     /// <summary>A contract specifying the ability to provide <see cref="ILog"/> instances.</summary>
-    public interface ILoggerSource
+    [DnnDeprecated(10, 3, 0, "Use Microsoft's ILogger instead", RemovalVersion = 12)]
+    public partial interface ILoggerSource
     {
         /// <summary>Gets the logger for the given <paramref name="type"/>.</summary>
         /// <param name="type">The type providing the name for the logger instance.</param>
