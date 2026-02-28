11dced4535f99699fa0c65c1eaef805cd5607ff1
Peter Donker <peter@bring2mind.net>
2026-02-28 22:18:11 +0100

    Add helper class to aid transition

---
+++ DNN Platform/DotNetNuke.Instrumentation/DnnLoggingController.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information
+
+namespace DotNetNuke.Instrumentation
+{
+    using Microsoft.Extensions.Logging;
+    using Serilog;
+    using Serilog.Extensions.Logging;
+
+    /// <summary>
+    /// Provides centralized logging functionality for DNN Platform using Serilog.
+    /// </summary>
+    public class DnnLoggingController
+    {
+        /// <summary>
+        /// Gets a strongly-typed logger instance for the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type for which to create the logger.</typeparam>
+        /// <returns>An <see cref="ILogger{T}"/> instance configured with Serilog.</returns>
+        /// <remarks>
+        /// If Serilog has not been initialized, this method will automatically initialize it
+        /// using the application's host path before creating the logger.
+        /// </remarks>
+        public static ILogger<T> GetLogger<T>()
+        {
+            if (Log.Logger == null)
+            {
+                // initialize Serilog
+                var hostMapPath = System.Web.Hosting.HostingEnvironment.MapPath("~");
+                SerilogController.AddSerilog(hostMapPath);
+            }
+
+            return new SerilogLoggerFactory(Log.Logger).CreateLogger<T>();
+        }
+    }
+}
