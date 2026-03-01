1dac006a9512388eff1126e08239489712d551c2
Peter Donker <peter@bring2mind.net>
2026-03-01 22:41:14 +0100

    Roll back deprecation of LoggerSource for now

--- DNN Platform/DotNetNuke.Instrumentation/ILog.cs
+++ DNN Platform/DotNetNuke.Instrumentation/ILog.cs
@@ -6,11 +6,8 @@ namespace DotNetNuke.Instrumentation
     using System;
     using System.Diagnostics.CodeAnalysis;
 
-    using DotNetNuke.Internal.SourceGenerators;
-
     /// <summary>A contract specifying the ability to log information.</summary>
-    [DnnDeprecated(10, 3, 0, "Use Microsoft's ILogger instead", RemovalVersion = 12)]
-    public partial interface ILog
+    public interface ILog
     {
         /// <summary>Gets a value indicating whether the Debug log level is enabled.</summary>
         bool IsDebugEnabled { get; }
