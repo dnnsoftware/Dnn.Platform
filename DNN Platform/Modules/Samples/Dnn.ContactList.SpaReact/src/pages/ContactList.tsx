c0a35e4848602eb925757cf1c74c4b7000921678
Brian Dukes <bdukes@engagesoftware.com>
2026-03-18 11:38:56 -0500

    Bump react-router in Dnn.ContactList.SpaReact
    Platform/Modules/Samples/Dnn.ContactList.SpaReact/src/pages/ContactForm.tsx
    Platform/Modules/Samples/Dnn.ContactList.SpaReact/src/pages/ContactList.tsx

--- DNN Platform/Modules/Samples/Dnn.ContactList.SpaReact/src/pages/ContactList.tsx
+++ DNN Platform/Modules/Samples/Dnn.ContactList.SpaReact/src/pages/ContactList.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import { Contact } from '../types/Contact';
 import { SecurityContext } from '../types/Security';
 import { ModuleContext } from '../types/Module';
