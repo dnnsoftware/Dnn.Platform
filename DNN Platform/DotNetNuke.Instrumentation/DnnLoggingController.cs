fc0303182a7b6c7f849fd4c96073d44e2a8321b8
Brian Dukes <bdukes@engagesoftware.com>
2026-03-24 13:23:32 -0500

    Add ILogger migration helpers

--- DNN Platform/DotNetNuke.Instrumentation/DnnLoggingController.cs
+++ DNN Platform/DotNetNuke.Instrumentation/DnnLoggingController.cs
@@ -4,18 +4,16 @@
 
 namespace DotNetNuke.Instrumentation
 {
+    using System;
+
     using Microsoft.Extensions.Logging;
     using Serilog;
     using Serilog.Extensions.Logging;
 
-    /// <summary>
-    /// Provides centralized logging functionality for DNN Platform using Serilog.
-    /// </summary>
+    /// <summary>Provides centralized logging functionality for DNN Platform using Serilog.</summary>
     public class DnnLoggingController
     {
-        /// <summary>
-        /// Gets a strongly-typed logger instance for the specified type.
-        /// </summary>
+        /// <summary>Gets a strongly-typed logger instance for the specified type.</summary>
         /// <typeparam name="T">The type for which to create the logger.</typeparam>
         /// <returns>An <see cref="ILogger{T}"/> instance configured with Serilog.</returns>
         /// <remarks>
@@ -32,6 +30,44 @@ namespace DotNetNuke.Instrumentation
             }
 
             return new SerilogLoggerFactory(Log.Logger).CreateLogger<T>();
+        }
+
+        /// <summary>Gets a strongly-typed logger instance for the specified type.</summary>
+        /// <param name="type">The type for which to create the logger.</param>
+        /// <returns>An <see cref="ILogger{T}"/> instance configured with Serilog.</returns>
+        /// <remarks>
+        /// If Serilog has not been initialized, this method will automatically initialize it
+        /// using the application's host path before creating the logger.
+        /// </remarks>
+        public static Microsoft.Extensions.Logging.ILogger GetLogger(Type type)
+        {
+            if (Log.Logger == null)
+            {
+                // initialize Serilog
+                var applicationMapPath = System.Web.Hosting.HostingEnvironment.MapPath("~");
+                SerilogController.AddSerilog(applicationMapPath);
+            }
+
+            return new SerilogLoggerFactory(Log.Logger).CreateLogger(type);
+        }
+
+        /// <summary>Gets a strongly-typed logger instance for the specified type.</summary>
+        /// <param name="categoryName">The category name to associated with the logger.</param>
+        /// <returns>An <see cref="ILogger{T}"/> instance configured with Serilog.</returns>
+        /// <remarks>
+        /// If Serilog has not been initialized, this method will automatically initialize it
+        /// using the application's host path before creating the logger.
+        /// </remarks>
+        public static Microsoft.Extensions.Logging.ILogger GetLogger(string categoryName)
+        {
+            if (Log.Logger == null)
+            {
+                // initialize Serilog
+                var applicationMapPath = System.Web.Hosting.HostingEnvironment.MapPath("~");
+                SerilogController.AddSerilog(applicationMapPath);
+            }
+
+            return new SerilogLoggerFactory(Log.Logger).CreateLogger(categoryName);
         }
     }
 }
